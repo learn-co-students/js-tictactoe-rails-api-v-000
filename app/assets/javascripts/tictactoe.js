319889614a4a54c00a33d7ce879eb2caaebf9c8c
benser1 <briganenser28@gmail.com>
2016-03-01 13:37:58 -0700

    Alternating players works. Occupy space working

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,25 +1,35 @@
 $(document).ready(function(){
-  updateState();
+  doTurn();
 });
 
 function attachListeners() {
 
 }
 
-function doTurn(event) {
-  
+function doTurn() {
+  updateState();
 }
 
-function player(num) {
-  if(num % 2 == 0) {
-    $(this).text('X');
-  } else {
-    $(this).text('O');
-  }
-}
+// function player(num) {
+//   if(num % 2 == 0) {
+//     $(this).text('O');
+//   } else {
+//     $(this).text('X');
+//   }
+// }
 
 function updateState() {
-  
+  var num = 1;
+  $("td").click(function(){
+    if($(this).html() == '' && (num % 2 == 0)) {
+       $(this).text("X");
+    } else if ($(this).html() == '' && (num % 2 != 0)){
+       $(this).text("O");
+    } else {
+      alert("This position is taken.");
+    }
+    num += 1;
+  });
 }
 
 function checkWinner() {
---
+++ notes.md
@@ -0,0 +1,4 @@
+http://jsfiddle.net/nishacodes/2Jvtu/
+
+winning comobos:
+http://stackoverflow.com/questions/19250519/testing-if-an-array-contains-any-one-of-eight-3-key-values-tic-tac-toe-game
