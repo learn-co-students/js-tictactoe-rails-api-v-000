f95ef23f76e10e70b78be83646be85f6286adf83
benser1 <briganenser28@gmail.com>
2016-03-01 16:05:27 -0700

    Reset working. Game Play functioning properly

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  doTurn();
+  updateBoard();
 });
 
 function attachListeners() {
@@ -17,9 +17,9 @@ function doTurn() {
 //     $(this).text('X');
 //   }
 // }
-
+var num;
 function updateBoard() {
-  var num = 1;
+  num = 1;
   $("td").on("click", function(){
     if($(this).html() == '' && (num % 2 == 0)) {
        $(this).text("X");
@@ -30,66 +30,98 @@ function updateBoard() {
     } else {
       alert("This position is taken.");
     }
+
     checkWinner();
-    if(num == 10) {
+
+    if (num == 10) {
       alert("Cats Game!");
-    };
+      $("table td").empty();
+      num = 1;
+    }
   });
 }
 
 function checkWinner() {
   if($("#one").html() == "X" && $("#two").html() == "X" && $("#three").html() == "X") {
     alert("X Wins!")
-    reset();
+    $("table td").empty();
+    num = 1;
+
   } else if($("#four").html() == "X" && $("#five").html() == "X" && $("#six").html() == "X") {
     alert("X Wins!")
-    reset();
+    $("table td").empty();
+    num = 1;
+
   } else if($("#seven").html() == "X" && $("#eight").html() == "X" && $("#nine").html() == "X") {
     alert("X Wins!")
-    reset();
+    $("table td").empty();
+    num = 1;
+
   } else if($("#one").html() == "X" && $("#four").html() == "X" && $("#seven").html() == "X") {
     alert("X Wins!")
-    reset();
+    $("table td").empty();
+    num = 1;
+
   } else if($("#two").html() == "X" && $("#five").html() == "X" && $("#eight").html() == "X") {
     alert("X Wins!")
-    reset();
+    $("table td").empty();
+    num = 1;
+
   } else if($("#three").html() == "X" && $("#six").html() == "X" && $("#nine").html() == "X") {
     alert("X Wins!")
-    reset();
+    $("table td").empty();
+    num = 1;
+
   } else if($("#one").html() == "X" && $("#five").html() == "X" && $("#nine").html() == "X") {
     alert("X Wins!")
-    reset();
+    $("table td").empty();
+    num = 1;
+
   } else if($("#three").html() == "X" && $("#five").html() == "X" && $("#seven").html() == "X") {
     alert("X Wins!")
-    reset();
+    $("table td").empty();
+    num = 1;
+
   } else if($("#one").html() == "O" && $("#two").html() == "O" && $("#three").html() == "O") {
     alert("O Wins!")
-    reset();
+    $("table td").empty();
+    num = 1;
+
   } else if($("#four").html() == "O" && $("#five").html() == "O" && $("#six").html() == "O") {
     alert("O Wins!")
-    reset();
+    $("table td").empty();
+    num = 1;
+
   } else if($("#seven").html() == "O" && $("#eight").html() == "O" && $("#nine").html() == "O") {
     alert("O Wins!")
-    reset();
+    $("table td").empty();
+    num = 1;
+
   } else if($("#one").html() == "O" && $("#four").html() == "O" && $("#seven").html() == "O") {
     alert("O Wins!")
-    reset();
+    $("table td").empty();
+    num = 1;
+
   } else if($("#two").html() == "O" && $("#five").html() == "O" && $("#eight").html() == "O") {
     alert("O Wins!")
-    reset();
+    $("table td").empty();
+    num = 1;
+
   } else if($("#three").html() == "O" && $("#six").html() == "O" && $("#nine").html() == "O") {
     alert("O Wins!")
-    reset();
+    $("table td").empty();
+    num = 1;
+
   } else if($("#one").html() == "O" && $("#five").html() == "O" && $("#nine").html() == "O") {
     alert("O Wins!")
-    reset();
+    $("table td").empty();
+    num = 1;
+
   } else if($("#three").html() == "O" && $("#five").html() == "O" && $("#seven").html() == "O") {
     alert("O Wins!")
-    reset();
+    $("table td").empty();
+    num = 1;
   } 
-}
-
-function boardFull() {
 }
 
 function message() {
