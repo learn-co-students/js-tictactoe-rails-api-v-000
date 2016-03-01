ba5b780d0e9f9b345ddbc6d3cfa01092dc9f9b1b
benser1 <briganenser28@gmail.com>
2016-03-01 11:45:59 -0700

    Player function

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,7 +1,31 @@
 $(document).ready(function(){
-  attachListeners();
+  updateState();
 });
 
 function attachListeners() {
+
+}
+
+function doTurn(event) {
+  
+}
+
+function player(num) {
+  if(num % 2 == 0) {
+    $(this).text('X');
+  } else {
+    $(this).text('O');
+  }
+}
+
+function updateState() {
+  
+}
+
+function checkWinner() {
+
+}
+
+function message() {
 
 }
