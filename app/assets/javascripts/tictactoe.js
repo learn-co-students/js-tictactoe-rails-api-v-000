c0bec0b0a45c6d8613793cff70e33194b38a0876
Kevin McCormack <wittyman37@yahoo.com>
2016-03-15 19:38:34 -0400

    checkWinner tests are passing

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,14 +1,18 @@
+//var board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 var turn = 0;
 
-var winningCombos =
-[[[0,0],[1,0],[2,0]],
-[[0,1],[1,1],[2,1]],
-[[0,2],[1,2],[2,2]],
-[[0,0],[1,1],[2,2]],
-[[0,0],[0,1],[0,2]],
-[[2,0],[2,1],[2,2]],
-[[1,0],[1,1],[1,2]],
-[[2,0],[1,1],[0,2]]]
+var winningCombos = [
+  ['[data-x="0"][data-y="0"]','[data-x="1"][data-y="0"]','[data-x="2"][data-y="0"]'], //[[0,0],[1,0],[2,0]],
+  ['[data-x="0"][data-y="1"]','[data-x="1"][data-y="1"]','[data-x="2"][data-y="1"]'], //[[0,1],[1,1],[2,1]],
+  ['[data-x="0"][data-y="2"]','[data-x="1"][data-y="2"]','[data-x="2"][data-y="2"]'], //[[0,2],[1,2],[2,2]],
+  ['[data-x="0"][data-y="0"]','[data-x="1"][data-y="1"]','[data-x="2"][data-y="2"]'], //[[0,0],[1,1],[2,2]],
+  ['[data-x="0"][data-y="0"]','[data-x="0"][data-y="1"]','[data-x="0"][data-y="2"]'], //[[0,0],[0,1],[0,2]],
+  ['[data-x="2"][data-y="0"]','[data-x="2"][data-y="1"]','[data-x="2"][data-y="2"]'], //[[2,0],[2,1],[2,2]],
+  ['[data-x="1"][data-y="0"]','[data-x="1"][data-y="1"]','[data-x="1"][data-y="2"]'], //[[1,0],[1,1],[1,2]],
+  ['[data-x="2"][data-y="0"]','[data-x="1"][data-y="1"]','[data-x="0"][data-y="2"]']  //[[2,0],[1,1],[0,2]]
+];
+
 
 function attachListeners() {
   //$('[data-x="0"][data-y="0"]').click(function() {
@@ -22,8 +26,8 @@ function attachListeners() {
 }
 
 function doTurn(cell) {
-  checkWinner();
   updateState(cell);
+  checkWinner();
   //debugger;
 
   turn += 1;
@@ -47,7 +51,14 @@ function updateState(cell) {
 }
 
 function checkWinner() {
-
+  $.each(winningCombos, function (index, combo) {
+    //debugger;
+    if ($(combo[0]).text() === player() && $(combo[1]).text() === player() && $(combo[2]).text() === player()) {
+      //debugger;
+      return message("Player " + player() + " Won!");
+    }
+  });
+  return false;
 }
 
 function message(string) {
