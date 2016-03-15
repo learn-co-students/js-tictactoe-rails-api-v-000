cd3ce933389c3063e0e47b3be2592bc7b76ff1ac
Kevin McCormack <wittyman37@yahoo.com>
2016-03-15 12:58:35 -0400

    working

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,17 +1,39 @@
-function attachListeners() {
+var turn = 0;
 
+function attachListeners() {
+  //$('[data-x="0"][data-y="0"]').click(function() {
+  //  doTurn();
+  //});
+  $('[data-x]').click(function() {
+    //var cell = this;
+    //debugger;
+    doTurn(this);
+  });
 }
 
-function doTurn() {
+function doTurn(cell) {
+  checkWinner();
+  updateState(cell);
+  //debugger;
 
+  turn += 1;
+  //console.log("clicky" + cell);
+  //$(this).text("X");
 }
 
 function player() {
-
+  if (turn % 2 === 0) {
+    return "X";
+  } else {
+    return "O";
+  }
 }
 
-function updateState() {
-
+function updateState(cell) {
+  //debugger;
+  if ($(cell).text() === "") {
+    $(cell).text(player());
+  }
 }
 
 function checkWinner() {
@@ -23,5 +45,5 @@ function message() {
 }
 
 $(document).ready(function() {
-
+  attachListeners();
 });
