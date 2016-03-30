5bbf500c642a899b72b7ec771a9120a1a472e463
tuckerbohman5 <tucker.bohman@gmail.com>
2016-03-30 10:11:21 -0600

    started update state method

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -3,5 +3,21 @@ $(document).ready(function() {
 });
 
 function attachListeners() {
+
+}
+
+function updateState() {
+  $('td').on('click', function() {
+    if($(this).html == '' && (num % 2 == 0)) {
+      $(this).text = 'X';
+    } else if ($(this.html == '' && (num % 2 != 0)) {
+      $(this).text = 'O';
+    } else {
+      alert("This position is taken.")
+    };
+  });
+}
+
+function player() {
   
 }
