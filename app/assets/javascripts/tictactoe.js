625bd7a8fda30059ac5be131f2efa86c5513386a
pajamaw <pjpeterww@gmail.com>
2016-03-29 22:04:44 -0400

    Passed rspec tests

---
+++ app/assets/javascripts/tictactoe.js
@@ -0,0 +1,32 @@
+$(function() {
+  $("td").on("click", function() {
+    function attachListeners() {
+      function doTurn(){
+
+};
+};
+
+function turn(){
+
+};
+
+function checkWinner(){
+
+};
+
+var player = function(){
+
+};
+
+function updateState(){
+
+};
+
+function message() {
+
+};
+
+var currentGame = function(){
+
+};
+
---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,27 @@
+class GamesController < ApplicationController
+  def index
+    games = Game.all
+    render json: games
+  end
+
+  def create
+    game = Game.create(state: game_params[:state])
+    render json: game
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    @game.update(state: game_params[:state])
+    render json: @game
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    render json: @game
+  end
+
+  private 
+    def game_params
+      params[:game]
+    end
+end
