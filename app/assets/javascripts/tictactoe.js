e86366a5a0d27be5909f7abcc5d3b9d48e2e97e6
MitulMistry <MitulMistryDev@gmail.com>
2016-05-08 18:52:19 -0500

    game conditions

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -2,6 +2,19 @@
 
 var turn = 0;
 
+var WIN_COMBOS = [
+  [0,1,2], // Top row
+  [3,4,5],  // Middle row
+  [6,7,8],
+
+  [0,3,6], //verticals
+  [1,4,7],
+  [2,5,8],
+
+  [0,4,8], //diagonals
+  [6,4,2]
+]
+
 $(document).ready(function(){
   attachListeners();
 });
@@ -22,9 +35,12 @@ function attachListeners(){
 }
 
 function doTurn(event){
-  turn++;
   updateState(event);
-  checkWinner();
+  if (checkWinner() || checkTie()){
+    reset();
+  } else {
+    turn++;
+  }
 }
 
 function player(){
@@ -40,8 +56,40 @@ function updateState(event){
   $(event.target).html(player());
 }
 
-function checkWinner(){
+function getBoard(){
+  return $('td').map(function(index, item){
+    return item.innerHTML; //here return adds to array
+  });
+}
 
+function checkWinner(){
+  var board = getBoard();
+
+  for(var i = 0; i < WIN_COMBOS.length; i++){
+    if (board[WIN_COMBOS[i][0]] !== '' && board[WIN_COMBOS[i][0]] === board[WIN_COMBOS[i][1]] && board[WIN_COMBOS[i][1]] === board[WIN_COMBOS[i][2]]){
+      message("Player " + board[WIN_COMBOS[i][0]] + " Won!");
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function checkTie(){
+  var board = getBoard();
+
+  for (var i = 0; i < board.length; i++ ) {
+    if (board[i] === ''){
+      return false; //if one of the spots on the board is empty, it's not a tie
+    }
+  }
+  message("Tie game");
+  return true; //all spots are full, so it's a tie
+}
+
+function reset(){
+  turn = 0;
+  $('td').html('');
 }
 
 function message(string){
