38aa28640fea0b35e4944e29e89518dd4bfe48f4
MitulMistry <MitulMistryDev@gmail.com>
2016-05-08 23:03:31 -0500

    finished save function

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,11 +1,12 @@
-"use strict";
+'use strict';
 
 var turn = 0;
+var currentGame = 0;
 
 var WIN_COMBOS = [
-  [0,1,2], // Top row
-  [3,4,5],  // Middle row
-  [6,7,8],
+  [0,1,2], //top row
+  [3,4,5], //middle row
+  [6,7,8], //bottom row
 
   [0,3,6], //verticals
   [1,4,7],
@@ -41,6 +42,7 @@ function attachListeners(){
 function doTurn(event){
   updateState(event);
   if (checkWinner() || checkTie()){
+    saveGame(); //autosave at end of game
     reset();
   } else {
     turn++;
@@ -49,21 +51,21 @@ function doTurn(event){
 
 function player(){
   if (turn % 2 === 0){
-    return "X";
+    return 'X';
   } else {
-    return "O";
+    return 'O';
   }
 }
 
 function updateState(event){
-  //sets the target (td) of the jquery event object to either "O" or "X"
+  //sets the target (td) of the jquery event object to either 'O' or 'X'
   $(event.target).html(player());
 }
 
 function getBoard(){
   return $('td').map(function(index, item){
     return item.innerHTML; //here return adds to array
-  });
+  }).toArray(); //.toArray() forces conversion to array, forgoing jquery objects
 }
 
 function checkWinner(){
@@ -71,7 +73,7 @@ function checkWinner(){
 
   for(var i = 0; i < WIN_COMBOS.length; i++){
     if (board[WIN_COMBOS[i][0]] !== '' && board[WIN_COMBOS[i][0]] === board[WIN_COMBOS[i][1]] && board[WIN_COMBOS[i][1]] === board[WIN_COMBOS[i][2]]){
-      message("Player " + board[WIN_COMBOS[i][0]] + " Won!");
+      message('Player ' + board[WIN_COMBOS[i][0]] + ' Won!');
       return true;
     }
   }
@@ -87,12 +89,13 @@ function checkTie(){
       return false; //if one of the spots on the board is empty, it's not a tie
     }
   }
-  message("Tie game");
+  message('Tie game');
   return true; //all spots are full, so it's a tie
 }
 
 function reset(){
   turn = 0;
+  currentGame = 0; //reset current game id
   $('td').html('');
 }
 
@@ -105,5 +108,29 @@ function displayGames(games){
 }
 
 function saveGame(){
-  
+  //console.log(getBoard());
+  var ajaxUrl, ajaxMethod;
+
+  //determine whether updating saved game or creating new save in db
+  if (currentGame) {
+    ajaxUrl = '/games/' + currentGame;
+    ajaxMethod = 'PATCH';
+  } else {
+    ajaxUrl = '/games';
+    ajaxMethod = 'POST';
+  }
+
+  $.ajax({
+    url: ajaxUrl,
+    method: ajaxMethod, //POST/PATCH
+    data: { //structure data in the form of a game object hash for rails
+      game: {
+        state: getBoard() //array of spots on the board
+      }
+    },
+    dataType: 'json'
+  }).success(function(response){
+    currentGame = response.game.id; //get the database game id from the response
+    console.log(response);
+  });
 }
