a4fff77b320e08d2e522ba8a9cedbab8c7d6c6ef
shinyrachel <rachel.kory@gmail.com>
2016-05-12 12:13:03 -0400

    mostly written, some persistence tests failing

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,4 +1,7 @@
+////// necessary variables
+
 var turn = 0;
+var currentGame = 0;
 var winCombos = [
   ['[data-x="0"][data-y="0"]', '[data-x="0"][data-y="1"]', '[data-x="0"][data-y="2"]'], 
   ['[data-x="1"][data-y="0"]', '[data-x="1"][data-y="1"]', '[data-x="1"][data-y="2"]'], 
@@ -10,11 +13,80 @@ var winCombos = [
   ['[data-x="2"][data-y="0"]', '[data-x="1"][data-y="1"]', '[data-x="0"][data-y="2"]'] 
 ]
 
+////// listeners
+
 function attachListeners(){
   $("td").click(function(event){
     doTurn(event);
   });
+  $("#previous").click(function(event){
+    getPreviousGames();
+  });
+  $("#save").click(function(event){
+    saveGame();
+  })
+  $("li").click(function(event){
+    loadGame(event);
+  });
 }
+
+////// persistence
+
+function getPreviousGames(){
+  $.get("/games", function(data){
+    if (data["games"].length > 0) {
+      var prevGames = "<ul>"
+      $.each(data["games"], function(index, game){
+        prevGames += '<li data-gameid="' + game["id"] + '">Game '  + game["id"] + '</li>'
+      });
+      prevGames += "</ul>"
+      $("#games").html(prevGames);
+    }
+  });
+}
+
+function saveGame(){
+  // build current gameData for saving
+  var gameState = []
+  $("td").each(function(index, td){
+    gameState.push($(td).text());
+  });
+  var gameData = {
+    game: {
+      id: currentGame,
+      state: gameState
+    }
+  }
+
+  // if currentGame = 0, it hasn't been saved to the db
+  if (currentGame === 0) {
+    $.post("/games", gameData).done(function(response){  
+      currentGame = response.game.id; // why is the response not returning the proper serialized @game? 
+                                      // the controller seems to be returning the right object...
+                                      // the tests pass because they stub the response
+    });
+  }
+  else {
+    $.ajax({
+      url: "/games/" + currentGame,
+      type: 'PATCH',
+      dataType: 'json',
+      data: gameData
+    });
+  }
+}
+
+function loadGame(event){
+  currentGame = $(event.target).data("gameid");
+  $.get("/games/" + currentGame, function(data){
+    var state = data.game.state;
+    $("td").each(function(index, td){
+      $(td).text(state[index]);
+    });
+  });
+}
+
+////// playing the game
 
 function doTurn(event){
   updateState(event);
@@ -40,9 +112,11 @@ function message(string){
 }
 
 function resetBoard(){
+  saveGame();
   turn = 0;
-  $("td").each(function(index, element){
-    $(this).text("")
+  currentGame = 0;
+  $("td").each(function(index, td){
+    $(this).text("");
   });    
 }
 
@@ -70,8 +144,7 @@ function checkWinner(){
   }
 }
 
-
-
+////// document ready!
 
 $(document).ready(function(){
   attachListeners();
