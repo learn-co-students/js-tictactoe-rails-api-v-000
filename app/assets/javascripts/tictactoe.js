42103ff8e4f07514668416582cb0dac26bd758fc
Brian Menard <brian@webdevbrian.com>
2016-05-23 20:06:20 +0000

    started work on getBoard and checkWinner functions

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,4 +1,16 @@
 var turn = 0;
+var win_combos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+var board = [];
 
 function attachListeners(){
     $("td").click(function(event){
@@ -6,13 +18,14 @@ function attachListeners(){
     });
     
     $("#save").click(function(){
-        alert("clicked!");
+        getBoard();
     });
 }
 
 function doTurn(event){
     turn++;
     updateState(event);
+    checkWinner();
 }
 
 function player(){
@@ -25,6 +38,20 @@ function player(){
 
 function updateState(event){
     $(event.target).text(player());
+}
+
+function getBoard(){
+    board = [];
+    $.each($("td"), function(index, cell){
+        board.push(cell); //NOT RETURNING CORRECT VALUE
+    });
+    alert(board);
+}
+
+function checkWinner(){
+    for(var i = 0; i > win_combos.length; i++){
+        //if indexes on board for each value in combo == X || Y, 
+    }
 }
 
 
