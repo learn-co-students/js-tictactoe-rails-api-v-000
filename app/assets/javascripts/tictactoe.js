2f88834c0ab36e6f88d0c7223dff199a6a82913f
Brian Menard <brian@webdevbrian.com>
2016-05-24 19:08:25 +0000

    added persistence, loadGame() currently not working

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,3 +1,5 @@
+///LOAD GAMES IS NOT WORKING
+
 var turn = 0;
 var win_combos = [
   [0, 1, 2],
@@ -9,19 +11,28 @@ var win_combos = [
   [0, 4, 8],
   [2, 4, 6]
 ];
-
+var gameId = 0;
+var currentGame = 0;
 var board = [];
 
+//SET LISTENERS ------------------------
 function attachListeners(){
     $("td").click(function(event){
         doTurn(event);
     });
     
     $("#save").click(function(){
-        //save 
+        save();
+    });
+    $("#previous").click(function(){
+        showGames();
+    });
+    $("#games").on("click", "[data-gameid]", function(event) {
+        loadGame(event);
     });
 }
 
+//GAMEPLAY -------------------------------
 function doTurn(event){
     turn++;
     updateState(event);
@@ -37,7 +48,7 @@ function player(){
 }
 
 function updateState(event){
-    $(event.target).text(player());
+    $(event.target).text(player()); //add if already occupied, cannot play there
 }
 
 function getBoard(){
@@ -51,15 +62,74 @@ function checkWinner(){
     for(var i = 0; i < win_combos.length; i++){
         //if all three indexes of win_combo are X, return win message
         //else if all three indexes of win combo are O, return win message
-        //else if board is full, return tie message
+        //else if board is full and there is no winner, return tie message
         
         getBoard();
         if (board[win_combos[i][0]] == 'X' && board[win_combos[i][1]] == 'X' && board[win_combos[i][2]] == 'X'){
-            alert('Player X won!'); //updated to message()
+            message('Player X won!');
         } else if (board[win_combos[i][0]] == 'O' && board[win_combos[i][1]] == 'O' && board[win_combos[i][2]] == 'O'){
-            alert'Player O won!'); //update to message()
+            message('Player O won!');
         } 
     }
+}
+
+function message(text){
+    $('#message').text(text);
+}
+
+//PERSISTENCE ------------------------------
+
+function save(){
+    getBoard();
+    
+    var gameData = {
+        game: {
+            id: gameId, //need to increment gameID on new game
+            state: board
+        }
+    };
+    
+    if (gameId === 0){ //if its the first game, need to create new (otherwise we just need to update)
+        $.ajax({
+            url:'/games',
+            type:'POST',
+            dataType: 'json',
+            data: gameData
+        });
+    } else {
+        $.ajax({
+            url:'/games',
+            type:'PATCH',
+            dataType: 'json',
+            data: gameData
+        });
+    }
+}
+
+function showGames(){
+    $.get('/games', function(data){
+        if (data.games.length > 0){
+            var html = '';
+            $.each(data.games, function(i, game){
+                html += "<p data-gameId='" + game.id + "'>Game " + game.id + "</p>";
+            });
+            $("#games").html(html);
+        } else{
+            $("#games").html("No previous games")
+        }
+    });
+}
+
+function loadGame(event){
+    currentGame = $(event.target).data('gameId');
+    alert(currentGame);
+    var url = '/games/'+ currentGame;
+    $.get(url, function(data){
+        board = data.game.state;
+        $('td').each(function(index, cell){
+            $(this).text(board[index]);     
+        });
+    });
 }
 
 
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,32 @@
 class GamesController < ApplicationController
 
+    def index
+       @games = Game.all
+       render json: @games
+    end
+
+    def create
+      @game = Game.create()
+      @game.state = params[:game][:state]
+      @game.save
+      render json: @game, status: 201
+    end
+    
+    def show
+       @game = Game.find(params: :id) 
+       render json: @game
+    end
+    
+    def update
+        @game = Game.find(params: id)
+        @game.state = params[:game][:state]
+        @game.save
+        render json: @game
+    end
+    
+    
+    def game_params
+        params.require(:game).permit(:state)
+    end
     
 end
