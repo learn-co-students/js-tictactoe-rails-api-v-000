a897b371c8a70c38cdb11c548e6094060bb2f608
agdavid <antonio.g.david@gmail.com>
2016-08-11 12:28:53 -0400

    add checkWinner function and supporting checkCells, noCellMatch functions

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -5,6 +5,17 @@ $(function() {
 
 var turn = 0;
 
+var winningCombos = [
+[[0,0],[1,0],[2,0]], 
+[[0,1],[1,1],[2,1]], 
+[[0,2],[1,2],[2,2]], 
+[[0,0],[1,1],[2,2]], 
+[[0,0],[0,1],[0,2]], 
+[[2,0],[2,1],[2,2]], 
+[[1,0],[1,1],[1,2]], 
+[[2,0],[1,1],[0,2]]
+];
+
 var attachListeners = function() {
   // call function doTurn and pass param of the event
   $('td').on('click', function(e) {
@@ -36,10 +47,35 @@ var updateState = function(e) {
   $(e.target).html(token);
 };
 
-var checkWinner = function() {
-  // evaluate board to see if winner
+var noCellMatch = function(cell) {
+  return (cell.html() != player())
+};
 
-  // if winner, return either "Player X Won!" or "Player O Won!", pass string to function message
+var checkCells = function(winningCombos) {
+  // return false if winningCombo does not match current player token
+  for(var i = 0; i < winningCombos.length; i++) {
+    var winningCombo = winningCombos[i];
+    var x = winningCombo[0];
+    var y = winningCombo[1];
+    var selector = $('[data-x="' + x + '"][data-y="' + y + '"]')
+    if( noCellMatch(selector)) {
+      return false;
+    }
+  }
+  // else return true
+  return true;
+};
+
+var checkWinner = function() {
+  // check array of winningCombos
+  for(var i = 0; i < winningCombos.length; i++) {
+    // if current player token matches winningCombo return string
+    if(checkCells(winningCombos[i]) == true) {
+      message("Player " + player() + " Won!");
+      return true;
+    }
+  }
+  return false;
 };
 
 var message = function(string) {
