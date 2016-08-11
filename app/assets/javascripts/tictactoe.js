fd0904c0cff9ca934ace8174783b152e36656753
agdavid <antonio.g.david@gmail.com>
2016-08-11 11:46:49 -0400

    stub out functions in tictactoe.js based on README

---
+++ app/assets/javascripts/tictactoe.js
@@ -0,0 +1,40 @@
+$(function() {
+  // call function attachListeners after DOM loaded
+  attachListeners();
+});
+
+var turn = 0;
+
+var attachListeners = function() {
+  // call function doTurn and pass param of the event
+
+};
+
+var doTurn = function(e) {
+  // increment variable turn by one
+
+  // call function updateState and pass param of the event
+
+  // call function checkWinner
+
+};
+
+var player = function() {
+  // if turn is even, return X as string, else return O as string
+};
+
+var updateState = function() {
+  // call function player, which provides return value, add return value to clicked cell
+
+};
+
+var checkWinner = function() {
+  // evaluate board to see if winner
+
+  // if winner, return either "Player X Won!" or "Player O Won!", pass string to function message
+};
+
+var message = function(string) {
+  // add string to div with id of message
+}
+
