7d1de29c3ea585109de4f72392fc19af4bc18801
Smulligan85 <sean.mulligan85@gmail.com>
2016-08-16 21:02:56 -0700

    Work on checkWinner()

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,19 +1,26 @@
 var turn = 0;
+var currentGame = 0;
+var winCombos = [
+  [[0,0], [1,0], [2,0]],
+  [[0,1], [1,1], [2,1]],
+  [[0,2], [1,2], [2,2]],
+  [[0,0], [1,1], [2,2]],
+  [[2,0], [1,1], [0,2]],
+  [[0,0], [0,1], [0,2]],
+  [[1,0], [1,1], [1,2]],
+  [[2,0], [2,1], [2,2]]
+];
 
 function attachListeners() {
-  var cells = document.getElementsByTagName("td");
-    for (i = 0; i < cells.length; i++) {
-        cells[i].addEventListener("click", function(event) {
-          event.preventDefault();
-          doTurn(event);
-        });
-    }
+  $("tbody").on("click", function(event) {
+    doTurn(event);
+  });
 }
 
 function doTurn(event) {
-  turn++;
   updateState(event);
   checkWinner();
+  turn++;
 }
 
 function player() {
@@ -26,16 +33,35 @@ function player() {
 
 function updateState(event) {
   var playerToken = player();
-  var cell = event.currentTarget;
-  cell.html(playerToken);
+  var cell = event.target;
+  $(cell).html(playerToken);
 }
 
-function message() {
+function message(str) {
+  $("#message").html(str);
+}
 
+function won() {
+  for (var n = 0; n < winCombos.length; n++) {
+    for (var j = 0; j < winCombos[n].length; j++) {
+      var winIndexOne = winCombos[n][j][0];
+      var winIndexTwo = winCombos[n][j][1];
+      var winCell = $('[data-x="' + winIndexOne + '"][data-y="' + winIndexTwo + '"]');
+      if(winCell.html() != player()) {
+        return false;
+      }
+    }
+  }
+      return true;
 }
 
 function checkWinner() {
-
+  if(won()) {
+    console.log("You Won!");
+    // message("Player " + player() + " Won!");
+  } else {
+    console.log("You Lost!");
+  }
 }
 
 $(document).ready(function () {
