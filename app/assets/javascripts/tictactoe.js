4a52ec300f55e6f8fa4280ce2d2c3cc99d5fbf47
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-08-30 16:36:03 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -2,9 +2,21 @@ $(document).ready(function(){
   attachListeners();
 })
 
-var turn = 0;
-var bool = false;
+var win_combos = [
+      [0,1,2], // Top Row
+      [3,4,5], // Middle Row
+      [6,7,8], // Bottom Row
+      [0,4,8], // Diagonal Row # 1
+      [6,4,2], // Diagonal Row # 2
+      [0,3,6], // Vertical Column 1
+      [1,4,7], // Vertical Column 2
+      [2,5,8]  // Vertical Column 3
+  ];
+var turn        = 0;
+var currentGame = 0;
+var bool        = false;
 var token;
+var boardCells  = [];
 
 var player = function() {
   bool  = !bool;
@@ -27,6 +39,18 @@ var doTurn = function(obj) {
   checkWinner();
 }
 
+var getTable = function() {
+  // body...
+  $('table td').each(function() {
+    boardCells.push($(this).text());
+  })
+}
+
+var resetGame = function() {
+  $('table tr').remove();
+  turn = 0;
+}
+
 var checkWinner = function() {
   // this function will be used to test that combos have the same elements
   Array.prototype.sameValues = function() {
@@ -37,35 +61,19 @@ var checkWinner = function() {
    return true
   }; 
   
-  var WIN_COMBINATIONS = [
-      [0,1,2], // Top Row
-      [3,4,5], // Middle Row
-      [6,7,8], // Bottom Row
-      [0,4,8], // Diagonal Row # 1
-      [6,4,2], // Diagonal Row # 2
-      [0,3,6], // Vertical Column 1
-      [1,4,7], // Vertical Column 2
-      [2,5,8]  // Vertical Column 3
-  ];
+  var resultCombo = [];
+  
+  // populating boardCells using table values
+  getTable();    
 
-  var boardCells = [],
-     resultCombo = [];
-          
-  $('table td').each(function() {
-    boardCells.push($(this).text());
-  })
-
-  for (var i = 0; i < WIN_COMBINATIONS.length; i++) {
-    debugger
-    for (var x = 0; x < WIN_COMBINATIONS[i].length; x++) {
-      resultCombo.push(boardCells[WIN_COMBINATIONS[i][x]]); // pushing values at win combo indeces into a result array
-      if (resultCombo.length == 3) {      // once resultCombo has 3 elements
-        if (resultCombo[0] !== ""){       // test for empty combos
-          if (resultCombo.sameValues()) { // if combos  are not empty check if they actually have the same values
-            debugger
+  for (var i = 0; i < win_combos.length; i++) {
+    for (var x = 0; x < win_combos[i].length; x++) {
+      resultCombo.push(boardCells[win_combos[i][x]]); // pushing values at win combo indeces into a resultCombo array
+      if (resultCombo.length == 3) {                        // once resultCombo has 3 elements
+        if (resultCombo[0] !== ""){                         // test for empty combos
+          if (resultCombo.sameValues()) {                   // if combos  are not empty check if they actually have the same values
             message('Player '+resultCombo[0]+' Won!');
-            $('table tr').remove();
-            turn = 0;
+            resetGame();
             return true;
           } else {
             resultCombo = [];
@@ -79,8 +87,7 @@ var checkWinner = function() {
 
   if (turn === 8 || turn === 7) {
     message('Tie game');
-    $('table tr').remove();
-    turn = 0;
+    resetGame();
   } else {
     return false;
   }
@@ -90,5 +97,11 @@ var attachListeners = function() {
   $('tr td').click(function(event) {
     // maybe need to pass event to function() and then to doTurn
     doTurn($(this));
+  })
+
+  $('#previous').click(function(event) {
+    $.get('/games'), function(data) {
+      // get previous games here
+    }
   })
 }
