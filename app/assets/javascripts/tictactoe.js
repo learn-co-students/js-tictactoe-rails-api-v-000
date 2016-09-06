bf121de69a60313816bdd57dcdb2555b7ab28290
aprietof <adrianprieto00@gmail.com>
2016-09-06 16:24:31 -0400

    Blank functions added with comments, and token function

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,13 +1,26 @@
 var turn = 0;
 
-function attachListeners() {
+winners = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [7, 5, 3]
+]
 
+function attachListeners() {
+  // When a client clicks on a cell,
+  // the function doTurn() should be called and passed a
+  // parameter of the event
 }
 
 
-
-
 function player() {
+  // Should return the string "X",
+  // else it should return the string "O"
   if ((turn % 2) == 0) {
     return "X"
   } else {
@@ -16,6 +29,24 @@ function player() {
 };
 
 
-function updateState(cell) {
-  cell.text(player());
+function updateState(clickedCell) {
+  // Should call on player() and add the return
+  // value of this function to the clicked cell on the table
+  clickedCell.text(player());
 };
+
+function token(clickedCell) {
+  // Returns token value of current player
+  clickedCell.text();
+}
+
+function checkWinner() {
+  // Should evaluate the board to see if anyone has won
+
+}
+
+function message() {
+  // Should accept a string and add the string to the
+  // div with an id of "message"
+
+}
