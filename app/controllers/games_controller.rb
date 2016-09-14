52dbfe9b5337fd2bbc7d62b78f3febfadac150d9
michaelsimon <michael.simon@gmail.com>
2016-09-13 22:11:12 -0400

    add routes for create, patch, get games and associated controller actions

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,24 @@
 class GamesController < ApplicationController
 
   def create
+    @game = Game.create(game_params)
+    render json: @game, status: 200
   end
 
   def update
+    @game = Game.find(params[:id])
+    puts @game
+    @game.update(state: params[:game][:state])
+    render json: @game, status: 200
   end
 
   def index
-    #code
+    @games = Game.all
+    render json: @games, status: 200
   end
+end
+
+private
+def game_params
+  params.require(:game).permit(:id, :state => [])
 end
