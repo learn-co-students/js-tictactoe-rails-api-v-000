6fccecf7f1a24ecb6fdf42d2322ec0e25c2e0afb
sensei100 <jenanton01@gmail.com>
2016-10-05 07:47:31 -0400

    Done.

---
+++ app/assets/javascripts/tictactoe.js
@@ -0,0 +1,25 @@
+function attachListeners() {
+  $("td").on("click", function() {
+  
+  })
+}
+
+function doTurn() {
+
+}
+
+function player() {
+
+}
+
+function updateState() {
+
+}
+
+function checkWinner() {
+
+}
+
+function message() {
+
+}
