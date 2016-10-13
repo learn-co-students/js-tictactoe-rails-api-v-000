0734abbdcb4d5d95f62dc38b8b4fb82aa1ef3370
jd2rogers2 <jd2rogers2@gmail.com>
2016-10-13 16:56:53 -0400

    12 tests to go

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
   attachListeners();
 });
 
-var turn = 0, currentGame = 1, outcome = false;
+var turn = 0, currentGame = 1, outcome = false, state = [];
+// board and state are the same thing, board is local variable or parameter reping state
 
 var winningCombos = [
   [0, 1, 2], 
@@ -13,7 +14,7 @@ var winningCombos = [
   [2, 5, 8], 
   [0, 4, 8], 
   [2, 4, 6]
-]
+];
 
 function attachListeners(){
   $('td').on("click", function(){
@@ -30,29 +31,23 @@ function player(){
 }
 
 function doTurn(td_tag){
-  if (checkWinner() === true) {
-    //
-  } else {
-    updateState(td_tag);
-    turn ++;
-    checkWinner();
-  }
+  updateState(td_tag);
+  checkWinner();
+  turn ++;
 }
 
 function checkWinner(){
-  var tds = $('td')
-  var state = []
-  for (var count = 0; count < tds.length; count++) {
-    state.push(tds[count].innerHTML);
-  }
-  // i.e. state = ["X", "X", "X", "O", "O", "", "", "", ""]
-  for (var count = 0; count < winningCombos.length; count++) {
-    var box0 = winningCombos[count][0];
-    var box1 = winningCombos[count][1];
-    var box2 = winningCombos[count][2];
-    if (state[box0] === state[box1] && state[box1] === state[box2] && state[box2] != "") {
-      outcome = true;
-      message(`Player ${state[box2]} Won!`);
+  if (state.length > 0) {
+    for (var count = 0; count < winningCombos.length; count++) {
+      var box0 = winningCombos[count][0];
+      var box1 = winningCombos[count][1];
+      var box2 = winningCombos[count][2];
+      if (state[box0] === state[box1] && state[box1] === state[box2] && state[box2] != "") {
+        outcome = true;
+        message(`Player ${state[box2]} Won!`);
+      } else if (state.includes("") === false) {
+        message("Tie game");
+      }
     }
   }
   return outcome;
@@ -60,6 +55,12 @@ function checkWinner(){
 
 function updateState(td_tag){
   $(td_tag).text(player())
+  var tds = $('td')
+  var board = [];
+  for (var count = 0; count < tds.length; count++) {
+    board.push(tds[count].innerHTML);
+  }
+  state = board;
 }
 
 function message(msg){
