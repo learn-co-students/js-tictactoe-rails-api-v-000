51b1dbb5a1c7750822e776f5377f5604a4cef304
James Novak <jamesnovak90@gmail.com>
2016-10-16 01:02:16 -0400

    Done.

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,32 @@
+class GamesController < ApplicationController
+  def index
+    @games = Game.all
+    render json: @games
+  end
+
+  def create
+    @game = Game.create(game_params)
+    render json: @game, status: 201
+  end
+
+  def show
+    respond_to do |f|
+      f.html {render :show}
+      f.json {render json: @game}
+    end
+  end
+
+  def destroy
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    @game.update(game_params)
+    render json: @game
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:state)
+  end
+end
