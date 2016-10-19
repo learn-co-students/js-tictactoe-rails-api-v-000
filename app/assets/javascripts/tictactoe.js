f7cc49b4352f1a3d342b55a7274a468fdc064fba
pacosta613 <pedro.acosta613@gmail.com>
2016-10-19 15:16:25 -0400

    Created an attachListeners skeleton function, along with 3 variables.

---
+++ app/assets/javascripts/tictactoe.js
@@ -0,0 +1,20 @@
+$(function(){
+  attachListeners();
+})
+
+var win_combos = [    
+  [0,1,2], 
+  [3,4,5],
+  [6,7,8], 
+  [0,4,8], 
+  [6,4,2], 
+  [0,3,6], 
+  [1,4,7], 
+  [2,5,8]
+];
+var turn = 0;
+var currentGame = 0;
+
+function attachListeners(){
+
+}
