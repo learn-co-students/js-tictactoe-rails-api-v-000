76b6420719616a42d1e81c97cc1d979b2bcd1517
Kailee <kaileeagray@gmail.com>
2016-10-31 13:24:27 -0500

    Tie game working

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,32 +1,36 @@
 var turn = 0;
-var winCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+var winCombinations = [["00","10","20"], ["01","11","21"], ["02","12","22"],
+    ["00","01","02"], ["10","11","12"], ["20","21","22"],
+    ["00","11","22"], ["02","11","20"]
+   ];
 
 var attachListeners = function() {
 // call to attach the click handlers to the page after the DOM has been loaded
 //When a client clicks on a cell, the function doTurn() should be called and passed a parameter of the event
 
   $('td').on('click', function(event) {
-    doTurn(event);
-  })
+    doTurn(event.target);
+  });
+  $('#reset').on('click', function(event) {
+    resetGame();
+  });
 }
 
 var taken = function(turnEvent) {
-  return !!$(turnEvent.target).html();
+  return !!$(turnEvent).html();
 }
 
 var doTurn = function(turnEvent) {
   // Increment the variable turn by one
   // Should call on the function updateState() and pass it the event
   // Should call on checkWinner()
-      if(taken(turnEvent)){
-        console.log("taken")
-      } else {
-      turn++;
-
-      updateState(turnEvent);
-
-      checkWinner();
-      }
+    if(taken(turnEvent)){
+      message("That square is taken. Please select another.");
+    } else if (!over()) {
+    updateState(turnEvent);
+    checkWinner();
+    turn++;
+    }
 }
 
 var updateState = function(turnEvent) {
@@ -34,8 +38,7 @@ var updateState = function(turnEvent) {
 //and add the return value of this function to the clicked cell on the table
 
   var currentPlayer = player();
-  $(turnEvent.target).text(currentPlayer);
-
+  $(turnEvent).html(currentPlayer);
 }
 
 var player = function() {
@@ -48,24 +51,65 @@ var player = function() {
   }
 }
 
+var full = function() {
+  // @board.count("X") + @board.count("O") == 9 ?
+  // true : false
+  return $('td').text().length > 8;
+}
+
+var cat = function() {
+  if (won()) {
+    return false;
+  } else if (!full()) {
+    return false;
+  } else {
+    return true;
+  }
+
+}
+
+var won = function() {
+
+}
+
+var over = function() {
+  if (cat() || won()){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
 
 
 var checkWinner = function() {
   //This function should evaluate the board to see if anyone has won
   //If there is a winner, this function should make one of two strings:
   //"Player X Won!" or "Player O Won!". It should then pass this string to message().
-
+  if (over()) {
+    if (won()) {
+      message("Player " + player() + " Won!");
+      resetGame();
+    } else {
+      message("Tie game");
+    }
+  } else {
+    return false;
+  }
 }
+
 
 var message = function(string) {
   //This function should accept a string and add the string to the div with an id of "message"
-  resetGame();
+  $('#message').text(string);
 }
 
 var resetGame = function() {
   turn = 0;
   $("td").html("");
-
+  $('#message').text(" ");
 }
 
 $(function() {
