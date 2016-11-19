84e1f06b5e6f2bb2ff4d98eef6e2d976535f5246
steveafrost <ffrostt@gmail.com>
2016-11-19 17:12:16 -0500

    basic gameplay improvements

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,7 +1,7 @@
-// The grid is made by a table. Each square is in a table row, or tr and each square is a table data, or td (you could also call this a cell).
-// Each td has two data attributes: x and y coordinates. The top left td had an x of 0 and a y of 0.
-
 var turn = 0;
+
+// Use this for refactor of checkWinner()
+var winningCombos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
 function attachListeners() { // COMPLETE
 // You must have a function called attachListeners() which the tests call to attach the click handlers to the page after the DOM has been loaded
@@ -23,8 +23,8 @@ function doTurn(move) { // COMPLETE
   // Should call on checkWinner()
   updateState(move.target);
 
-  turn++;
   checkWinner();
+  turn++;
 }
 
 function player() {  // COMPLETE
@@ -41,9 +41,12 @@ function updateState(move) { // COMPLETE
   $(move).text(player());
 }
 
-function checkWinner() {
+function checkWinner() { // COMPLETE
   // This function should evaluate the board to see if anyone has won
   // If there is a winner, this function should make one of two strings: "Player X Won!" or "Player O Won!". It should then pass this string to message().
+  // 3 Horizontal Wins
+  // 2 Diagonal Wins
+  // 3 Vertical Wins
   if (board()[0] === board()[1] && board()[1] === board()[2] && board()[2] !== "") {
     return message("Player " + board()[0] + " Won!");
   } else if (board()[3] === board()[4] && board()[4] === board()[5] && board()[5] !== "") {
@@ -54,16 +57,24 @@ function checkWinner() {
     return message("Player " + board()[0] + " Won!");
   } else if (board()[2] === board()[4] && board()[4] === board()[6] && board()[6] !== "") {
     return message("Player " + board()[2] + " Won!");
+  } else if (board()[0] === board()[3] && board()[3] === board()[6] && board()[6] !== "") {
+    return message("Player " + board()[0] + " Won!");
+  } else if (board()[1] === board()[4] && board()[4] === board()[7] && board()[7] !== "") {
+    return message("Player " + board()[1] + " Won!");
+  } else if (board()[2] === board()[5] && board()[5] === board()[8] && board()[8] !== "") {
+    return message("Player " + board()[2] + " Won!");
   } else if ($.inArray("", board()) === -1) {
-    return message("Cats Game");
+    return message("Tie game");
   } else {
-    return message("It is " + player() + "s turn to go");
+    return false;
   }
 }
 
 function message(string) { // COMPLETE
   // This function should accept a string and add the string to the div with an id of "message"
-  $('#message').html("<h4>" + string + "</h4>");
+  $('#message').html(string);
+  $('td').empty();
+  turn = 0;
 }
 
 
