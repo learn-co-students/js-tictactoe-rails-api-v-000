1c6e5f87ee69ea06a43e017fac36819b8172e4a2
steveafrost <ffrostt@gmail.com>
2016-11-20 16:59:14 -0500

    adjusted final save to happen via POST per spec. cleaned up comments

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,6 @@
 var turn = 0;
 var currentGame = 0;
+var gameState = board();
 var winningCombos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
 function attachListeners() {
@@ -9,40 +10,29 @@ function attachListeners() {
 
   $('#save').click(saveGameState);
 
-  // NOTE: Game # Link(s)
-  // (1) Clicking on an individual (Game#) link makes an ajax call via getJSON().
-  // (2) With the data-id attribute, getJSON makes a 'GET' request for the specified game resource.
-  // (3) The request gets processed by the #show action in games controller and returned in the response.
-  // (4) TODO: Take the response variable and populate the board with those values and resume game.
-  //
-  $('#games').on('click', 'a', function(event) {
-    event.preventDefault();
-    var id = $(this).attr("data-id");
-
-    $.getJSON(`/games/${id}.json`, function(response){
-      // TODO: edit response below
-      var gameState = response.game.state;
-    });//end
-  });//end of on()
-
-  // NOTE: "Show Previous" button
-  // Game History (clicking the "Show Previous Games")
-  // (1) Clicking on the "Show Previous" button makes an ajax call via getJSON().
-  // (2) getJSON makes a 'GET' request for all available game resources.
-  // (3) The request gets processed by the #index action in games controller and returned in the response.
-  $("#previous").on('click', function() {
+  $("#previous").click(function() {
     $.getJSON("/games", function(response) {
 
-      // var html = "<h2>Game History</h2>";
       var html = "";
       $.each(response.games, function(i, game){
         var id = game.id;
         html += `<li><a href="#" class"js-game" data-id="${id}">Game #${id}</a></li>`;
-      }); //end of each()
-      $("#games").html(html);
+      });
 
-    });//end of get()
-  });//end of on()
+      $("#games").html(html);
+    });
+  });
+
+  // Ideal Product: Take the response variable and populate the board with those values and resume game.
+  //
+  // $('#games').on('click', 'a', function(event) {
+  //   event.preventDefault();
+  //   var id = $(this).attr("data-id");
+
+  //   $.getJSON(`/games/${id}.json`, function(response){
+  //     var gameState = response.game.state;
+  //   });
+  // });
 }
 
 function board() {
@@ -50,10 +40,6 @@ function board() {
     return square.innerHTML;
   });
   return $.makeArray(allTd);
-}
-
-function loadBoard(save) {
-
 }
 
 function clearBoard() {
@@ -102,17 +88,17 @@ function checkWinner() {
 
 function message(string) {
   $('#message').html(string);
-  saveGameState();
+
+  $.ajax({
+    type: 'POST',
+    url: '/games',
+    dataType: 'json',
+    data: { game: {id: currentGame, state: gameState} },
+  });
   clearBoard();
 }
 
-// NOTE: Saves or updates game resource to the db
 function saveGameState() {
-  var gameState = board();
-
-  // TODO: Set "ajaxMethod" and "ajaxUrl" variables
-  // (1) "ajaxType" should be set to "POST" if game is new or set to "PATCH" if game is in progress.
-  // (2) Set "ajaxUrl" to "games/id_value" if updating an existing resource.
   $.ajax({
     type: (currentGame === 0) ? 'POST' : 'PATCH',
     url: (currentGame === 0) ? '/games' : '/games/' + currentGame,
@@ -123,7 +109,6 @@ function saveGameState() {
     }
   });
 }
-
 
 $(function() {
   attachListeners();
