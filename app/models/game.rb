4a9f573b5a8bfb63ad120397955cfdeb3d91bdef
Dom McKellar <dom.mckellar@gmail.com>
2016-11-20 04:07:02 -0800

    Update games controller and routes, and set up ajax calls

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,3 @@
 class Game < ActiveRecord::Base
   serialize :state, JSON #(optional enforcement of data type)
-
-  # game = Game.new
-  # game.state = {game_board_keys: value}
-  # game.save #=>saved as YAML in a text field, later retrieved as a
 end
