727b1661fd456c370d1e1a29191f8eedb5495ac8
Christian Whitesides <whitesidescr@gmail.com>
2016-11-21 16:08:26 -0700

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,11 +1,25 @@
+$(function() {
+  attachListeners();
+});
+
+
+
 var turn = 0;
 
 function attachListeners() {
-
+  $("td").click(function() {
+    var $td = $(this);
+    var xCoord = $td.data('x');
+    var yCoord = $td.data('y');
+    doTurn(xCoord, yCoord);
+    console.log($("td[data-x='"+xCoord+"'][data-y='"+yCoord+"']").html(), $("td[data-x='"+xCoord+"'][data-y='"+yCoord+"']").selector)
+  });
 }
 
-function doTurn() {
-
+function doTurn(x, y) {
+  updateState(x, y);
+  turn++;
+  checkWinner();
 }
 
 function player() {
@@ -17,6 +31,32 @@ function player() {
   return move
 }
 
-function updateState(event) {
-  
+function updateState(x, y) {
+  $("td[data-x='"+x+"'][data-y='"+y+"']").text(player);
+}
+
+function checkWinner() {
+  var vertWin1 = [$("td[data-x='0'][data-y='0']").html(), $("td[data-x='0'][data-y='1']").html(), $("td[data-x='0'][data-y='2']").html()]
+  var vertWin2 = [$("td[data-x='1'][data-y='0']").html(), $("td[data-x='1'][data-y='1']").html(), $("td[data-x='1'][data-y='2']").html()]
+  var vertWin3 = [$("td[data-x='2'][data-y='0']").html(), $("td[data-x='2'][data-y='1']").html(), $("td[data-x='2'][data-y='2']").html()]
+  var horizWin1 = [$("td[data-x='0'][data-y='0']").html(), $("td[data-x='1'][data-y='0']").html(), $("td[data-x='2'][data-y='0']").html()]
+  var horizWin2 = [$("td[data-x='0'][data-y='1']").html(), $("td[data-x='1'][data-y='1']").html(), $("td[data-x='2'][data-y='1']").html()]
+  var horizWin3 = [$("td[data-x='0'][data-y='2']").html(), $("td[data-x='1'][data-y='0']").html(), $("td[data-x='2'][data-y='2']").html()]
+  var diagWin1 = [$("td[data-x='0'][data-y='0']").html(), $("td[data-x='1'][data-y='1']").html(), $("td[data-x='2'][data-y='2']").html()]
+  var diagWin2 = [$("td[data-x='0'][data-y='2']").html(), $("td[data-x='1'][data-y='1']").html(), $("td[data-x='2'][data-y='0']").html()]
+
+  var winPoss = [vertWin1, vertWin2, vertWin3, horizWin1, horizWin2, horizWin3, diagWin1, diagWin2]
+
+  var xWin = ["X", "X", "X"]
+  var oWin = ["O", "O", "O"]
+
+  // if turn = 9 and nobody has won, tie game...clear the board
+  $.each(winPoss, function(index, sequence) {
+    // Loop through each sequence and compare with xWin and oWin
+
+  });
+}
+
+function message(text) {
+  $("div#message").html(text)
 }
