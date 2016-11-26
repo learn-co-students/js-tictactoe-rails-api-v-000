715944b9f1de4d351aa74064414e7dd77973964b
lenn hypolite <lennhydesign@gmail.com>
2016-11-25 21:20:42 -0500

    changed the win function

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -15,50 +15,45 @@ var pos_8=  $("tr").children("td:td:nth(7)");
 var pos_9=  $("tr").children("td:td:nth(8)");
 
 //  constructer function where each parameter is a position on the board
-function Board(pos_1, pos_2, pos_3, pos_4, pos_5, pos_6, pos_7, pos_8, pos_9 ){
-                this.pos_1 = pos_1;
-                this.pos_2 = pos_2;
-                this.pos_3 = pos_3;
-                this.pos_4 = pos_4;
-                this.pos_5 = pos_5;
-                this.pos_6 = pos_6;
-                this.pos_7 = pos_7;
-                this.pos_8 = pos_8;
-                this.pos_9 = pos_9;
-}
+var board = [
+              pos_1,
+              pos_2,
+              pos_3,
+              pos_4,
+              pos_5,
+              pos_6,
+              pos_7,
+              pos_8,
+              pos_9
+            ]
 
 var winCombos = [
-                          [0,1,2],
-                          [3,4,5],
-                          [6,7,8],
-                          [0,3,6],
-                          [1,4,7],
-                          [2,5,8],
-                          [0,4,8],
-                          [6,4,2]
+              [0,1,2],
+              [3,4,5],
+              [6,7,8],
+              [0,3,6],
+              [1,4,7],
+              [2,5,8],
+              [0,4,8],
+              [6,4,2]
 ]
 
-function winStatus(winner, board){
+function win(){
+  var k = 0;
   var e = 1;
   var f = 2;
-  for(var j = 0; j < winCombos.length; j++){
-     for(var k = 0; k < winCombos[j].length; k++,e++, f++){
-            if (board[winCombos[j][k]] === "X" && board[winCombos[j][e]] === "X" && board[winCombos[j][f]] === "X"){
-                 winner = "X";
-                return winner;
+     for(var j = 0; j < winCombos.length; j++){
+        if (board[winCombos[j][k]] === board[winCombos[j][e]] && board[winCombos[j][e]] === board[winCombos[j][f]] && board[winCombos[j][k]] !== "O"){
+          winner = "X";
         }
-        else if(board[winCombos[j][k]] === "O" && board[winCombos[j][e]] === "O" && board[winCombos[j][f]] === "O"){
+        else if(board[winCombos[j][k]] === board[winCombos[j][e]] && board[winCombos[j][e]] === board[winCombos[j][f]] && board[winCombos[j][k]] !== "X"){
           winner = "O";
-         return winner;
         }
         else{
-          return false;
+          winner = false;
         }
      }
-
-  }
-
-
+     return winner;
 }
 
 // ================================ MAIN FUNCTION ==================================
