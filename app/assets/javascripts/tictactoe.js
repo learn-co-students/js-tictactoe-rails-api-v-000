9b12295741478ec32aaea6428d5d9152e9616530
lenn hypolite <lennhydesign@gmail.com>
2016-11-27 04:25:48 -0500

    revised the checkWinner method

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,61 +1,49 @@
-$(function () {
-    attachListeners();
-})
-
 var turn = 0;
-var winner ="";
-var pos_1=  $("tr").children("td:td:nth(0)");
-var pos_2=  $("tr").children("td:td:nth(1)");
-var pos_3=  $("tr").children("td:td:nth(2)");
-var pos_4=  $("tr").children("td:td:nth(3)");
-var pos_5=  $("tr").children("td:td:nth(4)");
-var pos_6=  $("tr").children("td:td:nth(5)");
-var pos_7=  $("tr").children("td:td:nth(6)");
-var pos_8=  $("tr").children("td:td:nth(7)");
-var pos_9=  $("tr").children("td:td:nth(8)");
+var winCombos =  [
+                    [0,1,2],
+                    [3,4,5],
+                    [6,7,8],
+                    [0,3,6],
+                    [1,4,7],
+                    [2,5,8],
+                    [0,4,8],
+                    [6,4,2]
+              ];
+var board = [];
 
-//  constructer function where each parameter is a position on the board
-var board = [
-              pos_1,
-              pos_2,
-              pos_3,
-              pos_4,
-              pos_5,
-              pos_6,
-              pos_7,
-              pos_8,
-              pos_9
-            ];
+function checkWinner(){
+  var response;
+  for(var j = 0; j < winCombos.length; j++){
+      var winCombo = winCombos[j];
+      // for(var i=0; i < winCombo.length; i++){
+        var win_index_1 = winCombo[0];
+        var win_index_2 = winCombo[1];
+        var win_index_3 = winCombo[2];
+      // }
+        // console.log(win_index_1, win_index_2, win_index_3);
 
-const winCombos = [
-                  [0,1,2],
-                  [3,4,5],
-                  [6,7,8],
-                  [0,3,6],
-                  [1,4,7],
-                  [2,5,8],
-                  [0,4,8],
-                  [6,4,2]
-            ];
+        var pos_1 = board[win_index_1];
+        var pos_2 = board[win_index_2];
+        var pos_3 = board[win_index_3];
+        // console.log(pos_1, pos_2, pos_3);
+        console.log(pos_1);
 
-function win(){
-  var k = 0;
-  var e = 1;
-  var f = 2;
-     for(var j = 0; j < winCombos.length; j++){
-        if (board[winCombos[j][k]] === board[winCombos[j][e]] && board[winCombos[j][e]] === board[winCombos[j][f]] && board[winCombos[j][k]] !== "O"){
-          winner = "X";
+
+        if (pos_1 === "X" && pos_2 === "X" && pos_3 === "X"){
+          response = "Player X Won!";
         }
-        else if(board[winCombos[j][k]] === board[winCombos[j][e]] && board[winCombos[j][e]] === board[winCombos[j][f]] && board[winCombos[j][k]] !== "X"){
-          winner = "O";
+        else if(pos_1 === "O" && pos_2 === "O" && pos_3 === "O"){
+          response = "Player O Won!";
         }
-        else{
-          winner = false;
-        }
-     }
-     return winner;
-}
 
+      }
+
+    message(response);
+  }
+
+  // var pos_1  = board[winCombos[j][0]];
+  // var pos_2 = board[winCombos[j][1]];
+  // var pos_3 = board[winCombos[j][2]];
 // ================================ MAIN FUNCTION ==================================
 function attachListeners(){
   $("td").on('click', function(event) {
@@ -63,8 +51,15 @@ function attachListeners(){
   });
 }
 
+function boardPositions(event){
+  var element = event.target
+  var indexVal = $("td").index(event.target);
+  board[indexVal] =  element.innerText;
+}
+
 function doTurn(event){
   updateState(event);
+  boardPositions(event);
   checkWinner();
   turn +=1;
 }
@@ -82,17 +77,12 @@ function updateState(event){
   $(event.target).html(value);
 }
 
-function checkWinner(){
-  win();
-  var response ="";
-  if (winner==="X"){
-    response = "Player X Won!"
-  }else{
-    response = "Player O Won!"
-  }
-  return message(response);
-}
+
 
 function message(response){
   $('#message').html(response);
 }
+
+$(function () {
+    attachListeners();
+});
