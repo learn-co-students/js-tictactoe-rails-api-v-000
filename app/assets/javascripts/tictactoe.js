6092d7e4d32a77a084c51239bb6cb155bbf4e4a5
lenn hypolite <lennhydesign@gmail.com>
2016-11-27 15:13:15 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,4 +1,5 @@
 var turn = 0;
+
 var winCombos =  [
                     [0,1,2],
                     [3,4,5],
@@ -14,34 +15,29 @@ var board = [];
 function checkWinner(){
   var response;
   for(var j = 0; j < winCombos.length; j++){
-      var winCombo = winCombos[j];
-      // for(var i=0; i < winCombo.length; i++){
-        var win_index_1 = winCombo[0];
-        var win_index_2 = winCombo[1];
-        var win_index_3 = winCombo[2];
-      // }
-        // console.log(win_index_1, win_index_2, win_index_3);
+    var winCombo = winCombos[j];
 
-        var pos_1 = board[win_index_1];
-        var pos_2 = board[win_index_2];
-        var pos_3 = board[win_index_3];
-        // console.log(pos_1, pos_2, pos_3);
-        console.log(pos_1);
+    var win_index_1 = winCombo[0];
+    var win_index_2 = winCombo[1];
+    var win_index_3 = winCombo[2];
 
+    var pos_1 = board[win_index_1];
+    var pos_2 = board[win_index_2];
+    var pos_3 = board[win_index_3];
 
-        if (pos_1 === "X" && pos_2 === "X" && pos_3 === "X"){
-          response = "Player X Won!";
-        }
-        else if(pos_1 === "O" && pos_2 === "O" && pos_3 === "O"){
-          response = "Player O Won!";
-        }
-
-      }
-      if (response == undefined){
-        return false;
-      }
-    message(response);
+    if (pos_1 === "X" && pos_2 === "X" && pos_3 === "X"){
+      response = "Player X Won!";
+    }
+    else if(pos_1 === "O" && pos_2 === "O" && pos_3 === "O"){
+      response = "Player O Won!";
+    }
   }
+  if (response == undefined){
+    tieGame();
+    return false;
+  }
+  message(response);
+}
 
 //================================ MAIN FUNCTIONS ==================================
 function attachListeners(){
@@ -55,7 +51,6 @@ function boardPositions(event){
   var indexVal = $("td").index(event.target);
   if(element !== undefined){
     board[indexVal] = element.innerText;
-    // console.log(element.innerText);
   }
 }
 
@@ -77,6 +72,10 @@ function player(){
 function updateState(event){
   var value = player();
   $(event.target).html(value);
+}
+
+function tieGame(){
+  message("Tie game");
 }
 
 function message(response){
