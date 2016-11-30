d1437f303c59c51449f642b0af085fb3886d8834
Caleb Faught <faughtcj@gmail.com>
2016-11-29 22:21:43 -0600

    Fixed board reset bug, added some documentation

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,9 @@
+// Main Tic Tac Toe functions
+
 var turn = 0;
 
 function attachListeners() {
+  // Head of program, sets eventListeners for click events
   $('td').on('click', function(event) {
     var x = $(this).attr("data-x");
     var y = $(this).attr("data-y");
@@ -9,13 +12,28 @@ function attachListeners() {
 }
 
 function doTurn(event) {
+  // Game loop, responds to event and checks for game end
   updateState(event);
   if (!checkForGameOver()) {
     turn += 1;
+  } else {
+    resetBoard();
   }
 }
 
+function checkForGameOver() {
+  // Checks for game ending event, directly called by game loop
+
+  if (checkWinner() || checkForTie(getBoardState())) {
+    return true;
+  }
+  return false;
+}
+
 function checkWinner() {
+  // Checks board state against winning combinations,
+  // returns true or false
+
   var winningCombos = [
     [0, 1, 2],
     [3, 4, 5],
@@ -34,33 +52,28 @@ function checkWinner() {
 
     if (board[position0] === board[position1] && board[position1] === board[position2] && board[position0] !== "") {
       message("Player " + board[position0] + " Won!");
-      resetBoard();
       return true;
-    } else if (checkForTie(board)) {
-      message("Tie game");
-      resetBoard();
     }
   }
   return false;
 }
 
 function checkForTie(board) {
+  // Checks for a tie game, returns true or false
+  // called by checkForGameOver()
+
   for (var element in board) {
     if (board[element] === "") {
+      message("Tie game");
       return false;
     }
   }
   return true;
 }
 
-function checkForGameOver() {
-  if (checkWinner() || checkForTie(getBoardState())) {
-    return true;
-  }
-  return false;
-}
-
 function resetBoard() {
+  // Resets board and turn variable
+
   turn = 0;
   for (var i = 0; i < 3; i++) {
     for (var j = 0; j < 3; j++) {
@@ -70,10 +83,15 @@ function resetBoard() {
 }
 
 function updateState(event) {
+  // Writes player token to cell clicked, accepts event from
+  // eventListener on board.
+
   $(event.currentTarget).text(player());
 }
 
 function player() {
+  // updates player token based on turn. X goes first.
+
   if (turn % 2 === 0) {
     return "X";
   } else {
@@ -82,10 +100,14 @@ function player() {
 }
 
 function message(message) {
+  // writes whether game is a tie, or which player won (by token).
   $("#message").text(message);
 }
 
 function getBoardState() {
+  // function grabs board state and returns that state, used
+  // to check for win conditions.
+
   var state = [];
   for (var i = 0; i < 3; i++) {
     for (var j = 0; j < 3; j++) {
@@ -96,6 +118,9 @@ function getBoardState() {
 }
 
 
+/////////////////////////////
+// Start game on page load //
+/////////////////////////////
 $(function() {
   attachListeners();
 });
