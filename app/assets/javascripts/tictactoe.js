76cfe8f6ec1a871ac57d50939408afa63d1fbefc
Clare <clare.e.horton@gmail.com>
2017-01-19 20:38:02 -0600

    game is functional!

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -27,6 +27,21 @@ function checkWinner() {
   // checks to see if anyone has won
   // if winner, calls on player to see who won
   // and passes "Player X/O Won" to message()
+  var board = {};
+  var winCombinations = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ];
+
+  $("td").each(function(index) {
+    // adds text value of each cell to board
+    board[index] = $(this).text();
+  });
+
+  for (var i in winCombinations) {
+      if ( (board[winCombinations[i][0]] === "X") && (board[winCombinations[i][1]] === "X") && (board[winCombinations[i][2]] === "X") ) {
+        message("Player " + player() + " Won");
+      } else if ( (board[winCombinations[i][0]] === "O") && (board[winCombinations[i][1]] === "O") && (board[winCombinations[i][2]] === "O") ) {
+        message("Player " + player() + " Won");
+      }
+  }
 }
 
 function player() {
