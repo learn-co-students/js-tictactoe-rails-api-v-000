c730a2c3d6b0783519e3c746a3c06066f8904e9b
Clare <clare.e.horton@gmail.com>
2017-01-19 18:34:42 -0600

    setup tictactoe.js

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,33 @@
+var turn = 0;
 
+function attachListeners() {
+  // will be called in $(document).ready to attach click handlers
+  // click handlers pass params of clicked cell to doTurn()
+}
+
+function doTurn() {
+  // turn + 1
+  // calls updateState() and checkWinner()
+}
+
+function updateState() {
+  // calls player(), adds return (x or o) to the clicked cell
+}
+
+function checkWinner() {
+  // checks to see if anyone has won
+  // if winner, calls on player to see who won
+  // and passes "Player X/O Won" to message()
+}
+
+function player() {
+  // returns x or o depending on whether turn is odd or even
+}
+
+function message() {
+  // adds the given string to div#message
+}
+
+$(document).ready(function () {
+
+});
