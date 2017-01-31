009d2013d5eec1cae81062cc05000d55625a8e8e
hilaryml <hilyttle@gmail.com>
2017-01-31 16:15:07 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,39 +1,18 @@
 //pseudo code
 
-//work on getting cell clicks to make x's and o's appear
-//then work on save
-//then work on previous
 var turn = 0
-var board = []
-var gameCount = 0
 
 function attachListeners() {
   $('td').on('click', function(event) {
     //passes doTurn a param of the event
-    doTurn(event)
+    doTurn(event);
     event.preventDefault();
   })
-  $('#save').on('click', function(event) {
-    event.preventDefault();
-    //save game to database
-    var posting = $.post('/games', board);
-    //append game number to #games div
-    posting.done(function() {
-      gameCount += 1;
-      $('#games').append(gameCount);
-    })
-  })
-  $('#previous').on('click', function(event) {
-    //previous games are hidden until #previous is clicked - then show them like an index
-    event.preventDefault();
-  })
-  //when the user clicks on a previous game, it loads that game - like a show view
 }
 
 function doTurn(event) {
   turn += 1;
   updateState(event);
-  checkWinner();
 }
 
 function player() {
@@ -46,30 +25,11 @@ function player() {
 
 function updateState(event) {
   var player = player();
-  var cell = event.getAttribute("data-x") && event.getAttribute("data-y");
+  var x = event.getAttribute("data-x");
+  var y = event.getAttribute("data-y");
+  var xCell = $('td').find("[data-x= '" + x + "']")
+  var cell = xCell.find("[data-y]= '" + y + "']");
   cell.text(player);
-}
-
-function checkWinner() {
-  //check board to see if anyone won
-  //if someone has one, return "Player _ Won!" and pass the string to message()
-  //new game needs to be created when one is finished
-}
-
-function message(string) {
-  //add string to the message div
-  $('#message').append(string);
-}
-
-function gameBoard() {
-  //returns an array of the current board, which you can use when showing a game to
-  //iterate over to add each element as text to that cell...?
-  //can also iterate through the array to check if someone has won
-  $.each($("td"), function(index, cell) {
-    //does this add nil values for blank cells? ... because I need it to...
-    board.push($(cell).text());
-    return board
-  })
 }
 
 $(document).ready(function() {
