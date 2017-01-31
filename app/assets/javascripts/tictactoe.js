2c6ea70beef569f4e05a81c48fb8dcba0015b8d7
hilaryml <hilyttle@gmail.com>
2017-01-31 16:32:59 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -2,19 +2,6 @@
 
 var turn = 0
 
-function attachListeners() {
-  $('td').on('click', function(event) {
-    //passes doTurn a param of the event
-    doTurn(event);
-    event.preventDefault();
-  })
-}
-
-function doTurn(event) {
-  turn += 1;
-  updateState(event);
-}
-
 function player() {
   if (turn % 2 === 0) {
     return "X"
@@ -23,13 +10,27 @@ function player() {
   }
 }
 
-function updateState(event) {
-  var player = player();
-  var x = event.getAttribute("data-x");
-  var y = event.getAttribute("data-y");
-  var xCell = $('td').find("[data-x= '" + x + "']")
-  var cell = xCell.find("[data-y]= '" + y + "']");
-  cell.text(player);
+function updateState(cell) {
+  var currentPlayer = player();
+  //var x = event.getAttribute("data-x");
+  //var y = event.getAttribute("data-y");
+  //var xCell = $('td').find("[data-x= '" + x + "']")
+  //var cell = xCell.find("[data-y]= '" + y + "']");
+  $(cell).text(currentPlayer);
+}
+
+function doTurn(cell) {
+  turn += 1;
+  updateState(cell);
+}
+
+function attachListeners() {
+  $('td').on('click', function(event) {
+    event.preventDefault();
+    //passes doTurn a param of the event
+    //grabs cell that was clicked
+    doTurn(event.toElement);
+  })
 }
 
 $(document).ready(function() {
