aa2af8685276493931654b8e7398da568981761d
hilaryml <hilyttle@gmail.com>
2017-02-01 15:47:08 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -5,48 +5,40 @@ var gameCount = 0; //could just display game id + 1 instead of keeping track of 
 var board = [];
 var currentGame = undefined;
 
-function gameBoard() {
-  //returns an array of the current board, which you can use when showing a game to
-  //iterate over to add each element as text to that cell...?
-  //can also iterate through the array to check if someone has won
-  $.each($("td"), function(index, cell) {
-    //does this add nil values for blank cells? ... because I need it to...
-    board.push($(cell).text());
-  })
-}
-
-function player() {
-  if (turn % 2 === 0) {
-    return "X"
-  } else {
-    return "O"
-  }
-}
-
-function updateState(cell) {
-  var currentPlayer = player();
-  //var x = event.getAttribute("data-x");
-  //var y = event.getAttribute("data-y");
-  //var xCell = $('td').find("[data-x= '" + x + "']")
-  //var cell = xCell.find("[data-y]= '" + y + "']");
-  $(cell).text(currentPlayer);
-  gameBoard();
-}
-
-function doTurn(cell) {
-  turn += 1;
-  updateState(cell);
-}
-
-//split these into separate functions once working
-function attachListeners() {
-  $('td').on('click', function(event) {
+function games() {
+  $('#games').on('click', function(event) {
+    //when the user clicks on a previous game, it loads that game - like a show view
     event.preventDefault();
-    //passes doTurn a param of the event
-    //grabs cell that was clicked
-    doTurn(event.toElement);
-  })
 
+    var id = event.target.innerText
+    $.get("/games/" + id, function(data) {
+      //display state as current board
+      var array = data.state
+      $.each($("td"), function(index, cell) {
+        //does this add nil values for blank cells? ... because I need it to...
+        $(cell).text(array[index]);
+      })
+    })
+  })
+}
+
+function previous() {
+  $('#previous').on('click', function(event) {
+    //previous games are hidden until #previous is clicked - then show them like an index
+    event.preventDefault();
+    $.get("/games", function(data) {
+      var games = data.games
+      var html = "<ul>"
+      $.each(games, function(index, game) {
+        html += "<li>" + game.id + "</li>"
+      })
+      html += "</ul>"
+      $('#games').append(html);
+    });
+  })
+}
+
+function save() {
   $('#save').on('click', function(event) {
     event.preventDefault();
 
@@ -82,35 +74,58 @@ function attachListeners() {
       }
     })
   })
+}
 
-  $('#previous').on('click', function(event) {
-    //previous games are hidden until #previous is clicked - then show them like an index
+function cell() {
+  $('td').on('click', function(event) {
     event.preventDefault();
-    $.get("/games", function(data) {
-      var games = data.games
-      var html = "<ul>"
-      $.each(games, function(index, game) {
-        html += "<li>" + game.id + "</li>"
-      })
-      html += "</ul>"
-      $('#games').append(html);
-    });
+    //passes doTurn a param of the event
+    //grabs cell that was clicked
+    doTurn(event.toElement);
   })
+}
 
-  $('#games').on('click', function(event) {
-    //when the user clicks on a previous game, it loads that game - like a show view
-    event.preventDefault();
-
-    var id = event.target.innerText
-    $.get("/games/" + id, function(data) {
-      //display state as current board
-      var array = data.state
-      $.each($("td"), function(index, cell) {
-        //does this add nil values for blank cells? ... because I need it to...
-        $(cell).text(array[index]);
-      })
-    })
+function gameBoard() {
+  //returns an array of the current board, which you can use when showing a game to
+  //iterate over to add each element as text to that cell...?
+  //can also iterate through the array to check if someone has won
+  $.each($("td"), function(index, cell) {
+    //does this add nil values for blank cells? ... because I need it to...
+    board.push($(cell).text());
   })
+}
+
+function player() {
+  if (turn % 2 === 0) {
+    return "X"
+  } else {
+    return "O"
+  }
+}
+
+function updateState(cell) {
+  var currentPlayer = player();
+  //var x = event.getAttribute("data-x");
+  //var y = event.getAttribute("data-y");
+  //var xCell = $('td').find("[data-x= '" + x + "']")
+  //var cell = xCell.find("[data-y]= '" + y + "']");
+  $(cell).text(currentPlayer);
+  gameBoard();
+}
+
+function doTurn(cell) {
+  turn += 1;
+  updateState(cell);
+}
+
+function attachListeners() {
+  cell();
+
+  save();
+
+  previous();
+
+  games();
 }
 
 $(document).ready(function() {
