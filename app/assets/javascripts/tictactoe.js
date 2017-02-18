2c23635edf64fac0ff104479cc33f88f1c2ae0b9
bmolina-nyc <bruce.molina.81@gmail.com>
2017-02-18 11:44:50 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -31,25 +31,86 @@ function player(){
 
 function updateState(selector){
   var selector = selector
-  $(selector).html(player())
-  checkWinner();
+  $(selector).html(player());
+  checkWinner(); 
 }
 
 function checkWinner(){
-
+  if (horizontalCheck() === "true" || verticalCheck() == "true" || diagnolCheck() == "true" ){
+    if (player() === "O"){
+      var winner = "Player X Won!"
+      message(winner);
+    } else {
+      var winner = "Player O Won!"
+      message(winner);
+    }
+  } else {
+  return false 
+  }
 }
 
 function horizontalCheck(){
-  if ( $('td')[0].innerHTML === "X" ){
-    console.log ("First step working")
+  if ( $('td')[0].innerHTML === "X" && $('td')[1].innerHTML === "X" && $('td')[2].innerHTML === "X" ||
+       $('td')[3].innerHTML === "X" && $('td')[4].innerHTML === "X" && $('td')[5].innerHTML === "X" || 
+       $('td')[6].innerHTML === "X" && $('td')[7].innerHTML === "X" && $('td')[8].innerHTML === "X" ||
+       // check the O's for the same 
+       $('td')[0].innerHTML === "O" && $('td')[1].innerHTML === "O" && $('td')[2].innerHTML === "O" ||
+       $('td')[3].innerHTML === "O" && $('td')[4].innerHTML === "O" && $('td')[5].innerHTML === "O" || 
+       $('td')[6].innerHTML === "O" && $('td')[7].innerHTML === "O" && $('td')[8].innerHTML === "O" )
+  {
+       return "true"
+  } else {
+       return "false"
+  }
+}
+
+function verticalCheck(){
+  if ( $('td')[0].innerHTML === "X" && $('td')[3].innerHTML === "X" && $('td')[6].innerHTML === "X" ||
+       $('td')[1].innerHTML === "X" && $('td')[4].innerHTML === "X" && $('td')[7].innerHTML === "X" || 
+       $('td')[2].innerHTML === "X" && $('td')[5].innerHTML === "X" && $('td')[8].innerHTML === "X" ||
+       // check the O's for the same 
+       $('td')[0].innerHTML === "O" && $('td')[3].innerHTML === "O" && $('td')[6].innerHTML === "O" ||
+       $('td')[1].innerHTML === "O" && $('td')[4].innerHTML === "O" && $('td')[7].innerHTML === "O" || 
+       $('td')[2].innerHTML === "O" && $('td')[5].innerHTML === "O" && $('td')[8].innerHTML === "O" )
+  {
+       return "true"
+  } else {
+       return "false"
+  }
+}
+
+function diagnolCheck(){
+  if ( $('td')[0].innerHTML === "X" && $('td')[4].innerHTML === "X" && $('td')[8].innerHTML === "X" || 
+       $('td')[2].innerHTML === "X" && $('td')[4].innerHTML === "X" && $('td')[6].innerHTML === "X" ||
+       // check the O's for the same
+       $('td')[0].innerHTML === "O" && $('td')[4].innerHTML === "O" && $('td')[8].innerHTML === "O" || 
+       $('td')[2].innerHTML === "O" && $('td')[4].innerHTML === "O" && $('td')[6].innerHTML === "O" )
+  {
+      return "true"
+  } else {
+      return "false"
+  }
+}
+
+function fullBoard() {
+  if ( $('td')[0].innerHTML === "" && $('td')[1].innerHTML === "" &&
+       $('td')[2].innerHTML === "" && $('td')[3].innerHTML === "" &&
+       $('td')[4].innerHTML === "" && $('td')[5].innerHTML === "" &&
+       $('td')[6].innerHTML === "" && $('td')[7].innerHTML === "" &&
+       $('td')[8].innerHTML === "" ){
+    return "false"
+  } else {
+    return "true"
   }
 }
 
 
-function turn(){
+function turn (){
 
 }
 
-function message(){
 
+
+function message(winner){
+  $("#message").html(winner)
 }
