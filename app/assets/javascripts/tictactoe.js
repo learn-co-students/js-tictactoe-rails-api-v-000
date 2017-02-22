ff911a37099f3e2f28f4dbbd98b0875aaad2313c
Laurent Milne <lmilne360@gmail.com>
2017-02-22 04:52:35 -0500

    clean up unruly code

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -3,84 +3,94 @@ $(function() {
 });
 
 var turn = 0;
-var winning = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+var winning = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 function attachListeners() {
-  $('td').click(function(event){
-    doTurn(event);
-  });
+    $('td').click(function(event) {
+        doTurn(event);
+    });
 
-  $('#previous').click(function(event){
-    getAllGames();
-  });
+    $('#previous').click(function(event) {
+        getAllGames();
+    });
 
-  $('#save').click(function(event){
-    save();
-  });
+    $('#save').click(function(event) {
+        save();
+    });
 
-  $('#previous').click(function(event){
-    getAllGames();
-  });
+    $('#previous').click(function(event) {
+        getAllGames();
+    });
 }
 
 function doTurn(event) {
 
-  updateState(event);
-  checkWinner();
+    updateState(event);
+    checkWinner();
     turn++;
 }
 
 function player() {
-  return (turn % 2 === 0) ? "X" : "O";
+    return (turn % 2 === 0) ? "X" : "O";
 }
 
 function updateState(event) {
-  $(event.target).html(player());
+    $(event.target).html(player());
 }
 
 function checkWinner() {
-  var board = selections();
+    var board = selections();
 
-  if (checkTie()) {
-    resetGame();
-    message("Tie game");
-    return false;
-  }
-
-  for (var i = 0; i < winning.length; i++) {
-    if( (board[winning[i][0]] === "X") && (board[winning[i][1]] ===  "X") && (board[winning[i][2]] === "X")){
-      console.log("Player 1 wins");
-      message(`Player ${player()} Won!`);
-      resetGame();
-      return true;
-    }else if ((board[winning[i][0]] === "O") && (board[winning[i][1]] ===  "O") && (board[winning[i][2]] === "O")) {
-      console.log("Player 2 wins");
-      message(`Player ${player()} Won!`);
-      resetGame();
-      return true;
+    if (checkTie()) {
+        resetGame();
+        message("Tie game");
+        return false;
     }
-  }return false; //for end
-}//checkwinner end
+
+    for (var i = 0; i < winning.length; i++) {
+        if ((board[winning[i][0]] === "X") && (board[winning[i][1]] === "X") && (board[winning[i][2]] === "X")) {
+            console.log("Player 1 wins");
+            message(`Player ${player()} Won!`);
+            resetGame();
+            return true;
+        } else if ((board[winning[i][0]] === "O") && (board[winning[i][1]] === "O") && (board[winning[i][2]] === "O")) {
+            console.log("Player 2 wins");
+            message(`Player ${player()} Won!`);
+            resetGame();
+            return true;
+        }
+    }
+    return false; //for end
+} //checkwinner end
 
 //get all the inputs
 function selections() {
-  var board = [];
-  $('td').each(function() {
-    board.push($(this).text());
-  });
-  return board;
+    var board = [];
+    $('td').each(function() {
+        board.push($(this).text());
+    });
+    return board;
 }
 
 function message(str) {
- $('#message').text(str);
+    $('#message').text(str);
 }
 
 function resetGame() {
-  $('td').empty();
-  turn = -1;
+    $('td').empty();
+    turn = -1;
 
 }
 
 function checkTie() {
-  return turn === 8 ? true : false ;
+    return turn === 8 ? true : false;
 }
