b6c7814ef67a88f1339e4ce59290761621dc6609
Scott Young <young.scottw@gmail.com>
2017-03-21 13:42:33 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,18 +1,23 @@
 var turn = 0;
 var winCombinations = [[[0,0],[1,0],[2,0]], [[0,1],[1,1],[2,1]], [[0,2],[1,2],[2,2]], [[0,0],[1,1],[2,2]], [[0,0],[0,1],[0,2]], [[2,0],[2,1],[2,2]], [[1,0],[1,1],[1,2]], [[2,0],[1,1],[0,2]]]
+var board = [[,,], [,,], [,,]]
 
+//TODO: work out checkWinner logic
 var checkWinner = function() {
-  for(let i=0; i<winCombinations.length; i++) {
-    // TODO: work out checkWinnder logic
-    return false;
-  }
-  return true;
+//   for(let row=0; row<8; row++) {
+//     for(let col=0; col<3; col++) {
+//       $('[data-x="' + row + '"][data-y="' + col + '"]').html()
+//       // console.log($('[data-x="' + row + '"][data-y="' + col + '"]').html());
+//       // console.log(board[row][col]);
+//     }
+//   }
+   return false;
 }
 
-var tie = function() {
-  // TODO: work out tie logic
-  return false;
-}
+// TODO: workout checkTie logic
+// var checkTie = function() {
+//   return false;
+// }
 
 var player = function() {
   if(turn % 2 == 0) {
@@ -23,29 +28,39 @@ var player = function() {
 }
 
 var updateState = function(event) {
-  $(event.target).html(player());
+   $(event.target).html(player());
+//
+// //TODO: update board with new position
+// //  debugger;
+// //  board[row][col] = $(event.target).html();
 }
 
 var message = function(message) {
   $("#message").html(message);
 }
-
 var doTurn = function(event) {
-  updateState(event);
-  if(checkWinner() || tie()) {
+  if(checkWinner()) {
     console.log("winner or tie");
   } else {
-    turn += 1;
+    turn++;
+    updateState(event);
+    debugger;
   }
-
 }
 
 var attachListeners = function() {
-  $("tbody").click(function(event) {
-    doTurn(event);
-  });
+    $("tbody").click(function(event) {
+        doTurn(event);
+    });
 }
 
 $(function() {
   attachListeners();
 });
+
+// $("tbody").find('td').click(function(event) {
+//     col = $(this).index();
+//     $("tbody").find('tr').click(function(event) {
+//         row = $(this).index();
+//     });
+// });
