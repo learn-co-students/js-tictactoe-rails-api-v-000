3ef5ced67c164616a0bfb43ded04e134dac0faee
ldgarber <leah.garber88@gmail.com>
2017-03-24 17:21:01 -0400

    finished first part of task - no persistence

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,13 +1,72 @@
 var turn = 0; 
 var winningCombos = [ [[0,0],[0,1],[0,2]], [[1,0],[1,1],[1,2]], [[2,0], [2,1], [2,2]], [[0,0],[1,1],[2,2]], [[0,0],[1,0],[2,0]], [[0,1],[1,1],[2,1]], [[0,2],[1,2],[2,2]], [[2,0],[1,1],[0,2]] ];  
+var board = getCurrentBoard(); 
 
-var checkForWin = function(combo) {
-   
-
+function getCurrentBoard() {
+  var currentBoard = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]; 
+  for (var i = 0; i < currentBoard.length; i++) {
+    for (var j = 0; j < currentBoard[i].length; j++) {
+      currentBoard[i][j] = $('[data-x="'+ i + '"][data-y="'+ j + '"]').html();  
+    }
+  }
+  return currentBoard; 
 }
 
-var tie = function() {
-  if checkWinner()
+function resetBoard() {
+  var dimension = 3; 
+  for (var i = 0; i < dimension; i++) {
+    for (var j = 0; j < dimension; j++) {
+      $('[data-x="'+ i + '"][data-y="'+ j + '"]').html(''); 
+    }
+  }
+  return; 
+}
+
+var isEmpty = function(space) {
+  if (space == "X"){  
+    return false; 
+  } else if (space == "O") {
+    return false;  
+  } else {
+    return true;  
+  }
+}
+
+var checkForWin = function(combo) {
+  var currentBoard = getCurrentBoard(); 
+  for (var i = 0; i < combo.length; i++) {
+    var currentSquare = currentBoard[combo[i][0]][combo[i][1]]; 
+    if (currentSquare == player()) {
+      continue;  
+    } else {
+      return false;  
+    }
+  }   
+  return true; 
+}
+
+function tie() {
+  return (!checkWinner() && boardIsFull()); 
+}
+
+function checkForTie() {
+  if ( tie() ) {
+    message("Tie game"); 
+    return true; 
+  }
+  return false; 
+}
+
+var boardIsFull = function() {
+  var board = getCurrentBoard(); 
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[i].length; j++) {
+      if (isEmpty(board[i][j])) {
+        return false;  
+      }
+    } 
+  }
+  return true; 
 }
 
 var attachListeners = function() {
@@ -18,14 +77,18 @@ var attachListeners = function() {
 
 var doTurn = function(event) {
   updateState(event); 
-  checkWinner(); 
-  turn++; 
+  if (checkWinner() || checkForTie() ){ 
+    resetBoard(); 
+    turn = 0; 
+   } else {
+    turn++; 
+   } 
 }
 
 var checkWinner = function() {
   for (var i = 0; i < winningCombos.length; i++) {
     if (checkForWin(winningCombos[i])) {
-      message("Player " + player() + "Won!"); 
+      message("Player " + player() + " Won!"); 
       return true;  
     } 
   }
