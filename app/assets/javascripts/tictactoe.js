8b5aea0d1d633033bd66f197caf20ce0e1be4b5f
lizdenhup <lizdenhup@gmail.com>
2017-04-03 13:41:08 -0500

    first commit

---
+++ app/assets/javascripts/tictactoe.js
@@ -0,0 +1,26 @@
+function attachListeners() {
+  $(function() {
+
+  })
+}
+
+function doTurn() {
+  var turn = 0;
+  
+}
+
+function checkWinner() {
+
+}
+
+function updateState() {
+
+}
+
+function player() {
+
+}
+
+function message() {
+
+}
