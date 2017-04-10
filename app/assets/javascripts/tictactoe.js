19a4450bd8d463c1c8b14cf14ae00fd9df77672c
lizdenhup <lizdenhup@gmail.com>
2017-04-10 08:22:13 -0500

    made a lot of changes, starting over(ish)

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,72 +1,18 @@
-messages = {
-  tryAgain: "That move is not available. Please pick an open spot.",
-  tieGame: "Tie game",
-  xWon: "Player X Won!",
-  yWon: "Player Y Won!"
-}
-
-var winningCombinations = [
-   [[0,0], [1,0], [2,0]],
-   [[0,1], [1,1], [2,1]],
-   [[0,2], [1,2], [2,2]],
-   [[0,0], [0,1], [0,2]],
-   [[1,0], [1,1], [1,2]],
-   [[2,0], [2,1], [2,2]],
-   [[0,0], [1,1], [2,2]],
-   [[2,0], [1,1], [0,2]]
-]
-
-$(function() {
-  attachListeners();
-})
-
+$(document).ready( function() {
+   attachListeners();
+});
+  
 function attachListeners() {
-$(document).on("click","td", function(e){
-//  coordinate = [$(this).attr('data-x'), $(this).attr('data-y')]
-  potentialSpot = $(this)
-  doTurn(potentialSpot);
+  $("tbody").on("click", "td",  function(e){
+  doTurn(e.target)
+ //   console.log(e.target); => returns clicked cell with data-x and data-y properties
   });
 
-$("#save").on("click", function(e) {
+  $('#save').on("click", function() {
 
-})
+  })
 
-$("previous").on("click", function(e) {
+  $('#previous').on("click", function() {
 
-})
-}
-
-//bind click listener to square
-//call doTurn
-//if space value is nil or an empty string 
-//update value to player token
-
-function doTurn(potentialSpot) {
-  turn = 0;
-  if (potentialSpot.val() === "" || !potentialSpot) {
-    updateState();
-    turn++;
-  } else {
-    message(tryAgain);
-  }
-}
-
-function checkWinner() {
-
-}
-
-function updateState() {
-  potentialSpot.val() = player(); 
-}
-
-var player = function player() {
-  if (turn % 2 == 0) {
-    token = 'X';
-  } else {
-    token ='O';
-  }
-}
-
-function message(input) {
-  $("#message").append((messages[input]));
+  })
 }
