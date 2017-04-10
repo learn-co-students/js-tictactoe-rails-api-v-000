bd17ddada24252a150cd25541a96ed3fc8e244f2
lizdenhup <lizdenhup@gmail.com>
2017-04-10 08:36:57 -0500

    added 3d array of winning combos

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,18 +1,47 @@
+var turn = 0;
+var winningCombos = [
+  [[0,0],[1,0],[2,0]],
+  [[0,1],[1,1],[2,1]],
+  [[0,2],[1,2],[2,2]],
+  [[0,0],[0,1],[0,2]],
+  [[1,0],[1,1],[1,2]],
+  [[2,0],[2,1],[2,2]],
+  [[0,0],[1,1],[2,2]],
+  [[2,0],[1,1],[0,2]]
+]
+
 $(document).ready( function() {
    attachListeners();
 });
   
 function attachListeners() {
-  $("tbody").on("click", "td",  function(e){
+  $("table").on("click", "td",  function(e){
   doTurn(e.target)
- //   console.log(e.target); => returns clicked cell with data-x and data-y properties
+    console.log(e.target); 
+    //=> returns clicked cell with data-x and data-y properties
   });
 
   $('#save').on("click", function() {
-
+    //make AJAX request to post '/games'
   })
 
   $('#previous').on("click", function() {
-
+    //make AJAX request to get '/games'
   })
+}
+
+function doTurn(target) {
+
+}
+
+function updateState() {
+
+}
+
+function checkWinner() {
+
+}
+
+function player() {
+
 }
