420a7c9bf76d8059a8a37aec0edc2e00aa0b3650
lizdenhup <lizdenhup@gmail.com>
2017-04-11 16:52:57 -0500

    8 test failures.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,7 @@
 var turn = 0;
 var state = ["", "", "", "", "", "", "", "", ""];
+var currentGame;
+var endGame = false;
 
 var winStates = [
   [0, 1, 2], 
@@ -15,89 +17,196 @@ var winStates = [
 $(document).ready( function() {
    attachListeners();
 });
-  
-function attachListeners() {
-  $("table").on("click", "td",  function(e){
-    target = e.target
-    xCoord = parseInt(target.attributes['data-x']['nodeValue']);
-    yCoord = parseInt(target.attributes['data-y']['nodeValue']);
-    doTurn(target)
+
+var attachListeners = function() {
+  $("table").click(function(event) {
+    xCoord = parseInt(event.target.attributes['data-x']['nodeValue']);
+    yCoord = parseInt(event.target.attributes['data-y']['nodeValue']);
+    doTurn(event);
   });
-
-  $('#save').on("click", function() {
-    //make AJAX request to post '/games'
-  })
-
-  $('#previous').on("click", function() {
-    //make AJAX request to get '/games'
-  })
+  $("#games").click(function(event) {
+    var state = parseState(event);
+    swapGame(state, getGameId(event))
+  });
+  $("#save").click(function() {
+    save();
+  });
+  $("#previous").click(function() {
+    getAllGames();
+  });
 }
 
-function player() {
+var cellIsAvailable = function(event) {
+  if ($(event.target).html() === "") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var player = function() {
   if (turn % 2 === 0) {
-    return 'X'
+    return "X"
   } else {
-    return 'O'
+    return "O"
   }
 }
 
-function doTurn(target) {
-  if (target.innerHTML === "") {
-    updateState(target);
-    turn++;
+var doTurn = function(event) {
+  if(cellIsAvailable(event)) {
+      updateState(event);
+      message("");
+    } else {
+      message("This cell is taken. Please choose another.");
+    }
+  if(checkWinner() || checkTie(turn)) {
+    save(true);
+    resetGame();
   } else {
-    alert("That spot is occupied.")
+    turn ++;
   }
-  debugger;
-  checkWinner(state);
 }
 
-function updateState(target) {
-  target.innerHTML = player();
+var updateState = function(event) {
+  $(event.target).html(player());
   var i = yCoord * 3 + xCoord;
   state[i] = player();
-
 }
 
-function checkWinner(state) {
-  //debugger;
-  if (state === undefined || state === ["", "", "", "", "", "", "", "", ""]) {
+//////////////////////////////consolidate into one checkWinner method and one tie method//////////
+/*var checkWinner(state) {
+   if (state === undefined || state === ["", "", "", "", "", "", "", "", ""]) {
     return false; 
   }
- // debugger;
- // these lines break everything and I don't know why
- // [0,1,2]
- //that does make sense - so I thought the way I had it written was checking if the state was a win State?
  for (var i = 0; i < winStates.length; i++) {
     if (state[winStates[i][0]] !== "" && state[winStates[i][0]] === state[winStates[i][1]] && state[winStates[i][1]] === state[winStates[i][2]]) {
         message("Player " + player() + " Won!");
-        return true; // return breaks out of loops and just returns the value, no more code will  be executed within the function
+        return true;
     }
   }
   return false;
 }
+*/
+function checkWinner(state){
+  if(state === undefined){
+    return false;
+  }
+ for (var i = 0; i < winStates.length; i++) {
+    if (state[winStates[i][0]] !== "" && state[winStates[i][0]] === state[winStates[i][1]] && state[winStates[i][1]] === state[winStates[i][2]]) {
+        message("Player " + player() + " Won!");
+        return true;
+    }
+  }
+  return false;
 
-function message(input) {
-  $('#message').html(input)
+  if(endGame === true){
+    saveGame(endGame);
+    reset();
+  }
 }
 
-//it does make sense but i'm not sure why it's still broken, because I think I 
-//am iterating through the win states combo on the states variable?
+function checkTie(turn){
+    if(turn === 9){
+    message("Tie game");
+    endGame = true;
+    saveGame(endGame);
+    reset();
+  }
+}
 
+//////////////////////////////
 
+var resetGame = function() {
+  $("td").html("");
+  turn = 0;
+  currentGame = 0;
+}
 
+var message = function(message) {
+  $("#message").html(message);
+}
 
+var parseState = function(event) {
+  return $(event.target).data("state").split(",");
+}
 
+var getGameId = function(event) {
+  return $(event.target).data("gameid");
+}
 
+var getAllGames = function() {
+  $.getJSON("/games").done(function(response) {
+    showGames(response.games);
+  });
+}
 
+var showGames = function(games) {
+  var dom = $()
+  games.forEach(function(game) {
+    dom = dom.add(showGame(game));
+  });
+  $("#games").html(dom);
+}
 
+var showGame = function(game) {
+  return $('<li>', {'data-state': game.state, 'data-gameid': game.id, text: game.id});
+}
 
+var swapGame = function(state, id) {
+  placeMarks(state);
+  currentGame = id;
+  turn = findTurn(state);
+}
 
+var findTurn = function(state) {
+  var turn = 0;
+  state.forEach(function(item) {
+    if(item != "") {
+      turn += 1;
+    }
+  })
+  return turn;
+}
 
+var placeMarks = function(marks) {
+  $("td").each(function(i) {
+    $(this).text(marks[i]);
+  })
+}
+var getMarks = function() {
+  var marks = []
+  $("td").each(function(i) {
+    marks.push($(this).text())
+  })
+  return marks;
+}
 
+var save = function(resetCurrentGame) {
+  var url, method;
+  if(currentGame) {
+    url = "/games/" + currentGame
+    method = "PATCH"
+  } else {
+    url = "/games"
+    method = "POST"
+  }
 
-
-
-
-
+  $.ajax({
+    url: url,
+    method: method,
+    dataType: "json",
+    data: {
+      game: {
+        state: getMarks()
+      }
+    },
+    success: function(data) {
+      if(resetCurrentGame) {
+        currentGame = undefined;
+      } else {
+        currentGame = data.game.id;
+      }
+    }
+  });
+}
 
