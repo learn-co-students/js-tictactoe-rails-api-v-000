43094896695bbcde6882e3c9ee4966ddfb6fd8aa
github username <jilustrisimo@users.noreply.github.com>
2017-04-15 10:01:03 -0400

    building games_controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,14 @@
 class GamesController < ApplicationController
 
   def index
-    
+    @games = Game.all
+    render json: @games
+  end
+
+  def create
+    # binding.pry
+    @game = Game.new
+
   end
 
 end
