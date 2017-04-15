f642607b4c3d7983f0db704e6b84bf43617408ee
github username <jilustrisimo@users.noreply.github.com>
2017-04-15 18:07:04 -0400

    built controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,5 @@
 class GamesController < ApplicationController
+  before_action :set_game, only: %i[show update]
 
   def index
     @games = Game.all
@@ -8,8 +9,24 @@ class GamesController < ApplicationController
   def create
     @game = Game.new
     @game.state = params[:state]
+    @game.turn = params[:turn]
     @game.save
     render json: @game
+  end
+
+  def show
+    render json: @game
+  end
+
+  def update
+    @game.update(state: params[:state], turn: params[:turn])
+    render json: @game
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
   end
 
 end
