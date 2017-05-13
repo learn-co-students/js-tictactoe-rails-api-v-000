1512f922e4614b784c970961dd594e100bf809fc
github username <github email address>
2017-05-13 17:59:09 -0400

    finished player() - three tests now passing

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -3,21 +3,21 @@ $(function() {
 })
 
 var turn = 0
-const winningCombos = {
-  [0, 1, 2], //top row
-  [3, 4, 5], //middle row
-  [6, 7, 8], //bottom row
-  [0, 3, 6], //column 1
-  [1, 4, 7], //column 2
-  [2, 5, 8], //column 3
-  [0, 4, 8], //diagonal 1
-  [2, 4, 6] //diagonal 2
-}
+// const winningCombos = {
+//   [0, 1, 2], //top row
+//   [3, 4, 5], //middle row
+//   [6, 7, 8], //bottom row
+//   [0, 3, 6], //column 1
+//   [1, 4, 7], //column 2
+//   [2, 5, 8], //column 3
+//   [0, 4, 8], //diagonal 1
+//   [2, 4, 6] //diagonal 2
+// }
 
 
 //GAME FUNCTIONALITY
 
-var attachListeners = () => {
+function attachListeners() {
   $('td').on('click', function(e) {
     e.PreventDefault();
     doTurn(e)
@@ -31,7 +31,10 @@ function doTurn(e) {
 }
 
 function player() {
-  return (turn % 2) ? "X" : "O"
+  if (turn % 2 === 0) { //if no remainder then the turn is even
+    return "X"; //return terminates the function, so the "O" return value will not get executed
+  }
+  return "O"; //returns "O" if the if statement was false 
 }
 
 function updateState(cell) {
