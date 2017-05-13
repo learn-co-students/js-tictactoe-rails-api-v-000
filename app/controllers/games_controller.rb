b8ad34c5c94d4379a492c7cae98158a87cf9ea8e
Liah Wallace <liahwallace1@gmail.com>
2017-05-13 13:06:49 -0400

    Update games_controller.rb

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,27 @@
 class GamesController < ApplicationController
+  def create
+    @game = Game.create(game_params)
+    render json: @game
+  end
 
+  def update
+    @game.update(params[:id], game_params)
+    render json: @game
+  end
+
+  def index
+    @games = Game.all
+    render json: @games
+  end
+
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:state => [])
+  end
 end
