3ba44e6d607bf1e0d34d402d2ebbde212a791618
Brie Coyle <brie.coyle@gmail.com>
2017-05-14 08:59:05 -0400

    Add message function to append string to div with id of message.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -4,13 +4,13 @@
 
 var turn = 0
 const winningCombos = [ //these combos are for x, y coordinates in the readme - there may be a better way
-  [[0,0],[1,0],[2,0]], //this is the combo for the top row - 0, 0 = top left; 1, 0 = top center; 2, 0 = top right 
+  [[0,0],[1,0],[2,0]], //this is the combo for the top row - 0, 0 = top left; 1, 0 = top center; 2, 0 = top right
   [[0,1],[1,1],[2,1]],
-  [[0,2],[1,2],[2,2]], 
-  [[0,0],[1,1],[2,2]], 
-  [[0,0],[0,1],[0,2]], 
-  [[2,0],[2,1],[2,2]], 
-  [[1,0],[1,1],[1,2]], 
+  [[0,2],[1,2],[2,2]],
+  [[0,0],[1,1],[2,2]],
+  [[0,0],[0,1],[0,2]],
+  [[2,0],[2,1],[2,2]],
+  [[1,0],[1,1],[1,2]],
   [[2,0],[1,1],[0,2]]
   ]
 
@@ -31,7 +31,7 @@ function doTurn(e) {
 }
 
 function player() {
-  return (turn % 2 === 0) ? "X" : "O"; //if no remainder then x 
+  return (turn % 2 === 0) ? "X" : "O"; //if no remainder then x
 }
 
 function updateState(e) { //I changed it to "e" to follow the convention from doTurn()
@@ -41,14 +41,14 @@ function updateState(e) { //I changed it to "e" to follow the convention from do
 
 function checkWinner() { //I started building this - not sure it's on the right track yet
   winningCombos.forEach(function(combo) { //iterates over Combos array
-    if (winner(combo) == true) { //checks combo against function 
+    if (winner(combo) == true) { //checks combo against function
       message("Player " + player() + " Won!") //calls message() based on current player
     }
   })
 }
 
-function message() {
-
+function message(string) {
+  $("div#message").append(string)
 }
 
 var winner = function() {
