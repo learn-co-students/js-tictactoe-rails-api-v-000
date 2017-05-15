46eb4dcd42bb6efc62b7f8fc9ee8549fa7ae7e7a
github username <github email address>
2017-05-15 12:26:57 -0400

    added function call for previous games - function not built yet

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -3,27 +3,19 @@ $(function() {
 })
 
 var turn = 0
-// const winningCombos = [ //these combos are for x, y coordinates in the readme - there may be a better way
-//   [[0,0],[1,0],[2,0]], //this is the combo for the top row - 0, 0 = top left; 1, 0 = top center; 2, 0 = top right
-//   [[0,1],[1,1],[2,1]],
-//   [[0,2],[1,2],[2,2]],
-//   [[0,0],[1,1],[2,2]],
-//   [[0,0],[0,1],[0,2]],
-//   [[2,0],[2,1],[2,2]],
-//   [[1,0],[1,1],[1,2]],
-//   [[2,0],[1,1],[0,2]]
-//   ]
 
-const winningCombos = [   [0, 1, 2], [3, 4, 5],[6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ]
+const winningCombos = [ [0, 1, 2], [3, 4, 5],[6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ]
 
 
 //GAME FUNCTIONALITY
 
 function attachListeners() {
-
   $('td').on('click', function(e) {
     e.preventDefault();
     doTurn(e)
+  })
+  $('#previous').on('click', function() { //if id previous is clicked, call getAllGames function
+    getAllGames(); 
   })
 }
 
@@ -56,18 +48,6 @@ function checkWinner() {
   return false
 }
 
-// function checkWinner() { //I started building this - not sure it's on the right track yet
-//   var board = getBoard();
-//   winningCombos.forEach(function(combo) { //iterates over Combos array
-//     if ((board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X") || (board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O")) {
-//       message("Player " + player() + " Won!"); //calls message() based on current player
-//       clearBoard();
-//     } else if (boardFull(board)){
-//       message("Tie game")
-//     }
-//   })
-// }
-//
 function getBoard() {
   var board = [];
   $td = $("td");
@@ -86,15 +66,16 @@ function getBoard() {
 //   }
 // }
 //
+
 function clearBoard() {
   $("td").html("");
   turn = 0; //restarts games
 }
-//
+
 function message(string) {
   $("div#message").text(string) //edited to replace text instead of append
 }
-//
+
 // var winner = function() {
 //   //this method can be used to check against checkWinner
 // }
