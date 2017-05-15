f651f0a8a3f506ac91401232a274b2069bb3625c
github username <logicae@users.noreply.github.com>
2017-05-15 12:44:51 -0400

    added eevnt arguments to getAllGames() and Save() functions

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -14,10 +14,10 @@ function attachListeners() {
     e.preventDefault();
     doTurn(e)
   })
-  $('#previous').on('click', function() { //if id previous is clicked, call getAllGames function
-    getAllGames(); 
+  $('#previous').on('click', function(e) { //if id previous is clicked, call getAllGames function
+    getAllGames(e); 
   })
-  $('#save').on('click', function() { //if id save is clicked, call save function
+  $('#save').on('click', function(e) { //if id save is clicked, call save function
     save(); 
   })
 }
