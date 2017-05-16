ea8899d07805daee30120ed32495bd0ce97b6225
Liah Wallace <liahwallace1@gmail.com>
2017-05-16 11:41:08 -0400

    Comment out failing tests per Learn Experts

--- spec/tictactoe.spec.js
+++ spec/tictactoe.spec.js
@@ -258,29 +258,29 @@ describe('#integration tests of persistence', function() {
     });
   });
 
-  it("if i click the save game button a second time it should send a PATCH to /games/:id", function() {
-    setFixtures('<body><table border="1" cellpadding="40"><tr><td data-x="0", data-y="0"></td><td data-x="1", data-y="0"></td><td data-x="2", data-y="0"></td></tr><tr><td data-x="0", data-y="1"></td><td data-x="1", data-y="1"></td><td data-x="2", data-y="1"></td></tr><tr><td data-x="0", data-y="2"></td><td data-x="1", data-y="2"></td><td data-x="2", data-y="2"></td></tr></table><div id="games"></div><div id="message"></div><button id="save">Save Game</button><button id="previous">Show Previous Games</button></body>');
-    attachListeners()
-    jasmine.Ajax.withMock(function() {
-      var data = {
-        game: {
-          id:1,
-          state: ["X","","","","","","","",""]
-        }
-      }
-      var response = {
-        "status": 200,
-        "contentType": 'application/json',
-        "responseText" : JSON.stringify(data)
-      }
-      $('#save').click()
-      jasmine.Ajax.requests.mostRecent().respondWith(response);
-      $('#save').click()
-      var request = jasmine.Ajax.requests.mostRecent();
-      expect(request.url).toBe('/games/1');
-      expect(request.method).toBe('PATCH');
-    });
-  });
+//   it("if i click the save game button a second time it should send a PATCH to /games/:id", function() {
+//     setFixtures('<body><table border="1" cellpadding="40"><tr><td data-x="0", data-y="0"></td><td data-x="1", data-y="0"></td><td data-x="2", data-y="0"></td></tr><tr><td data-x="0", data-y="1"></td><td data-x="1", data-y="1"></td><td data-x="2", data-y="1"></td></tr><tr><td data-x="0", data-y="2"></td><td data-x="1", data-y="2"></td><td data-x="2", data-y="2"></td></tr></table><div id="games"></div><div id="message"></div><button id="save">Save Game</button><button id="previous">Show Previous Games</button></body>');
+//     attachListeners()
+//     jasmine.Ajax.withMock(function() {
+//       var data = {
+//         game: {
+//           id:1,
+//           state: ["X","","","","","","","",""]
+//         }
+//       }
+//       var response = {
+//         "status": 200,
+//         "contentType": 'application/json',
+//         "responseText" : JSON.stringify(data)
+//       }
+//       $('#save').click()
+//       jasmine.Ajax.requests.mostRecent().respondWith(response);
+//       $('#save').click()
+//       var request = jasmine.Ajax.requests.mostRecent();
+//       expect(request.url).toBe('/games/1');
+//       expect(request.method).toBe('PATCH');
+//     });
+//   });
 
   it("if i click the previous game button and no games have been saved i should get no games in the DOM", function() {
     setFixtures('<body><table border="1" cellpadding="40"><tr><td data-x="0", data-y="0"></td><td data-x="1", data-y="0"></td><td data-x="2", data-y="0"></td></tr><tr><td data-x="0", data-y="1"></td><td data-x="1", data-y="1"></td><td data-x="2", data-y="1"></td></tr><tr><td data-x="0", data-y="2"></td><td data-x="1", data-y="2"></td><td data-x="2", data-y="2"></td></tr></table><div id="games"></div><div id="message"></div><button id="save">Save Game</button><button id="previous">Show Previous Games</button></body>');
