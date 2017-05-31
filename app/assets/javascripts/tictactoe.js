54917cb6732dd6af9355fa6b90382f45fad38208
Ray Mulligan <rmullig2@optonline.net>
2017-05-31 19:58:03 -0400

    Change to 0 based grid

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -27,7 +27,7 @@ function player() {
 }
 
 function updateState(event) {
-  //console.log(event);
+  console.log(event);
   test = $( Object.values(event)[0] ).text();
   //console.log($( Object.values(event)[0] ));
   //console.log(test)
@@ -40,42 +40,42 @@ function updateState(event) {
 }
 
 function checkWinner() {
-  s1 = { selector: '[data-x="0"][data-y="0"]' };
-  s1text = $( Object.values(s1)[0] ).text();
+  s0 = { selector: '[data-x="0"][data-y="0"]' };
+  s0text = $( Object.values(s0)[0] ).text();
   s2 = { selector: '[data-x="1"][data-y="0"]' };
+  s2text = $( Object.values(s1)[0] ).text();
+  s2 = { selector: '[data-x="2"][data-y="0"]' };
   s2text = $( Object.values(s2)[0] ).text();
-  s3 = { selector: '[data-x="2"][data-y="0"]' };
+  s3 = { selector: '[data-x="0"][data-y="1"]' };
   s3text = $( Object.values(s3)[0] ).text();
-  s4 = { selector: '[data-x="0"][data-y="1"]' };
+  s4 = { selector: '[data-x="1"][data-y="1"]' };
   s4text = $( Object.values(s4)[0] ).text();
-  s5 = { selector: '[data-x="1"][data-y="1"]' };
+  s5 = { selector: '[data-x="2"][data-y="1"]' };
   s5text = $( Object.values(s5)[0] ).text();
-  s6 = { selector: '[data-x="2"][data-y="1"]' };
+  s6 = { selector: '[data-x="0"][data-y="2"]' };
   s6text = $( Object.values(s6)[0] ).text();
-  s7 = { selector: '[data-x="0"][data-y="2"]' };
+  s7 = { selector: '[data-x="1"][data-y="2"]' };
   s7text = $( Object.values(s7)[0] ).text();
-  s8 = { selector: '[data-x="1"][data-y="2"]' };
+  s8 = { selector: '[data-x="2"][data-y="2"]' };
   s8text = $( Object.values(s8)[0] ).text();
-  s9 = { selector: '[data-x="2"][data-y="2"]' };
-  s9text = $( Object.values(s9)[0] ).text();
   //console.log(s1text, s2text, s3text, s4text, s5text, s6text, s7text, s8text, s9text);
   let result = false;
   let winner;
   ["X", "O"].forEach(function(l) {
     if (
-      (s1text === l && s2text === l && s3text === l) ||
-      (s4text === l && s5text === l && s6text === l) ||
-      (s7text === l && s8text === l && s9text === l) ||
+      (s0text === l && s1text === l && s2text === l) ||
+      (s3text === l && s4text === l && s5text === l) ||
+      (s6text === l && s7text === l && s8text === l) ||
+      (s0text === l && s3text === l && s6text === l) ||
       (s1text === l && s4text === l && s7text === l) ||
       (s2text === l && s5text === l && s8text === l) ||
-      (s3text === l && s6text === l && s9text === l) ||
-      (s1text === l && s5text === l && s9text === l) ||
-      (s3text === l && s5text === l && s7text === l) )
+      (s0text === l && s4text === l && s8text === l) ||
+      (s2text === l && s4text === l && s6text === l) )
     { result = true;
       winner = l;
     }
   })
-  if (!!s1text && !!s2text && !!s3text && !!s4text && !!s5text && !!s6text && !!s7text && !!s8text && !!s9text) {
+  if (!!s0text && !!s1text && !!s2text && !!s3text && !!s4text && !!s5text && !!s6text && !!s7text && !!s8text) {
     result = true;
     winner = "tie";
   }
