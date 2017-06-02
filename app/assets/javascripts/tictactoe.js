52388bbe6215cccca7b63c0e96d43e5588e81f30
bentonwong <bentonwong@gmail.com>
2017-06-01 22:35:22 -0700

    add logic to functions

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,34 +1,68 @@
 var turn = 0;
+var board = [];
+const board_length = 9;
+//var xWon = false;
+//var yWon = false;
 
-var doTurn = function(this) {
+$(document).ready(function() {
+    attachListeners();
+});
+
+var player = function() {
+  if (turn % 2 === 0) {
+   return "X"
+  } else {
+   return "O"
+  }
+}
+
+function updateState (event) {
+  const token = player();
+  $(event.target).text(token);
+}
+
+function updateBoard() {
+  board[0] = $('td[data-x="0"][data-y="0"]').html();
+  board[1] = $('td[data-x="1"][data-y="0"]').html();
+  board[2] = $('td[data-x="2"][data-y="0"]').html();
+  board[3] = $('td[data-x="0"][data-y="1"]').html();
+  board[4] = $('td[data-x="1"][data-y="1"]').html();
+  board[5] = $('td[data-x="2"][data-y="1"]').html();
+  board[6] = $('td[data-x="0"][data-y="2"]').html();
+  board[7] = $('td[data-x="1"][data-y="2"]').html();
+  board[8] = $('td[data-x="2"][data-y="2"]').html();
+}
+
+function checkBoardForWinner() {
+  
+}
+function evaluateBoard() {
+  updateBoard();
+  console.log(board)
+  //checkBoardForWinner();
+}
+
+function checkWinner () {
+  evaluateBoard();
+//  if (xWon) {
+//    message("Player X Won!");
+//  } else if (yWon) {
+//    message("Player O Won!");
+//  }
+}
+
+function doTurn(event) {
   turn += 1;
-  updateState(this);
+  updateState(event);
   checkWinner();
 }
 
-var attachListeners = function() {
-  $('td').click(function() {
-    doTurn(this);
+function attachListeners () {
+  $('td').on('click', function(event) {
+    doTurn(event);
+  });
 }
 
-var player = function() {
-
-}
-
-var updateState = function() {
-
-}
-
-var checkWinner = function() {
-
-}
-
-var message = function() {
-
-}
-
-
-$(document).ready(function() {
-  attachListeners();
-});
-$(this).text('O');
+//var message = function(message) {
+  //$("#message").text("message")
+//}
