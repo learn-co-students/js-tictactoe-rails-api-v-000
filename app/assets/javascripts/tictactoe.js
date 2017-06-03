e7d4708971a2c15bdaaa4dd94c5dddbec2bb5d8a
bentonwong <bentonwong@gmail.com>
2017-06-02 18:37:19 -0700

    fix bug with display of messages

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,9 +1,10 @@
 var turn = 0;
 var board = new Array(9);
 var xWon = false;
-var yWon = false;
-var draw = false;
+var oWon = false;
+var tie = false;
 let gameOver = false;
+var msg;
 
 const win_combos = [
   [0,1,2],
@@ -24,11 +25,6 @@ var player = function() {
   }
 }
 
-function updateState(event) {
-  const token = player();
-  $(event.target).text(token);
-}
-
 function updateBoard() {
   board[0] = $('td[data-x="0"][data-y="0"]').html();
   board[1] = $('td[data-x="1"][data-y="0"]').html();
@@ -41,67 +37,68 @@ function updateBoard() {
   board[8] = $('td[data-x="2"][data-y="2"]').html();
 }
 
-function checkBoardForWinner() {
+function updateState(event) {
+  const token = player();
+  $(event.target).text(token);
+  updateBoard();
+}
+
+function checkForTie() {
+  for (var i=0; i < board.length; i++) {
+    if (board[i] === "") {
+      return;
+    }
+  }
+  tie = true;
+  msg = "Tie game"
+}
+
+function evaluateBoard() {
   const token = player();
   for (let i=0; i < win_combos.length; i++) {
     pos_1 = win_combos[i][0];
     pos_2 = win_combos[i][1];
     pos_3 = win_combos[i][2];
-    if (board[pos_1] === token && board[pos_2] === token && board[pos_3] === token ) {
-      return token === "X" ? xWon = true : yWon = true;
-    }
-  }
-}
-
-function checkForDraw() {
-  for (var i=0; i < board.length; i++) {
-    if (board[i] === "") {
-      return draw = false;
-    }
-  }
-  draw = true;
-}
-
-function evaluateBoard() {
-  updateBoard();
-  checkBoardForWinner();
-  if (xWon === false || yWon === false) {
-    checkForDraw();
-  }
-}
-
-function checkWinner() {
-  evaluateBoard();
-  if (xWon === true) {
-    message("Player X Won!");
-  } else if (yWon === true) {
-    message("Player O Won!");
-  } else if (draw === true) {
-    message("It's a Draw!")
-  }
-}
-
-function doTurn(event) {
-  turn += 1;
-  updateState(event);
-  checkWinner();
-}
-
-function attachListeners() {
-    $('td').on('click', function(event) {
-      if (!gameOver) {
-        if ($(event.target).html() === "") {
-          doTurn(event);
-        } else {
-          alert("That space is occupied. Try again.")
-        }
+    if (board[pos_1] === token && board[pos_2] === token && board[pos_3] === token) {
+      if (token === "X") {
+        xWon = true;
+        msg = "Player X Won!";
+        return
+      } else {
+        oWon = true;
+        msg = "Player O Won!";
+        return
       }
-    });
+    }
+  }
+  checkForTie();
 }
 
 function message(msg) {
   $("#message").text(msg);
-  gameOver = true;
+}
+
+function checkWinner() {
+  evaluateBoard();
+  if (xWon === true || oWon === true || tie === true) {
+    message(msg);
+  } else {
+    return false;
+  }
+}
+
+function doTurn(event) {
+  updateState(event);
+  checkWinner();
+  turn += 1;
+}
+
+var attachListeners = function() {
+    $('td').on('click', function(event) {
+      if ($(event.target).html() === "") {
+        doTurn(event);
+      }
+  });
 }
 
 $(document).ready(function() {
