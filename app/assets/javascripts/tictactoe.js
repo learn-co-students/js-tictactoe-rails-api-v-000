b5a7d6cc929d5bf899ae134c4f8e86462cccaf98
kkterai <kkterai@gmail.com>
2017-06-09 11:42:22 -0700

    start work on ttt.js

---
+++ app/assets/javascripts/tictactoe.js
@@ -0,0 +1,16 @@
+$(function() {
+  attachListeners()
+});
+
+function attachListeners() {
+  // var selector = '[data-x="0"][data-y="0"]'
+  // document.querySelector(selector).addEventListener("click", doTurn()); 
+}
+
+function turn() {
+
+}
+
+function doTurn() {
+
+}
