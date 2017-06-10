055e936e11f054bb27c8e3847dc08fbaa39d1a05
David Kennell <davidkennell0@gmail.com>
2017-06-10 13:48:04 -0400

    Start work on attachListeners()

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -3,8 +3,22 @@ $(function() {
 });
 
 function attachListeners() {
-  // var selector = '[data-x="0"][data-y="0"]'
-  // document.querySelector(selector).addEventListener("click", doTurn()); 
+	var allRows = document.getElementsByTagName("tr")
+
+	//Goal : iterate through allRows to get rows
+	// iterate through each of those rows to get cells
+
+	  Array.prototype.forEach.call(allRows
+	  	[0].children, function(row){
+	    console.log(row)
+	})
+
+
+	  onClick(function(event){
+	    console.log("Success!")
+	})
+	  // var selector = '[data-x="0"][data-y="0"]'
+	  // document.querySelector(selector).addEventListener("click", doTurn()); 
 }
 
 function turn() {
@@ -14,3 +28,5 @@ function turn() {
 function doTurn() {
 
 }
+
+
