b010069647f82fbc29cff1081679ed44981d1b8e
Seth <itz.saga@gmail.com>
2017-06-12 10:43:43 -0500

    Finished index route.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,18 @@
 class GamesController < ApplicationController
 
+  def index
+    games = Game.all
+    # respond_to do |f|
+    #   f.json { render json: games }
+    # end
+    render json: games, root: "games"
+  end
+
   def create
     Game.create(state: ['', '', '', '', '', '', '', '', ''])
   end
 
   def update
-    # binding.pry
     game = Game.find(params[:id])
     game.update(game_params)
   end
