fedd643dd7892fe92f40e403cc27ad2b9e83af53
bortch89 <bortch@gmail.com>
2017-06-12 18:53:32 +0200

    Added listeners for blocks

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,2 +1,11 @@
 'use strict'
 
+function attachListeners() {
+  $("td").on('click', function(data) {
+    doTurn($(this).data("x"), $(this).data("y"));
+  });
+}
+
+function doTurn(x,y) {
+  console.log(x, y)
+}
