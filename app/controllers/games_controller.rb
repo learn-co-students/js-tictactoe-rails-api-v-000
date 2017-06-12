5e0421b7f05c90273f1d95afb83fa221c692cadf
Seth <itz.saga@gmail.com>
2017-06-12 18:13:33 -0500

    Extended controller. Added #show route.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,16 +2,22 @@ class GamesController < ApplicationController
 
   def index
     games = Game.all
-    render json: games, root: "games"
+    render json: games
   end
 
   def create
-    Game.create(state: ['', '', '', '', '', '', '', '', ''])
+    game = Game.create(game_params)
+    render json: game
   end
 
   def update
     game = Game.find(params[:id])
     game.update(game_params)
+  end
+
+  def show
+    game = Game.find(params[:id])
+    render json: game
   end
 
   private
