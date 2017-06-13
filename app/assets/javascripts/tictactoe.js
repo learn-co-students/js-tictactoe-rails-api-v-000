1984e742d8303a625bf2cb021a9739f38a26d4dd
bortch89 <bortch@gmail.com>
2017-06-13 12:35:44 +0200

    Worked on loading of games

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -10,29 +10,32 @@ const  winningCombos = [
     [0,4,8],
     [2,4,6],
   ];
-let turn = 0
-let currentGame
+let turn = 0;
+let currentGame;
 
 $(() => {
-  attachListeners()
+  attachListeners();
 })
 
 const attachListeners = () => {
   $('td').click(function () {
     doTurn($(this).data('x'), $(this).data('y'));
-  })
+  });
   $('#previous').click(() => {
-    getAllGames()
-  })
+    getAllGames();
+  });
   $('#save').click(() => {
-    saveGame()
-  })
+    saveGame();
+  });
+  $('.game').click(function () {
+    loadGame($(this).data('game'));
+  });
 }
 
 function doTurn(x,y) {
-  updateState(x,y)
-  checkWinner()
-  turn += 1
+  updateState(x,y);
+  checkWinner();
+  turn += 1;
 }
 
 function updateState (x,y) {
@@ -40,45 +43,46 @@ function updateState (x,y) {
 }
 
 function checkWinner () {
-  let winner = false
-  let board = checkBoard ()
+  let winner = false;
+  let board = checkBoard ();
   winningCombos.forEach(function(position) {
     if (board[position[0]] === board[position[1]] && board[position[1]] === board[position[2]] && board[position[0]] !== ""){
-      winner = true
+      winner = true;
     }
   })
   if (winner === true) {
-    message(`Player ${player()} Won!`)
-    resetBoard()
+    message(`Player ${player()} Won!`);
+    resetBoard();
   } else if (turn === 8) {
-    message('Tie game')
-    resetBoard()
+    message('Tie game');
+    resetBoard();
   } else {
-    return false
+    return false;
   }
 }
 
 function resetBoard () {
-  turn = -1
-  $('td').html("")
+  saveGame(true);
+  turn = -1;
+  $('td').html("");
 }
 
 function checkBoard () {
   return $('td').map(function() {
-    return this.innerHTML
+    return this.innerHTML;
   }).toArray()
 }
 
 function player() {
   if (turn % 2 === 0) {
-    return 'X'
+    return 'X';
   } else {
-    return 'O'
+    return 'O';
   }
 }
 
 function message (message) {
-  $('#message').html(message)
+  $('#message').html(message);
 }
 
 // 'Show Previous Games' button fires this.
@@ -86,27 +90,36 @@ function getAllGames () {
   $.get('/games')
     .done((response) => {
       if (response.games.length > 0) {
-        let games = $()
+        let games = $();
         response.games.forEach(function(e) {
-          games = games.add(`<li id="${e.id}">${e.id}</li>`)
+          games = games.add(`<li class="game" id="${e.id}" data-game="${e.id}">${e.id}</li>`);
         })
-        $('#games').html(games)
+        $('#games').html(games);
       }
-    })
+    });
 }
 
+var loadGame = (gameid) => {
+  $.get("/games/"+ gameid, function(response) {
+
+    var state = response.game.state
+    $('td').each(function(index){
+      this.innerHTML = state[index];
+    });
+  });
+ }
 // 'Save Game' button fires this.
-function saveGame () {
+function saveGame (gameOver) {
   let url
   let method
   // If the game exists in the db already
   if (currentGame) {
-    url = `/games/${currentGame}`
-    method = 'PATCH'
+    url = `/games/${currentGame}`;
+    method = 'PATCH';
   // If this is a new game not in the db
   } else {
-    url = '/games'
-    method = 'POST'
+    url = '/games';
+    method = 'POST';
   }
   // I chose .ajax over two jQuery calls in the block above.
   $.ajax({
@@ -121,7 +134,12 @@ function saveGame () {
     success: (data) => {
       // Set the currentGame to the just saved game.
       // This might need to be changed. Not sure just yet.
-      currentGame = data.game.id
+      if(gameOver){
+        currentGame = undefined;
+      } else {
+        currentGame = data.game.id;
+      }
+
     }
   })
 }
