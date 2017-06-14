4d4f911d4f568367961c30f70e24669d38faee90
Omar Mason <mr.omarmason@gmail.com>
2017-06-14 22:42:19 +0000

    games controller return json object. started working on js for games

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -8,6 +8,10 @@ function attachListeners() {
   $('td').on('click', function(event) {
     doTurn(event)
   })
+  $('#save').on('click', function(event) {
+    var values = getTableValues();
+    $.post('/games/:id')
+  })
 }
 
 function doTurn(event) {
@@ -15,6 +19,23 @@ function doTurn(event) {
   updateState(event)
   checkWinner()
 }
+function senddata() {
+  $('#form').on('submit',(function(e) {
+    var formData = new FormData(this);
+
+    formData.append('_method', 'patch');
+    $.ajax({
+      type: "POST",
+      url:'/slide/' + id,
+      data: formData,
+      cache: false,
+      contentType: false,
+      processData: false
+    }).done(function(r) {
+    console.log(r);
+  });
+}
+
 
 function player() {
   if (turn % 2 === 0) {
