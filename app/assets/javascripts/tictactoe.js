28478c54be6311aed1dea86020b6cddf8a977895
David Kennell <davidkennell0@gmail.com>
2017-06-17 14:30:55 -0400

    refactor checkWinner() function

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,3 @@
-// const winCombos = [[[0,0],[1,0],[2,0]], [[0,1],[1,1],[2,1]], [[0,2],[1,2],[2,2]], [[0,0],[0,1],[0,2]], [[1,0],[1,1],[1,2]], [[2,0],[2,1],[2,2]],[[0,0],[1,1],[2,2]],[[2,0],[1,1],[0,2]]];
-
 const winCombos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
 var turn = 0
 
@@ -14,20 +12,17 @@ function attachListeners() {
 
 function doTurn(event) {
   updateState(event);
-  checkWinner();
-  turn += 1
+  if (checkWinner() == true){
+    debugger;
+    turn = 0
+  }else{
+    turn += 1
+  }
 }
     
 function checkWinner() {
   var board = document.getElementsByTagName("td")
-  // Check to see if there's a winner
-  winCombos.forEach(function(combo){ 
-    if (board[combo[0]].innerHTML === "X" && board[combo[1]].innerHTML === "X" && board[combo[2]].innerHTML === "X") {
-      return message("Player X Won!"); //Player X won
-    } else if (board[combo[0]].innerHTML == "O" && board[combo[1]].innerHTML == "O" && board[combo[2]].innerHTML == "O") {
-        return message("Player O Won!"); //Player O won
-    }
-  })
+
    // Check to see if there's a tie
   var tie = true
   Array.prototype.forEach.call(board, function(cell){
@@ -35,8 +30,24 @@ function checkWinner() {
       tie = false
     }
   })
-  if (tie === true){message("Tie game")}
-  return false
+  if (tie === true){
+    message("Tie game")
+    // "true" here just means that the games has ended
+    return true
+  }
+
+  // Check to see if there's a winner
+  winCombos.forEach(function(combo){ 
+    if (board[combo[0]].innerHTML === "X" && board[combo[1]].innerHTML === "X" && board[combo[2]].innerHTML === "X") {
+      message("Player X Won!"); //Player X won
+      return true
+    } else if (board[combo[0]].innerHTML == "O" && board[combo[1]].innerHTML == "O" && board[combo[2]].innerHTML == "O") {
+        message("Player O Won!"); //Player O won
+        return true
+    }
+  })
+    return false
+  
 }
       
 function updateState(event) {
@@ -54,6 +65,11 @@ function player() {
 
 function message(winner) {
   document.getElementById("message").innerHTML = winner;
+  var board = document.getElementsByTagName("td")
+  Array.prototype.forEach.call(board, function(cell){
+    cell.innerHTML = ""
+  })
+  turn = 0
 }
 
 $(function() {
