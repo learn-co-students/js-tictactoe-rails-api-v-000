00136e5db36311ff224df6e19b4e61af2a97e6d9
kkashuda <kkashuda@gmail.com>
2017-06-24 19:11:31 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -5,7 +5,7 @@ let currentGame = null;
 let url = null;
 let method = null;
 
-const  winningCombos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+const winning_combos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]];
 
 $(document).ready(function() {
   attachListeners();
@@ -45,10 +45,11 @@ function updateState (pos) {
 }
 
 function checkWinner() {
-  let winner;
+  let winner = null;
   let board = getBoard();
-  	winningCombos.forEach(function(position) {
-    if (board[position[0]] === board[position[1]] && board[position[1]] === board[position[2]] && board[position[0]] !== ""){
+  let p = player(); 
+  winning_combos.forEach(function(pos) {
+    if (board[pos[0]] === p && board[pos[1]] === p && board[pos[2]] === p) {
       winner = true;
     }
   }); 
@@ -57,7 +58,6 @@ function checkWinner() {
     message(`Player ${player()} Won!`);
     turn = -1;
     resetBoard();
-
   } else if (turn === 8) {
     message('Tie game');
     turn = -1;
@@ -92,27 +92,6 @@ function message(msg) {
   $('#message').html(msg);
 }
 
-function getGames() {
-  let games = $();
-  $.get('/games').done((object) => {
-      if (object.games.length > 0) {
-        object.games.forEach(function(game) {
-          games = games.add(`<li class="game" id="${game.id}">${game.id}</li>`);
-        })
-        $('#games').html(games);
-      }
-    });
-}
-
-function loadGame(id) {
-  $.get("/games/"+ id, function(object) {
-    var state = object.game.state;
-    $('td').each(function(index){
-      this.innerHTML = state[index];
-    });
-  });
- }
-
 function saveGame(game) {
   if (currentGame) {
     url = `/games/${currentGame}`;
@@ -139,3 +118,24 @@ function saveGame(game) {
     }
   });
 }
+
+function getGames() {
+  let games = $();
+  $.get('/games').done((object) => {
+      if (object.games.length > 0) {
+        object.games.forEach(function(game) {
+          games = games.add(`<li class="game" id="${game.id}">${game.id}</li>`);
+        })
+        $('#games').html(games);
+      }
+    });
+}
+
+function loadGame(id) {
+  $.get("/games/"+ id, function(object) {
+    var state = object.game.state;
+    $('td').each(function(index){
+      this.innerHTML = state[index];
+    });
+  });
+ }
