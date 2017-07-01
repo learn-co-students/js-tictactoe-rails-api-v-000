9753f45802246a8975b451761d9147266cddddaf
jwolfe890 <jwolfe890@gmail.com>
2017-07-01 19:50:47 -0400

    all

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,51 @@
-// Code your JavaScript / jQuery solution here
+
+$(document).ready(function() {
+    $('body').on("click", function (e) {
+      if (e.target.dataset) {
+        debugger
+        e.target.innerHTML = player()
+      }
+  }); 
+});
+
+// $('tr[id^=mytablerow-]').on('click', function(){
+//    if ( $('a#action-toggle').text() == 'Enabled' ) {
+//        alert('action enabled'); 
+//    }
+// });
+
+
+var turn = 0
+
+function updateState() {
+   player()
+}
+
+function attachListeners() {
+}
+
+function message() {
+
+}
+
+function checkWinner() {
+
+}
+
+function player() {
+  if (turn % 2 == 0) {
+    return "X"
+  } else {
+    return "O"
+  }
+}
+
+function doTurn() {
+  var turn = turn + 1
+  updateState()
+  checkWinner()
+}
+
+
+
+
