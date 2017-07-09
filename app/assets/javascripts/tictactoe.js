17acd6f6bb7fa27dbb2a9fc840637a895f4ce89e
christinechoi <smi77y@gmail.com>
2017-07-09 14:39:34 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,37 @@
 // Code your JavaScript / jQuery solution here
+
+function player() {
+  var turn = 0
+  if (turn % 2 == 0) {
+    return 'O'
+  } else {
+    return 'X'
+  };
+}
+
+function updateState() {
+
+
+}
+
+
+
+function message() {
+
+}
+
+function checkWinner(){
+
+
+}
+
+
+function doTurn() {
+
+
+}
+
+function attachListeners() {
+
+
+}
