5df526e42b983b05c3960f4c864fc63bd6496cd7
christinechoi <smi77y@gmail.com>
2017-07-10 22:22:54 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -18,15 +18,31 @@ function message(string) {
   $('div#message').append(string);
 }
 
-function checkWinner(){
 
+function checkWinner() {
 
+  var combos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]//, [0, 4, 8]] //diagonal win positions
+  var sq = window.document.querySelectorAll('td')
+  var win = false
+  
+  combos.forEach (combo => {
+
+    if (sq[combo[0]].innerHTML == sq[combo[1]].innerHTML &&
+    sq[combo[1]].innerHTML == sq[combo[2]].innerHTML) {
+      win = true 
+    } else {
+      return false
+    };
+  });
+
+  return win 
 }
 
 
 function doTurn() {
-
-
+  turn += 1;
+  updateState();
+  checkWinner();
 }
 
 function attachListeners() {
