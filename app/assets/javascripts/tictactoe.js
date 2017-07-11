06ad7919a85425902be6f9ee275dbb59f49110a7
christinechoi <smi77y@gmail.com>
2017-07-10 23:00:05 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -22,20 +22,23 @@ function message(string) {
 function checkWinner() {
 
   var combos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]//, [0, 4, 8]] //diagonal win positions
-  var sq = window.document.querySelectorAll('td')
-  var win = false
+  var sq = window.document.querySelectorAll('td');
+  var win = false;
+  var winning = [];
   
   combos.forEach (combo => {
 
     if (sq[combo[0]].innerHTML == sq[combo[1]].innerHTML &&
     sq[combo[1]].innerHTML == sq[combo[2]].innerHTML) {
-      win = true 
+      winning = combo;
+      win = true;
     } else {
-      return false
+      win
     };
   });
 
-  return win 
+  message("Player " + sq[winning[0]].innerHTML + " Won!");
+  return win;
 }
 
 
