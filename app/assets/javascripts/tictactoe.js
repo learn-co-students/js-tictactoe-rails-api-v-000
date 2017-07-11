5226bff81e2d21e41f8b591f65c6dbf8bbc19638
Hui Jiang <hui1021@yahoo.com>
2017-07-11 05:48:48 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,5 @@
-const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
-                        [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 var turn = 0;
+var currentGame = 0;
 
 $(document).ready(function(){
   attachListeners();
@@ -13,44 +12,81 @@ function attachListeners(){
     }
   });
 
-  $('#save').on('click', () => save());
+  $('#save').on('click', () => saveGame());
   $('#previous').on('click', () => previous());
-  $('#clear').on('click', () => clear());
+  $('#clear').on('click', () => clearBoard());
 }
-
 
 function doTurn(td){
   if (turn == 0){message("");}
   updateState(td);
   turn += 1;
   if (checkWinner()){
-    //save();
-    //clear();
+    saveGame();
+    clearBoard();
   }
   else if (turn == 9){
     message("Tie game.");
-    //save();
-    //clear();
+    saveGame();
+    clearBoard();
   }
 }
 
 function updateState(td){
-  $(td).text(player());
+  var token = player();
+  $(td).text(token);
 }
 
-function save(){
+function saveGame(){
+  var gameState = { state: board([],0) }
 
+  if (currentGame == 0) {
+  //not a saved game
+    $.ajax({
+      method: 'POST',
+      url: `/games`,
+      data: gameState,
+      success: function(result){
+        currentGame = result.data.id;
+      }
+    });
+  }
+  else{
+    //previously saved game
+    $.ajax({
+      method: 'PATCH',
+      url: `/games/${currentGame}`,
+      data: gameState
+    });
+  }
 }
 
-function previous(){
+var previous = function() {
+// function previous(){
+  $.get('/games', function(data){
+    var list = data.data.map(function(game){
+      return $(
+        `<button><li class="game" data-id="${game.id}">${game.id}</li></button>`
+      ).on('click', () => reloadGame(game.id));
+    });
+    $('#games').html(list);
+  });
+};
 
+function reloadGame(id){
+  $.get('/games/' + id, function(data) {
+    currentGame = id;
+    state = data.data.attributes.state;
+    board(state, 1);
+    turn = state.join("").length;
+    checkWinner();
+  });
 }
 
-function clear(){
+function clearBoard(){
   turn = 0;
-  var nullArray = new Array(9);
-  board(nullArray.fill(""),1);
-  message("game reset!");
+  currentGame = 0;
+  $("td").html('');
 }
 
 //update = 1 : 0
@@ -67,11 +103,14 @@ function player(){
   return turn % 2 == 0 ? "X" : "O";
 }
 
-function message(str){
-  $('#message').text(str);
+var message = function(str){
+  $('#message').html(str);
 }
 
 function checkWinner(){
+  const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
+                          [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
   current_board = board([],0);
   let result = false;
   WINNING_COMBOS.forEach(function(win_combo){
