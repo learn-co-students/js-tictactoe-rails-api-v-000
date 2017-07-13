32d296e39fd36e9d6241a83864fe345dc4da26c0
Menachem Hornbacher <mendelh1537@gmail.com>
2017-07-13 07:30:31 -0400

    first few tests pass

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,12 @@
 // Code your JavaScript / jQuery solution here
+function player(){
+    if (isNaN(window.turn) || window.turn == undefined) {
+        return "X"
+    };
+    return window.turn % 2 === 0 ? "X" : "O" ;
+}
+
+function updateState(square){
+    square = player();
+    window.turn += 1
+}
