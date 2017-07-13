34c527fd1123caff8f2a1fff956bffdd0676050a
Menachem Hornbacher <mendelh1537@gmail.com>
2017-07-13 07:49:45 -0400

    checkWinner done

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,4 +1,9 @@
 // Code your JavaScript / jQuery solution here
+
+var WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+var turn = 0;
+var currentGame = 0;
+
 function player(){
     if (isNaN(window.turn) || window.turn == undefined) {
         return "X"
@@ -7,6 +12,49 @@ function player(){
 }
 
 function updateState(square){
-    square = player();
-    window.turn += 1
+    var token = player();
+    $(square).text(token);
+}
+
+function message(string){
+    $("#message").text(string);
+}
+
+function checkWinner(){
+    var board = [];
+    var winner = false;
+
+    $('td').text(function(index, square) { board[index] = square }); // get all the td's in order andput them in the board
+
+    WINNING_COMBOS.forEach( function(combo) {
+        if (board[combo[0]] != "" && board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]]) {
+            message("Player " + board[combo[0]] + " Won!");
+            return winner = true;
+        }
+    });
+
+    return winner;
+}
+
+function doTurn(square){
+    updateState(square);
+    turn++;
+    if (checkWinner()) {
+        saveGame();
+        resetBoard();
+    } else if (turn === 9) {
+        message("Tie game.");
+        saveGame();
+        resetBoard();
+    }
+}
+
+function saveGame() {
+
+}
+
+function resetBoard(){
+    $('td').empty();
+    turn = 0;
+    currentGame = 0;
 }
