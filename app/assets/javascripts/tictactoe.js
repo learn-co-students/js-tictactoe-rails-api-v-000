a3b8ae49e0173af54ac8ce714bf76ba36d9bde39
Hima Chitalia <hima_chhag@yahoo.com>
2017-07-17 12:21:17 -0400

    Done with checkWinner, player, restBOard, loadBOard, winConditions

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,50 @@
 // Code your JavaScript / jQuery solution here
+var winConditions = [[0,1,2],
+                      [3,4,5],
+                      [6,7,8],
+                      [0,3,6],
+                      [1,4,7],
+                      [2,5,8],
+                      [0,4,8],
+                      [2,4,6]];
+
+var turn = 0;
+var currentGame = 0;
+
+var player = function(){
+  return turn % 2 === 0 ? "X" : "O";
+}
+
+var updateState = function(e){
+  var result = player()
+  $(e.target).text(result);
+}
+
+var message = function(message){
+  $("#message").text(message);
+}
+
+var loadBoard = function(){
+     board = []
+    document.querySelectorAll("[data-y]").forEach(function(cell){
+     board.push(cell.innerHTML)
+ })
+}
+
+var resetBoard = function() {
+  turn = 0
+  $("td").html('');
+}
+
+function checkWinner() {
+  //LOAD BOARD TO GET THE BOARD STATE
+  loadBoard()
+  for (var i = 0; i < winConditions.length; i++) {
+    var row = winConditions[i];
+    if (board[row[0]] == board[row[1]] && board[row[2]] == board[row[1]] && board[row[0]] != ""){
+      message(`Player ${board[row[0]]} Won!`)
+      return true;
+    }
+  }
+    return false
+}
