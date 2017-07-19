c03ad24e919e574c219fa21532412bceab3a2fdd
Andrew Bonner <andrewbonner92@gmail.com>
2017-07-19 12:12:01 -0400

    Refactored.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,46 +1,26 @@
 var current = 0
 var turn = 0
-var WINNING_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
+var WINNING_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+                            [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
-$(function () {
-  attachListeners()
-})
+$(() => attachListeners())
 
-function player() {
-  if (turn % 2 === 0) {
-    return "X"
-  } else {
-    return "O"
-  }
-}
+var player = () => turn % 2 === 0 ? "X" : "O"
 
-function updateState(square) {
+var updateState = (square) => {
   square.innerHTML = player()
   return square
 }
 
-function message(string) {
-  $("#message").html(string)
-}
+var message = (string) => $("#message").html(string)
 
 function checkWinner() {
   var board = {}
   var winner = false
 
-  $("td").text(function (i, square) {
-    board[i] = square
-  })
+  $("td").text((i, square) => board[i] = square)
 
-  WINNING_COMBINATIONS.some(function (combo) {
+  WINNING_COMBINATIONS.some((combo) => {
     if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
       message(`Player ${board[combo[0]]} Won!`)
       return winner = true
@@ -73,10 +53,10 @@ function addGameButton(game) {
 }
 
 function showPrevious() {
-  $.get('/games', function (savedGames) {
+  $.get('/games', (savedGames) => {
     if (savedGames.data.length) {
       var $games = $("#games")[0].innerHTML
-      savedGames.data.forEach(function (game) {
+      savedGames.data.forEach((game) => {
         if (!$games.includes(game.id)) {
           addGameButton(game)
         }
@@ -87,14 +67,16 @@ function showPrevious() {
 }
 
 function reloadGame() {
-  $("td").empty()
   var id = this.id
-  $.get("/games/" + id, function (game) {
+  $.get("/games/" + id, (game) => {
     current = parseInt(game.data.id, 10)
-    game.data.attributes.state.forEach(function (data, i) {
+    var $td = $("td")
+    game.data.attributes.state.forEach((data, i) => {
       if (data) {
-        $("td")[i].innerHTML = data
+        $td[i].innerHTML = data
         turn++
+      } else {
+        $td[i].innerHTML = ""
       }
     })
   })
@@ -102,9 +84,7 @@ function reloadGame() {
 
 function saveGame() {
   var state = []
-  $("td").text(function (i, data) {
-    state.push(data)
-  })
+  $("td").text((i, data) => state.push(data))
   if (current) {
     $.ajax({
       url: `/games/${current}`,
@@ -115,7 +95,7 @@ function saveGame() {
       type: 'PATCH'
     })
   } else {
-    $.post('/games', {state: state}, function (game) {
+    $.post('/games', {state: state}, (game) => {
       current = parseInt(game.data.id, 10)
     })
   }
@@ -131,13 +111,7 @@ function attachListeners() {
       doTurn(this)
     }
   })
-  $("#previous").click(function () {
-    showPrevious()
-  })
-  $("#save").click(function () {
-    saveGame()
-  })
-  $("#clear").click(function () {
-    clearGame()
-  })
+  $("#previous").click(() => showPrevious())
+  $("#save").click(() => saveGame())
+  $("#clear").click(() => clearGame())
 }
