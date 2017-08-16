92ee49355fff1ae45a61e4ad5a79328b115df7c0
ETphonehomeslice <mk.etlinger@gmail.com>
2017-08-16 18:04:46 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,20 @@
 // Code your JavaScript / jQuery solution here
+var turn = 0;
+const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
+                        [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
+function player() {
+   return (turn % 2) === 0 ? 'X' : 'O'
+}
+
+function updateState(el) {
+   $(el).text(player())
+}
+
+function message(string) {
+    $('#message').text(string)
+}
+
+function checkWinner() {
+    
+}
