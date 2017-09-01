a4d6e4c94c2546d22fb0b9e22f10abce3fb1cc68
J Fernandez <jffernan@comcast.net>
2017-09-01 00:49:04 +0000

    working on lab

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,21 +1,49 @@
+$(document).ready(function() {
+  attachListeners();
+});
+const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
+                        [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+var turn = 0;
+var currentGame = 0;
+
 function player() {
-  console.log("Returns the token of the player whose turn it is, X when the turn variable is even and O when it is odd.")
+  if (turn % 2 == 0) {
+    return "X"
+  }
+  else {
+    return "O"
+  }
 }
 
-function updateState() {
-    console.log("Invokes player() and adds the returned string (X or O) to the clicked square on the game board.")
+function updateState(square) {
+  var token = player();
+  $(square).text(token);
 }
 
-function message() {
-    console.log("Accepts a string and adds it to the div#message element in the DOM.")
+function message(string) {
+  $('#message').text(string);
 }
 
 function checkWinner() {
-    console.log("Returns true if the current board contains any winning combinations three X or O tokens in a row, vertically, horizontally, or diagonally. Otherwise, returns false. If there is a winning combination on the board, checkWinner should invoke message, passing in the appropriate string based on who won: Player X Won! or Player O Won!")
+  var board = {};
+  var winner = false;
+
+  $('td').text((index, square) => board[index] = square);
+
+  WINNING_COMBOS.some(function(combo) {
+    if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
+      message(`Player ${board[combo[0]]} Won!`);
+      return winner = true;
+    }
+  });
+
+  return winner;
 }
 
 function doTurn() {
-  console.log("Increments the turn variable by 1. Invokes the updateState() function, passing it the element that was clicked. Invokes checkWinner() to determine whether the move results in a winning play.")
+  console.log("Increments the turn variable by 1. Invokes the updateState function, passing it the element that was clicked. Invokes checkWinner f to determine whether the move results in a winning play.")
+}
 
 function attachListeners() {
-  console.log("Attaches the appropriate event listeners to the squares of the game board as well as for the button#save, button#previous, and button#clear elements.  When a user clicks on a square on the game board, the event listener should invoke doTurn() and pass it the element that was clicked.")
+  console.log("Attaches the appropriate event listeners to the squares of the game board as well as for the button save, button previous, and button clear elements.  When a user clicks on a square on the game board, the event listener should invoke doTurn f  and pass it the element that was clicked.")
+}
