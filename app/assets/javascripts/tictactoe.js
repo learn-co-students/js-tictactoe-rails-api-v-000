9fad756823f524b3ce0769384c256ef946570489
Luisa Scavo <luisascavo@gmail.com>
2017-09-06 00:38:11 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,21 @@
 // Code your JavaScript / jQuery solution here
+  var turn = 0;
+
+  function player(){
+    if(turn % 2 === 0){return "X"}
+    else{return "O"}
+  }
+  
+  function updateState(square){
+    var x = $(square).data("x");
+    var y = $(square).data("y");
+    $('td[data-x = "' + x + '"][data-y = "'+ y +'"]').text(player());
+    turn++;
+  }
+
+$(function(){
+  $("td").on('click', function(){
+    updateState(this);
+  });
+
+});
