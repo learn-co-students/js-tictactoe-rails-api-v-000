8544d8b3c6be8819fa3641c26d8b73d560739d9b
Luisa Scavo <luisascavo@gmail.com>
2017-09-07 01:09:54 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,21 +1,43 @@
-// Code your JavaScript / jQuery solution here
-  var turn = 0;
+// Code your JavaScript / jQuery solution
+// Game only saved when... saved. Or ended.
+// Otherwise, it takes place entirely on the front-end
+const WIN_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
+                    [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+var turn = 0;
 
-  function player(){
-    if(turn % 2 === 0){return "X"}
-    else{return "O"}
+function player(){
+  if(turn % 2 === 0){return "X"}
+  else{return "O"}
+}
+
+function updateState(square){
+  $(square).text(player());
+}
+
+function doTurn(square){
+  if($(square).text() === ""){
+    updateState(square);
   }
+  checkWinner();
+  turn++;
+}
+
+function checkWinner() {
+  board = {};
+  $('td').text((index,square) => board[index] = square);
   
-  function updateState(square){
-    var x = $(square).data("x");
-    var y = $(square).data("y");
-    $('td[data-x = "' + x + '"][data-y = "'+ y +'"]').text(player());
-    turn++;
-  }
+}
+
+function message(string) {
+  $('#message').text(string);
+}
+
+function attachListeners(){
+  $("td").on('click', function(){
+    doTurn(this);
+  });
+}
 
 $(function(){
-  $("td").on('click', function(){
-    updateState(this);
-  });
-
+  attachListeners();
 });
