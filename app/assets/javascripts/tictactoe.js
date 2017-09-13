c7a1678540fb0fcb4ae012cb9ca67dfb48902557
jessica fletcher <cara.kane@gmail.com>
2017-09-13 18:40:57 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,38 @@
 // Code your JavaScript / jQuery solution here
+$(function () {
+  $('#save').click(function(event) {
+    //prevent form from submitting the default way
+    event.preventDefault();
+    var values = $(this).serialize();
+    debugger;
+    var posting = $._method('/games', values);
+
+    posting.done(function(data) {
+
+    });
+  });
+});
+
+function player(){
+
+}
+
+function updateState() {
+
+}
+
+function message() {
+
+}
+
+function checkWinner() {
+
+}
+
+function doTurn() {
+
+}
+
+function attachListeners() {
+  
+}
