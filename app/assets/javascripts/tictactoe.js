c374d5871f67e538e719806366ea49b5170b3fb4
FreeBreadsticks <erika.hughes@dbspublishing.com>
2017-09-27 16:16:17 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,44 @@
 // Code your JavaScript / jQuery solution here
+$(function () {
+  attachListeners();
+})
+
+var turn = 0
+
+function player() {
+  if (isEven(turn)) {
+    return "X"
+  } else {
+    return "O"
+  }
+}
+
+function isEven(num) {
+  if (num % 2 === 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function doTurn() {
+  $(this).text(player());
+  turn += 1
+}
+
+function updateState() {
+
+}
+
+function setMessage() {
+
+}
+
+function checkWinner() {
+
+}
+
+function attachListeners() {
+  $("#ttt tbody tr").on("click", "td", doTurn);
+}
