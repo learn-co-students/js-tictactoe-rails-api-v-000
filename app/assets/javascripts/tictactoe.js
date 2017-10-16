0e30e1b9b0023b7a55a8b035a3b8e8c9c91171df
Matteo Piccini <gnappo.1@live.it>
2017-10-16 08:25:41 +0000

    defined function player(), updateState(), setMessage() and checkWinner()

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,28 @@
-// Code your JavaScript / jQuery solution here
+var WINNING_COMBOS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+var turn = 0;
+
+var player = () => turn % 2 ? 'O' : 'X';
+
+function updateState(square) {
+  var token = player();
+  $(square).text(token);
+}
+
+function setMessage(message) {
+  $('#message').text(message);
+}
+
+function checkWinner() {
+  var winner = false;
+  var board = {};
+  $('td').text((index, square) => board[index] = square);
+
+  WINNING_COMBOS.some( function(combo) {
+    if ( board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
+      setMessage(`Player ${board[combo[0]]} Won!`);
+      winner = true;
+    }
+  });
+
+  return winner;
+}
