18ed56a302b26ed874ed0628ca480aa9a96af1e9
Andrew <andrew.ramell@gmail.com>
2017-12-05 15:23:03 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,14 @@
 // Code your JavaScript / jQuery solution here
+var origBoard;
+const playerone = 'O'
+const playertwo = 'X'
+const winCombos = [
+  [0,1,2]
+  [3,4,5]
+  [6,7,8]
+  [0,3,6]
+  [1,4,7]
+  [2,5,8]
+  [0,4,8]
+  [6,4,2]
+]
