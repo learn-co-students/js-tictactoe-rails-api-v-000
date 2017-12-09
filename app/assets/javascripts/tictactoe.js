78fe18eb3c335212fbf9363f2a6b3ca413de6e54
Jim Stricker <jmstricker93@gmail.com>
2017-12-09 15:52:23 -0500

    added DOMCoordinatesMapper function

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -5,6 +5,19 @@ var turn = 0
 var currentBoard = ["","","",
                     "","","",
                     "","",""]
+const DOMCoordinatesMapper = function(x, y){
+// takes in xy coordinates as inputs and converts to
+if (x === 0){
+  let rowArr = [0, 1, 2]
+  return rowArr[y]
+} else if (x===1){
+  let rowArr = [3, 4, 5]
+  return rowArr[y]
+} else if (x===2) {
+  let rowArr = [6, 7, 8]
+  return rowArr[y]
+}
+};
 
 
 function player(){
@@ -17,13 +30,11 @@ function player(){
 
 
 function updateState(element){
-  const DOMCoordinatesMapper = function(x, y){
-
   if ($(element).text() === ""){
     //needs to actually update the game object in the database
 
     $(element).text(player())
-    // $.patch('/games/' + id)
+    // CAPTURE THE ELEMENT'S DATASET IN VARIABLES, then put them into DOMCoordinatesMapper
     turn ++
   }
 }
