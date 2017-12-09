0624fe0e07ed9d06a8c94bb95db18a716eac9263
Jim Stricker <jmstricker93@gmail.com>
2017-12-09 14:49:36 -0500

    board can now add X to each square on click

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,44 @@
 // Code your JavaScript / jQuery solution here
+//
+var turn = 0
+
+function player(){
+  if (turn % 2 === 0){
+    return 'X'
+  } else {
+    return 'O'
+  }
+};
+
+
+function updateState(element){
+  debugger
+  $(element).text(player())
+}
+
+function setMessage(msg){
+  let $messageDiv =  $("#message")
+  $messageDiv.text(msg)
+}
+
+function checkWinner(){
+
+}
+
+function doTurn(){
+
+}
+
+function attachListeners(){
+  let $theBoard = $("td")
+
+  for (let i of $theBoard){
+    $(i).on("click", function(e){
+      updateState(this)
+    })
+  }
+}
+
+$(function(){
+  attachListeners();
+});
