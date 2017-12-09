8d4a4a5a41cfe5c9c7e02631264b2b7393c7f75e
Jim Stricker <jmstricker93@gmail.com>
2017-12-09 15:42:14 -0500

    Got board updating properly in DOM; added  var currentBoard and const WinningCombos.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,11 @@
 // Code your JavaScript / jQuery solution here
-//
+
+//NEED TO HIDE THESE VARIABLES FROM GLOBAL VIEW EVENTUALLY
 var turn = 0
+var currentBoard = ["","","",
+                    "","","",
+                    "","",""]
+
 
 function player(){
   if (turn % 2 === 0){
@@ -12,8 +17,15 @@ function player(){
 
 
 function updateState(element){
-  debugger
-  $(element).text(player())
+  const DOMCoordinatesMapper = function(x, y){
+
+  if ($(element).text() === ""){
+    //needs to actually update the game object in the database
+
+    $(element).text(player())
+    // $.patch('/games/' + id)
+    turn ++
+  }
 }
 
 function setMessage(msg){
@@ -21,7 +33,23 @@ function setMessage(msg){
   $messageDiv.text(msg)
 }
 
-function checkWinner(){
+function checkWinner(board){
+  const WinningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+  ]
+
+  for (const i of currentBoard){
+
+  }
+
+  //check to see if there are any winning combos on the board
 
 }
 
