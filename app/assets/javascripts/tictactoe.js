af22968adcdaee467a0eb3e4b475fb77ac3c59a5
Sophia Hubeny <shhubeny@gmail.com>
2017-12-11 20:19:08 +0000

    minor refactoring

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,4 +1,3 @@
-// Code your JavaScript / jQuery solution here
 var turn = 0
 const win_combos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
@@ -37,9 +36,12 @@ function checkWinner(){
 }
 
 function doTurn(){
-  turn += 1
-  checkWinner()
-  updateState(player())
+  var board = getBoard()
+
+  if (checkWinner() === false && turn <= 9){
+    turn += 1
+    updateState(player())
+  }
 }
 
 function attachListeners(){
