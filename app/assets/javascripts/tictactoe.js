73035f65306d0b5106f658ed43e3de8d00b1a965
Sophia Hubeny <shhubeny@gmail.com>
2017-12-11 20:02:13 +0000

    getting tests to pass

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,11 @@
 // Code your JavaScript / jQuery solution here
 var turn = 0
+const win_combos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+function getBoard(){
+  return $("td").toArray().map((e) => {return e.innerHTML})
+}
+
 function player(){
   if(turn % 2 === 0){
     return "X"
@@ -17,7 +23,17 @@ function setMessage(string){
 }
 
 function checkWinner(){
+  let board = getBoard()
 
+  for(c of win_combos){
+    if (board[c[0]] === "X" && board[c[1]] === "X" && board[c[2]] === "X" ) {
+    setMessage('Player X Won!')
+    return true
+  } else if (board[c[0]] === "O" && board[c[1]] === "O" && board[c[2]] === "O" ){
+    setMessage('Player O Won!')
+    return true
+  }
+} return false
 }
 
 function doTurn(){
