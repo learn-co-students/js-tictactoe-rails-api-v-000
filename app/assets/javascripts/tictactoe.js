b24b314f9f3e0c2254280776939235153d82ae06
Joanna <jmgsese@gmail.com>
2017-12-15 16:58:10 -0600

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -72,7 +72,6 @@ window.onload = () => {attachListeners()}
 var WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
 var turn = 0
-
 var currentGame = 0
 
 // if turn modulus 2 = 0, it is falsey, and therefore returns right side (X). any odd number % 2 = 1, and so would be truthy, and return left side (O)
@@ -90,7 +89,6 @@ function checkWinner() {
   var board = []
   var winner = false
 
-// same as $('td').text((index, td) => board[index] = td);
   $('td').text(function(index, td){
   	board[index] = td
   })
@@ -103,26 +101,25 @@ function checkWinner() {
     }
   })
   return winner
-
-  // if (!$.text(this) && winner === false) {
-  //   doTurn(this)
-  // }
 }
 
 function doTurn(td) {
   updateState(td)
   turn++
   if (checkWinner()) {
+    saveGame()
     resetBoard()
   } else if (turn === 9){
     setMessage("Tie game.")
+    saveGame()
     resetBoard()
   }
 }
 
 function resetBoard(){
-  turn = 0
-  $('td').empty()
+  turn = 0;
+  $('td').empty();
+  currentGame = 0;
 }
 
 function attachListeners () {
@@ -133,6 +130,9 @@ function attachListeners () {
   $('#save').on('click', function(){
     saveGame()
   });
+  $('#clear').on('click', function(){
+    resetBoard()
+  });
 
   $('td').on('click', function(){
     if (!$.text(this) && !checkWinner()) {
@@ -141,31 +141,100 @@ function attachListeners () {
   })
 }
 
-function previousGames(){
-  $.get('/games', function(games){
-    console.log(games.data.length)
+function saveGame() {
+  var board = [];
+  var gameData;
 
+  $('td').text((index, td) => {
+    board.push(td);
+  });
+
+  gameData = { state: board };
+  console.log(gameData)
+
+  if (currentGame) {
+    $.ajax({
+      type: 'PATCH',
+      url: `/games/${currentGame}`,
+      data: gameData
+    });
+  } else {
+    $.post('/games', gameData, function(game) {
+      currentGame = game.data.id;
+      $('#games').append(`<button id="${game.data.id}">Game ${game.data.id}</button><br>`);
+      $(`#${game.data.id}`).click(function(){
+        console.log(`when i click this, saved game ${game.data.id} should load`)
+        console.log(`the current game is supposedly ${currentGame}`)
+        reloadGame(currentGame)
+      })
+    });
+  }
+}
+
+function previousGames(){
+  $('#games').empty(); //prevents listing duplicates
+  $.get('/games', function(games){
     if (games.data.length){ //if array is not empty
       var list = $("#games").append('<ul></ul>').find('ul');
       games.data.forEach(function(game){
         list.append(`<li><button id="${game.id}">Game ${game.id}</button></li><br>`)
         $(`#${game.id}`).click(function(){
-          console.log("anything happen when i click?")
+          console.log(`when i click this, game ${game.id} should load`)
+          reloadGame(game.id)
         })
       })
     }
   })
 }
 
-// Clicking the button#previous element when previously-saved games exist in the database
-// adds those previous games as buttons in the DOM's div#games element:
+function reloadGame(gameID) {
+  // document.getElementById('message').innerHTML = '';
+
+  // const req = new XMLHttpRequest;
+  // // req.overrideMimeType('application/json');
+  // req.open('GET', `/games/${gameID}`);
+  // req(send)
+  console.log(`gameID: ${gameID}`)
+
+
+  fetch(`/games/${gameID}`)
+    .then(res => res.json())
+    .then(function(json){
+      var state = json.data.attributes.state
+      console.log(json.data.attributes.state)
+      var currentGame = json.data.id
+      console.log(`current game: ${currentGame}`)
+
+      for (i=0; i<state.length; i++){
+        for (j=0; j < $('td').length; j++){
+          if (i === j){
+            $('td')[j].append(state[i])
+          }
+        }
+      }
+    })
+
+
+
+}
+
+
+//parallel iteration
+//appending
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Clicking this button should grab all of the persisted games from the database
 // and create a button for each that, when clicked,
 // returns that saved game's state to our tic-tac-toe board.
 // All of the buttons should be added to the div#games element in the DOM.
-
-function saveGame() {
-
-}
