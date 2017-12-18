d4b9bdab97a22acbd73ac73af99a1506a6f1016c
John Tuttle <thejwt@gmail.com>
2017-12-18 19:59:25 +0000

    finished player and updateState functions

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,18 @@
 // Code your JavaScript / jQuery solution here
+var turn = 0
+
+$( function(){
+
+});
+
+function player() {
+  if (turn == 0 || turn % 2 == 0) {
+    return "X"
+  } else {
+    return "O"
+  }
+}
+
+function updateState(square) {
+  $(square).text(player())
+}
