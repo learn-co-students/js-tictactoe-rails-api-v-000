b0e6c81628264bd5555eff4cf91a9d6d4b352b4d
Andrew <andrew.ramell@gmail.com>
2017-12-31 12:51:46 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,36 +1,55 @@
 // // Code your JavaScript / jQuery solution here
-// var origBoard = []
-// const playerone = 'O'
-// const playertwo = 'X'
+var origBoard = []
+const playerone = 'O'
+const playertwo = 'X'
 const winCombos = [
-  [0,1,2]
-  [3,4,5]
-  [6,7,8]
-  [0,3,6]
-  [1,4,7]
-  [2,5,8]
-  [0,4,8]
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
   [2,4,6]
 ]
-var board = []
+var turn = 0
+$(function(){
+  var cell = document.getElementsByTagName('td')
+startGame()
 
 // // const turn = []
 
- var turn = 0
- function getBoard(){
-  board = 
+//  var turn = 0
+//  function getBoard(){
+//   board = []
+// }
+function startGame(){
+  
+  for(var i = 0; i < cell.length; i++)
+   {
+  cell[i].addEventListener('click',doTurn, false)
+  }
+}
+// function turnClick(square){
+//   console.log(square.target)
+// }
+})
+
+
+function doTurn(square){
+  turn++
+  updateState(square)
+  if (checkWinner() === true){
+    board = []
+  }
+  setMessage("Tie game.")
+  
 }
 
-function doTurn(){
-  //  var turn = 0
-  // updateState()
-  // checkWinner()
-  // turn++
-}
 
 //needs to ruturn x when turn count is even
 function player (){
-  
+  // debugger
   if(turn % 2 == 0)
   {
     return 'X';
@@ -39,10 +58,14 @@ function player (){
   {
     return 'O';
   }
+  
 }
 
-function updateState(space){
-  $(space).html(player())
+function updateState(square){
+  // console.log(square)
+  var token = player()
+  // debugger
+  square.target.innerText = token
 }
 
 function setMessage(message){
@@ -53,48 +76,44 @@ function checkWinner(){
 
 }
 
-$(document).ready(function() {
-  function attachListeners(){
-   
-     $('tbody').on('click', function(){
 
-       alert('clicked')       
-     //   // var clicked = $(this).attr({num1: 'data-x',num2: 'data-y'}) 
-
-     //   // doTurn(clicked) //attached clicked on element to doTurn
-     })
-   
-   
-     // save button
-    //  $('#save').on('click', function (){
-    //    alert('clicked')
-    //  })
-    //  // show previous game
-    //  $('#previous').on('click', function (){
-    //    alert('clicked')
-    //     })
-    //  //clear current
-    //  $('#clear').on('click', function (){
-    //    alert('clicked')
-    //   })
-     }
   
-})
-
   
 
 
-
-// $(function(){
+function attachListeners(){
+  var click = document.getElementsByTagName('td')
+  // debugger
+  }
   
-//   $('td').on('click', function(){
-//     var window = $(this).attr({num1: 'data-x',num2: 'data-y'})
-//     // $('#'+window).append('clicked here')
-//      debugger
-//     alert("you clicked (this.")
-//   })
+  
+  
+
+
+$(document).ready(function attachlisteners(){
+  // function attachListeners(){
+  $('td').eq(function(){
+    debugger
+    // var window = $(this).attr({num1: 'data-x',num2: 'data-y'})
+    // $('#'+window).append('clicked here')
+     console.log(this)
+    // alert("you clicked (this.")
+      })
+  // save button
+       $('#save').on('click', function (){
+        //  alert('clicked')
+       })
+      //  show previous game
+    $('#previous').on('click', function (){
+    //  alert('clicked previous')
+        })
+        //  clear current
+    $('#clear').on('click', function (){
+      //  alert('clicked')
+      })
+
+  })
 // })
-
 // // startGame()
 // // const cells = document.querySelectorAll('table')
 // // startGame()
@@ -107,3 +126,10 @@ $(document).ready(function() {
 // // function turnClick(click){
 // //   console.log(click.target.id)
 // // 
+// const route = 
+// $.ajax({
+//   url: 'http://local/games'
+
+// }).done(function(){
+//   $(this).append()
+// })
