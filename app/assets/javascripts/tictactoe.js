32ebfca7c04a7a1b53f4d70386c62fb7e498613e
jamesonbass1987 <jamesonbass@gmail.com>
2018-01-05 19:38:47 -0800

    doTurn() mostly done and tieGame() complete

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,6 @@
 // Code your JavaScript / jQuery solution here
 var turn = 0
+var board = document.querySelectorAll('td')
 
 var winningBoard = [
    [0,1,2],
@@ -11,7 +12,6 @@ var winningBoard = [
    [0,4,8],
    [2,4,6]
  ]
-
 
 function isEven(turn) {
   return turn % 2 === 0;
@@ -29,19 +29,45 @@ function setMessage(string){
   $("#message").html(string);
 }
 
+function setBoard(){
+  var board = []
+  $('td').toArray().forEach(x => board.push(x.innerHTML))
+  return board
+}
+
 function checkWinner(){
-	var currentBoard = []
-	$('td').toArray().forEach(x => currentBoard.push(x.innerHTML))
+  var currentBoard = setBoard();
 	function winningCombo(currentVal) {
 		if (currentBoard[currentVal[0]] === currentBoard[currentVal[1]] && currentBoard[currentVal[1]] === currentBoard[currentVal[2]] && currentBoard[currentVal[0]] !== "") {
-			return true;
+      winner = currentBoard[currentVal[0]]
+      setMessage(`Player ${winner} Won!`);
+      return true;
 		}
 	}
 	return winningBoard.some(winningCombo)
 }
 
-function doTurn(position){
+function tieGame(){
+  var currentBoard = setBoard();
+  if (currentBoard.every(x => x !== "")){
+    setMessage("Tie game.");
+  }
+}
 
+function doTurn(position){
+  //doTurn() increments the value of the "turn" variable
+  turn++;
+  //doTurn() invokes the checkWinner() function
+  //doTurn() resets the board and the "turn" counter when a game is won
+
+  if (checkWinner()){
+    $('td').html("")
+    turn = 0;
+  };
+  //doTurn() invokes the updateState() function
+  updateState(position);
+  //doTurn() invokes the setMessage() function with the argument "Tie game." when the game is tied
+  tieGame();
 
 }
 
