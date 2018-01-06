b825569fdec404cbd3335512d0223148d35248e3
jamesonbass1987 <jamesonbass@gmail.com>
2018-01-05 17:48:37 -0800

    first round

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,32 @@
 // Code your JavaScript / jQuery solution here
+var turn = 0
+
+
+function isEven(turn) {
+  return turn % 2 === 0;
+}
+
+function player(){
+  return isEven(turn) ? 'X' : 'O';
+}
+
+function updateState(position){
+  position.innerHTML = player();
+}
+
+function setMessage(string){
+  $('div#message')
+}
+
+function checkWinner(){
+
+}
+
+function doTurn(position){
+
+
+}
+
+function attachListeners(){
+
+}
