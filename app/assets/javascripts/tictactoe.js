964689432797f9c009a49b41d30b9e1f973fdd82
jamesonbass1987 <jamesonbass@gmail.com>
2018-01-06 12:25:52 -0800

    function refactoring

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,36 +1,20 @@
 // Code your JavaScript / jQuery solution here
-var turn = 0
-var winningBoard = [
-   [0,1,2],
-   [3,4,5],
-   [6,7,8],
-   [0,3,6],
-   [1,4,7],
-   [2,5,8],
-   [0,4,8],
-   [2,4,6]
- ]
+let turn = 0
 
 $(document).ready(function(){
   attachListeners();
 });
 
 function attachListeners(){
-  $('td').each(function(key, value){
-    this.addEventListener("click", function(){
-      if(!checkWinner() && !checkTie()){
-        doTurn(this);
-      }}
-    )
-  })
-}
-
-function isEven(turn) {
-  return turn % 2 === 0;
+  $('td').click(function(){
+    if(!checkWinner() && !checkTie()){
+      doTurn(this);
+    }}
+  )
 }
 
 function player(){
-  return isEven(turn) ? 'X' : 'O';
+  return turn % 2 === 0 ? 'X' : 'O';
 }
 
 function updateState(position){
@@ -42,14 +26,15 @@ function setMessage(string){
 }
 
 function setBoard(){
-  var board = []
+  let board = []
   $('td').toArray().forEach(x => board.push(x.innerHTML))
   return board
 }
 
 function checkWinner(){
-  var win = false;
-  var currentBoard = setBoard();
+  let winningBoard = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+  let win = false;
+  let currentBoard = setBoard();
 	function winningCombo(currentVal) {
 		if (currentBoard[currentVal[0]] === currentBoard[currentVal[1]] && currentBoard[currentVal[1]] === currentBoard[currentVal[2]] && currentBoard[currentVal[0]] !== "") {
       setMessage(`Player ${currentBoard[currentVal[0]]} Won!`);
