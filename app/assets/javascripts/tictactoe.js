c644560a857e88340f654643a4ebc2527f5aef84
JingruZhang <zhang.jingru.93@gmail.com>
2018-01-05 18:41:49 -0800

    checkWinner() downgit add .

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,17 @@
 // Code your JavaScript / jQuery solution here
 var turn = 0
 
+var winningBoard = [
+   [0,1,2],
+   [3,4,5],
+   [6,7,8],
+   [0,3,6],
+   [1,4,7],
+   [2,5,8],
+   [0,4,8],
+   [2,4,6]
+ ]
+
 
 function isEven(turn) {
   return turn % 2 === 0;
@@ -15,11 +26,18 @@ function updateState(position){
 }
 
 function setMessage(string){
-  $('div#message')
+  $("#message").html(string);
 }
 
 function checkWinner(){
-
+	var currentBoard = []
+	$('td').toArray().forEach(x => currentBoard.push(x.innerHTML))
+	function winningCombo(currentVal) {
+		if (currentBoard[currentVal[0]] === currentBoard[currentVal[1]] && currentBoard[currentVal[1]] === currentBoard[currentVal[2]] && currentBoard[currentVal[0]] !== "") {
+			return true;
+		}
+	}
+	return winningBoard.some(winningCombo)
 }
 
 function doTurn(position){
