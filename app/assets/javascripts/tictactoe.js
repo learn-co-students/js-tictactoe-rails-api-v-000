21e0dfc7a299cf8a2a137286a362e979db5e9be0
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-01-10 23:06:21 -0800

    player() passing

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,48 +1,20 @@
 // Code your JavaScript / jQuery solution here
+var turn = 0;
+
 $(document).ready(function() {
   console.log("ready!");
+  // let turn = 0;
   saveGame();
 });
 
-// <script type="text/javascript" charset="utf-8">
-// $(function () {
-//   $(".js-more").on('click', function() {
-//     // get the id from the data attribute
-//     var id = $(this).data("id");
-//     $.get("/posts/" + id + "/body", function(data) {
-//       // Replace text of body-id div
-//       $("#body-" + id).text(data);
-//     });
-//   });
-// });
-// </script
+function player() {
+  return turn % 2 === 0 ? "X" : "O";
+}
+
+
 
 function saveGame() {
   $("#save").on('click', function() {
-    // debugger;
-    // let params = $('table').serialize();
-    let postRequest = $.post('/games', { 'state[]': [ "", "", "", "", "", "", "", "", ""] })
-
-    postRequest.done(function(response) {
-      console.log("my hypothetical game got saved");
-    })
+    $.post('/games', { 'state[]': [ "", "", "", "", "", "", "", "", ""] })
   });
 }
-
-// <script type="text/javascript" charset="utf-8">
-//   $(function() {
-//     $('form').submit(function(event) {
-//       event.preventDefault();
-//       var params = $(this).serialize();
-//       var postRequest = $.post('/products', params);
-//
-//       postRequest.done(function(renderedJSONHash) {
-//         var product = renderedJSONHash;
-//         $("#productName").text(product["name"]);
-//         $("#productInventory").text(product["inventory"]);
-//         $("#productPrice").text("$" + product["price"]);
-//         $("#productDescription").text(product["description"]);
-//       });
-//     });
-//   });
-// </script>
