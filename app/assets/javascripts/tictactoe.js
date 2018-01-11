8f2651b42e3d4cb65b97c481b51f558f28e473da
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-01-11 02:51:16 -0800

    do turn finished

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,9 +1,15 @@
 // Code your JavaScript / jQuery solution here
 var turn = 0;
+// var squares = document.querySelectorAll('td'); //why won't this load
+var winCombinations = [[0,1,2],[3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
+// window.onload = function() {
+//   console.log("window got loaded");
+//   var squares = document.querySelectorAll('td'); //why won't this load
+// }
 
 $(document).ready(function() {
   console.log("ready!");
-  // let turn = 0;
   saveGame();
 });
 
@@ -11,7 +17,43 @@ function player() {
   return turn % 2 === 0 ? "X" : "O";
 }
 
+function updateState(square) {
+  square.innerHTML = player();
+}
 
+function setMessage(message) {
+  $('#message').text(message);
+}
+
+function checkPositionTaken(square) {
+  return square.innerHTML !== "";
+}
+
+function checkWinner() {
+  var squares = document.querySelectorAll('td');
+  var winnerPresent = winCombinations.filter(function(combo){
+    return (squares[combo[0]].innerText === squares[combo[1]].innerText && squares[combo[1]].innerText === squares[combo[2]].innerText && checkPositionTaken(squares[combo[0]]))
+  });
+  if (winnerPresent.length > 0) {
+    setMessage(`Player ${squares[winnerPresent[0][0]].innerText} Won!`);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function doTurn(square) {
+  updateState(square)
+  turn++;
+  if (checkWinner()) {
+    $('td').empty();
+    turn = 0;
+  } else if (turn === 9 ) {
+    setMessage('Tie game.');
+    $('td').empty();
+    turn = 0;
+  }
+}
 
 function saveGame() {
   $("#save").on('click', function() {
