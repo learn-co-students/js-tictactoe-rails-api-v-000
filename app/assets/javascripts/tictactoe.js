97eb5431baae77505d2b3a14f3e2492424f46443
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-01-12 00:52:03 -0800

    final submission where all tests pass in mocha.run() in browser, but not by npm test

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,6 @@
 // Code your JavaScript / jQuery solution here
 var turn = 0;
-// var squares; //hoisting trick part 1.
+// var squares; //hoisting trick part 1. //ruins tests though
 // var squares = document.querySelectorAll('td'); //why won't this load
 var winCombinations = [[0,1,2],[3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
