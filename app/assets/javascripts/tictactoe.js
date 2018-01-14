835122e71fa25d9802264d1251de878b63ee97ca
github username <github email address>
2018-01-13 22:46:18 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -2,6 +2,7 @@
 
 
 var turn = 0;
+var current_game = 0;
 var WINNING_COMBOS = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
                       [0, 3, 6], [1, 4, 7], [2, 5, 8],
                       [0, 4, 8], [2, 4, 6]];
@@ -37,6 +38,7 @@ function checkWinner() {
 
 function resetBoard() {
   turn = 0;
+  current_game = 0;
   document.querySelectorAll('td').forEach(function(square) {
     square.innerHTML = ""
   })
@@ -45,9 +47,11 @@ function resetBoard() {
 function doTurn(cell) {
   updateState(cell);
   if (checkWinner()) {
+    saveGame();
     resetBoard();
   } else if (turn === 9) {
     setMessage('Tie game.');
+    saveGame();
     resetBoard();
   }
 }
@@ -63,17 +67,18 @@ function attachListeners() {
     }
   });
 
-  $("#save").click(function() {
+  $("button#save").click(function() {
     saveGame();
   });
 
-  $("#previous").click(function() {
+  $("button#previous").click(function() {
     previousGame();
   });
 
-  $("#clear").click(function() {
-    clearGame();
+  $("button#clear").click(function() {
+    resetBoard();
   });
+
 }
 
 function previousGame() {
@@ -86,11 +91,47 @@ function previousGame() {
 function addGamesButtons(games) {
   var gamesHTML = "";
   games.data.forEach(function(game) {
-    gamesHTML += "<button>" + game.id + "</button><br>"
+    gamesHTML += "<button id='" + game.id + "'>" + game.id + "</button><br>"
   })
-  $("#games").append(gamesHTML)
+  $("div#games").append(gamesHTML);
+  $("div#games > button").click(function() {
+    reloadGame(this.id);
+  })
+}
+
+function reloadGame(id) {
+  $.get("/games/" + id, function(game) {
+    var board = game.data.attributes.state
+    var cells = document.querySelectorAll('td')
+    turn = 0;
+    current_game = game.data.id
+    for (var i = 0; i < 9; i++) {
+      cells[i].innerHTML = board[i]
+      if (board[i] !== "") { turn++ }
+    }
+  })
 }
 
 function saveGame() {
-  
+  var boardArray = []
+  document.querySelectorAll("td").forEach(function(td) {
+    boardArray.push(td.innerHTML);
+  })
+  var board = {state: boardArray}
+  if (current_game === 0) {
+    $.post("/games", board)
+    .done(function(game){
+      current_game = game.data.id
+    })
+  } else {
+    updateGame(board, current_game);
+  }
+}
+
+function updateGame(squares, current_game) {
+  $.ajax({
+    url: `/games/${current_game}`,
+    method: 'patch',
+    data: squares
+  })
 }
