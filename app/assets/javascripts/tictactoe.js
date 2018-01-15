fea0c9122975372e4bb368ce758bb8c18f092ed8
Aimee Carney <acarney29@gmail.com>
2018-01-15 22:32:08 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,15 @@
-let turn = 0
-let gameId = 0
+var turn = 0
+var gameId = 0
+var win_combinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6]
+  ]
 
 function player() {
   if(turn % 2) {
@@ -19,16 +29,6 @@ function setMessage(message) {
 
 function checkWinner(){
   var board = $("td").toArray().map((el) => { return el.innerHTML })
-  const win_combinations = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,4,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [2,4,6]
-    ]
 
   for (let el of win_combinations) {
     if(board[el[0]] === "X" && board[el[1]] === "X" && board[el[2]] === "X") {
@@ -63,7 +63,6 @@ function currentBoard() {
 
 function saveGame() {
   let game = {state: currentBoard()}
-  let gameId = 0
 
   if (gameId === 0) {
     $.post("/games", game, function(resp) {
@@ -90,7 +89,7 @@ function previousGames() {
 function clearBoard() {
   turn = 0;
   $('td').empty()
-  gameId = undefined;
+  gameId = 0;
 }
 
 
