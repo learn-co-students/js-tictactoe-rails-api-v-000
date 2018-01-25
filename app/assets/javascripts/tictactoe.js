2725c0fd9a78ae35c9b1c16910cdca0898d0b8cb
github username <github email address>
2018-01-24 22:29:53 -0800

    redefined all functions

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,6 @@
 // Code your JavaScript / jQuery solution here
 
-/// GLOBAL ///
+/// GLOBAL VARIABLES///
 let turn = 0;
 let gameId = 0;
 const winningCombo = [
@@ -13,68 +13,68 @@ const winningCombo = [
   [0, 4, 8], // diagonal from left
   [6, 4, 2]  // diagonal from right
 ]
-/// END GLOBAL ///
 
 /// ON LOAD ///
-  //document ready for Events
+  // document ready for Events
 $(function(){
   attachListeners();
 });
 
-  //template ~> window.onload = () => {}
-  //zero = false; one = true
-// let player = () => (turn % 2) ? "O" : "X"
-/// END ON LOAD ///
+  /// Set the Board ///
+    // the board has nine squares but eight in computer speak
+    // each square ('td') has its own index
+    // so... define the board & make the squares clickable
+const squares = document.getElementsByTagName('td'); //=> [td, td, td, td, td, td, td, td, td]
 
-/// Set the Player's Token ///
-function player(){
-  // returns "X" when turn count is even
-  // returns "O" when turn count is odd
-  if (turn % 2 === 0) {
-    return "X";
-  } else {
-    return "O";
-  };
+  /// Set the turnCount ///
+    // to determine the currentPlayer
+    // turn will increment in doTurn()
+function turnCount(turn){
+  turn % 2 === 0
 }
-/// END Player ///
 
-/// Build the Board ///
-  //the board has nine squares but eight in computer speak
-  //each square has its own index
-  //so... define the square
-  //the square is noted as 'td' in html
-const square = document.getElementsByTagName("td")
+  /// Set the currentPlayer
+    // based on turnCount
+      // returns "X" when turnCount is even
+      // returns "O" when turnCount is odd
+function player(){
+  return turnCount(turn) ? "X" : "O";
+}
 
+/// STARTING THE GAME ///
 
-// def display_board
-//     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-//     puts "-----------"
-//     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-//     puts "-----------"
-//     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-//   end
-/// END Board ///
+  /// Place the Player's token on the board's squares ///
+    // doTurn() PASSES "clicked element"; it will be invoked by updateState()
+    // Player clicks on PASSED element
+    // the PASSED element gets token ("X" or "O") based on turn count, which is incremented in doTurn()
+      //the PASSED element can't change the token once it's been set
+        //if the PASSED element is empty, then set the token
+        //otherwise show the existing token
+function updateState(){
+  passedElement.click(function(){
+    this.html = player();
+  });
+  // let isSquareTaken = board.click(function(){
+  //   if (this.textContent === "X" || "O") {
+  //     this.textContent;
+  //   } else {
+  //     this.textContent = player();
+  //   }
+  // })
 
-/// MESSAGE FOR WINNER ///
+    /////getAttribute = .attr
+    // $("table tr td").click(function(){
+    //   let mark = $(this).attr('data-x');
+    //   let yark = $(this).attr('data-y');
+    //   alert("this is " + mark + yark);
+    // })
+}
+
 function setMessage(){
   //if winner is "X" setMessage() "Player X Won!";
   //if winner is "O" setMessage() "Player O Won!";
-  $("#message").innerHTML = `"Player ${winner} Won!"`
+  $("#message").innerHTML = `Player ${winner} Won!`
 }
-/// end of message ///
-
-
-
-
-
-/// UPDATE THE STATE OF THE GAME ///
-function updateState(){
-  //player clicks on a box, the box gets an X or O depending on turn count(which tells us which player is playing in player())
-  // $("td").on("click", function(){
-  //   $(this).innerHTML = player();
-  // })
-}
-/// End of Update ///
 
 /// Checking the Winner ///
 function checkWinner(){
@@ -108,41 +108,25 @@ function checkWinner(){
         //if winner is "O" setMessage() "Player O Won!";
       //setMessage(winner);
 }
-/// end checking winner ///
 
-/// PLAYING THE GAME ///
+
+
 function doTurn(){
+
+  //invokes updateState() & passes the element that was clicked
+  updateState(board)
+
   //increments value of "turn" variable
   //invokes checkWinner()
-  //invokes updateState()
+
   //invokes setMessage()
     //"Tie game" if tied
   //resets board and "turn" counter when game is Won
 
 
-  /////getAttribute = .attr
-  /////finding out if the box is X or O
-  $("table tr td").click(function(){
-    let mark = $(this).attr('data-x');
-    let yark = $(this).attr('data-y');
-    alert("this is " + mark + yark);
-  })
-
-  //Get each table td box, add 1 to its position, then filter/compare to the winning combo
-  //$("td")
-  //=>[0,1,2,3,4,5,6,7,8] 9 boxes
-
-  //add the token to the td's index
-  // $("td")[0].innerHTML = "X"
-  const myBoard = $("td")
-  for (const tokenPiece of myBoard) {
-    console.log(tokenPiece);
-  }
-
 
 
 }
-/// End of Play ///
 
 /// Buttons ////
 function attachListeners(){
