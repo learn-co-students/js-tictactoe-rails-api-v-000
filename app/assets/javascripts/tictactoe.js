6ac98a572aa1dd95a1fbcc3351341d80497b7adb
Nourski <nour.kodeih@gmail.com>
2018-01-26 13:49:18 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,79 +1,97 @@
-const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
-                        [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
-var turn = 0;
-var currentGame = 0;
-
 $(document).ready(function() {
   attachListeners();
 });
 
-var player = () => turn % 2 ? 'O' : 'X';
+var turn = 0
+var currentGame = 0
+var b = []
+var winningSets = []
+function getBoard(){
+    var array = []
+    var tableItems = $("tr td")
+    tableItems.each(function(inx, td) {
+		array.push($(td).html())
+    })
+	return array
+}
+function getWinningSets(){
+  var array = [
+  [b[0],b[1],b[2]],
+  [b[3],b[4],b[5]],
+  [b[6],b[7],b[8]],
+  [b[0],b[3],b[6]],
+  [b[1],b[4],b[7]],
+  [b[2],b[5],b[8]],
+  [b[0],b[4],b[8]],
+  [b[2],b[4],b[6]]
+]
+return array
+}
 
-function doTurn(square) {
-  updateState(square);
-  turn++;
-  if (checkWinner()) {
-    saveGame();
-    resetBoard();
-  } else if (turn === 9) {
-    setMessage("Tie game.");
-    saveGame();
-    resetBoard();
+function player () {
+  if (turn%2) {
+    return "O"
+  }
+  else {
+    return "X"
   }
 }
 
-function resetBoard() {
-  $('td').empty();
-  turn = 0;
-  currentGame = 0;
+function updateState(element){
+    if ($(element).text() === "") {
+      $(element).text(player())
+      turn++
+    }
+}
+
+function setMessage(string){
+    $("#message").html("<p>" + string + "<p>")
+}
+
+function checkWinner(){
+  var winner = false
+  b = getBoard()
+  winningSets = getWinningSets()
+  win = winningSets.filter(array => (array[0] === array[1] && array[1] === array[2] && array[2] !== ""))
+  if (win.length !== 0){
+    setMessage(`Player ${win[0][0]} Won!`);
+    return winner = true }
+  return winner
+}
+
+function doTurn(element) {
+  if (!checkWinner()){
+    updateState(element)
+    if (checkWinner()){
+      saveGame();
+      resetBoard()
+    }
+    else if (turn===9) {
+      setMessage("Tie game.")
+      saveGame();
+      resetBoard();
+    }
+  }
 }
 
 function attachListeners() {
-  $('td').on('click', function() {
-    if (!$.text(this) && !checkWinner()) {
-      doTurn(this);
-    }
-  });
-
-  $('#save').on('click', () => saveGame());
-  $('#previous').on('click', () => showPreviousGames());
-  $('#clear').on('click', () => resetBoard());
-}
-
-function checkWinner() {
-  var board = {};
-  var winner = false;
-
-  $('td').text((index, square) => board[index] = square);
-
-  WINNING_COMBOS.some(function(combo) {
-    if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
-      setMessage(`Player ${board[combo[0]]} Won!`);
-      return winner = true;
-    }
-  });
-
-  return winner;
-}
-
-function updateState(square) {
-  var token = player();
-  $(square).text(token);
-}
-
-function setMessage(string) {
-  $('#message').text(string);
+  $("td").click(function(){
+    var element = this
+    doTurn(element)
+  })
+  $("#save").click(function(){
+    saveGame()
+  })
+  $("#previous").click(function(){
+    showPreviousGames()
+  })
+  $("#clear").click(function(){
+    resetBoard()
+  })
 }
 
 function saveGame() {
-  var state = [];
-  var gameData;
-
-  $('td').text((index, square) => {
-    state.push(square);
-  });
-
-  gameData = { state: state };
+  gameData = { state: b };
 
   if (currentGame) {
     $.ajax({
@@ -88,6 +106,13 @@ function saveGame() {
       $("#gameid-" + game.data.id).on('click', () => reloadGame(game.data.id));
     });
   }
+  currentGame = 0
+}
+
+function resetBoard() {
+  $('td').empty();
+  turn = 0
+  currentGame = 0
 }
 
 function showPreviousGames() {
@@ -114,22 +139,16 @@ function reloadGame(gameID) {
     const data = JSON.parse(xhr.responseText).data;
     const id = data.id;
     const state = data.attributes.state;
-
-    let index = 0;
-    for (let y = 0; y < 3; y++) {
-      for (let x = 0; x < 3; x++) {
-        document.querySelector(`[data-x="${x}"][data-y="${y}"]`).innerHTML = state[index];
-        index++;
-      }
-    }
-
+    var tableItems = $("tr td")
+   	tableItems.each(function(inx, td) {
+        $(td).html(state[inx])
+    })
     turn = state.join('').length;
     currentGame = id;
 
     if (!checkWinner() && turn === 9) {
-      setMessage('Tie game.');
+      setMessage('Tie game.')
     }
-  };
-
-  xhr.send(null);
-}
+    }
+    xhr.send(null)
+  }
