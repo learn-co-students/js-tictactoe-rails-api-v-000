3f6a11aa1ddcc693929bb861b3f1cc4e99970503
github username <github email address>
2018-01-25 19:54:56 -0800

    defined the event listener(position) for doTurn in attachListeners

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -24,7 +24,7 @@ $(function(){
     // the board has nine squares but eight in computer speak
     // each square ('td') has its own index
     // so... define the board & make the squares clickable
-const square = $('td'); //=> [td, td, td, td, td, td, td, td, td]
+const square = document.getElementsByTagName('td'); //=> [td, td, td, td, td, td, td, td, td]
 
   /// 3. Set the turnCount ///
     // to determine the currentPlayer
@@ -44,11 +44,11 @@ function player(){
 /// STARTING THE GAME ///
 
   /// Place the Player's token on the board's squares ///
-    // doTurn() PASSES "clicked element" (the square itself from setBoard())
-    // Player clicks on PASSED "clicked element"; it will be invoked by updateState()
-    // the PASSED "clicked element" gets token ("X" or "O") based on turn count, which is incremented in doTurn()
-      //the PASSED "clicked element" can't change the token once it's been set
-        //if the PASSED "clicked element" is empty, then set the token
+    // doTurn() PASSES "clicked square" (the square itself from setBoard())
+    // Player clicks on PASSED "clicked square"; it will be invoked by updateState()
+    // the PASSED "clicked square" gets token ("X" or "O") based on turn count, which is incremented in doTurn()
+      //the PASSED "clicked square" can't change the token once it's been set
+        //if the PASSED "clicked square" is empty, then set the token
         //otherwise show the existing token
 function updateState(position){
   $(position).text(player());
@@ -61,7 +61,7 @@ function setMessage(message){
   $("div#message").text(message)
 }
 
-  /// Checking the Winner ///
+  /// Checking the winner ///
     // Compare the setBoard()'s indexes to those of winningCombo
     // setBoard and winningCombo are both nested arrays
       // when player wins horizontally return true
@@ -69,6 +69,8 @@ function setMessage(message){
       // when player wins vertically return true
       // return false if no winning combination is present
 function checkWinner(){
+
+  // setMessage(`Player ${winner} Won!`);
 
 }
 
@@ -84,17 +86,52 @@ function checkWinner(){
           // RESETS board and turnCount when game is won
         //...invokes checkWinner() to find a Tie
           // iteration stopped at the setBoard's length + 1
-function doTurn(move){
-
-  //increments value of "turn" variable
-
-
-  // invokes updateState() & passes the element that was clicked
-  // if updateState(clickMove) === 0
+function doTurn(position){
+  updateState(position);
+  turn++;
 
 
+  // if won {
+  //   resetBoard();
+  //   turn = 0
+  // }
+  //
+  // if tied {
+  //   setMessage("Tie game");
+  // }
+
+  // let won = square[i[0]] === square[i[1]] && square[i[1]] === square[i[2]] && square[i[2]] === square[i[0]]
+  // let full = index === "X" || "O"
+  // let draw = full && !won
+  // let over = won? || draw?
 
 }
+
+function resetBoard(){
+  $("td").each(function(){
+    $(this).empty();
+  });
+}
+
+/// Actions for the buttons ///
+// function saveGame(){
+//   // $("td")
+//   gameId++;
+//   // this.id = gameId
+//   setMessage("")
+// }
+//
+// function previousGame(){
+//   $("td").empty();
+//   turn = 0;
+//   setMessage("")
+// }
+//
+// function clearGame(){
+//   // $("td").empty();
+//   turn = 0;
+//   setMessage("")
+// }
 
 /// Buttons ////
 function attachListeners(){
@@ -114,8 +151,22 @@ function attachListeners(){
 
   $("#clear").on("click", function(e){
     e.preventDefault;
-    $("td").empty();
+    clearGame();
   });
 
-  // $("td").doTurn();
+    //passes the clicked-on td element to doTurn() as position
+    //need to define position
+    //the event listeners that invoke doTurn() when a square is clicked on
+  let position = $('td').on("click", function(e){
+    e.preventDefault;
+    doTurn(this.innerHTML)
+  })
+
+/// !!!!!! DO NOT DELETE THIS COMMENT ///
+  /// the clicked square on board ///
+
+  // $('td').click(function(){
+  //   this.innerHTML = "x"
+  // })
+
 }
