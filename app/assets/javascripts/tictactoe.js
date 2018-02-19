b213eb605af8531be5d3d17ff33b2b0f2ca4bee2
jss530 <jsifre01@gmail.com>
2018-02-18 20:03:55 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,20 @@
 // Code your JavaScript / jQuery solution here
+function player() {
+
+}
+
+function updateState() {
+
+}
+
+function checkWinner() {
+
+}
+
+function doTurn() {
+
+}
+
+function attachListeners() {
+
+}
