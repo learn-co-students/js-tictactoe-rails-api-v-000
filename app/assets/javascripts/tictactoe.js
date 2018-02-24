a9f1c47685f98130d78b2cc83fe66b900b8471d3
arwehrman <arwehrman@gmail.com>
2018-02-24 16:15:49 -0500

    not finished

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -2,7 +2,7 @@ var turn = 0; //even = X, odd = O
 var currntGameID = 0;
 var board = document.getElementsByTagName('td');  //creates board positions
 
-const WINNING_COMBINATOINS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+const winningCombo = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
 
 //event listeners
@@ -11,19 +11,13 @@ $(document).ready(function() {
 });
 
 function attachListeners(){
-  
 
-  $('#save').on('click', function() {
-    saveGame();
-  });
 
-  $('#previous').on('click', function() {
-    previousGames();
-  });
+  $('#save').on('click', function() { saveGame();});
 
-  $('#clear').on('click', function() {
-    clearGame();
-  });
+  $('#previous').on('click', function() {previousGames();});
+
+  $('#clear').on('click', function() {clearGame();});
 }
 
 //returns token of the player whose turn it is
@@ -35,23 +29,40 @@ function player(){
   }
 };
 
-function doTurn(){
+function doTurn(square){
   turn += 1
 
-  return updateState() //pass element that was clicked
+  updateState(square) //pass element that was clicked
 
-  return checkWinner() //checks to see if there is a winner after turn
+  if (checkWinner()) {
+    saveGame()
+    clearGame()
+  } else if (turn === 9) {
+    setMessage("Tie Game.")
+    saveGame()
+    clearGame()
+    }
 };
 
-function updateState(){
-  //invokes player()
-  return player()
-  //adds X or O to clicked square
+function updateState(square){
+  $(square).text(player())
 };
 
 function checkWinner(){
-  //returns true if current board contains a winner
-  //invokes setMessage with "Player _ Won!"
+  var winner = false;
+  var squares = $('td')
+    squares.text(function(index, square) {
+      board[index] = square;
+    })
+
+ winningCombo.some(function(combo) {
+   if (board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && board[combo[0]] != "") {
+     winner = true;
+     setMessage("Player " + board[combo[0]] + " Won!");
+   };
+ });
+
+ return winner;
 };
 
 function setMessage(string){
@@ -70,7 +81,9 @@ function previousGames(){
   //create a button for each that can be clicked
   //when clicked should show saved game's state
 };
+
 function clearGame(){
-  //clear board
-  //start a completly new game
+  $('td').empty();
+   turn = 0;
+   currentGameId = 0;
 };
