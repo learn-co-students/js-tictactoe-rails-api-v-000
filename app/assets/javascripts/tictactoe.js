4dbfe976cd35a111cdc6546382cc31762202a295
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-27 00:12:13 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,24 +1,92 @@
+var turn = 0;
+var gameId = 0;
+var board = $('td')
+const winCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+                   [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                   [0, 4, 8], [2, 4, 6]]
+
 function player() {
 //Returns the token of the player whose turn it is, 'X' when the turn variable is even and 'O' when it is odd.
+  return turn % 2 === 0 ? 'X' : 'O'
 }
 
-function updateState(){
+function updateState(square){
 //Invokes player() and adds the returned string ('X' or 'O') to the clicked square on the game board.
+  square.innerHTML += player()
 }
 
-function setMessage() {
+function setMessage(str) {
 //Accepts a string and adds it to the div#message element in the DOM.
+  $('#message').append(str)
 }
 
 function checkWinner() {
 // Returns true if the current board contains any winning combinations (three X or O tokens in a row, vertically, horizontally, or diagonally). Otherwise, returns false.
 // If there is a winning combination on the board, checkWinner() should invoke setMessage(), passing in the appropriate string based on who won: 'Player X Won!' or 'Player O Won!'
+  const winner = winCombos.find(combo => {
+    return combo.every(idx => board[idx].innerHTML === 'X' || combo.every(idx => board[idx].innerHTML === 'O'))
+  });
+
+  if (winner) {
+    setMessage(`Player ${board[winner[0]].innerHTML} Won!`);
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
-function doTurn() {
+function newGame() {
+  board.empty()
+  turn = 0;
+  gameId = 0;
+}
+
+function saveGame() {
+  var state = [];
+  var gameData;
+
+  board.text((square) => {
+    state.push(square)
+  });
+
+  gameData = {state: state};
+
+  if (gameId) {
+    $.ajax({
+      type: 'PATCH',
+      url: `/games/${gameId}`,
+      data: gameData
+    })
+  }
+  else {
+    $.post('/games', gameData, function(game) {
+      gameId = game.data.id
+    })
+  }
+
+}
+
+function tieGame() {
+  if (turn === 9 ) {
+    saveGame();
+    return true;
+  }
+  else {
+    return false
+  }
+}
+
+function doTurn(square) {
 // Increments the turn variable by 1.
 // Invokes the updateState() function, passing it the element that was clicked.
 // Invokes checkWinner() to determine whether the move results in a winning play.
+  if ($(square).html() != 'X' && $(square).html() != 'O') {
+    updateState(square);
+    turn++;
+  }
+
+
 }
 
 function attachListeners() {
