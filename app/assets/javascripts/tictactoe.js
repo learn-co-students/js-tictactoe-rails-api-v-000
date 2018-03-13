3e6fe7a2263c2ba8c7a94a8483bfb9abb2d178bf
github username <github email address>
2018-03-13 16:27:57 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -43,3 +43,14 @@ function checkWinner() {
 
   return winner;
 }
+
+function doTurn(square) {
+  updateState(square);
+  turn++;
+  if (checkWinner()) {
+    $('td').empty();
+    turn = 0;    
+  } else if (turn === 9) {
+    setMessage("Tie game.")
+  }
+}
