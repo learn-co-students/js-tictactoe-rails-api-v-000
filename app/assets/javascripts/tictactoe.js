33d2ae7e48bae85201a3e01aca8810c66654ee4d
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-03-25 02:12:58 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,11 +1,14 @@
 // Code your JavaScript / jQuery solution here
+// https://github.com/Booligan/ttt-with-ai-project-v-000/blob/master/lib/game.rb
+var turn = 0;
+
 function player() {
-    // debugger
-    turn%2 === 0 ? 'X':'O';
+    return turn%2 === 0 ? 'X':'O';
 }
 
-function updateState() {
-    
+function updateState(tdElement) {
+    let token = player();
+    $(tdElement).html(token) ;
 }
 
 function setMessage(message) {
@@ -13,5 +16,6 @@ function setMessage(message) {
 }
 
 function checkWinner() {
+    let currentBoard = [];
     
 }
