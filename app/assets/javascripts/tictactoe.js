749fa47e80985e1553904e5e6e6d5feb411a8f59
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-03-25 16:28:05 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,16 @@
 // Code your JavaScript / jQuery solution here
 // https://github.com/Booligan/ttt-with-ai-project-v-000/blob/master/lib/game.rb
 var turn = 0;
+var  winCombinations = [
+                          [0,1,2],
+                          [3,4,5],
+                          [6,7,8],
+                          [0,3,6],
+                          [1,4,7],
+                          [2,5,8],
+                          [0,4,8],
+                          [2,4,6]
+                        ]
 
 function player() {
     return turn%2 === 0 ? 'X':'O';
@@ -17,5 +27,28 @@ function setMessage(message) {
 
 function checkWinner() {
     let currentBoard = [];
+    const squares = window.document.querySelectorAll('td');
+    squares.forEach(x => currentBoard.push(x.innerHTML));
+    // if (squares[0].innerHTML === squares[1].innerHTML === squares[2].innerHTML || squares[3].innerHTML === squares[4].innerHTML === squares[5].innerHTML || squares[6].innerHTML === squares[7].innerHTML === squares[8].innerHTML) {
+    //     // setMessage("Player" + squares[0].innerHTML +  "Won!")
+    //     return true;
+    // }
+    for (let i=0; i < winCombinations.length; i++) {
+        let win_index_1 = winCombinations[i][0]
+        let win_index_2 = winCombinations[i][1]
+        let win_index_3 = winCombinations[i][2]
+
+        let position_1 = currentBoard[win_index_1]
+        let position_2 = currentBoard[win_index_2]
+        let position_3 = currentBoard[win_index_3]
+
+        if (position_1 === "X" && position_2 === "X" && position_3 === "X" || position_1 === "O" && position_2 === "O" && position_3 === "O") {
+            setMessage('Player ' + position_1 +  ' Won!');
+            return true;
+        }
+    }
     
+    return false;
+    
+
 }
