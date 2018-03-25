94803970703721f8bc7044a62c0aded1017be825
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-03-25 01:28:42 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -3,3 +3,11 @@ function player() {
     // debugger
     return window.turn%2 === 0 ? 'X':'O';
 }
+
+function updateState() {
+    
+}
+
+function setMessage(message) {
+    $('div#message').html(message) ;
+}
