634c8c2c9202ed63c6ea80f406d52f12032402c8
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-03-27 02:51:15 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -12,13 +12,28 @@ var  winCombinations = [
                           [2,4,6]
                         ]
 
+var currentGame;
+
 function player() {
     return turn%2 === 0 ? 'X':'O';
 }
 
+function taken(tdElement) {
+    if ($(tdElement).innerHTML === "") {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 function updateState(tdElement) {
     let token = player();
-    $(tdElement).html(token) ;
+    if (tdElement.innerHTML === "") {
+        $(tdElement).html(token);
+    }
+    else {alert ("Invalid play");
+        turn = turn -1;
+    }
 }
 
 function setMessage(message) {
@@ -41,6 +56,7 @@ function checkWinner() {
 
         if (position_1 === "X" && position_2 === "X" && position_3 === "X" || position_1 === "O" && position_2 === "O" && position_3 === "O") {
             setMessage('Player ' + position_1 +  ' Won!');
+            // $("tbody td").off("click");
             return true;
         }
     }
@@ -59,28 +75,61 @@ function getCurrentBoard() {
 function resetBoard() {
     const squares = window.document.querySelectorAll('td');
     squares.forEach(x => x.innerHTML='');
+    turn = 0;
+}
+
+function saveGame() {
+    // ajax patch
+    var method;
+    var url;
+    if (currentGame) {
+        method = "PATCH";
+        url = `/games/${currentGame}`;
+    } else {
+        method = "POST";
+        url = '/games';
+    }
+    
+    $.ajax({
+        url: url,
+        method: method,
+        dataType: "json",
+        data: {
+            state: getCurrentBoard()
+        },
+        success: function(result){
+            // console.log(result)
+            currentGame = result.data.id
+        }
+    })
 }
 
 function doTurn(element) {
+
     updateState(element);
-    turn = turn + 1;
     
+    turn = turn + 1;
+        
     if (checkWinner()) {
-        turn = 0;
-        resetBoard()
+        // $("tbody td").off("click");
+        saveGame();
+        resetBoard();
+        debugger;
     } else if (turn === 9 ) {
         setMessage("Tie game.");
-        turn = 0;
+        saveGame();
         resetBoard()
     }
+    
 }
 
 function attachListeners() {
-    
     $("tbody td").click(function() {
-        
-        console.log("Clicked");
         doTurn(this)});
+        
+    $("button#save").click(function() {
+        saveGame()
+    })
 }
 
 // $(document).ready(attachListeners());
