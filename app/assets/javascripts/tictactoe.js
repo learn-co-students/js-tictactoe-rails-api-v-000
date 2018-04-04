b649f7ebd7e52b47d9c40ec71afd46c1dc5c3d28
Alexander Urizar <urizaraj@gmail.com>
2018-04-04 15:40:38 -0400

    changes

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,57 @@
-// Code your JavaScript / jQuery solution here
+var game
+var turn = 0
+
+class Game {
+  constructor(id, state) {
+    this.id = id
+    this.state = state
+  }
+}
+
+window.onload = () => attachListeners()
+
+function attachListeners() {
+  const save = $('#save')
+  const previous = $('#previous')
+  const clear = $('#clear')
+
+  save.on('click', saveGame)
+}
+
+function saveGame() {
+  game ? updateGame() : createGame()
+}
+
+function updateGame() {
+  $.ajax({
+    type: 'PATCH',
+    url: `/games/${game.id}`,
+    data: {
+      state: boardData()
+    }
+  })
+}
+
+function createGame() {
+  const val = {
+    state: boardData()
+  }
+
+  let r = $.post('/games', val)
+  r.done(data => {
+    const newID = data.data.id
+    const newState = data.data.attributes.state
+    game = new Game(newID, newState)
+  })
+}
+
+function boardData() {
+  return $('td').toArray().map(square => {
+    // console.log(square)
+    return square.innerText
+  })
+}
+
+function player() {
+  return (turn % 2 == 0) ? 'X' : 'O'
+}
