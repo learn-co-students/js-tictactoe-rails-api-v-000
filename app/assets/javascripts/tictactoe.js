cb19713ba7e60626661e7aa1c1c8513d5f9e67eb
Vicky Lau <vlau.nyc@gmail.com>
2018-04-09 17:46:48 +0000

    added winCombinations and function checkWinner()

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,4 +1,17 @@
 var turn = 0;
+
+
+var  winCombinations = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
 function player() {
   return turn %2 === 0 ? 'X':'O';
 }
@@ -9,13 +22,26 @@ function updateState(cell) {
 }
 
 function setMessage(string) {
-  $('#message').text(string);
+  $('div#message').html(string);
 }
 
 
 function checkWinner() {
+  var board = {};
+  var winner = false;
 
+  $('td').text((index, square) => board[index] = square);
+
+  winCombinations.some(function(combo) {
+    if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
+      setMessage(`Player ${board[combo[0]]} Won!`);
+      return winner = true;
+    }
+  });
+
+  return winner;
 }
+
 
 function doTurn() {
 
