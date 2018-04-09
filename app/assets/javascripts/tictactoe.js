5f4a823f55e70a64d03c2c3f84c37171ca282093
Vicky Lau <vlau.nyc@gmail.com>
2018-04-09 16:10:38 +0000

    defined functions; player turn count.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,25 @@
-// Code your JavaScript / jQuery solution here
+var turn = 0;
+function player() {
+  return turn %2 === 0 ? 'X':'O';
+}
+
+
+function updateState() {
+
+}
+
+function setMessage() {
+
+}
+
+function checkWinner() {
+
+}
+
+function doTurn() {
+
+}
+
+function attachListeners() {
+
+}
