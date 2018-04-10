7eba3412da2d2cc1db4ae282b443810bacc7d9c8
chrisjamesr <chrisjames.r@gmail.com>
2018-04-09 23:05:04 -0400

    add onload function, saveGame function to call button function

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,34 @@
 // Code your JavaScript / jQuery solution here
+window.onload = function(){
+  console.log("The window has loaded")
+  saveGame()
+}
+  
+function gameControl(){
+  document.getElementsByTagName("button")
+}
+
+function saveGame(){
+  const buttons = document.querySelectorAll("button")
+  buttons.forEach((button)=> {
+    button.addEventListener('click', function(e){
+      window[`${this.id}`]()      
+    });
+  });
+}
+
+
+function save(){
+  console.log("SAVE")
+  fetch('/games', {
+    method: 'PATCH'
+  })
+}
+
+function previous(){
+  console.log("PREVIOUS")
+}
+
+function clear(){
+  console.log("CLEAR")
+}
