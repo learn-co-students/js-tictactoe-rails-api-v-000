1a69ab3b146838e182a0a6bede187d7d1aa2f861
chrisjamesr <chrisjames.r@gmail.com>
2018-04-10 23:03:53 -0400

    stub checkWinner(). Build player() returns correctly. building updateState and then complete doTurn(). Corrected squareEvents() so that handler function isn't executed onload

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,19 +1,19 @@
 // Code your JavaScript / jQuery solution here
 window.onload = function(){
   console.log("The window has loaded")
-  buttonEvents()
-  squareEvents()
-  checkWinner()
+  attachListeners() 
 }
-  
-function gameControl(){
-  document.getElementsByTagName("button")
-}
+let turn = 0
 
 function buttonEvents(){
   $("body").on("click", "button", function(){
+    // THIS IS A VULNERABILITY
     window[`${this.id}`]()
   })
+}
+
+function squareEvents(){
+  $("tbody").on("click", "td", doTurn)
 }
   // const buttons = document.querySelectorAll("button")
   // buttons.forEach((button)=> {
@@ -24,10 +24,11 @@ function buttonEvents(){
 
 
 
+
 function save(){
   console.log("SAVE")
   let gameStatus = 
-  fetch('/game/', {
+  $.post("/games/", {
     method: 'PUT',
     body: boardStatus()
   })
@@ -37,29 +38,27 @@ function previous(){
   console.log("PREVIOUS")
   // get All games and display on DOM
   $.get("/games", function(data){
-    debugger
   })
 }
 
 function clear(){
   // start new game
   console.log("CLEAR")
- $.post("/games", function(data){
+  $.post("/games", { state: ["","","","","","","","",""] })
+    .done(function(data){
+      console.log(data);
+  })
+}
+
+
+
+function player(){
+  return turn % 2 === 0 ? "X" : "O"
+}
+
+function updateState(square){
+  let token = player(turn)
   debugger
- }
-  )
-}
-
-function squareEvents(){
-  $("tbody").on("click", "td", function(){
-    console.log(this)
-  });
-}
-
-function player(){}
-
-function updateState(){
-  player()
 }
 
 function setMessage(string){
@@ -67,18 +66,32 @@ function setMessage(string){
 }
 
 function checkWinner(){
+  console.log("ne winners yet")
+}
+
+function boardStatus(){
 let state = []
 let boardArray = Array.from($("tbody").children("tr").children("td"))
 boardArray.forEach(function(square){
   state.push(square.innerText) 
 });
+return state
 }
 
 function doTurn(){
-  let turn = turn || 0;
+  // this = td
+  updateState(this)
   return turn += 1;
 }
 
 function attachListeners(){
+  buttonEvents()
+  squareEvents()
+}
 
+class Game {
+  constructor(id, state){
+    this.id = id;
+    this.state = state;
+  }
 }
