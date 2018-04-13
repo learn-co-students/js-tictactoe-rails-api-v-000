26dfff9c6b635c71136ee043999b1b97de9977fd
chrisjamesr <chrisjames.r@gmail.com>
2018-04-12 22:11:54 -0400

    refactoring checkWinner()

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -3,29 +3,26 @@ window.onload = function(){
   console.log("The window has loaded")
   attachListeners() 
 }
-let turn = 0
+var turn = 0
 
-function buttonEvents(){
+// EventListeners
+
+function attachListeners(){
+  $("td").on("click", function(){
+    // debugger
+    doTurn(this)
+  })
   $("body").on("click", "button", function(){
-    // THIS IS A VULNERABILITY
+  
+  // THIS IS A VULNERABILITY
     window[`${this.id}`]()
   })
 }
 
-function squareEvents(){
-  $("tbody").on("click", "td", doTurn)
-}
-  // const buttons = document.querySelectorAll("button")
-  // buttons.forEach((button)=> {
-  //   button.addEventListener('click', function(e){
-  //     window[`${this.id}`]()      
-  //   });
-  // });
 
+// Button Functions
 
-
-
-function save(){
+function saveGame(){
   console.log("SAVE")
   let gameStatus = 
   $.post("/games/", {
@@ -34,14 +31,14 @@ function save(){
   })
 }
 
-function previous(){
+function previousGames(){
   console.log("PREVIOUS")
   // get All games and display on DOM
   $.get("/games", function(data){
   })
 }
 
-function clear(){
+function clearGame(){
   // start new game
   console.log("CLEAR")
   $.post("/games", { state: ["","","","","","","","",""] })
@@ -50,48 +47,89 @@ function clear(){
   })
 }
 
-
+// GamePlay Function
 
 function player(){
-  return turn % 2 === 0 ? "X" : "O"
-
+  return turn % 2 === 0 ? 'X' : 'O';
 }
 
-function updateState(square){
-  square.innerText = player(turn)
+function updateState(thisSquare){
+  return thisSquare.innerHTML = player()
 }
 
-function setMessage(string){
-
+function setMessage(winnerString){
+  document.getElementById("message").innerHTML = winnerString
 }
 
 function checkWinner(){
-  console.log("ne winners yet")
-}
+  let winnerString = `Player ${ winCombo() } Won!`
 
-function boardStatus(){
-let state = []
-let boardArray = Array.from($("tbody").children("tr").children("td"))
-boardArray.forEach(function(square){
-  state.push(square.innerText) 
-});
-return state
-}
+  function winCombo(){
+    const winCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    let winnerToken;
+    
+    function testToken(token){
+      return function(combo){
+        return combo.every((index)=> board()[index] === token)
+      } 
+    }
 
-function doTurn(){
-  // this = td
-  updateState(this)
-  return turn += 1;
-}
+    for (let combo of winCombinations){
+      if ( testToken("X")(combo) ){
+          return winnerToken = "X" 
+      } else if (testToken("O")(combo) ){
+          return winnerToken = "O"
+      } else {
+        false
+      }
+    }
+  }
 
-function attachListeners(){
-  buttonEvents()
-  squareEvents()
-}
-
-class Game {
-  constructor(id, state){
-    this.id = id;
-    this.state = state;
+    if ( winCombo() ){
+      setMessage(winnerString)
+      return true
+    } else if (boardFull()){
+      setMessage("Tie game.")
+      return false
+    } else {
+      return false
   }
 }
+
+function doTurn(element){
+  // debugger
+  updateState(element)
+  checkWinner()
+  turn += 1;
+}
+
+function board(){
+  let state = []
+  let boardArray = Array.from($("tbody").children("tr").children("td"))
+  boardArray.forEach(function(square){
+    state.push(square.innerText) 
+  });
+  return state
+}
+
+
+function boardFull(){
+ return board().every(function(element){
+    return element !== ""
+  })
+}
+
+
+
+// function testToken(token){
+//   return function(combo){
+//     return combo.every((index)=> board()[index] === token)
+//   } 
+// }
+
+// class Game {
+//   constructor(id, state){
+//     this.id = id;
+//     this.state = state;
+//   }
+// }
