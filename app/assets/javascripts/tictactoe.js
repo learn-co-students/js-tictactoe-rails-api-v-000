773fbd0429fcef57016125d1a9be377d7c255e85
chrisjamesr <chrisjames.r@gmail.com>
2018-04-15 09:16:12 -0400

    create Game class constructor

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,4 +1,3 @@
-// Code your JavaScript / jQuery solution here
 window.onload = function(){
   console.log("The window has loaded")
   attachListeners() 
@@ -21,40 +20,9 @@ function attachListeners(){
   $("#save").on("click", ()=> saveGame())
   $("#previous").on("click", ()=> previousGames())
   $("#clear").on("click", ()=> clearGame())     
-  // THIS IS A VULNERABILITY
-    // window[`${this.id}`]()
-
 }
 
 // Button Functions
-
-function checkWinner(){
-  let board = boardStatus();
-  const WINCOMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]  
-  let result = false;
-  WINCOMBINATIONS.some(function(combo){
-    if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]){
-      setMessage(`Player ${board[combo[0]]} Won!`);
-      result = true
-    }
-  });
-  // WINCOMBINATIONS.some(function(combo){
-  //   if (combo.every((element)=> board[element] === player())){
-  //     result = true
-  //     setMessage(`Player ${board[array[0]]} Won!`)
-  //   }    
-  // });
-  return result;
-} 
-
-function updateState(element){
-  const token = player()
-  $(element).text(token)  
-}
-
-function setMessage(messageString){
-  $("div#message").append(messageString)
-}
 
 function saveGame(){
   console.log("SAVE")
@@ -67,8 +35,8 @@ function saveGame(){
 
 function previousGames(){
   console.log("PREVIOUS")
-  // get All games and display on DOM
   $.get("/games", function(data){
+    debugger
   })
 }
 
@@ -81,7 +49,30 @@ function clearGame(){
   })
 }
 
+
 // GamePlay Function
+
+function checkWinner(){
+  let board = boardStatus();
+  const WINCOMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]  
+  let result = false;
+  WINCOMBINATIONS.some(function(combo){
+    if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]){
+      setMessage(`Player ${board[combo[0]]} Won!`);
+      result = true
+    }
+  });
+  return result;
+} 
+
+function updateState(element){
+  const token = player()
+  $(element).text(token)  
+}
+
+function setMessage(messageString){
+  $("div#message").append(messageString)
+}
 
 function doTurn(element){
   updateState(element);
@@ -91,12 +82,8 @@ function doTurn(element){
   } else if ( turn === 9 ) {
     setMessage("Tie game.");
     resetBoard();
-  } else {   
-    
-  }
+  } 
 }
-
-  
   
 function resetBoard(){
   $('td').empty()
@@ -109,13 +96,27 @@ function boardStatus(){
   return board;
 }
 
-function taken(element){
-  element.textContent !==  "" 
+
+function createGame(){
+  let allGames = []
+  let id = 0
+  return class Game{
+    constructor(state){
+      this.id = ++id
+      this.state = state
+      debugger
+      Game.addGame(this)
+    }
+    static addGame(game){
+      allGames.push(game)
+    }
+    static All(){
+      debugger
+      return allGames
+    }
+  }
 }
-// class Game {
-//   constructor(id, state){
-//     this.id = id;
-//     this.state = state;
-//   }
-// }
+
+const Game = createGame()
+
  
