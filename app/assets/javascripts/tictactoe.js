debe723fcf49c4ab55fba3cca60d54d7a4e82ea7
Jacqueline <anginblu@gmail.com>
2018-04-16 22:22:16 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,22 +1,50 @@
-// Code your JavaScript / jQuery solution here
+// Default setup
 const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
                         [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 var turn = 0;
 var currentgameID = 0;
+var player = () => turn % 2 ? 'O' : 'X';
 
+//when doc is ready this sets up listeners
 $(document).ready(function() {
   attachListeners();
 });
 
-var player = () => turn % 2 ? 'O' : 'X';
+//event listeners for buttons
+function attachListeners() {
+  $('#clear').on('click', () => resetBoard());
+  $('#save').on('click', () => saveGame());
+  $('#previous').on('click', () => showPreviousGames());
+  $("td").on('click', function() {
+    if (validMove(this) && !checkWinner()) {
+      doTurn(this);
+    }
+  });
+  $("#games").on('click', 'button', function(e) {
+    var gameId = $(e.target).attr('id');
+    reloadGame(gameId);
+  });
 
-function updateState(position) {
-  position.innerHTML = player();
 }
 
+//updates a message to a User
 function setMessage(string) {
   $('#message').empty();
   $('#message').text(string);
+}
+
+//plays the game
+function doTurn(position) {
+  updateState(position);
+  turn++;
+  if (checkWinner() || checkTie()) {
+    saveGame();
+    resetBoard();
+  }
+}
+
+function updateState(position) {
+  position.innerHTML = player();
 }
 
 function checkWinner() {
@@ -50,13 +78,18 @@ function validMove(position) {
   else {return false;}
 }
 
-function doTurn(position) {
-  updateState(position);
-  turn++;
-  if (checkWinner() || checkTie()) {
-    saveGame();
-    resetBoard();
-  }
+//Handles the board status
+function currentBoard(){
+  var board = {};
+  $('td').text((index, square) => board[index] = square);
+  return board;
+}
+
+function updateBoard(game){
+  var gameState = game.data.attributes.state
+  for (let index = 0; index < gameState.length; index++) {
+    $("table td").eq(`${index}`).text(`${gameState[index]}`);
+  };
 }
 
 function resetBoard() {
@@ -66,6 +99,7 @@ function resetBoard() {
   currentgameID = 0;
 }
 
+//Save the game button
 function saveGame() {
   var state = currentBoard();
 
@@ -90,10 +124,19 @@ function saveGame() {
   }
 }
 
+//Show previous games button
+function showPreviousGames() {
+  $('#games').empty();
+
+  $.get('/games').done(function(data){
+    var games = data.data;
+    games.forEach(addButton);
+  });
+}
+
 function addButton(game){
   var button = `<button id="${game.id}">${game.id}</button><br>`
   $("#games").append(button);
-  // addTime(game);
 }
 // function addTime(game){
 //   var time = `<div id="${game.id}-time">${isoDateReviver(game.attributes.updated_at)}</div><br>`
@@ -110,53 +153,15 @@ function addButton(game){
 //   return value;
 // }
 
-function showPreviousGames() {
-  $('#games').empty();
-
-  $.get('/games').done(function(data){
-    var games = data.data;
-    games.forEach(addButton);
-  });
-}
-
-function currentBoard(){
-  var board = {};
-  $('td').text((index, square) => board[index] = square);
-  return board;
-}
-
-function updateBoard(gameState){
-  for (let index = 0; index < gameState.length; index++) {
-    $("table td").eq(`${index}`).text(`${gameState[index]}`);
-  };
-}
-
+//When a previous game id button is clicked
 function reloadGame(gameID) {
   $.get(`/games/${gameID}`).done(function(game) {
+    updateBoard(game);
     var state = game.data.attributes.state;
-    updateBoard(state);
     turn = state.join('').length;
     currentgameID = game.data.id;
-    if (!checkWinner() && turn === 9) {
-      setMessage('Tie game.');
-    }
-    else {
+    if (!checkTie() && !checkWinner()) {
       setMessage(`Game #${currentgameID} has been loaded.`)
     };
-  });
-}
-
-function attachListeners() {
-  $('#clear').on('click', () => resetBoard());
-  $('#save').on('click', () => saveGame());
-  $('#previous').on('click', () => showPreviousGames());
-  $("td").on('click', function() {
-    if (validMove(this) && !checkWinner()) {
-      doTurn(this);
-    }
-  });
-  $("#games").on('click', 'button', function(e) {
-    var gameId = $(e.target).attr('id');
-    reloadGame(gameId);
   });
 }
