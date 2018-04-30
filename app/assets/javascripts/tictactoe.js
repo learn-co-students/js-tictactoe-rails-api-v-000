82eb3962d02b6d5996b9727bb390294b350bcc4d
BuffaloRob <peckham.rob@gmail.com>
2018-04-30 15:49:38 -0400

    inProg

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,34 @@
 // Code your JavaScript / jQuery solution here
+const WINNING_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+var turn = 0;
+
+function player() {
+  if (turn % 2 === 0) {
+    return 'X';
+  } else {
+    return 'O';
+  }
+}
+
+function updateState(square) {
+  var currentPlayer = player();
+  $(square).text(currentPlayer);
+}
+
+function setMessage(message) {
+  $("#message").text(message);
+}
+
+function checkWinner() {
+
+}
