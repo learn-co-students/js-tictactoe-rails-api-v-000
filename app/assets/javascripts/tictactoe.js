4aa4ea8b35434370bed669b1e176acd217fbd946
Anastasia  Barkett <anastasia.barkett@gmail.com>
2018-05-03 00:14:23 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,100 @@
-// Code your JavaScript / jQuery solution here
+const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
+var turn = 0;
+var currentGame = 0;
+
+$(document).ready(function(){
+  attachListeners();
+})
+
+function player() {
+  return turn % 2 ? 'O' : 'X';
+}
+
+function attachListeners(){
+  $('td').on('click', function() {
+    if (!$.text(this) && !checkWinner()){
+      doTurn(this);
+    }
+  });
+
+  $('#save').on('click', () => saveGame());
+  $('#previous').on('click', () => showPreviousGames());
+  $('#clear').on('click', () => resetBoard());
+}
+
+function doTurn(square){
+  updateState(square);
+  turn ++;
+  if (checkWinner()){
+    saveGame();
+    reset();
+  } else if (turn === 9){
+    setMessage('Tie game.');
+    saveGame();
+    reset();
+  }
+}
+
+function updateState(square){
+  var token = player();
+  $(square).text(token);
+}
+
+function setMessage(string){
+  $('#message').text(string);
+}
+
+function checkWinner(){
+  var board = {};
+  var winner = false;
+
+  $('td').text((index, square) => board[index] = square);
+
+  WINNING_COMBOS.some(function(combo){
+    if (board[combo[0]] !== "" && board[combo[0]]=== board[combo[1]] && board[combo[1]] === board[combo[2]]){
+      setMessage(`Player ${board[combo[0]]} Won!`)
+      return winner = true;
+    }
+  });
+  return winner;
+}
+
+function reset(){
+  $('td').empty();
+  turn = 0;
+  currentGame = 0;
+}
+
+function saveGame(){
+  var state = [];
+  var gameData = {state: state};
+
+  $('td').text((index, square) => {
+    state.push(square)
+  });
+
+  gameData = { state: state };
+
+  if (currentGame) {
+    $.ajax({
+      type: 'PATCH',
+      url: `/games/${currentGame}`,
+      data: gameData
+    });
+  } else {
+    $.post('/games', gameData, function(game) {
+      currentGame = game.data.id;
+      $('#games').append(`<button id="gameid-${game.data.id}">${game.data.id}</button><br>`);
+      $("#gameid-" + game.data.id).on('click', () => reloadGame(game.data.id));
+    });
+  }
+}
+
+function showPreviousGames(){
+
+}
+
+function reloadGame(){
+
+}
