cc197503309687fd4c1ac8e624c9425feec1455f
ingridwong0715 <ingrid830715@gmail.com>
2018-05-06 22:24:10 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,62 @@
-// Code your JavaScript / jQuery solution here
+var turn = 0;
+
+$(function(){
+
+  attachListeners();
+
+
+});
+
+
+function saveGame(){
+  $("#save").click(function(e){
+    e.preventDefault();
+
+  })
+}
+
+function player(){
+  var token
+    if (turn % 2 == 0){
+      token = "X"
+    } else {
+      token = "O"
+    }
+  return token
+}
+
+// function turnIsEven(){
+//ASK WHY CAN"T I USE THIS HELPER
+// FUNCTION IN MY PLAYER()????
+//
+//   return turn % 2 == 0;
+// }
+
+
+function doTurn(){
+  turn += 1;
+
+  updateState($(this));
+}
+
+function updateState(clickedElement){
+  var token = player();
+  debugger;
+  clickedElement.html(token)
+}
+
+function setMessage(){
+
+}
+
+function checkWinner(){
+
+}
+
+function attachListeners(){
+
+  $("td").click(doTurn)
+
+  saveGame();
+
+}
