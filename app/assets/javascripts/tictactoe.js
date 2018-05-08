5d265d8dcc285477fe94c1417788f03af9a8580c
Chris Boujoukos <github email address>
2018-05-08 17:10:59 -0400

    still working

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,64 @@
-// Code your JavaScript / jQuery solution here
+$(function() {
+  attachListeners()
+})
+
+function attachListeners() {
+  ///////////// SAVE GAME ////////////////
+  $("#save").click(function(e) {
+    e.preventDefault();
+    saveGame()
+  });
+  ///////////// SHOW PREVIOUS GAMES ////////////////
+  $("#previous").click(function(e) {
+    e.preventDefault();
+    previousGames()
+  });
+  ////////////// CLEAR CURRENT GAME ////////////////
+  $("#clear").click(function(e) {
+    e.preventDefault();
+    clearGame()
+  });
+  $("td").on("click", function() {
+    // $(this).html(player())
+    doTurn($(this))
+  })
+}
+
+function saveGame() {
+  alert("pop pop")
+}
+function previousGames() {
+
+}
+function clearGame() {
+  alert("nice gams!")
+}
+
+///////////////// GAMEPLAY /////////////////
+var turn = 1
+
+function player() {
+  if (turn % 2 === 0) {
+    return "X"
+  } else {
+    return "O"
+  }
+}
+
+function doTurn(element) {
+  turn++;
+  updateState(element);
+  checkWinner()
+}
+
+function updateState(element) {
+  $(element).text(player())
+}
+
+function checkWinner() {
+
+}
+
+function setMessage(string) {
+  $("#message").html(string)
+}
