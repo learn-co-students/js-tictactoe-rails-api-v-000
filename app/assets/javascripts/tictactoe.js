57cede4588e01de09abe184154c612562cb44d68
ingridwong0715 <ingrid830715@gmail.com>
2018-05-07 22:27:46 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,20 +1,54 @@
-const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
-                        [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+//const WINNING_COMBOS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 var turn = 0;
-var currentState = new Array(9).fill("")
+var currentGameState = new Array(9).fill("")
+var currentGameId
 
 $(function(){
   attachListeners();
 });
 
-
 function saveGame(){
-  $("#save").click(function(e){
+
+
+    $("#save").click(function(e){
+      e.preventDefault();
+      gameData = {state: currentGameState}
+
+      if (currentGameId){
+        alert("saving existing game")
+        $.ajax({
+          method: "PATCH",
+          url: "/games/" + currentGameId,
+          data: gameData
+        }).done(function(){
+          alert("SUCCESS")
+        }).fail(function(){
+          alert("FAIL")
+        })
+      } else {
+        $.post('/games', gameData, function(data){
+
+          currentGameId = data["data"]["id"]
+        }).fail(function(){
+          alert("ERROR")
+          debugger;
+        });
+      }
+
+    });
+
+
+}
+
+function clearGame(){
+  $("#clear").click(function(e){
     e.preventDefault();
+    resetBoard();
   })
 }
 
-function player(){
+
+function player(){ // return the token for the NEXT player (i.e. the one about to play)
   var token
     if (turn % 2 == 0){
       token = "X"
@@ -24,17 +58,35 @@ function player(){
   return token
 }
 
-function doTurn(){
+function doTurn(clickedElement){
+if ($(clickedElement).text() == ""){
+  updateState($(clickedElement));
+
   turn += 1;
-  updateState($(this));
-  checkWinner();
+  if (checkWinner()){
+    resetBoard();
+  } else if (turn >= 8){
+    setMessage("Tie game.");
+    resetBoard();
+
+  }
+} else {
+  setMessage("Can't select a taken field")
+}
+
+}
+
+function resetBoard() {
+  $('td').empty();
+  turn = 0;
 }
 
 function updateState(clickedElement){
-  var token = player();
-  var tableIndex = getTableIndex(clickedElement);
+  var token = player(); // right now player() returns the player that JUST PLAYED
   $(clickedElement).text(token)
-  currentState[tableIndex] = token
+  squareIndex = getTableIndex(clickedElement);
+  currentGameState[squareIndex] = token;
+
 
 }
 
@@ -51,38 +103,70 @@ function setMessage(message){
   $("#message").text(message);
 }
 
-function getPositionsOfCurrentPlayer(){
-  var token = player();
-  var result = []
-  currentState.forEach(function(el, i){
-    if (el == token){
-      result.push(i);
-    }
+function getTakenPositions(){
+
+  var XPositionIndices = [];
+  var OPositionIndices = [];
+
+  var XPositions = $("td").filter(function(td){
+    return this.innerHTML == "X";
   });
-  return result
+  $(XPositions).each(function(){
+    var tableIndex = getTableIndex(this)
+    XPositionIndices.push(tableIndex)
+  });
+
+  var OPositionIndices = [];
+  var OPositions = $("td").filter(function(td){
+    return this.innerHTML == "O";
+  });
+  $(OPositions).each(function(){
+    var tableIndex = getTableIndex(this)
+    OPositionIndices.push(tableIndex)
+  });
+
+  return {"X": XPositionIndices, "O": OPositionIndices};
+
 }
 
 function checkWinner(){
-  var playerPositions = getPositionsOfCurrentPlayer();
+  var positions = getTakenPositions();
+
   var hasWinner = false;
-  debugger;
+  var winner
+  const WINNING_COMBOS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
   WINNING_COMBOS.forEach(function(combo){
-    // check if the combo is included in result
-    var matched = combo.every(function(element){
-      return playerPositions.indexOf(element) >= 0;
+    var isWinningCombo = combo.every(function(element){
+      return positions["O"].indexOf(element) >= 0;
     });
-    if (matched){
+    if (isWinningCombo){
       hasWinner = true;
+      winner = "O"
+    }
+  });
+
+  WINNING_COMBOS.forEach(function(combo){
+    var isWinningCombo = combo.every(function(element){
+      return positions["X"].indexOf(element) >= 0;
+    });
+    if (isWinningCombo){
+      hasWinner = true;
+      winner = "X"
     }
   });
 
   if (hasWinner){
-    alert("SOMEONE WON!!")
+    setMessage(`Player ${winner} Won!`);
   }
   return hasWinner;
 }
 
 function attachListeners(){
-  $("td").click(doTurn)
+  $("td").on('click', function(){
+    doTurn(this);
+  });
+
   saveGame();
+  clearGame();
 }
