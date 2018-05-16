ce051895d1f5758e65a37baa4c6abcd7237c937a
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-05-15 20:07:50 -0500

    EOB

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,63 @@
 // Code your JavaScript / jQuery solution here
+let turn = 0
+let currentGame = () => turn > 0 ? true : false
+const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
+                        [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+$(document).ready(function() {
+  attachListeners()
+})
+
+function player() {
+  if (turn % 2 === 0) {
+    return 'X';
+  }  else {
+    return 'O';
+  }
+}
+
+function updateState(square) {
+  let token = player()
+  $(square).text(token)
+}
+
+function setMessage(string) {
+  $("#message").text(string)
+}
+
+function checkWinner() {
+  let winner = false
+  let board = []
+  $('td').text((i, square) => board.push(square))
+  WINNING_COMBOS.forEach(function(combo) {
+    if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
+      setMessage(`Player ${board[combo[1]]} Won!`)
+      return winner = true
+    }
+  })
+  return winner
+}
+
+function doTurn(square) {
+  updateState(square)
+  turn++
+  if (checkWinner()) {
+    resetBoard()
+  }
+  else if (turn === 9) {
+    setMessage('Tie Game.')
+    resetBoard()
+  }
+}
+
+function resetBoard() {
+  $('td').empty()
+  turn = 0
+}
+
+function attachListeners() {
+  $('td').on('click', function() {
+    if (!$.text(this)) {
+      doTurn(this)
+    }
+  })
+}
