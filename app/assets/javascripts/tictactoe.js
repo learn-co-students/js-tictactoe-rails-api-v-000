854d3f797c6d77a2d3f70183260764ca32fa1325
Allyson Wesman <allysonwesman@gmail.com>
2018-05-18 15:18:21 -0700

    stub out functions

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,47 @@
-// Code your JavaScript / jQuery solution here
+$(function () {
+  turn = 0;
+  attachListeners();
+});
+
+function player() {
+  if (turn % 2 == 0) {
+    return 'X'
+  }
+  else {
+    return 'O'
+  }
+}
+
+function updateState(target) {
+  console.log(target)
+  //.dataset.x
+  // Invokes player() and adds the returned string ('X' or 'O') to the clicked square on the game board.
+  $(target).html(player())
+}
+
+function setMessage(message) {
+  $("#message").html(message);
+}
+
+function checkWinner() {
+  if (winningCombination()) {
+    console.log("someone won")
+  }
+  else {
+    console.log("no winning combo")
+  }
+}
+
+function winningCombination() {
+  return false;
+}
+
+function doTurn(event) {
+  updateState(event.target);
+  checkWinner();
+  turn += 1;
+}
+
+function attachListeners() {
+  $("tbody").on("click", "td", doTurn)
+}
