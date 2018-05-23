0fa4ca69fbe8e2131df456a67d60491d29479b1a
btcline04 <btcline04@gmail.com>
2018-05-22 20:38:38 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,21 @@
-// Code your JavaScript / jQuery solution here
+var turn = 0;
+var currentGame = 0;
+const winCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  ];
+
+// var player = () => turn % 2 === 0 ? 'X' : 'O'
+function player() {
+  if (turn % 2 === 0) {
+    return 'X'
+  } else {
+    return 'O'
+  }
+};
