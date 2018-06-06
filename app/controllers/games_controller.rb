78e99ad8ca63fe5cee58711f86b117b5d479113e
terryblue99 <terryblue@comcast.net>
2018-06-06 16:22:18 -0500

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -22,11 +22,14 @@ class GamesController < ApplicationController
 
   private
 
-  def game_params
-    params.permit(state: [])
-  end
-
+  # Use callbacks to share common setup or constraints between actions.
   def set_game
     @game = Game.find(params[:id])
   end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def game_params
+    params.permit(:state => [])
+  end
+  
 end
