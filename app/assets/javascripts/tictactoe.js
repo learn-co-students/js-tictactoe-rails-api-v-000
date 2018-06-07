0e4385a7afa592defa789b04e6a5db50a7db49df
Christina <github email address>
2018-06-07 10:04:47 -0600

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,6 @@
 // Code your JavaScript / jQuery solution here
 const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+var current_game = 0
 
 var turn = 0;
 
@@ -34,18 +35,47 @@ function previousGames() {
     $('#games').empty()
     games.forEach( function(game) {
       $( "div#games" ).append(`<button id="gameid-${game.id}">${game.id}</button>`);
+      $(`#gameid-${game.id}`).on('click', function(){
+        loadGame(game);
+      })
     })
-
   });
 }
 
 function saveGame() {
+  var state = [];
+  $('td').text((index, square) => {
+    state.push(square);
+  });
+  console.log(current_game);
+  if (current_game === 0){
+    $.post("/games", {state: state}, function(game) {
+      current_game = game.data.id
+    })
+  } else {
+    $.ajax({
+      method: "PATCH",
+      url: `/games/${current_game}`,
+      data: {state: state}
+    })
+  }
+}
 
+function loadGame(game) {
+  $.get(`/games/${game.id}`, function(resp) {
+    let state = resp.data.attributes.state;
+    turn = state.filter(position => position !== "").length
+    current_game = resp.data.id
+    for(let i = 0; i < 9; i++){
+      $('td')[i].innerHTML = state[i]
+    }
+  })
 }
 
 function clearBoard() {
   $('td').empty();
   turn = 0;
+  current_game = 0; //change current_game to currentGame
 }
 
 function player() {
@@ -85,8 +115,11 @@ function doTurn(square) {//{$('td').empty();
   updateState(square);
   turn++;
   if (checkWinner()) {
+    saveGame();
     clearBoard();
   } else if (turn === 9) {
     setMessage("Tie game.");
+    saveGame();
+    clearBoard();
   }
 }
