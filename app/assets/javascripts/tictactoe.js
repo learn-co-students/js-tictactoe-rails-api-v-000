89f6bae81237bb9498de3553957eec1c7f0e5dcb
Alli Tzadok <allisontzadok@gmail.com>
2018-06-26 11:19:39 -0500

    add player js function

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,7 +1,11 @@
 //  Code your JavaScript / jQuery solution here
 
-let turn = 0;
+var turn = 0;
 
 function player() {
-  
+  if (turn % 2 === 0){
+    return 'X';
+  } else{
+    return 'O';
+  }
 }
