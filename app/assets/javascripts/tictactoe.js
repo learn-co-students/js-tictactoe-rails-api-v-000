c9cf630f1cf41df2c65d694e5fe601dd8dff34bb
Alli Tzadok <allisontzadok@gmail.com>
2018-06-26 12:34:55 -0500

    add winning combows, updateState, setMessage, checkWinner, doTurn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,4 +1,14 @@
 //  Code your JavaScript / jQuery solution here
+const WINNING_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+];
 
 var turn = 0;
 
@@ -7,5 +17,41 @@ function player() {
     return 'X';
   } else{
     return 'O';
+  }
+}
+
+function updateState(square) {
+  var currentPlayer = player();
+  $(square).text(currentPlayer);
+}
+
+function setMessage(message){
+  $('#message').text(message);
+}
+
+function checkWinner(){
+  var board = {};
+  var winner = false;
+  //board is an empty object
+
+  $('td').text((index, square) => board[index] = square);
+
+  WINNING_COMBOS.some(function(position){
+    if (board[position[0]] !== "" && board[position[0]]  === board[position[1]] && board[position[1]] === board[position[2]]){
+      setMessage(`Player ${board[position[0]]} Won!`);
+      return winner = true;
+    }
+  });
+  return winner;
+}
+
+function doTurn(square){
+  updateState(square);
+  turn++;
+  if (checkWinner()){
+    $('td').empty();
+    turn = 0;
+  }else if(turn === 9){
+    setMessage("Tie game.")
   }
 }
