5288b4d127bb57bf4ac98cc3e3dc8a954b26660a
James Ferguson <ncjamesferguson@gmail.com>
2018-07-06 20:50:36 +0000

    win check completed

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -29,6 +29,27 @@ function setMessage(message) {
   $("#message").text(message);
 }
 
+function checkWinner() {
+  board = []
+  winner = false
+
+  for (var i = 0; i < 9; i++) {
+    board.push($("td")[i].innerHTML)
+  } 
+  
+  combos.some(function(combo) {
+   if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
+     
+    setMessage(`Player ${board[combo[0]]} Won!`);
+     
+     return winner = true;
+   }
+   
+ });
+
+ return winner;
+}
+  
 
 function doTurn(square) {
   updateState(square);
@@ -45,4 +66,16 @@ function resetBoard() {
   $("td").empty();
   turn = 0;
   currentGame = 0;
+}
+
+function attachListeners() {
+  $('td').on('click', function() {
+    if (!$.text(this) && !checkWinner()) {
+      doTurn(this);
+     }
+  });
+
+  $('#save').on('click', () => saveGame());
+  $('#previous').on('click', () => previousGames());
+  $('#clear').on('click', () => resetBoard());
 }
