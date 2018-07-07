5171ba47009a20f308ebe3b092023edc1740696f
ronald vilorio <soccer23415@gmail.com>
2018-07-07 20:21:29 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,20 @@
 // Code your JavaScript / jQuery solution here
+let turn = 0;
+function player(){
+  return turn % 2 === 0 ? "X" : "O"
+}
+function updateState(){
+
+}
+function setMessage(){
+
+}
+function checkWinner(){
+
+}
+function doTurn(){
+
+}
+function attachListeners(){
+
+}
