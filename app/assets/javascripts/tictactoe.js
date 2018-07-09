360454ca8fc1f1199a8c3ff6f1e08fd797adf625
Kelsey Fecho <kfecho30@gmail.com>
2018-07-09 15:32:21 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,21 @@
 // Code your JavaScript / jQuery solution here
+var turn = 0
+
+function doTurn(square){
+  updateState(square)
+  turn++
+}
+
+var player = () => turn % 2 ? "O": "X";
+
+function updateState(){
+  player()
+}
+
+function checkWinner() {
+  
+}
+
+function attachListeners() {
+  
+}
