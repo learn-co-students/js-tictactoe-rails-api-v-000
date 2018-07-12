62eba2defd6b999ef49ba5a6b25a194d3c6295a9
ronald vilorio <soccer23415@gmail.com>
2018-07-12 18:33:03 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -11,23 +11,32 @@ function setMessage(string){
 }
 function checkWinner(){
 
-  if($("td:eq(0)").html() === $("td:eq(1)").html() && $("td:eq(1)").html() === $("td:eq(2)").html()){
+    // Horizontal Next
+  if($("td:eq(0)").html() === $("td:eq(1)").html() && $("td:eq(1)").html() === $("td:eq(2)").html() && $("td:eq(2)").html() != ""){
+    setMessage("Player " + $("td:eq(0)").html() + " Won!")
     return true
-  }else if ($("td:eq(3)").html() === $("td:eq(4)").html() && $("td:eq(4)").html() === $("td:eq(5)").html()) {
+  }else if ($("td:eq(3)").html() === $("td:eq(4)").html() && $("td:eq(4)").html() === $("td:eq(5)").html() && $("td:eq(5)").html() != "") {
+    setMessage("Player " + $("td:eq(3)").html() + " Won!")
     return true
-  }else if ($("td:eq(6)").html() === $("td:eq(7)").html() && $("td:eq(7)").html() === $("td:eq(8)").html()) {
+  }else if ($("td:eq(6)").html() === $("td:eq(7)").html() && $("td:eq(7)").html() === $("td:eq(8)").html() && $("td:eq(8)").html() != "") {
+    setMessage("Player " + $("td:eq(6)").html() + " Won!")
     return true
     // Diagonal Next
-  }else if ($("td:eq(0)").html() === $("td:eq(4)").html() && $("td:eq(4)").html() === $("td:eq(8)").html()) {
+  }else if ($("td:eq(0)").html() === $("td:eq(4)").html() && $("td:eq(4)").html() === $("td:eq(8)").html() && $("td:eq(8)").html() != "") {
+    setMessage("Player " + $("td:eq(0)").html() + " Won!")
     return true
-  }else if ($("td:eq(2)").html() === $("td:eq(4)").html() && $("td:eq(4)").html() === $("td:eq(6)").html()) {
+  }else if ($("td:eq(2)").html() === $("td:eq(4)").html() && $("td:eq(4)").html() === $("td:eq(6)").html() && $("td:eq(6)").html() != "") {
+    setMessage("Player " + $("td:eq(2)").html() + " Won!")
     return true
     // Vertical Next
-  }else if ($("td:eq(0)").html() === $("td:eq(3)").html() && $("td:eq(3)").html() === $("td:eq(6)").html()) {
+  }else if ($("td:eq(0)").html() === $("td:eq(3)").html() && $("td:eq(3)").html() === $("td:eq(6)").html() && $("td:eq(6)").html() != "") {
+    setMessage("Player " + $("td:eq(0)").html() + " Won!")
     return true
-  }else if ($("td:eq(1)").html() === $("td:eq(4)").html() && $("td:eq(4)").html() === $("td:eq(7)").html()) {
+  }else if ($("td:eq(1)").html() === $("td:eq(4)").html() && $("td:eq(4)").html() === $("td:eq(7)").html() && $("td:eq(7)").html() != "") {
+    setMessage("Player " + $("td:eq(1)").html() + " Won!")
     return true
-  }else if ($("td:eq(2)").html() === $("td:eq(5)").html() && $("td:eq(5)").html() === $("td:eq(8)").html()) {
+  }else if ($("td:eq(2)").html() === $("td:eq(5)").html() && $("td:eq(5)").html() === $("td:eq(8)").html() && $("td:eq(8)").html() != "") {
+    setMessage("Player " + $("td:eq(2)").html() + " Won!")
     return true
   }else {
     return false
