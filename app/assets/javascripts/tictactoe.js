45b3736a6b57f57198f3f0b7af47a38ac1f08e6e
badlychadly <chadmontoya21@yahoo.com>
2018-07-22 13:56:23 -0700

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -105,36 +105,26 @@ function buttonizePreviousGame(game) {
 }
 
 function reloadGame(gameID) {
-  document.getElementById('message').innerHTML = '';
+  $('message').html('')
 
-  $.get(`/games/${gameID}`, function (data) {
-    debugger;
+  $.get(`/games/${gameID}`, function (resp) {
+    const data = resp.data
+    const id = data.id 
+    const state = data.attributes.state 
+      let index = 0;
+      for (let y = 0; y < 3; y++) {
+        for (let x = 0; x < 3; x++) {
+          debugger;
+          $(`[data-x="${x}"][data-y="${y}"]`).html(state[index])
+          index++;
+        }
+      }
+  
+      turn = state.join('').length;
+      currentGame = id;
+  
+      if (!checkWinner() && turn === 9) {
+        setMessage('Tie game.');
+      }
   })
-
-  // const xhr = new XMLHttpRequest;
-  // xhr.overrideMimeType('application/json');
-  // xhr.open('GET', `/games/${gameID}`, true);
-  // xhr.onload = () => {
-  //   debugger;
-  //   const data = JSON.parse(xhr.responseText).data;
-  //   const id = data.id;
-  //   const state = data.attributes.state;
-
-  //   let index = 0;
-  //   for (let y = 0; y < 3; y++) {
-  //     for (let x = 0; x < 3; x++) {
-  //       document.querySelector(`[data-x="${x}"][data-y="${y}"]`).innerHTML = state[index];
-  //       index++;
-  //     }
-  //   }
-
-  //   turn = state.join('').length;
-  //   currentGame = id;
-
-  //   if (!checkWinner() && turn === 9) {
-  //     setMessage('Tie game.');
-  //   }
-  // };
-
-  // xhr.send(null);
 }
