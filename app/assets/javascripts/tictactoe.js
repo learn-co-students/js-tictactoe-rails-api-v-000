cd85f71f1dba8df784a0ee9e4118319848da63de
Kim Foukaris <kimfoukaris@outlook.com>
2018-08-08 13:58:17 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,12 +1,38 @@
 $(document).ready(attachListeners);
 
 const squares = document.getElementsByTagName('td');
+let turn = 0;
 
 function attachListeners() {
   document.getElementById("save").addEventListener('click', saveGame);
   document.getElementById("previous").addEventListener('click', previousGame);
   document.getElementById("clear").addEventListener('click', clearGame);
-  squares[0].addEventListener('click', saveGame);
+  squares[0].addEventListener('click', doTurn, false);
+  squares[1].addEventListener('click', doTurn, false);
+  squares[2].addEventListener('click', doTurn, false);
+  squares[3].addEventListener('click', doTurn, false);
+  squares[4].addEventListener('click', doTurn, false);
+  squares[5].addEventListener('click', doTurn, false);
+  squares[6].addEventListener('click', doTurn, false);
+  squares[7].addEventListener('click', doTurn, false);
+  squares[8].addEventListener('click', doTurn, false);
+}
+
+function updateState(square) {
+  square.innerHTML = player();
+}
+
+function player() {
+  if (turn % 2 === 0) {
+    return "X"
+  } else {
+    return "O"
+  }
+}
+
+function doTurn() {
+  turn = turn + 1;
+  updateState(this);
 }
 
 function saveGame() {
