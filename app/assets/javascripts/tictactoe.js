ec69d3a59380de3c7ab96b5bf8f5265ce01e65b1
Kim Foukaris <kimfoukaris@outlook.com>
2018-08-08 16:29:05 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -7,15 +7,49 @@ function attachListeners() {
   document.getElementById("save").addEventListener('click', saveGame);
   document.getElementById("previous").addEventListener('click', previousGame);
   document.getElementById("clear").addEventListener('click', clearGame);
-  squares[0].addEventListener('click', doTurn, false);
-  squares[1].addEventListener('click', doTurn, false);
-  squares[2].addEventListener('click', doTurn, false);
-  squares[3].addEventListener('click', doTurn, false);
-  squares[4].addEventListener('click', doTurn, false);
-  squares[5].addEventListener('click', doTurn, false);
-  squares[6].addEventListener('click', doTurn, false);
-  squares[7].addEventListener('click', doTurn, false);
-  squares[8].addEventListener('click', doTurn, false);
+  squares[0].addEventListener('click', doTurn);
+  squares[1].addEventListener('click', doTurn);
+  squares[2].addEventListener('click', doTurn);
+  squares[3].addEventListener('click', doTurn);
+  squares[4].addEventListener('click', doTurn);
+  squares[5].addEventListener('click', doTurn);
+  squares[6].addEventListener('click', doTurn);
+  squares[7].addEventListener('click', doTurn);
+  squares[8].addEventListener('click', doTurn);
+}
+
+function checkWinner() {
+  let board = [];
+  for (let i=0; i<9; i++) {
+    board[i] = squares[i].innerHTML;
+  }
+  if (board[0] !== "" && board[0] === board[1] && board[1] === board[2]) {
+    setMessage("Player " + board[0] + " Won!");
+    return true;
+  } else if (board[3] !== "" && board[3] === board[4] && board[4] === board[5]) {
+    setMessage("Player " + board[3] + " Won!");
+    return true;
+  } else if (board[6] !== "" && board[6] === board[7] && board[7] === board[8]) {
+    setMessage("Player " + board[6] + " Won!");
+    return true;
+  } else if (board[0] !== "" && board[0] === board[3] && board[3] === board[6]) {
+    setMessage("Player " + board[0] + " Won!");
+    return true;
+  } else if (board[1] !== "" && board[1] === board[4] && board[4] === board[7]) {
+    setMessage("Player " + board[1] + " Won!");
+    return true;
+  } else if (board[2] !== "" && board[2] === board[5] && board[5] === board[8]) {
+    setMessage("Player " + board[2] + " Won!");
+    return true;
+  } else if (board[0] !== "" && board[0] === board[4] && board[4] === board[8]) {
+    setMessage("Player " + board[0] + " Won!");
+    return true;
+  } else if (board[6] !== "" && board[6] === board[4] && board[4] === board[2]) {
+    setMessage("Player " + board[6] + " Won!");
+    return true;
+  } else {
+    return false;
+  };
 }
 
 function updateState(square) {
@@ -23,7 +57,7 @@ function updateState(square) {
 }
 
 function player() {
-  if (turn % 2 === 0) {
+  if (turn % 2 == 0) {
     return "X"
   } else {
     return "O"
@@ -33,16 +67,22 @@ function player() {
 function doTurn() {
   turn = turn + 1;
   updateState(this);
+  checkWinner();
+
 }
 
 function saveGame() {
-  document.getElementById("message").innerHTML = "testing save";
+  setMessage("testing save");
 }
 
 function previousGame() {
-  document.getElementById("message").innerHTML = "testing previous";
+  setMessage("testing previous");
 }
 
 function clearGame() {
-  document.getElementById("message").innerHTML = "testing clear";
+  setMessage("testing clear");
+}
+
+function setMessage(text) {
+  document.getElementById("message").innerHTML = text;
 }
