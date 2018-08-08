d3b6f3216f123f94a28d80c9163e373135a47615
github username <github email address>
2018-08-08 01:02:46 -0700

    player() is complete

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,5 @@
 // Code your JavaScript / jQuery solution here
-// button#save
-// Clicking this button should save the current game state. If the current game already exists in the database, saving it should update that previously-saved game. If the current game has not yet been persisted to the database, saving it should do so. As a brief example, if we start with a blank board that has not been saved and click button#save, the current game should be persisted to our database. If we then click button#save a second time, the persisted game should be updated (though, since we have yet to make any moves, the board will still be blank in the updated game state).
-
-// need to get elementById("save") and on('click', function(state){update the state});
+function player (turn) {
+  let playerToken = (turn % 2 === 0) ? "X" : "O";
+  return playerToken;
+}
