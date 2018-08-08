53ec4941525ccc245c4b55c30e1880a6578507ec
Kim Foukaris <kimfoukaris@outlook.com>
2018-08-08 13:33:41 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,22 @@
-// Code your JavaScript / jQuery solution here
+$(document).ready(attachListeners);
+
+const squares = document.getElementsByTagName('td');
+
+function attachListeners() {
+  document.getElementById("save").addEventListener('click', saveGame);
+  document.getElementById("previous").addEventListener('click', previousGame);
+  document.getElementById("clear").addEventListener('click', clearGame);
+  squares[0].addEventListener('click', saveGame);
+}
+
+function saveGame() {
+  document.getElementById("message").innerHTML = "testing save";
+}
+
+function previousGame() {
+  document.getElementById("message").innerHTML = "testing previous";
+}
+
+function clearGame() {
+  document.getElementById("message").innerHTML = "testing clear";
+}
