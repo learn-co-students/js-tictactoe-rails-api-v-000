2e856b1f873ded4041a1b336d81f07fe757819b2
Kim Foukaris <kimfoukaris@outlook.com>
2018-08-09 20:12:19 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,8 @@
-$(document).ready(attachListeners);
+$(document).ready(function () {
+  attachListeners();
+});
 
-const squares = document.getElementsByTagName('td');
+const spaces = document.getElementsByTagName('td');
 //let state = Array.prototype.slice.call(squares);
 let turn = 0;
 
@@ -8,31 +10,31 @@ function attachListeners() {
   document.getElementById("save").addEventListener('click', saveGame);
   document.getElementById("previous").addEventListener('click', previousGame);
   document.getElementById("clear").addEventListener('click', clearGame);
-  squares[0].addEventListener('click', function() {
+  spaces[0].addEventListener('click', function() {
     doTurn(this);
   });
-  squares[1].addEventListener('click', function() {
+  spaces[1].addEventListener('click', function() {
     doTurn(this);
   });
-  squares[2].addEventListener('click', function() {
+  spaces[2].addEventListener('click', function() {
     doTurn(this);
   });
-  squares[3].addEventListener('click', function(){
+  spaces[3].addEventListener('click', function(){
     doTurn(this);
   });
-  squares[4].addEventListener('click', function(){
+  spaces[4].addEventListener('click', function(){
     doTurn(this);
   });
-  squares[5].addEventListener('click', function(){
+  spaces[5].addEventListener('click', function(){
     doTurn(this);
   });
-  squares[6].addEventListener('click', function(){
+  spaces[6].addEventListener('click', function(){
     doTurn(this);
   });
-  squares[7].addEventListener('click', function(){
+  spaces[7].addEventListener('click', function(){
     doTurn(this);
   });
-  squares[8].addEventListener('click',function(){
+  spaces[8].addEventListener('click',function(){
     doTurn(this);
   });
 }
@@ -40,7 +42,7 @@ function attachListeners() {
 function checkWinner() {
   let board = [];
   for (i=0; i< 9; i++) {
-    board[i] = squares[i].innerHTML;
+    board[i] = spaces[i].innerHTML;
   }
   if (board[0] !== "" && board[0] === board[1] && board[1] === board[2]) {
     setMessage("Player " + board[0] + " Won!");
@@ -83,6 +85,13 @@ function checkWinner() {
     turn = 0;
     return true;
   } else {
+    if (turn === 9) {
+      setMessage("Tie game.");
+      resetSquares();
+      console.log(turn);
+      turn = 0;
+      console.log(turn);
+    }
     return false;
   };
 }
@@ -93,26 +102,27 @@ function updateState(square) {
 
 function player() {
   if (turn % 2 === 0) {
-    return "X"
-  } else {
     return "O"
+  } else {
+    return "X"
   }
 }
 
 function doTurn(square) {
+  if (square.innerHTML === "") {
+    turn = turn + 1;
+    console.log(turn);
 
-
-  //console.log(turn);
-
-  //console.log(player());
-  updateState(square);
-  checkWinner();
-  ++turn;
+    //console.log(player());
+    updateState(square);
+    checkWinner();
+    console.log(turn);
+  }
 }
 
 function resetSquares() {
   for (i=0; i< 9; i++) {
-    squares[i].innerHTML = "";
+    spaces[i].innerHTML = "";
   }
 }
 
