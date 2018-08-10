90d82c67d44fa51e7c36e59d7a950053e0035375
Kim Foukaris <kimfoukaris@outlook.com>
2018-08-10 15:56:03 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
 const spaces = document.getElementsByTagName('td');
 //let state = Array.prototype.slice.call(squares);
 var turn = 0;
+var won = false;
 
 function attachListeners() {
   document.getElementById("save").addEventListener('click', saveGame);
@@ -40,55 +41,46 @@ function attachListeners() {
 }
 
 function checkWinner() {
-  let won = false;
   let board = [];
   for (i=0; i< 9; i++) {
     board[i] = spaces[i].innerHTML;
   }
   if (board[0] !== "" && board[0] === board[1] && board[1] === board[2]) {
-    won = true;
     setMessage("Player " + board[0] + " Won!");
     resetSquares();
     turn = 0;
     return true;
   } else if (board[3] !== "" && board[3] === board[4] && board[4] === board[5]) {
-    won = true;
     setMessage("Player " + board[3] + " Won!");
     resetSquares();
     turn = 0;
     return true;
   } else if (board[6] !== "" && board[6] === board[7] && board[7] === board[8]) {
-    won = true;
     setMessage("Player " + board[6] + " Won!");
     resetSquares();
     turn = 0;
     return true;
   } else if (board[0] !== "" && board[0] === board[3] && board[3] === board[6]) {
-    won = true;
     setMessage("Player " + board[0] + " Won!");
     resetSquares();
     turn = 0;
     return true;
   } else if (board[1] !== "" && board[1] === board[4] && board[4] === board[7]) {
-    won = true;
     setMessage("Player " + board[1] + " Won!");
     resetSquares();
     turn = 0;
     return true;
   } else if (board[2] !== "" && board[2] === board[5] && board[5] === board[8]) {
-    won = true;
     setMessage("Player " + board[2] + " Won!");
     resetSquares();
     turn = 0;
     return true;
   } else if (board[0] !== "" && board[0] === board[4] && board[4] === board[8]) {
-    won = true;
     setMessage("Player " + board[0] + " Won!");
     resetSquares();
     turn = 0;
     return true;
   } else if (board[6] !== "" && board[6] === board[4] && board[4] === board[2]) {
-    won = true;
     setMessage("Player " + board[6] + " Won!");
     resetSquares();
     turn = 0;
