9266b2697f4401e5da5418fca438ee4fc2e87b0c
github username <github email address>
2018-08-09 22:20:33 -0700

    stubbing out checkWinner. need to get the wincons set up.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -4,10 +4,30 @@ function player (turn) {
   return playerToken;
 }
 
+// let turn = 0;
+// turnCounter will be increased each time doTurn() gets called.
+// function doTurn() {
+//   return turn +=
+// }
+
 function updateState(square) {
   // increase the turnCounter, then use it as the argument for player(). that output is then used to getElementById="x-0, y-1" as an example for the middle left square and set that element's innerHTML to the playerToken?
 }
 
-function setMessage (string) {
+function setMessage(string) {
   $("#message").html(string);
+}
+
+// let horizontalWin
+// let verticalWin
+// let diagonalWin
+// create the win conditions array and then set horizontalWin to be true if any of the horizontal combos is true using something like include. do the same for vertical and diagonal.
+
+function checkWinner() {
+  if (horizontalWin || verticalWin || diagonalWin) {
+    setMessage(`Player ${"player(turn)"} Won!`);
+    return true;
+  } else {
+    return false;
+  }
 }
