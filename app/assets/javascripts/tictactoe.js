bb27634caadb5c758bcb6909ab65372995a727f4
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-10 04:21:42 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,48 @@
-// Code your JavaScript / jQuery solution here
+$(function(){
+  attachListeners()
+})
+
+const win_combos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+var turn = 0
+var currentGame = 0
+
+function player(){
+  return turn % 2 ? 'O' : 'X'
+}
+
+function updateState(sq){
+  $(sq).text(player())
+}
+
+function setMessage(msg){
+  $('#messages').text(msg)
+}
+
+function checkWinner(){
+  var winner = false
+  board = []
+  $('td').text((index, square) => board[index] = square);
+  win_combos.some(function(a){
+    var wc = []
+    a.forEach(function(i){
+      wc.push(board[i])
+    })
+    if (wc[0] !== "" && wc[0] === wc[1] && wc[1] === wc[2])
+    //setMessage(`Player ${wc[0]} Won!`)
+    return winner = true
+  })
+  return winner
+}
+
+function doTurn(sq){
+  ++turn
+  updateState(sq)
+}
+
+function attachListeners(){
+  $('td').on('click', function(){
+    if (!$.text(this) && !checkWinner()) {
+      doTurn(this)
+    }
+  })
+}
