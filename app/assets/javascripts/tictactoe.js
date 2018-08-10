a9a38dcf35e9ec49599aa9d0cefbdc39b146ac0d
github username <github email address>
2018-08-09 23:16:49 -0700

    down to 30 errors. working through how to find the right <td> in updateState()

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -10,22 +10,24 @@ function player (turn) {
 //   return turn +=
 // }
 
-function updateState(square) {
   // increase the turnCounter, then use it as the argument for player(). that output is then used to getElementById="x-0, y-1" as an example for the middle left square and set that element's innerHTML to the playerToken?
+  function updateState(square) {
+    $()
 }
 
 function setMessage(string) {
   $("#message").html(string);
 }
 
-// let horizontalWin
-// let verticalWin
-// let diagonalWin
 // create the win conditions array and then set horizontalWin to be true if any of the horizontal combos is true using something like include. do the same for vertical and diagonal.
 
+
 function checkWinner() {
+  let horizontalWin
+  let verticalWin
+  let diagonalWin
   if (horizontalWin || verticalWin || diagonalWin) {
-    setMessage(`Player ${"player(turn)"} Won!`);
+    setMessage(`Player ${player(turn)} Won!`);
     return true;
   } else {
     return false;
