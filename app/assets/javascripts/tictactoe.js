c1a851ee5bc989ad4be81cd5bcd8bbf278c65b3b
github username <github email address>
2018-08-09 20:46:59 -0700

    setMessage complete

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -3,3 +3,11 @@ function player (turn) {
   let playerToken = (turn % 2 === 0) ? "X" : "O";
   return playerToken;
 }
+
+function updateState(square) {
+  // increase the turnCounter, then use it as the argument for player(). that output is then used to getElementById="x-0, y-1" as an example for the middle left square and set that element's innerHTML to the playerToken?
+}
+
+function setMessage (string) {
+  $("#message").html(string);
+}
