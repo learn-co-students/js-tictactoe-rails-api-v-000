4635ed3e691f62d2eac06cde2d2c607b6026e7bf
funemployeddad <funemployeddad@gmail.com>
2018-08-12 17:51:29 -0700

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,27 +1,28 @@
 // Code your JavaScript / jQuery solution here
-const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
-let turn = 0;
-let currentGame = 0;
+const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
+                        [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+var turn = 0;
+var currentGame = 0;
 
 $(document).ready(function() {
   attachListeners();
 });
 
-let player = () => turn % 2 ? "O" : "X";
+var player = () => turn % 2 ? 'O' : 'X';
 
 function doTurn(square) {
-   updateState();
-   turn++ ;
-   if (checkWinner()) {
-     saveGame();
-     resetBoard();
-   } else if (turn === 9) {
-     setMessage("Tie game.");
-     saveGame();
-     resetBoard();
-     }
-   }
+  updateState(square);
+  turn++;
+  if (checkWinner()) {
+    saveGame();
+    resetBoard();
+  } else if (turn === 9) {
+    setMessage("Tie game.");
+    saveGame();
+    resetBoard();
+  }
+}
 
 function resetBoard() {
   $('td').empty();
@@ -35,36 +36,102 @@ function attachListeners() {
       doTurn(this);
     }
   });
+
+  $('#save').on('click', () => saveGame());
+  $('#previous').on('click', () => showPreviousGames());
+  $('#clear').on('click', () => resetBoard());
 }
 
-$('#save').on('click', () => saveGame());
-$('#previous').on('click', () => showPreviousGames());
-$('#clear').on('click', () => resetBoard());
+function checkWinner() {
+  var board = {};
+  var winner = false;
 
+  $('td').text((index, square) => board[index] = square);
 
-function updateState() {
-  $("td").html(player(turn));
-  // I know this isn't right because it's not precise to the specific td I want to alter. also turn is not currently a real thing, it's still just a stub.
-  //the square's coordinates are going to be available when this gets called in the on.click event listener
-  //set the innerHTML of that <td> to the return value of player(turn)
+  WINNING_COMBOS.some(function(combo) {
+    if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
+      setMessage(`Player ${board[combo[0]]} Won!`);
+      return winner = true;
+    }
+  });
+
+  return winner;
+}
+
+function updateState(square) {
+  var token = player();
+  $(square).text(token);
 }
 
 function setMessage(string) {
-  $("#message").html(string);
+  $('#message').text(string);
 }
 
-// create the win conditions array and then set horizontalWin to be true if any of the horizontal combos is true using something like include. do the same for vertical and diagonal.
+function saveGame() {
+  var state = [];
+  var gameData;
 
+  $('td').text((index, square) => {
+    state.push(square);
+  });
 
-function checkWinner() {
-  let horizontalWin
-  let verticalWin
-  let diagonalWin
-  if (horizontalWin || verticalWin || diagonalWin) {
-    setMessage(`Player ${player(turn)} Won!`);
+  gameData = { state: state };
 
-    return true;
+  if (currentGame) {
+    $.ajax({
+      type: 'PATCH',
+      url: `/games/${currentGame}`,
+      data: gameData
+    });
   } else {
-    return false;
+    $.post('/games', gameData, function(game) {
+      currentGame = game.data.id;
+      $('#games').append(`<button id="gameid-${game.data.id}">${game.data.id}</button><br>`);
+      $("#gameid-" + game.data.id).on('click', () => reloadGame(game.data.id));
+    });
   }
+}
+
+function showPreviousGames() {
+  $('#games').empty();
+  $.get('/games', (savedGames) => {
+    if (savedGames.data.length) {
+      savedGames.data.forEach(buttonizePreviousGame);
+    }
+  });
+}
+
+function buttonizePreviousGame(game) {
+  $('#games').append(`<button id="gameid-${game.id}">${game.id}</button><br>`);
+  $(`#gameid-${game.id}`).on('click', () => reloadGame(game.id));
+}
+
+function reloadGame(gameID) {
+  document.getElementById('message').innerHTML = '';
+
+  const xhr = new XMLHttpRequest;
+  xhr.overrideMimeType('application/json');
+  xhr.open('GET', `/games/${gameID}`, true);
+  xhr.onload = () => {
+    const data = JSON.parse(xhr.responseText).data;
+    const id = data.id;
+    const state = data.attributes.state;
+
+    let index = 0;
+    for (let y = 0; y < 3; y++) {
+      for (let x = 0; x < 3; x++) {
+        document.querySelector(`[data-x="${x}"][data-y="${y}"]`).innerHTML = state[index];
+        index++;
+      }
+    }
+
+    turn = state.join('').length;
+    currentGame = id;
+
+    if (!checkWinner() && turn === 9) {
+      setMessage('Tie game.');
+    }
+  };
+
+  xhr.send(null);
 }
