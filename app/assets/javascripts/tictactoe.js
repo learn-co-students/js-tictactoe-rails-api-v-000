08806ae4bdc31d4b4c528d3fbdc51aa35b8d5fd2
jennianelson <jennifernelson118@gmail.com>
2018-08-14 11:12:12 -0500

    Functions created: player(), doTurn(), updateState(), attachListeners()

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,29 @@
 // Code your JavaScript / jQuery solution here
+let turn = 1
+
+const player = () => {
+  return turn%2 == 0 ? "O" : "X"
+}
+
+const doTurn = () => {
+  return turn++
+}
+
+function updateState(event) {
+  let token = player(turn)
+  event.target.textContent = token
+  doTurn(turn)
+}
+
+const attachListeners = () => {
+  var cells = Array.from(document.getElementsByTagName("td"))
+  cells.forEach(cell => {
+    cell.addEventListener("click", (e) => {updateState(e)}, {once: true})
+  })
+  
+}
+
+$(function() {
+  attachListeners()
+ 
+});
