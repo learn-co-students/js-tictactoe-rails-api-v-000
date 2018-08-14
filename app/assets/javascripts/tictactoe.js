13f38766dae19a198c781f14de57ae8f54f87f3d
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-08-13 20:42:19 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -2,6 +2,7 @@
 // document.addEventListener("DOMContentLoaded", function(){
 $(document).ready(attachListeners) ;
 
+// win combos from ruby ttt
 const winningCombos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
                         [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 var turn = 0;
