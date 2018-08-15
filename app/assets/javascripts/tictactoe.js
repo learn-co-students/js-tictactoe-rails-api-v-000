ec11b4c5b364ac0a2a1f6e16545072f33e9ea374
njpowers7915 <njpowers7915@gmail.com>
2018-08-15 11:36:07 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,29 @@
 // Code your JavaScript / jQuery solution here
+function player(turn) {
+  debugger
+  if (turn == null || turn % 2 == 0) {
+    return 'X'
+  } else {
+    return 'O'
+  }
+}
+
+function updateState() {
+
+}
+
+function setMessage() {
+
+}
+
+function checkWinner() {
+
+}
+
+function doTurn() {
+
+}
+
+function attachListener() {
+
+}
