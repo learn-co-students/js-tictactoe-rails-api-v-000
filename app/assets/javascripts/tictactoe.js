a5ce52176f13aeecd805fbe0f3575975577110a7
njpowers7915 <njpowers7915@gmail.com>
2018-08-16 10:16:53 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,15 +1,44 @@
 // Code your JavaScript / jQuery solution here
 const winCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
+//Begin helper functions
 let board = function () {
   let array = Array.prototype.slice.call(document.querySelectorAll('td'))
   return array.map(function (i) {return i.innerHTML})
 }
 
 function positionTaken(index) {
-  if (board()[index] != "") { return true }
+  if (board()[index] !== "") { return true }
   else { return false }
 }
+
+function won(array) {
+  //debugger
+  if (positionTaken(array[0]) == true) {
+    if (board()[array[0]] == board()[array[1]] && board()[array[0]] == board()[array[2]] &&
+    board()[array[1]] == board()[array[2]]) { return true }
+      else { return false }
+  } else { return false }
+}
+
+function resetBoard() {
+  let newBoard = Array.prototype.slice.call(document.querySelectorAll('td'))
+  $('td').html("")
+}
+
+function fullBoard() {
+
+}
+
+function tieGame() {
+  if (fullBoard() == true && won() == false) {
+    return true
+  } else {
+    return false
+  }
+}
+
+//End Helper Functions
 
 var turn = 0
 
@@ -30,47 +59,34 @@ function setMessage(string) {
   $("#message").html(string)
 }
 
-function won(array) {
-  if (positionTaken(array[0]) == true) {
-    if (board()[array[0]] == board()[array[1]] && board()[array[0]] == board()[array[2]] &&
-    board()[array[1]] == board()[array[2]]) { return true }
-      else { return false }
-  } else { return false }
-}
 
 function checkWinner() {
-  debugger
-  let winner = winCombinations.find(function (e) {
-    return won(e) == true
-  })
+  for (var i = 0; i < winCombinations.length; i++) {
+    if (won(winCombinations[i]) == false) {
+      continue
+    } else {
+      if (board()[winCombinations[i][0]] == 'X') {
+        setMessage('Player X Won!')
+      } else if (board()[winCombinations[i][0]] == 'O') {
+        setMessage('Player O Won!')
+      }
+      return true
+      break
+    }
+  }
+  return false
 }
-//      if (won(e) == true) {
-//        if (e[0] == 'X') {
-//          setMessage('Player X Won!')
-//        } else {
-//          setMessage('Player O Won!')
-//        }
-//      } else {
-//        setMessage('LOSE')
-//      }
-//  })
-//}
 
-//    board()[e[0]] == board()[e[1]] && board()[e[0]] == board()[e[2]] &&
-//    board()[e[1]] == board()[e[2]] && board()[0] != "" && board()[1] != ""
-//  })
-//  return winningArray()
-//}
-
-//def won?
-//    WIN_COMBINATIONS.detect do |win_combo|
-//      @board[win_combo[0]] == @board[win_combo[1]] && @board[win_combo[0]] == @board[win_combo[2]] && position_taken?(win_combo[0])
-//    end
-//  end
-function doTurn() {
+function doTurn(cell) {
   this.turn += 1
+  updateState(cell)
   checkWinner()
-  updateState()
+  if (checkWinner() == true) {
+    this.turn = 0
+    resetBoard()
+  } else {
+    if (
+  }
 }
 
 function attachListener() {
