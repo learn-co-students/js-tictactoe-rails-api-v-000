f7ad4aa22d164c92874c5c3e01971fb3e132f587
njpowers7915 <njpowers7915@gmail.com>
2018-08-17 14:36:31 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,13 @@
 // Code your JavaScript / jQuery solution here
 $(document).ready(attachListeners)
+$(function () {
+  $("#save").on("click", (function (e) {
+    e.preventDefault();
+    alert("SAVE")
+  })
+)
+})
+
 
 const winCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
