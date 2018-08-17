1f8b103864ea68f7bfc46866d4ca12758b2b0e64
njpowers7915 <njpowers7915@gmail.com>
2018-08-16 21:23:10 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,13 @@
 // Code your JavaScript / jQuery solution here
 const winCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
+function attachListeners(tdElement) {
+  document.body.addEventListener('click', function(element) {
+    if 
+  }
+  tdElement.addEventListener('click', doTurn(tdElement))
+}
+
 //Begin helper functions
 let board = function () {
   let array = Array.prototype.slice.call(document.querySelectorAll('td'))
@@ -93,8 +100,4 @@ function doTurn(cell) {
       resetBoard()
     }
   }
-}
-//
-function attachListeners() {
-
 }
