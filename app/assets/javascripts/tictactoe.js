93372291032cbae2952d7f88f0fdf72a17309d67
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-08-19 03:32:40 +0000

    aaq help

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,4 +1,7 @@
 // Code your JavaScript / jQuery solution here
+$(document).ready(function() {
+  attachListeners();
+});
 
 class Game {
   constructor(attr) {
@@ -13,7 +16,37 @@ var WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
 var turn = 0
 var savedGame = 0
 
-$(document).ready($('#save').on('click', console.log(this)))
+
+
+function doTurn(box) {
+  updateState(box);
+  turn++;
+  if (checkWinner()) {
+    saveGame()
+    clearBoard()
+  } else if (turn === 9) {
+    setMessage("Tie game.")
+    saveGame()
+    clearBoard()
+  }
+}
+
+function clearBoard() {
+  $('td').each((index, box) => {
+      box.innerHTML = ""
+  })
+  savedGame = 0
+  turn = 0
+}
+
+function previousGame() {
+    $.get('/games', (games) => {
+        games.data.forEach((game) => {
+            $('#games').append(`<button id="gameid-${game.id}">${game.id}</button><br>`)
+            $("#gameid-" + game.id).on('click', () => reloadGame(game.id))
+        })
+    })
+}
 
 function saveGame() {
     var state = []
@@ -21,14 +54,26 @@ function saveGame() {
       state.push(data);
     });
     var sendData = { state: state }
-    console.log('test')
     if (savedGame) {
-        $.patch(`/games/${savedGame}`, sendData, function() {
-            console.log('LOGGGGED!!!!!!!!!!!!!!!!!!')
+        debugger
+        $.patch(`/games/${savedGame}`, sendData)
+    } else {
+        $.post('/games', sendData, (game) => {
+            savedGame = game.data.id
+            $('#games').append(`<button id="gameid-${game.data.id}">${game.data.id}</button><br>`)
+            $("#gameid-" + game.data.id).on('click', () => reloadGame(game.data.id))
         })
     }
 }
 
+function reloadGame(id) {
+    var a = $('td')
+    $.get(`/games/${id}`, (data) => {
+        data.data.attributes['state'].forEach((x, i) => {
+            a[i].innerHTML = x
+        })
+    })
+}
 function player() {
     if (turn%2 === 0) {
         return "X"        
@@ -39,28 +84,28 @@ function player() {
 
 function updateState(box) {
     let marker = player()
-    $(box).text(marker)
+    box.innerHTML = marker
 }
 
 function setMessage(msg) {
     $('#message').text(msg)
 }
 
-function checkWinner(state) {
-    var board = {}
-    var winner = false
-    $('td').text((index, string) => board[index] = string)  // text() can be set to a callback passing the index and text od the elements parses.
-    WINNING_COMBOS.some((pattern) => {  // .some iterates through arrays and returns wether or not the any of the elements passes the callback truthfully
-        let a = board[pattern[0]] !== ""
-        let b = board[pattern[0]] === board[pattern[1]] && board[pattern[1]] == board[pattern[2]]
-        if (a && b) {
-            setMessage(`Player ${board[pattern[0]]} Won!`)
-            return winner = true
-        }
-    })
-    return winner
-}
+function checkWinner() {
+  var board = {};
+  var winner = false;
 
+  $('td').text((index, square) => board[index] = square);
+
+  WINNING_COMBOS.some(function(combo) {
+    if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
+      setMessage(`Player ${board[combo[0]]} Won!`);
+      return winner = true;
+    }
+  });
+
+  return winner;
+}
 
 $.patch = function(url, data, callback, type){
  
@@ -77,4 +122,20 @@ $.patch = function(url, data, callback, type){
     data: data,
     contentType: type
   });
+}
+
+
+function attachListeners() {
+    $(() => {
+    $('td').each((index, box) => {
+        box.addEventListener('click', () => {
+            if (!checkWinner() && !$.text(box)) {
+                doTurn(box)
+            }
+        })
+    })
+    $('#save').on('click', () => saveGame())
+    $('#previous').on('click', () => previousGame())
+    $('#clear').on('click', () => clearBoard())
+})
 }
