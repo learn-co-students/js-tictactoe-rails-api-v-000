db12e93c65bf60a7894f7e6de5d97a6d99d9d20b
Dee Race <deerace12@gmail.com>
2018-08-23 15:49:19 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,48 @@
 // Code your JavaScript / jQuery solution here
+
+$(document).ready(function(){
+    attachListeners()
+})
+
+var turn = 0 
+
+const WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7], 
+    [2, 5, 8], 
+    [0, 4, 8], 
+    [2, 4, 6]
+]
+
+var board = 0
+
+function player() {
+    if (turn % 2 == 0) {
+        return "X"
+    } else {
+        return "O"
+    }
+}
+
+function updateState(square) {
+    $(square).text(player())
+}
+
+function setMessage(string) {
+    $("div#message").text(string)
+}
+
+function checkWinner() {
+
+}
+
+function doTurn() {
+
+}
+
+function attachListeners() {
+
+}
