6a4da6991dad43de14599eec8192efaeb151760a
Maverick Tesini <mavericktesini@gmail.com>
2018-08-23 07:50:56 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,38 @@
-// Code your JavaScript / jQuery solution here
+var turnCount = 0;
+const winCombos= [
+  [012], [345], [678], //vertical
+  [036], [147], [258], //horizontal
+  [048], [246]  //diagonal
+]
+
+$(document).ready(function() {
+  console.log("I am ready, sir.");
+  attachListeners();
+})
+
+function attachListeners() {
+  // listen for click on what?
+  console.log("I am listening...");
+}
+
+function player() {
+  if (turnCount % 2 === 0) {
+    turnCount += 1;
+    return "X";
+  } else {
+    turnCount += 1;
+    return "O";
+  }
+}
+
+function updateState(element) {
+  $(element).text(player());
+}
+
+function setMessage(message_string) {
+  $('#message').html(message_string);
+}
+
+function checkWinner() {
+
+}
