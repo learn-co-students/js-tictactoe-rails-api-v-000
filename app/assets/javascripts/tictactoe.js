d0f33c38f70ea83954dc3c7e2d70c3884743aed4
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-08-28 21:25:31 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -19,18 +19,17 @@ function setMessage(message){
 }
 
 function checkWinner() {
-  // const WINNING_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     var board = getBoard();
     WINNING_COMBINATIONS.some(function (combo){
-
       if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]){
+        setMessage(`Player ${board[combo[0]]} Won!`)
         return winner = true;
       }
       else { 
         return winner = false;
       }
-      debugger;
     })
+    return winner 
 }
 
 function getBoard(){
