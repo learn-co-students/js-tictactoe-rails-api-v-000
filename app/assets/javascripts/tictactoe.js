bc30554b56c0fe05f57f30689bb16f850b655eb2
anthonygharvey <anthony@anthonygharvey.com>
2018-08-30 11:19:32 -0400

    Added updateState(), squareAvailable() and setMessage() functions.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,19 @@
 var turn = 0;
 
 function player() {
-	return (turn % 2 === 0 ? 'X' : 'O')
+	return turn % 2 === 0 ? 'X' : 'O'
+}
+
+function updateState(square) {
+	if (squareAvailable) {
+		return $(square).text(player());
+	}
+};
+
+function squareAvailable(square) {
+	return $(square).text === "" ? true : false
+}
+
+function setMessage(string) {
+	return $('div#message').append(string)
 }
