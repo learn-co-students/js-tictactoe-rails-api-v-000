c003c165ccf36961e02f3d398deb0c23b18272a8
Eric DeSimone <eric.desimone@cortland.edu>
2018-08-31 18:31:23 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -2,6 +2,16 @@
 // $("td[data-x='1'][data-y='0'")
 // $( "td:contains('x')" )
 let turnCount = 0
+const winCombinations = [
+  [[0,0],[1,0],[2,0]],
+  [[0,1],[1,1],[2,1]],
+  [[0,2],[1,2],[2,2]],
+  [[0,0],[0,1],[0,2]],
+  [[1,0],[1,1],[1,2]],
+  [[2,0],[2,1],[2,2]],
+  [[0,0],[1,1],[2,2]],
+  [[2,0],[1,1],[0,2]]
+]
 
 function isEven(n) {
    return n % 2 == 0;
@@ -13,5 +23,17 @@ function player(){
 
 function updateState(element){
   $(element).html(player())
-  turnCount += 1
+}
+
+function setMessage(){
+  $('#message').html(`Player ${player()} Won!`)
+}
+
+function checkWinner(){
+  winCombinations.find( combo => {
+    const value1 = $(`td[data-x='${combo[0][0]}'][data-y='${combo[0][1]}'`).html()
+    const value2 = $(`td[data-x='${combo[1][0]}'][data-y='${combo[1][1]}'`).html()
+    const value3 = $(`td[data-x='${combo[2][0]}'][data-y='${combo[2][1]}'`).html()
+    debugger
+  })
 }
