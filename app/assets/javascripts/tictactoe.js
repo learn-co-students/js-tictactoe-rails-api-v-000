7743317e5105c6870ac4bcba0cc9df83173bda6f
Eric DeSimone <eric.desimone@cortland.edu>
2018-08-31 18:46:30 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -30,10 +30,12 @@ function setMessage(){
 }
 
 function checkWinner(){
-  winCombinations.find( combo => {
+  let winningCombo = winCombinations.find( combo => {
+    debugger
     const value1 = $(`td[data-x='${combo[0][0]}'][data-y='${combo[0][1]}'`).html()
     const value2 = $(`td[data-x='${combo[1][0]}'][data-y='${combo[1][1]}'`).html()
     const value3 = $(`td[data-x='${combo[2][0]}'][data-y='${combo[2][1]}'`).html()
-    debugger
+    return value1 !== "" && value1 === value2 && value2 === value3
   })
+  return winningCombo ? true : false
 }
