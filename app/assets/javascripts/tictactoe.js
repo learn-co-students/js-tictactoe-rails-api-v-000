eb69d2fde53e2d5ce064a9a74f61a2a47124b621
ktamoguis <ktamoguis@gmail.com>
2018-09-07 05:53:56 +0800

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,18 @@
 // Code your JavaScript / jQuery solution here
+var turn = 0;
+
+function player(){
+  //debugger;
+  let remainder = turn % 2
+  if (remainder === 0){
+    return "X";
+  } else {
+    return "O";
+  }
+}
+
+function updateState(square){
+  //lplayer();
+  //debugger;
+  square.innerHTML = player();
+}
