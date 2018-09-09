42bf1cccf397fd1c10053e71d0d0741dd21b801c
ktamoguis <ktamoguis@gmail.com>
2018-09-10 05:57:18 +0800

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,8 @@
 // Code your JavaScript / jQuery solution here
 var turn = 0;
 
+$(document).ready(attachListeners);
+
 function player(){
   //debugger;
   let remainder = turn % 2
@@ -12,77 +14,76 @@ function player(){
 }
 
 function updateState(square){
-  //lplayer();
-  //debugger;
   square.innerHTML = player();
 }
 
 function setMessage(message){
   const messageDiv = window.document.getElementById('message')
+  //debugger;
   messageDiv.innerHTML = message;
 }
 
 function checkWinner(){
+  const WIN_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ]
   square = window.document.querySelectorAll("td");
   var state;
   var winplayer;
-  if (square[0].innerHTML ===square[1].innerHTML && square[1].innerHTML===square[2].innerHTML) {
-    state = true;
-    winplayer = square[0].innerHTML;
-  } else if (square[2].innerHTML ===square[4].innerHTML && square[4].innerHTML===square[6].innerHTML){
-    state = true;
-    winplayer = square[2].innerHTML;
-  } else if (square[2].innerHTML ===square[5].innerHTML && square[5].innerHTML===square[8].innerHTML){
-    state = true;
-    winplayer = square[2];
-  } else if (square[3].innerHTML ===square[4].innerHTML && square[4].innerHTML===square[5].innerHTML){
-    state = true;
-    winplayer = square[3].innerHTML;
-  } else {
-    state = false;
+
+  for (let combo of WIN_COMBINATIONS){
+    if (!(square[combo[0]].innerHTML === "") && square[combo[0]].innerHTML === square[combo[1]].innerHTML && square[combo[1]].innerHTML === square[combo[2]].innerHTML){
+      winplayer = square[combo[0]].innerHTML;
+      state = true;
+      break;
+    } else {
+      state = false;
+    }
   }
-  debugger;
+
   if (state === true){
-    if (winplayer === "X") {
-      debugger;
-      setMessage('Player X won!');
+    if (winplayer === 'X') {
+      setMessage('Player X Won!');
     } else{
-      setMessage('Player O won!');
+      setMessage('Player O Won!');
     }
     return true;
   } else {
+    if (turn === 9) {
+      setMessage('Tie game.')
+    }
+    setMessage('');
     return false;
   }
 }
 
+
+
 function doTurn(square){
-  squares = document.querySelectorAll("td");
-  let i;
-  turn += 1;
   debugger;
-  checkWinner();
-  debugger;
+  //checkWinner();
+  //debugger;
   updateState(square);
-  if (checkWinner() === false && turn === 9){
-    setMessage('Tie game.');
-    for (i = 0; i < 10; i++) {
+  turn += 1;
+  checkWinner();
+  if (turn === 9){
+    //debugger;
+    //setMessage('Tie game.');
+    turn = 0;
+    let squares = window.document.querySelectorAll('td');
+    for (let i=0; i < squares.length; i++) {
       squares[i].innerHTML = "";
     }
   }
 }
 
-function attachListeners(){
-  square = document.querySelectorAll("td");
-  //const main = document.getElementById('main');
-  square[0].addEventListener('click', doTurn(square[0]));
-  square[1].addEventListener('click', doTurn(square[1]));
-  square[2].addEventListener('click', doTurn(square[2]));
-  square[3].addEventListener('click', doTurn(square[3]));
-  square[4].addEventListener('click', doTurn(square[4]));
-  square[5].addEventListener('click', doTurn(square[5]));
-  square[6].addEventListener('click', doTurn(square[6]));
-  square[7].addEventListener('click', doTurn(square[7]));
-  square[8].addEventListener('click', doTurn(square[8]));
 
-
+function attachListeners() {
+  $("td").click(function() {
+    if (this.innerHTML === "" && checkWinner() === false ) {
+      doTurn(this)
+    }
+  })
 }
