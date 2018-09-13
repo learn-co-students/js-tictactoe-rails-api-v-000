fdce2f8f49dc99f137fd96f7fae2311f3fa1be13
Victoria Meng <nights1ststar@protonmail.com>
2018-09-13 04:55:03 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -12,24 +12,27 @@ function setMessage(message) {
 };
 
 function checkWinner() {
-  return checkRows("x") || checkRows("y") || checkDiagonals()
+  if (!!checkRows()) {
+    const winner = squares[checkRows()[0]].innerHTML;
+    setMessage("Player " + winner + " Won!")
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function checkRows(axis) {
-  return [0, 1, 2].some(function(row_index) {
-    const row = $("td[data-" + axis + "=" + row_index + "]");
-    const symbols = $.makeArray(row).map(square => square.innerHTML);
-    return threeInRow(symbols) && noneEmpty(symbols)
-  });
-}
-
-function checkDiagonals() {
-  const rows = [[0, 4, 8], [2, 4, 6]];
-  return rows.some(function(row) {
+function checkRows() {
+  const rows = [[0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8]];
+  return rows.find(function(row) {
     const symbols = row.map(s => squares[s].innerHTML);
     return threeInRow(symbols) && noneEmpty(symbols);
   });
-};
+  // return [0, 1, 2].some(function(row_index) {
+  //   const row = $("td[data-" + axis + "=" + row_index + "]");
+  //   const symbols = $.makeArray(row).map(square => square.innerHTML);
+  //   return threeInRow(symbols) && noneEmpty(symbols)
+  // });
+}
 
 function threeInRow(symbols) {
   return [...new Set(symbols)].length == 1
