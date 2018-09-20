49afaad5a27028922776eea2133ef5977c8ef99f
Michele Conway <mconway890@gmail.com>
2018-09-20 15:54:53 -0400

    first commit

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,31 @@
 // Code your JavaScript / jQuery solution here
+$(document).ready(function() {
+  attachListeners()
+})
+
+turn = 0
+
+function player() {
+  return turn % 2 === 0 ? "X" : "O";
+}
+
+
+  function updateState() {
+
+  }
+
+  function setMessage() {
+
+  }
+
+  function checkWinner() {
+
+  }
+
+  function doTurn() {
+    turn += 1
+  }
+
+  function attachListener() {
+    
+  }
