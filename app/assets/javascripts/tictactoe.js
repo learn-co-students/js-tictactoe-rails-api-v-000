e770dd35b043cffc8860861e865aa4f007f0b130
tsyaeger <tsyaeger@gmail.com>
2018-09-21 21:09:06 -0700

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,11 @@
 // Code your JavaScript / jQuery solution here
+
+var turn = 0
+
+function player () {
+	return (turn % 2 == 0) ? 'X' : 'O'
+}
+
+function updateState (square) {
+	square.innerHTML(player())
+}
