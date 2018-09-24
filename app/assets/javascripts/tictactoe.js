c32e62b45bbaef8e2168d81a810fc75e55df3425
tsyaeger <tsyaeger@gmail.com>
2018-09-23 17:27:45 -0700

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,8 +1,17 @@
-// Code your JavaScript / jQuery solution here
+
 
 const WINNING_COMBOS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7], [2,5,8],[0,4,8],[2,4,6]]
 
 var turn = 0
+var currentID = 0
+
+
+
+$(function(){ 
+  attachListeners()
+});
+
+
 
 function player () {
 	return (turn % 2 == 0) ? 'X' : 'O'
@@ -12,16 +21,17 @@ function updateState (square) {
 	$(square).text(player())
 }
 
-
 function setMessage(string) {
 	$("div#message").html(string)
 }
 
+
 function checkWinner() {
-	var board = {}
+	var board = []
 	var won = false
 
-	$('td').text((index,square) => board[index] = square)
+  $('td').text((index,square) => board[index] = square) 
+  // debugger
 
 	WINNING_COMBOS.forEach (function (position) {
 
@@ -34,28 +44,90 @@ function checkWinner() {
 }
 
 
+function resetBoard() {
+	$('td').empty()
+	turn = 0
+}
+
+
+function endGame() {
+	saveGame()
+	resetBoard()
+}
 
 function doTurn(square) {
-	updateState(square)
-	turn++
+  	updateState(square)
+  	turn++
+
 	if (checkWinner()) {
-		$('td').empty()
-		turn = 0
+		endGame()
 	} else if (turn === 9) {
 		setMessage("Tie game.")
+		endGame()
 	}
 }
 
 
+function clearGames() {
+  resetBoard()
+  currentID = null
+}
 
-function attachListeners(square) {
 
-	$('td').on('click', function () {
-		console.log(this)
-		doTurn(square)
-	})
-	
 
+function openGame(id) {
+
+  $.get(`/games/${id}`, function(game) {
+    // debugger
+    currentID = id
+
+    let state = game.data.attributes.state
+    turn = state.filter(String).length
+
+    $("td").html((index, player) => state[index])
+  })  
+}
+
+
+
+
+
+function previousGames() {
+  $("#games").empty()
+
+  $.get("/games", function(games) {
+
+      games.data.forEach(function(game) {
+
+        let gameBtn = `<button id="gameid-${game.id}">Open Game: #${
+            game.id}</button>`
+
+        $("#games").append(gameBtn)
+
+        $(`#gameid-${game.id}`).on("click", () => openGame(game.id))
+      })
+  })
+}
+
+
+
+
+
+
+
+
+function saveGame() {
+  let state = Array.from($("td"), e => e.innerText)
+
+  if (currentID) {
+    $.ajax({url: `/games/${currentID}`, data: { 'state': state }, type: "PATCH"})
+
+  } 
+  else {
+    $.post(`/games`, { 'state': state }, function(game) {
+      currentID = parseInt(game.data.id)
+    })
+  }
 }
 
 
@@ -66,13 +138,15 @@ function attachListeners(square) {
 
 
 
+function attachListeners() {
+  $("#save").on("click", () => saveGame())
+  $("#previous").on("click", () => previousGames())
+  $("#clear").on("click", () => clearGames())
 
 
-
-
-
-
-
-
-
-
+  $("td").on("click", function() {
+    if (!checkWinner() && !$.text(this)) {
+      doTurn(this)
+    }
+  })
+}
