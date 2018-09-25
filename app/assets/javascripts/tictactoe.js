6604c89f1628d282765b58f3905aec576cb877dc
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-09-24 17:18:40 -0700

    Check for winner/draw/invalid move and show message

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -2,23 +2,46 @@
 var turn = 0;
 
 function player() {
-    // var squares = window.document.querySelectorAll('td');
-    // let count = 0;
-    // squares.forEach ( function (square) {
-    //      if (square.innerHTML != "")  ++count;
-    // });
-    // console.log("player count",count);
-    // return count & 1 ? 'O' : 'X';
     return turn & 1 ? 'O' : 'X';
 } 
+
+function setMessage (message) {
+    $('#message').append(message);
+}
+
+function checkWinner () {
+    var squaresDOM = window.document.querySelectorAll('td');
+    var squares = $.makeArray(squaresDOM).map ( square => square.innerText);
+    console.log("Squares",squares);
+    const winners = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7],  [2,5,8], [0,4,8], [2,4,6]];
+
+    winner = winners.find ( function (combo) {       
+        return squares[combo[0]]!='' && squares[combo[0]]==squares[combo[1]] &&
+            squares[combo[0]]==squares[combo[2]]      
+    });
+    if (winner !== undefined) {
+        let winnerMark = squares[winner[0]];
+        setMessage(`Player ${winnerMark} Won!`);
+    }
+
+    return (winner === undefined) ? false : true;
+}
 
 function doTurn (square) {
  
     let x = parseInt(square.dataset.x) + 1;
     let y = parseInt(square.dataset.y) + 1;
     console.log("doTurn",x,y,player());
-  $(`tr:nth-of-type(${y}) td:nth-of-type(${x})`).text(player());
-    ++turn;
+    let currentSquare = $(`tr:nth-of-type(${y}) td:nth-of-type(${x})`).text();
+    if (currentSquare !== '') {
+        setMessage("Invalid move. Square already taken.  Choose again");
+    }
+    else {
+        $(`tr:nth-of-type(${y}) td:nth-of-type(${x})`).text(player());
+        ++turn;
+        if (!checkWinner() && turn>8)
+            setMessage("Draw!");
+    }
 }
 
 $(function () {
