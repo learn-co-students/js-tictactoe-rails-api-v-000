9824d6b1faafd89cd999f99a03e292e495dcced3
Chris Alberti <christopherjalberti@gmail.com>
2018-10-04 22:58:40 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,6 @@
 // Code your JavaScript / jQuery solution here
-var turn = 0
+var turn = 0;
+var id = 0;
 
 function player() {
   return turn % 2 === 0 ? "X" : "O";
@@ -11,6 +12,28 @@ function updateState(square) {
 
 function setMessage(string) {
   document.getElementById("message").innerHTML = string;
+}
+
+function saveGame() {
+  const board = [];
+  document.querySelectorAll("td").forEach(function(square) {
+    board.push(square.innerHTML);
+  });
+
+  if (id > 0) {
+    $.ajax({
+      url : `/games/${id}`,
+      data : {state: board},
+      type : 'PATCH',
+    });
+  } else {
+    $.post({
+      url : "/games",
+      data : {state: board},
+    }).done(function(game) {
+      id = game.data.id;
+    });
+  }
 }
 
 function checkWinner() {
@@ -36,15 +59,82 @@ function checkWinner() {
 
     if (checkX) {
       setMessage("Player X Won!");
-      winner = true;
+      return winner = true;
     } else if (checkO) {
       setMessage("Player O Won!");
-      winner = true;
+      return winner = true;
     }
   });
   return winner;
 }
 
-function doTurn() {
-  
+function resetGame() {
+  document.querySelectorAll("td").forEach(function(square) {
+    square.innerHTML = "";
+  });
+  turn = 0;
+  id = 0;
 }
+
+function doTurn(square) {
+  if (square.innerHTML === "") {
+    updateState(square);
+    turn += 1;
+  }
+
+  if (checkWinner()) {
+    saveGame();
+    resetGame();
+  } else if (turn >= 9) {
+    setMessage("Tie game.");
+    saveGame();
+    resetGame();
+  }
+}
+
+function attachListeners() {
+  document.querySelectorAll("td").forEach(function(square) {
+    square.addEventListener("click", function() {
+      if (!$.text(this) && !checkWinner()) {
+        doTurn(this);
+      }
+    });
+  });
+
+  document.getElementById('save').addEventListener("click", function() {
+    saveGame();
+  });
+
+  document.getElementById('previous').addEventListener("click", function() {
+    $.getJSON("/games", function(games) {
+      if (games.data.length > 0) {
+        $("#games").empty();
+        games.data.forEach(function(game) {
+          let $button = `<button id="gameid-${game.id}">${game.id}</button><br>`
+          $("#games").append($button);
+          document.getElementById(`gameid-${game.id}`).addEventListener("click", function() {
+            document.getElementById('message').innerHTML = '';
+            $.getJSON(`/games/${game.id}`, function(game) {
+              let index = 0;
+              document.querySelectorAll("td").forEach(function(square) {
+                square.innerHTML = game.data.attributes.state[index];
+                index += 1;
+              });
+              id = game.data.id;
+              turn = game.data.attributes.state.join("").length;
+            })
+          })
+        });
+      }
+    });
+  });
+
+  document.getElementById('clear').addEventListener("click", function() {
+    id = 0;
+    resetGame();
+  });
+}
+
+$(function() {
+  attachListeners();
+})
