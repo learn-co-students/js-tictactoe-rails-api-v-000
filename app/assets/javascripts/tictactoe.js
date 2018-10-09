a91fd4ee3d21a3bde6168fb15ec672c9361df5d6
Suchitra Naik <susanaik@gmail.com>
2018-10-09 13:19:51 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,13 @@
 // Code your JavaScript / jQuery solution here
+const WIN_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
+                    [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
 var turn = 0;
+
+$(document).ready(function() {
+    attachListeners();
+});
+
 var player = () => turn % 2 ? 'O' : 'X'
 
 
@@ -19,6 +27,21 @@ function doTurn(){
 
 }
 
+function resetBoard(){
+
+}
+
+function saveGame(){
+
+}
+
+function showPreviousGames(){
+
+}
+
 function attachListeners(){
 
+    $('#save').on('click', () => saveGame());
+   $('#previous').on('click', () => showPreviousGames());
+   $('#clear').on('click', () => resetBoard());
 }
