235e6f90cb16a0d1f0b4e65aa3a47ac343b94550
github username <github email address>
2018-10-10 20:20:18 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,14 +1,118 @@
 // Code your JavaScript / jQuery solution here
-document.addEventListener("DOMContentLoaded", function() {
-  let save = document.querySelector('button#save')
-  let previous = document.querySelector('button#previous')
-  let clear = document.querySelector('button#clear')
-
-  save.addEventListener('click', function(e) {
-    e.preventDefault();
-
-  })
+document.addEventListener("DOMContentLoaded", function(){
+  attachListeners()
 })
+
+function squares() {
+  return document.querySelectorAll('td')
+}
+
+function board() {
+  let state = ["", "", "", "", "", "", "", "", ""];
+  squares().forEach(function(td, i) {
+    state[i] = td.innerText
+  })
+  return state
+}
+
+function turn() {
+  return board().filter(p => p !== "").length
+}
+
+function player() {
+  if(turn() % 2 === 0) {
+    return "X"
+  } else {
+    return "O"
+  }
+}
+
+function updateState(position) {
+  if(position.innerText !== "") {
+    alert("Position taken, choose another one")
+  } else {
+    position.innerText = player()
+  }
+}
+
+function setMessage(string) {
+  document.getElementById('message').innerHTML = string
+}
+
+function checkWinner() {
+  let result = WIN_COMBO.some(function (w) {
+    return board()[w[0]] === board()[w[1]] &&
+      board()[w[1]] === board[w[2]] &&
+      board()[w[2]] !== ""})
+  if(result) {
+    setMessage('Player' + player() + "Won!")
+  }
+  return result
+}
+
+function draw() {
+  return !board().some(el => el !== "") && !checkWinner()
+}
+
+function reset() {
+  for(const td of squares()) {
+    td.innerHTML = ""
+  }
+}
+
+function doTurn(position) {
+  updateState(position);
+  if(checkWinner()) {
+    reset()
+  } else if (draw()) {
+    setMessage('Tie game.')
+    reset()
+  }
+}
+
+function attachListeners() {
+  squares().forEach(function(td) {
+    debugger
+    td.addEventListener("click", doTurn(td))
+  })
+}
+
+function saveGame() {
+  document.getElementById("save").addEventListener("click", function(e) {
+    e.preventDefault();
+    const gameID = document.querySelector('table').dataset.gameID
+    gameData = JSON.stringify({state: board()})
+    if(gameID === "") {
+      const reqData = {method: "POST", body: gameData}
+      fetch("/games", reqData).then(function(resp) {
+        gameID = resp.id
+      })
+    } else {
+      const reqData = {method: "PATCH", body: gameData}
+      fetch(`/games/${gameID}`, reqData)
+    }
+  })
+}
+
+function previousGame() {
+  document.getElementById("previous").addEventListener("click", function(e) {
+    e.preventDefault();
+    var gamesDiv = document.getElementById("games")
+    if(gamesDiv.children.length === 0) {
+      fetch("/games").then(function(games) {
+        if(games.data.length !== 0) {
+          games.data.forEach(function(game) {
+            gamesDiv.innerHTML +=
+            `<button data-id = "${game.id}">Game ${game.id}</button>`
+          })
+        }
+      })
+    } else {
+      var count = document.querySelectorAll("div#games button").length
+
+    }
+  })
+}
 
 const WIN_COMBO = [
     [0,1,2],
@@ -32,68 +136,3 @@ const POSITIONS = [
   [1, 2],
   [2, 2]
 ]
-
-function board() {
-  let state = [];
-  let tds = document.querySelectorAll('td')
-  for (let td of tds) {
-    state.append(td.innerText)
-  }
-  return state
-}
-
-function turn() {
-  return board().filter(p => p !== "").length
-}
-
-function player() {
-  if(turn() % 2 === 0) {
-    return "X"
-  } else {
-    return "O"
-  }
-}
-
-function updateState(position) {
-  position.innerHTML = player()
-}
-
-function setMessage(string) {
-  document.getElementById('message').innerHTML = string
-}
-
-function checkWinner() {
-  const board = board()
-  let result = WIN_COMBO.some(function () {
-    return board[w[0]] === board[w[1]] &&
-      board[w[1]] === board[w[2]] &&
-      board[w[2]] !== ""})
-  if(result) {
-    setMessage('Player' + player() + "Won!")
-  }
-  return result
-}
-
-function draw() {
-  return !board().some(el => el !== "") && !checkWinner()
-}
-
-function reset() {
-  for(const td of document.querySelectorAll('td')) {
-    td.innerHTML = ""
-  }
-}
-
-function doTurn(position) {
-  updateState(position);
-  if(checkWinner()) {
-    reset()
-  } else if (draw()) {
-    setMessage('Tie game.')
-    reset()
-  }
-}
-
-function attachListeners() {
-
-}
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Game.create(state: ['O', 'X', 'O', '', 'X', 'X', '', 'X', 'O'])
