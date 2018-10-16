7934ffa3b528c76bbed53c838c24667aa20df788
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-10-16 10:41:45 -0400

    check winner

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,8 @@
 // Code your JavaScript / jQuery solution here
 var turn = 0
 
+const winCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,4,8], [2,4,6], [0,3,6], [2,5,8], [1,4,7]]
+
 function player() {
   if (turn % 2) {
     return "O"
@@ -16,4 +18,19 @@ function updateState(box) {
 
 function setMessage(string) {
   $("#message").text(string);
+}
+
+function checkWinner() {
+  var board = {}
+  var winner = false
+
+  $('td').text(function(index, box) { board[index] = box});
+
+  winCombinations.some(function (winArray) {
+    if (board[winArray[0]] !== "" && board[winArray[0]] === board[winArray[1]] && board[winArray[1]] === board[winArray[2]]) {
+      setMessage(`Player ${board[winArray[0]]} Won!`)
+      winner = true
+    }
+  })
+  return winner
 }
