4fc2b328d15c71bbaf053df28655ce7130bfdb34
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-10-16 09:58:46 -0400

    player()

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,17 @@
 // Code your JavaScript / jQuery solution here
+var turn = 0
+
+function player() {
+  if (turn % 2 == 0) {
+    return "X"
+  } else {
+    return "O"
+  }
+}
+
+function updateState() {
+  var turn = player()
+  $("td").on('click', function() {
+    $(this).text(turn);
+  })
+}
