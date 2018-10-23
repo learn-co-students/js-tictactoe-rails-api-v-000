d99e13b4922d733f6ffa14995e2ae8a5bd2b73ef
github username <github email address>
2018-10-23 19:57:47 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,23 @@
 // Code your JavaScript / jQuery solution here
+
+$(document).ready(function() {
+	attachListeners();
+});
+
+const winCombos = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
+
+let turn = 0;
+let gameCount = 0;
+
+function player() {
+  return (turn % 2 ? "O" : "X")
+}
