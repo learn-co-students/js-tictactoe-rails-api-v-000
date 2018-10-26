ac0e61dc739b633508fb2ecd5789710885b25982
github username <github email address>
2018-10-26 12:34:23 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,5 @@
 // Code your JavaScript / jQuery solution here
-const WINNING_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+var WINNING_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
 var turn = 0;
 var gameCount = 0;
@@ -10,6 +10,19 @@ $(document).ready(function(){
   attachListeners();
 });
 
+
+function attachListeners(){
+  $('td').on('click', function(){
+    if (!$.text(this) && !checkWinner()){
+    doTurn(this)
+    }
+  })
+  $('#save').on('click', ()=> saveGame())
+  $('#previous').on('click', ()=> previousGames())
+  $('#reset').on('click', ()=> resetGame())
+}
+
+
 function player(){
   return turn % 2 === 0 ? 'X' : 'O'
 }
@@ -18,12 +31,45 @@ function updateState(square){
   (square).append(player())
 }
 
+
+function setMessage(str){
+  $('#message').append(str)
+}
+
+
+function checkWinner(){
+	var winner = false;
+  var board = {}
+
+  $('td').text((index, square) => board[index] = square);
+
+  WINNING_COMBINATIONS.some(function(position){
+    if (board[position[0]] == board[position[1]] && board[position[2]] == board[position[1]] && board[position[1]] !== "") {
+      saveGame()
+      setMessage(`Player ${board[position[0]]} Won!`);
+      return winner = true;
+      resetBoard()
+    }
+    else if (!winner && turn === 9){
+      setMessage('Tie game.')
+      saveGame()
+      resetBoard()
+    }
+  })
+  return winner
+}
+
+
+
 function doTurn(square){
   updateState(square)
   turn++
   checkWinner()
 }
 
-function setMessage(str){
-  $('#message').append(str)
+
+function resetBoard() {
+  $('td').empty();
+  turn = 0;
+  gameCount = 0;
 }
