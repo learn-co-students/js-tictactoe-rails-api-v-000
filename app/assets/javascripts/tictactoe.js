906a68a56d5dc760bd46dbd63fd42bd130a975b5
github username <github email address>
2018-10-26 12:08:36 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,5 @@
 // Code your JavaScript / jQuery solution here
-var WINNING_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+const WINNING_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
 var turn = 0;
 var gameCount = 0;
