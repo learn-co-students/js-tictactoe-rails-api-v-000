15946bf102ed2d4ca5b0a49c96d1cd945c2e4ca8
github username <github email address>
2018-10-29 15:05:11 -0600

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,6 @@
 let turn = 0
 let currentGame = 0
-const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
+var WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
                         [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
 
@@ -81,6 +81,7 @@ function checkWinner(){
   $('td').text((index, square) => board[index] = square)
 
   WINNING_COMBOS.some(function(combo){
+    console.log(combo)
     if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]){
       setMessage(`Player ${board[combo[0]]} Won!`)
       return winner = true
