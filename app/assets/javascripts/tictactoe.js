b6a097e5e83fcbcbde25695eb20a65edacb96256
enciie <enciie@gmail.com>
2018-11-11 00:27:21 -0800

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -12,18 +12,23 @@ var WIN_COMBOS = [
      [0,4,8],
      [2,4,6]
   ]
+
+// returns a string for player X or O
 function player() {
   return turn % 2 ? "O" : "X"
 }
 
+//update square(the td element) on game board
 function updateState(square) {
   square.innerHTML = player();
 }
 
+//adds a message 'Player X Won!' or 'Player O Won!'
 function setMessage(message) {
   $("#message").text(message);
 }
 
+//use WIN_COMBOS to check for winners
 function checkWinner(){
   let winner = false;
   let board = {}
@@ -40,6 +45,7 @@ function checkWinner(){
   return winner;
 }
 
+// game turn
 function doTurn(square) {
   updateState(square);
   turn++;
@@ -54,45 +60,36 @@ function doTurn(square) {
   }
 }
 
+//clears the game board
 function resetBoard(){
   $('td').empty()
   turn = 0;
   currentGame = 0;
 }
 
-function attachListeners(){
-  $("td").on("click", function(){
-    if (!$.text(this) && !checkWinner()){
-      doTurn(this);
-    }
-  })
-  $('#save').on('click', () => saveGame());
-  $('#previous').on('click', () => showPreviousGames());
-  $('#clear').on('click', () => resetBoard());
-}
-
+//saves a game
 function saveGame(){
   let state = Array.from($('td'), e => e.innerText);
-  if (currentGame) {
+  if (currentGame) { //if currentGame exists then "update"
     $.ajax({
       type: 'PATCH',
       url: `/games/${currentGame}`,
       dataType: 'json',
       data: { state: state }
     });
-  } else {
+  } else { // when currentGame is 0, create a new game board
     $.post('/games', { state: state }, function(game) {
-      currentGame = parseInt(game.data.id);
+      currentGame = parseInt(game.data.id); //set the game id to currentGame
     });
   }
 }
 
 function loadGame(gameId) {
   $('#message').text("");
-  let id = gameId
-  $.get(`/games/${gameId}`, function(game) {
-    let state = game.data.attributes.state;
-    $("td").text((index, square) => state[index]);
+  let id = gameId;
+  $.get(`/games/${gameId}`, function(game) { //show game with GET games/:id
+    let state = game.data.attributes.state; //sets the state to the game array ["", "", "X", "", "", "", "", "O", ""]
+    $("td").text((index, token) => state[index]);
     currentGame = id;
     turn = state.join("").length;
     checkWinner();
@@ -102,12 +99,22 @@ function loadGame(gameId) {
 function showPreviousGames() {
   $("#games").empty();
   $.get("/games", function(games){
-    // debugger
     games.data.map(function(game){
       $("#games").append(`<button id="gameid-${game.id}">${game.id}</button>`);
       $("#gameid-" + game.id).click(() => loadGame(game.id));
     })
   })
+}
+
+function attachListeners(){
+  $("td").on("click", function(){
+    if (!$.text(this) && !checkWinner()){ // this is the td element, if it's empty && not a winner
+      doTurn(this);
+    }
+  })
+  $('#save').on('click', () => saveGame());
+  $('#previous').on('click', () => showPreviousGames());
+  $('#clear').on('click', () => resetBoard());
 }
 
 $(document).ready(function(){
