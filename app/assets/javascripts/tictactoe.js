4bd9c57d444cc713aff5f12028efe4a6e30ddf09
Sara Stanton <sara.mpa13@gmail.com>
2018-11-11 04:24:23 -0600

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,45 @@
 // Code your JavaScript / jQuery solution here
+
+// button#save
+// button#previous
+// button#clear
+
+var turn = 0;
+let winner;
+
+function player() {
+  return turn % 2 === 0 ? "X" : "O";
+}
+
+
+function updateState(space) {
+  $(space).text(player())
+}
+
+
+function setMessage(winner) {
+  $("#message").text(`Player ${winner} Won!`)
+}
+
+
+function checkWinner() {
+  // if() {
+  //   setMessage(winner);
+  //   return true
+  // }; else {
+  //   return false
+  // };
+}
+
+
+function doTurn() {
+  // ++turn;
+  updateState();
+  // need to pass in clicked element above
+  checkWinner();
+}
+
+
+function attachListeners() {
+
+}
