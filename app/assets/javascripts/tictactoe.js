6908bbbc8752bd11d5cc5312f8da43f93801590c
Katrina Brinson <kbrinso@gmail.com>
2018-11-13 21:55:46 +0000

    restarting ide broken

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,22 @@
 // Code your JavaScript / jQuery solution here
+const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
+                        [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
+//X , Y
+// const LABELS = {[0,0]: 1, [1,0]: 2, [2, 0]: 3, [0,1]: 4, [1,1]: 5, [2,1]: 6,
+//                 [0,2]: 7, [1,2]: 8, [2,2]: 9};
+var turn = 0;
+var currentGame = 0;
+
+function player(){
+  if (turn % 2 == 0){
+    return 'X';
+  }else{
+    return 'O';
+  };
+};
+
+
+function updateState(){
+  var move = player();
+}
