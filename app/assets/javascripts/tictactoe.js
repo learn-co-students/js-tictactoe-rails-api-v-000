0a4d31e5cd9d0479106cab535d0c2d3b7831253c
MFeuer23 <mallory.feuer@gmail.com>
2018-11-14 22:47:44 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,15 +1,31 @@
 // Code your JavaScript / jQuery solution here
+var turn = 0;
+
 function player() {
-  if ($("td:empty").length % 2 === 1) {
-    return 'X';
-  } else {
+  if (window.turn % 2 === 1) {
     return 'O';
+  } else {
+    return 'X';
   }
 }
 
-function updateState() {
-  let token = player()
-  $("td").click(function(){
-    this.innerHTML = token;
-  });
+function updateState(square) {
+  square.innerHTML = player();
+  turn += 1
+}
+
+function setMessage(message) {
+  $("#message").html(message);
+}
+
+function checkWinner() {
+  const squares = window.document.querySelectorAll('td');
+  if (squares[0].innerHTML === squares[1].innerHTML === squares[2].innerHTML ||
+    squares[3].innerHTML === squares[4].innerHTML === squares[5].innerHTML ||
+    squares[6].innerHTML === squares[7].innerHTML === squares[8].innerHTML) {
+    return "true"
+  } else {
+    return "non horizontal"
+  }
+  squares[1].innerHTML
 }
