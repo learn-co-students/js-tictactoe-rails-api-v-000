2b4c441bc6ffd38104a5516c6358d7b2d56e4d0a
hkleindl <hkleindl@gmail.com>
2018-11-20 13:09:52 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -15,9 +15,17 @@ function setMessage(str) {
 }
 
 function checkWinner() {
-  const winCombos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+  const winCombos = [
+                    [0,1,2], 
+                    [3,4,5], 
+                    [6,7,8], 
+                    [0,3,6], 
+                    [1,4,7], 
+                    [2,5,8], 
+                    [0,4,8], 
+                    [2,4,6]
+                  ]
 
-  // debugger;
   var board = {};
   var winner = false;
   $('td').text(function(i, square) {
@@ -27,31 +35,34 @@ function checkWinner() {
 
     if (board[combo[0]] != "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
       setMessage(`Player ${board[combo[0]]} Won!`);
-      return winner = true;
-      // debugger
-      
+      return winner = true;      
     }
   })
-
-  // for (let i = 0; i < Object.keys(winCombos).length; i++) {
-  //   debugger
-  //   Object.values(winCombos)[i].every(function(e, i, winCombos){
-  //     if (e.textContent === "X") {
-  //       winner = true;
-  //     } else if (e.textContent === "O"){
-  //       winner = true;
-  //     } else {
-  //       winner = false;
-  //     }
-  //   })
-  // }
   return winner;
 }
 
-  function firstRow() {
-    squares[0].innerText = "X"
-    squares[1].innerText = "X"
-    squares[2].innerText = "X"
+ function doTurn(square) {
+  updateState(square);
+  turn++
+  if (checkWinner()) {
+    checkWinner();
+    $('td').empty();
+    turn = 0;
+  } else if (turn === 9) {
+    setMessage('Tie game.')
   }
+ } 
 
+ function attachListeners() {
+   if (!checkWinner() && turn !== 9) {
+    $('td').click(function() {
+      if (this.innerHTML === "") { 
+        doTurn(this);
+      }
+    })
+  }
+ }
 
+ $(document).ready(function() {
+   attachListeners();
+ });
