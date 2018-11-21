3bc4a8b6069a430dbc7ad84416f0fbf346da4878
Shirley Collins <shirley@m104.us>
2018-11-20 16:35:18 -0800

    adds player, updateState, setMessage, checkWinner functions

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,47 @@
 // Code your JavaScript / jQuery solution here
+var turn = 0;
+var currentGame = 0;
+
+function player() {
+  return turn % 2 === 0 ? 'X' : 'O'
+}
+
+function updateState(square) {
+  $(square).text(player())
+}
+
+
+function setMessage(str){
+  $('div#message').text(str)
+}
+
+
+function checkWinner(){
+  var winner = false;
+  var board = {};
+  const WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ]
+
+  $('td').text(function(index, str) {
+    board[index] = str
+  });
+
+  WIN_COMBINATIONS.forEach(function(combo) {
+    if (board[combo[0]] !== '') {
+      if (board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) {
+        setMessage(`Player ${board[combo[0]]} Won!`);
+        return winner = true;
+      }
+    }
+  });
+
+  return winner;
+};
