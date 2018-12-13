6a2377d2542bd14d9d248a87013d6372d3b9719c
bkhartmire <bkhartmire@gmail.com>
2018-12-12 20:33:40 -0800

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,12 @@
 // Code your JavaScript / jQuery solution here
+var turn = 0
+
+function player() {
+  //let turns = 0
+  //game.state.forEach(function(space) {
+  //  if (space.includes("X") || space.includes("O")) {
+  //    turns += 1
+  //  }
+  //})
+  return (turn % 2 === 0) ? "X" : "O"
+}
