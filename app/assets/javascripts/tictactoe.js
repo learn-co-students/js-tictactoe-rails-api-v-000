b7ae65bdcf8424d771a3295196a3b34ca1e278a4
bkhartmire <bkhartmire@gmail.com>
2018-12-12 23:47:12 -0800

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,6 @@
 // Code your JavaScript / jQuery solution here
 var turn = 0
+var currentGame = 0
 
 const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
                         [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
@@ -34,17 +35,18 @@ function doTurn(square) {
   updateState(square)
   turn++
   if (checkWinner()) {
-    reset()
     saveGame()
+    reset()
   } else if (turn === 9) {
     setMessage("Tie game.")
-    reset()
     saveGame()
+    reset()
   }
 }
 
 function reset() {
   turn = 0
+  currentGame = 0
   $('td').empty()
 }
 
@@ -64,8 +66,23 @@ function attachListeners() {
 }
 
 function saveGame() {
-  
-}
+  var gameStatus = []
+  $("td").text((index, square) => gameStatus.push(square))
+  var gameData = { state: gameStatus }
+  if (currentGame) {
+    $.ajax({
+      method: "PATCH",
+      url: `/games/${currentGame}`,
+      data: gameData
+    });
+  } else {
+    $.post("/games", gameData, function(game) {
+        currentGame = game.data.id;
+        $("#games").append(`<button id="button-${game.data.id}">${game.data.id}</button>`)
+        $(`#button-${game.data.id}`).on('click', () => reloadGame(game.data.id))
+      })
+    }
+  }
 
 function listPreviousGames() {
   $('#games').empty();
@@ -73,12 +90,23 @@ function listPreviousGames() {
      if (allGames.data.length > 0) {
        allGames.data.forEach(function(game) {
          $('#games').append(`<button id="gameid-${game.id}">${game.id}</button><br>`)
-         $(`#gameid-${game.id}`).on('click', () => reload(game.id))
+         $(`#gameid-${game.id}`).on('click', () => reloadGame(game.id))
        });
      }
    });
 }
 
 function reloadGame(gameId) {
-  //complete this
+  $.get(`/games/${gameId}`, function(response){
+    currentGame = response.data.id
+    gameArray = response.data.attributes.state
+    turn = gameArray.join("").length
+    var index = 0
+    for (var y = 0; y < 3; y ++) {
+      for (var x = 0; x < 3; x ++) {
+        document.querySelector(`[data-x="${x}"][data-y="${y}"]`).innerHTML = gameArray[index];
+        index += 1;
+      }
+    }
+  })
 }
