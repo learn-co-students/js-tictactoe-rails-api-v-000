4d92536323ea3a28e4f4fa8f1c8585a490e9eb11
github username <github email address>
2018-12-28 19:42:36 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,4 +1,8 @@
 // Code your JavaScript / jQuery solution here
+$(document).ready(function() {
+    attachListeners();
+  })
+
 const WINNING_COMBOS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
 var turn = 0;
@@ -44,6 +48,14 @@ function doTurn(square) {
     $(`td`).empty()
     turn = 0;
   } else if (turn === 9){
-    setMessage("Tie game.")
+    setMessage(`Tie game.`)
     }
+  }
+
+  function attachListeners() {
+    $(`td`).on(`click`, function() {
+      if (this.innerHTML === `` || ` ` === this.innerHTML){
+          doTurn(this).innerHTML;
+      };
+    });
   }
