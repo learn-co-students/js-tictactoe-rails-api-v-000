b75fb5b2c7b336fb0c020a308c3af8674db7e3b8
github username <github email address>
2018-12-29 11:37:06 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -59,3 +59,21 @@ function doTurn(square) {
       };
     });
   }
+
+
+  function saveGame () {
+   $('#save').submit(function(event) {
+     //prevent form from submitting the default way
+     event.preventDefault();
+
+     var values = $(this).serialize();
+
+     var posting = $.post('/games', values);
+
+     posting.done(function(data) {
+       var post = data;
+       $("#postTitle").text(post["title"]);
+       $("#postBody").text(post["description"]);
+     });
+   });
+ });
