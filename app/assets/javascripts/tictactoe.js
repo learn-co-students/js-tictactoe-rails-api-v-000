46da8306b082c5d720259d30877b152da19aeebe
amckean12 <amckean@me.com>
2019-01-08 15:32:46 -0600

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,14 +1,40 @@
 // Code your JavaScript / jQuery solution here
+var turn = 0;
+
+//Possible Winning Outcomes (This is a constant)
+const WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8,], //Horizontal Wins
+[0,3,6], [1,4,7], [2,5,8], //Vertical Wins
+[0,4,8], [6,4,2]] //Diagonal Wins
+
 function player(){
+  if(turn % 2 === 0){
+    return 'X';
+  }
+  else{
+    return 'O';
+  }
 }
 
-function updateState(){
-
+function updateState(square){
+  //square ==> <td data-x="0" data-y="1"></td>
+  //Setting a currentPlayer varaiable that will call the player function
+  var currentPlayer = player();
+  //Search for square and put the innerHTML or text as currentPlayer
+  $(square).text(currentPlayer);
 }
 
-function setMessage(){}
+function setMessage(message){
+  $('#message').text(message);
+}
 
-function checkWinner(){}
+function checkWinner(){
+  var winner = false;
+  //Board will start as an empty object
+  var board = {};
+  //Goal is to populate the board object with the index and the square
+  //square ==> <td data-x="0" data-y="1"></td>
+  $('td').text((index, square)=> board[index]= square);
+}
 
 function doTurn(){}
 
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
+  #Every game will have a unique id and a state
   serialize :state, Array
 end
