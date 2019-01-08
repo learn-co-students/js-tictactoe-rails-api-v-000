b1006068c84f59e7865ffd7476e8425c74bb4cd7
Jayon Kim <jpkim921@gmail.com>
2019-01-08 19:54:23 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,12 @@
 // Code your JavaScript / jQuery solution here
+
+var turn;
+
+function player() {
+  return turn % 2 ? "O" : "X";
+}
+
+function updateState(clickedSquare) {
+  let token = player();
+  $(clickedSquare).text(token);
+}
