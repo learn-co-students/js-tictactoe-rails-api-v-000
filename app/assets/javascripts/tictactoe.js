56cbc0f28314b2881b1412b225ac09d84955a7f0
Jayon Kim <jpkim921@gmail.com>
2019-01-09 08:57:27 -0500

    working on doTurn()

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,6 @@
 // Code your JavaScript / jQuery solution here
 
-var turn;
+var turn = 0;
 
 function player() {
   return turn % 2 ? "O" : "X";
@@ -16,8 +16,7 @@ function setMessage(string) {
 }
 
 
-var winningCombos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
-                        [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+var winningCombos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
 function checkWinner() {
 
@@ -57,21 +56,13 @@ function checkWinner() {
   return winner;
 }
 
+function doTurn(clickedSquare) {
+  turn++
+  updateState(clickedSquare);
+  checkWinner();
 
-// def won?(board) ----> from the ruby TICTACTOE
-
-
-//   WIN_COMBINATIONS.each do |win_combo|
-//       position1 = board[win_combo[0]]
-//       position2 = board[win_combo[1]]
-//       position3 = board[win_combo[2]]
-//       comparison_set = [position1, position2, position3]
-
-//       if comparison_set == ["X","X","X"] || comparison_set == ["O","O","O"]
-//           return win_combo
-//       else
-//         next
-//       end
-//     end
-//   false
-// end
+  if (turn > 8){
+    setMessage("Tie game.")
+    // debugger
+  }
+}
