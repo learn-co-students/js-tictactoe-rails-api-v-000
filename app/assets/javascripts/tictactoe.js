9452bbb80d74ff0cbeb05c57740a30f7061fe8ed
roitblatari <roitblatari@gmail.com>
2019-01-13 19:32:59 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,39 @@
 // Code your JavaScript / jQuery solution here
+
+const WIN_COMBINATIONS = [
+  [0, 1, 2], 
+  [3, 4, 5], 
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+var turn = 0
+// if turn modulus 2 = 0, that means it will be falsey, and therefore returns the right side (X), and any odd number % 2 = 1, so it would be truthy, and then return the left side (O)
+var player = () => turn % 2 == 0 ? "X" : "O" 
+
+
+
+var updateState = (square) => {
+  var token = player();
+  $(square).text(token);
+}
+
+function setMessage(string) {
+  $("#message").html('Player X Won!')
+  // string = 'Player X Won!'
+  // $("#message").innerHTML = string
+}
+
+function checkWinner(params) {
+  // let td = $("td").map(() => { this.innerHTML });
+  // WIN_COMBINATIONS.find(function (combo ) {
+
+  //   td[combo[0]] == td[combo[1]] &&
+  //     td[combo[1]] == td[combo[2]] &&
+  //     td[combo[0]] != " "
+  // }) 
+}
