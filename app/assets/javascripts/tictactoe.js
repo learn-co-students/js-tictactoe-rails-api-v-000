b01a3485ec2913831785554bcc6a8a69d66afb8e
ElleeB <laura.trager@gmail.com>
2019-01-22 10:43:21 -0500

    Fix winning combos

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,11 +1,10 @@
 // Game play functions //
 var turn = 0
-var winningCombos = [
-  [0, 1, 2], [4, 3, 5], [6, 7, 8],
-  [0, 4, 6], [1, 3, 7], [2, 5, 8],
-  [0, 5, 8], [3, 5, 6]
+const WINNINGCOMBOS = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8],
+  [0, 3, 6], [1, 4, 7], [2, 5, 8],
+  [0, 4, 8], [2, 4, 6]
 ]
-
 
 function player() {
   // Returns 'X' or 'O' if turn is even or odd
@@ -27,12 +26,22 @@ function setMessage() {
 }
 
 function checkWinner() {
-  var board = {};
+  var board = {}
   var winner = false
   // .text(function): fx return text content to set (for each element)
   // Receives the index position of the element in the set and the old text value as arguments.
   $('td').text(function(index, token){
     board[index] = token
+  })
+  // check board for winning combos
+  // .some() tests whether at least one element in the array passes the test implemented by the provided function.
+  WINNINGCOMBOS.forEach( function(combo) {
+    // combo values define the board's indices to check for winner
+    if (board[combo[0]] != "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]] && board[combo[0]] === board[combo[2]]) {
+      console.log("WIN!")
+    } else {
+      console.log("LOSE!")
+    }
   })
 }
 
@@ -51,11 +60,13 @@ $(function attachListeners() {
   // Attaches the appropriate event listeners to:
   $('td').on('click', function() {
     // If no text & no winner
-    if (!$.text(this) && !checkWinner()) {
+    if (!$.text(this) ) {
       // Make a play
       doTurn(this)
     }
   })
+
+  // && !checkWinner()
 
   $('#save').on('click', () => saveGame())
   $('#previous').on('click', () => showPreviousGames())
