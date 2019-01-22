29c285801da953e0bfdfa1b336d32eaf0ee2280e
ElleeB <laura.trager@gmail.com>
2019-01-22 09:34:39 -0500

    Begin checkWinner() code

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,11 @@
 // Game play functions //
 var turn = 0
+var winningCombos = [
+  [0, 1, 2], [4, 3, 5], [6, 7, 8],
+  [0, 4, 6], [1, 3, 7], [2, 5, 8],
+  [0, 5, 8], [3, 5, 6]
+]
+
 
 function player() {
   // Returns 'X' or 'O' if turn is even or odd
@@ -13,7 +19,7 @@ function player() {
 function updateState(element) {
   var cell = element
   // adds returned string ('X' or 'O') to clicked square on board
-  cell.append(player())
+  cell.textContent = player()
 }
 
 function setMessage() {
@@ -21,7 +27,13 @@ function setMessage() {
 }
 
 function checkWinner() {
-
+  var board = {};
+  var winner = false
+  // .text(function): fx return text content to set (for each element)
+  // Receives the index position of the element in the set and the old text value as arguments.
+  $('td').text(function(index, token){
+    board[index] = token
+  })
 }
 
 function doTurn(element) {
@@ -38,7 +50,6 @@ function doTurn(element) {
 $(function attachListeners() {
   // Attaches the appropriate event listeners to:
   $('td').on('click', function() {
-    debugger
     // If no text & no winner
     if (!$.text(this) && !checkWinner()) {
       // Make a play
@@ -49,5 +60,4 @@ $(function attachListeners() {
   $('#save').on('click', () => saveGame())
   $('#previous').on('click', () => showPreviousGames())
   $('#clear').on('click', () => resetBoard())
-
 })
