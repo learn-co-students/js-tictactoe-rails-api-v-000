393aad9ce7e2dce68b1247c448872744d30b50b1
Aellon Krider <aellonkrider@gmail.com>
2019-01-28 16:11:30 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,5 @@
 var turn = 0;
-
+var currentGame = 0;
 
 $(document).ready(function(){
 	attachListeners();
@@ -48,6 +48,7 @@ function checkWinner() {
 		if (board[position[0]] === board[position[1]] && board[position[1]] === board[position[2]] && board[position[0]] !== "") {
 		setMessage(`Player ${board[position[0]]} Won!`)
 		winner = true;
+		saveGame();
 		}
 	})
 	return winner;
@@ -58,11 +59,13 @@ function doTurn(square) {
 	turn++;
 
 	if (checkWinner(turn) === true) { 
-		turn = 0;
-		board = {};
-		$('td').empty(); 
+		saveGame();
+		clearGame();
 	} else if (turn === 9) {
+		saveGame();
+		turn = 0;
 		setMessage(`Tie game.`);
+		clearGame();
 	};
 }
 
@@ -70,6 +73,7 @@ function gameOver() {
 	let gameOver = false;
 	if (checkWinner() === true || setMessage() === `Tie game.`) {
 	gameOver = true;
+
 };
 	return gameOver;
 }
@@ -83,6 +87,77 @@ function attachListeners(){
 	};
 	
 	});
+	$('#previous').on("click", function() {
+			previousGame();
+		});
+	$('#save').on("click", function() {
+		saveGame();
+	});
+	$('#clear').on("click", function() {
+		clearGame();
+	});
 }
+
+function saveGame() {
+	let state = [];
+
+
+	$('td').text((index, square) => {
+    state[index] = square;
+  });
+	var gameData = { state: state }
+	
+  if (currentGame) {
+    $.ajax({
+      type: 'PATCH',
+      url: `/games/${currentGame}`,
+      data: gameData
+    });
+  } else {
+    $.post('/games', gameData, function(game) {
+      currentGame = game.data.id
+      $("#games").append(`<button id="game-${game.data.id}" onClick="loadGame(${game.data.id})">${game.data.id}</button><br>`);
+    });
+  }
+}
+
+
+
+function previousGame() {
+    $.get('/games', function(response) {
+    	const games = response.data;
+    	var list = $('div#games').text();
+
+    	games.forEach(function(game) {
+    		if (!list.includes(game.id)) {
+    			// iterate over each game button add an event listener to each button 
+    		$("#games").append(`<button id="game-${game.id}" onClick="loadGame(${game.id})">${game['id']}</button><br>`);
+    		};
+    	});
+    });
+}
+
+function loadGame(id) {
+	$.get('/games/' + id, function(response) {
+	let state = response.data.attributes.state;
+    let $tds = $('td');
+    for (let index = 0; index < state.length; index++) {
+      $tds[index].innerHTML = state[index];
+      if (state[index].innerHTML !== "") {turn++;};
+    };
+    currentGame = id;
+    turn = state.join('').length;
+
+	});
+}
+
+
+function clearGame() {
+	$('td').empty();
+	board = {};
+	turn = 0;
+	currentGame = 0;
+}
+
 
 
