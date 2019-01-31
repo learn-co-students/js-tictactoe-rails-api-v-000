fd20e277935bdb681b0f2af2f46496af7815a5fc
Alexandra Fren <alexandra.fren@gmail.com>
2019-01-31 20:32:53 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,67 +1,161 @@
-// Code your JavaScript / jQuery solution here
-let turn = 0;
+var turn = 0
+var currentGame = 0
 
-function player() {
-  if (turn%2 === 0) {
-    return "X"
+function player(){
+  if (turn%2 == 0) {
+    return 'X'
+  } else {
+    return 'O'
   }
-  else {
-    return "O"
+}
+
+function updateState(position){
+  $(position).text(player())
+}
+
+function setMessage(message){
+  $("#message").text(message)
+}
+
+function checkWinner(){
+  var board = $("td")
+  if (board[0].innerHTML === board[1].innerHTML && board[1].innerHTML === board[2].innerHTML && board[2].innerHTML !== ""){
+    setMessage(`Player ${board[0].innerHTML} Won!`)
+    $.post("/games")
+    return true
+  } else if (board[3].innerHTML === board[4].innerHTML && board[4].innerHTML === board[5].innerHTML && board[5].innerHTML !== "") {
+    setMessage(`Player ${board[4].innerHTML} Won!`)
+    $.post("/games")
+    return true
+  } else if (board[6].innerHTML === board[7].innerHTML && board[7].innerHTML === board[8].innerHTML && board[8].innerHTML !== "") {
+    setMessage(`Player ${board[6].innerHTML} Won!`)
+    $.post("/games")
+    return true
+  } else if (board[0].innerHTML === board[3].innerHTML && board[3].innerHTML === board[6].innerHTML && board[6].innerHTML !== "") {
+    setMessage(`Player ${board[0].innerHTML} Won!`)
+    $.post("/games")
+    return true
+  } else if (board[1].innerHTML === board[4].innerHTML && board[4].innerHTML === board[7].innerHTML && board[7].innerHTML !== "") {
+    setMessage(`Player ${board[1].innerHTML} Won!`)
+    $.post("/games")
+    return true
+  } else if (board[2].innerHTML === board[5].innerHTML && board[5].innerHTML === board[8].innerHTML && board[8].innerHTML !== "") {
+    setMessage(`Player ${board[2].innerHTML} Won!`)
+    $.post("/games")
+    return true
+  } else if (board[0].innerHTML === board[4].innerHTML && board[4].innerHTML === board[8].innerHTML && board[8].innerHTML !== "") {
+    setMessage(`Player ${board[0].innerHTML} Won!`)
+    $.post("/games")
+    return true
+  } else if (board[2].innerHTML === board[4].innerHTML && board[4].innerHTML === board[6].innerHTML && board[6].innerHTML !== "") {
+    setMessage(`Player ${board[2].innerHTML} Won!`)
+    $.post("/games")
+    return true
+  } else {
+    return false
   }
-};
+}
 
-function updateState(elem) {
-  $(elem).text(player());
-};
+function tieGame() {
+  var board = $("td")
+  if (board[0].innerHTML !== "" && board[1].innerHTML !== "" && board[2].innerHTML !== "" && board[3].innerHTML !== "" && board[4].innerHTML !== "" && board[5].innerHTML !== "" && board[6].innerHTML !== "" && board[8].innerHTML !== "" ){
+    setMessage("Tie game.")
+    $.post("/games")
+    return true
+  }
+}
 
-function setMessage(message) {
-  $("#message").html(message);
-};
-
-function checkWinner() {
-  let winner = "";
-  if (turn > 5){
-    if (state[0] === state[1] === state[2]) {
-      winner = state[0];
-    }
-    else if (state[3] === state[4] === state[5]){
-      winner = state[3];
-    }
-    else if (state[6] === state[7] === state[8]){
-      winner = state[6];
-    }
-    else if (state[0] === state[3] === state[6]){
-      winner = state[0];
-    }
-    else if (state[1] === state[4] === state[7]){
-      winner = state[1];
-    }
-    else if (state[2] === state[5] === state[8]){
-      winner = state[2];
-    }
-    else if (state[0] === state[4] === state[8]){
-      winner = state[0];
-    }
-    else if (state[2] === state[4] === state[6]){
-      winner = state[2];
+function doTurn(position) {
+  updateState(position)
+  turn += 1
+  if (checkWinner()) {
+    turn = 0
+    currentGame = 0
+    $("td").empty()
+  } else if (tieGame() === true) {
+    turn = 0
+    currentGame = 0
+    $("td").empty()
     }
   }
-    if (winner != ""){
-      return true;
-      let message = "Player " + winner + " Won!";
-      setMessage(message);
-    }
-    else {
-      return false;
-    }
-};
 
-function doTurn(elem) {
-  turn += 1;
-  checkWinner();
-  updateState(elem);
-};
+
+var previous = function() {
+  $("#previous").on("click", function(){
+    $.get("/games", function(data) {
+      var games = data
+      gameList = document.querySelector("#games")
+      gameButton = ""
+      games["data"].forEach(function(game){
+        gameButton += '<button id=' + game["id"] + ' onclick="getGame(' + game["id"] + ')">' + game + '</button>'
+        $("#games").html(gameButton)
+      })
+    })
+  })
+}
+
+function getGame(id){
+  $.get(`/games/${id}`, function(data){
+    currentGame = id
+    var newBoard = data["data"]["attributes"]["state"]
+    var board = $("td")
+    $("td").each(function(index, square){
+      square.innerHTML = newBoard[index]
+      if (newBoard[index] !== ""){
+        turn += 1
+      }
+    })
+  })
+}
+
 
 function attachListeners(){
+  $("td").click(function(){
+    if (this.innerHTML === "" && !checkWinner()) {
+      doTurn(this)
+    }
+  })
+  previous()
+  $("#save").on("click", function(){save()})
+  clear()
+}
 
-};
+var save = function(){
+  var state = [];
+  var gameData;
+  $("td").each(function(index, square){
+    state.push(square.innerHTML)
+  })
+  gameData = {state: state}
+
+    if (currentGame === 0) {
+      $.post("/games",gameData, function(data){
+        currentGame = data["data"]["id"]
+      })
+    } else {
+      $.ajax({
+      type: 'PATCH',
+      url: `/games/${currentGame}`,
+      data: gameData
+    });
+    }
+  }
+
+
+var clear = function(){
+  $("#clear").on("click", function(){
+    turn = 0
+    currentGame = 0
+    var nodes = document.querySelectorAll('td')
+    nodes.forEach(function(node) {
+        node.innerHTML = ""
+    })
+  })
+}
+
+
+
+$(document).ready(function(){
+  attachListeners()
+
+})
