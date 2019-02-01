3bdec1faa9ea397aec3861fb908cbd00e165e257
Hillevi Eklöw <hillevieklow@gmail.com>
2019-02-01 18:20:03 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -5,6 +5,10 @@ var turn = 0;
 var gameId = 0;
 var state = [];
 
+$(document).ready(function(){
+  attachListeners();
+});
+
 function player(){
   if (turn % 2 == 0 ){
     return 'X';
@@ -36,19 +40,83 @@ function checkWinner(){
 }
 
 function doTurn(cell){
-
+  updateState(cell);
+  turn = turn + 1;
+  if(checkWinner()){
+    saveGame();
+    clearGame();
+  }
+  else if (turn === 9 && !checkWinner()){
+    setMessage("Tie game.");
+    saveGame();
+    clearGame();
+  }
 }
 
 function attachListeners(){
+  $('td').on('click', function(){
+    if (!$.text(this) && !checkWinner()){
+      doTurn(this);
+    }
+  })
+  $('#save').on('click', function(){
+    saveGame();
+  });
+  $('#previous').on('click', function(){
+    previousGame();
+  });
+  $('#clear').on('click', function(){
+    clearGame();
+  });
 
 }
 
 function saveGame(){
-
+  $('td').text(function(index, cell){
+    state.push(cell);
+  });
+  if (gameId){
+    $.ajax({
+      data: {state: state},
+      type: 'PATCH',
+      url: `/games/${gameId}`
+    });
+  }
+  else {
+    $.post('/games', {state:state}, function(game){
+      gameId = game.data.id;
+    });
+  }
 }
 
 function previousGame(){
+  $.get('/games', function(previousGames) {
+    $('#games').empty();
+    $('#message').empty();
+    previousGames.data.forEach(function(game){
+      if (previousGames.data.length !== 0){
+        $('#games').append(`<button id="gameId-${game.id}">Game ${game.id}</button><br>`);
+        $('#gameId-' + game.id).on('click', function(){
+          showGame(game.id);
+        });
+      };
+    });
+  });
+}
 
+function showGame(id){
+  $.get(`/games/${id}`, function(game){
+    $('#games').empty();
+    $('#message').text(`/games/${id}`);
+    gameId = game.data.id;
+    state = game.data.attributes.state;
+    turn = state.join('').length;
+    i = 0;
+    state.forEach(function(element){
+      $('td')[i].innerHTML = element;
+      i++;
+    });
+  });
 }
 
 function clearGame(){
