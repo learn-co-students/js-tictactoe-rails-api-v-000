598d63160a72e13a58176684f3da590100a72563
Jenny Kam <jkam17@gmail.com>
2019-02-03 13:39:18 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -3,6 +3,7 @@ var WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
                         [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 window.turn = 0
 var player = () => window.turn % 2 ? 'O' : 'X';  //0 is falsy, X for even turn count
+var currentGame = 0  //id of currentgame in db, 0 (falsy) for non saved games
 
 $(document).ready(function() {
   attachListeners();
@@ -33,13 +34,13 @@ function doTurn(spot){
   updateState(spot)
   window.turn += 1
   if (checkWinner()) {
-    window.turn = 0
-    $('td').text('')
+    saveGame()
+    clearGame()
   }
-  if (window.turn === 9){
-    if (!checkWinner()) {setMessage('Tie game.')}
-    window.turn = 0
-    $('td').text('')
+  if (window.turn === 9 && !checkWinner()){
+    {setMessage('Tie game.')}
+    saveGame()
+    clearGame()
   }
 }
 
@@ -49,36 +50,64 @@ function attachListeners(){
       doTurn(this)
   })
 
-  $('#save').on('click', () => save())
-  $('#previous').on('click', () => previous())
-  $('#clear').on('click', () => clear())
+  $('#save').on('click', () => saveGame())
+  $('#previous').on('click', () => previousGame())
+  $('#clear').on('click', () => clearGame())
 
 }
 
-function previous(){
+function previousGame(){
   $.get('/games', function(data){
-    console.log(data)
+    if (data.data){
+      $('div#games').empty()
+      for (const game of data.data){
+         $('div#games').append(`
+          <button id=gameid-${game.id}> Game ${game.id} </button>
+          `)
+          $(`#gameid-${game.id}`).on('click', () => loadGame(game.id));
+      }
+    }
+  })
+}
+
+function loadGame(id){
+  $.get('/games/' + id, function(game){
+    let arr = game.data.attributes.state
+    currentGame = game.data.id
+    window.turn = arr.filter(Boolean).length
+    for (let i=0; i<9; i++){
+      $('td')[i].innerHTML = arr[i]
+    }
   })
 }
 
 
-function save(){
- $.post('/games', function(data){
-   console.log(data)
- })
+function saveGame(){
+  let gamedata = {state: []}
+  for (const el of $('td')){
+    gamedata.state.push(el.innerText)
+  }
+  if (currentGame){
+    $.ajax({
+      type: 'PATCH',
+      url: `/games/${currentGame}`,
+      data: gamedata
+    });
+  }
+  else{
+   $.post('/games', gamedata, function(data){
+     currentGame = data.data.id
+     $('div#games').append(`
+      <button id=gameid-${currentGame}> Game ${currentGame} </button>
+      `)
+      $(`#gameid-${currentGame}`).on('click', () => loadGame(currentGame));
+   })
+
+  }
 }
 
-
-function clear(){
-  $.get('/games', function(data){
-    console.log(data)
-  })
+function clearGame(){
+    currentGame = 0
+    window.turn = 0
+    $('td').text('')
 }
-
-
-
-
-
-
-
-//
