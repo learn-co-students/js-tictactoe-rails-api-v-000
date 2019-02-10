cde729f3fbdd9ef90cc5c0a10abcba3c4bf24628
Madeline-Stark <madeline.stark1@gmail.com>
2019-02-09 20:00:18 -0800

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,7 +1,12 @@
 
-let turn = 0; //will turn++ with each doTurn()
+var turn = 0;
+var currentGame = 0;
+var gameBoard = { state: []}
 
-$(document).ready(attachListeners());
+$(document).ready(function() {
+  attachListeners();
+});
+
 
 function player() {
   if (turn % 2) {
@@ -12,114 +17,127 @@ function player() {
 }
 
 function updateState(boxChoice) {
-  // basically is listening for which box you're clicking on
-  // then filling box with that players token
-  //call update state from doturn, which is called in event listeners using this
-  $(boxChoice).text(player()); //works in console!
+  $(boxChoice).text(player());
 }
 
 function setMessage(message) {
-  $('div#message').text(message);
+  $('#message').text(message);
 }
 
 function checkWinner() {
-  // here need to go through html of board and save in variables to compare against winning cobo
-  // in console, able to access squares like this
-  let squareZero = $('td')[0]
-  let squareOne = $('td')[1]
-  let squareTwo = $('td')[2]
-  let squareThree = $('td')[3]
-  let squareFour = $('td')[4]
-  let squareFive = $('td')[5]
-  let squareSix = $('td')[6]
-  let squareSeven = $('td')[7]
-  let squareEight = $('td')[8]
+  let squareZero = $('td')[0].textContent
+  let squareOne = $('td')[1].textContent
+  let squareTwo = $('td')[2].textContent
+  let squareThree = $('td')[3].textContent
+  let squareFour = $('td')[4].textContent
+  let squareFive = $('td')[5].textContent
+  let squareSix = $('td')[6].textContent
+  let squareSeven = $('td')[7].textContent
+  let squareEight = $('td')[8].textContent
 
-  let board = [squareZero, squareOne, squareTwo, squareThree, squareFour, squareSix, squareSeven, squareEight]
 
-  const winCombinations = [
-       [0,1,2],
-       [3,4,5],
-       [6,7,8],
-       [0,3,6],
-       [2,5,8],
-       [1,4,7],
-       [0,4,8],
-       [6,4,2]
-     ]
+  const board = [squareZero, squareOne, squareTwo, squareThree, squareFour, squareFive, squareSix, squareSeven, squareEight]
+  let winner = false;
+
+  var winCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [2,5,8], [1,4,7], [0,4,8], [6,4,2]];
 
   for (const element of winCombinations) {
-    if (board[element[0]] == 'X' && board[element[1]] == 'X' && board[element[0]] == 'X') {
+    if (board[element[0]] === 'X' && board[element[1]] === 'X' && board[element[2]] === 'X') {
       setMessage('Player X Won!');
-      return true;
-    } else if (board[element[0]] == 'O' && board[element[1]] == 'O' && board[element[0]] == 'O') {
+      winner = true;
+    } else if (board[element[0]] === 'O' && board[element[1]] === 'O' && board[element[2]] === 'O') {
       setMessage('Player O Won!');
-      return true;
-    } else {
-      return false;
+      winner = true;
     }
-
   }
-  //iterate through winCombinations
-  // for each iteration will get array like [0, 1, 2]
-  //use these to access those indices on board array
-  // check if each one is either x & x & x or all ys
+  return winner;
 }
 
 function doTurn(boxChoice) {
-  turn ++;
   updateState(boxChoice);
-  checkWinner();
+  turn ++;
+  if (checkWinner()) {
+    saveGame();
+    resetBoard();
+  } else if (turn === 9) {
+    setMessage("Tie game.");
+    saveGame();
+    resetBoard();
+  }
+}
+
+function resetBoard() {
+  $('td').text('');
+  turn = 0;
+  currentGame = 0;
+  setMessage('')
 }
 
 function attachListeners() {
-  // button event listeners go here!
   $('td').on('click', function() {
-    //check if empty first
-    doTurn(this);
-    //in debugger, this is full td
-    //get boxchoice here, pass to doturn, which passes to update game
-  })
-  // basically do same thing as above for buttons, just plug in below functions where doturn is
-}
+    if (!$.text(this) && !checkWinner()) {
+      doTurn(this);
+    }
+  });
 
-function currentGame() {
-  //how to access current game/know if a game has been created?
+  $('#save').on('click', () => saveGame());
+  $('#previous').on('click', () => showPreviousGames());
+  $('#clear').on('click', () => resetBoard());
 }
 
 function saveGame() {
-  let squareZero = $('td')[0]
-  let squareOne = $('td')[1]
-  let squareTwo = $('td')[2]
-  let squareThree = $('td')[3]
-  let squareFour = $('td')[4]
-  let squareFive = $('td')[5]
-  let squareSix = $('td')[6]
-  let squareSeven = $('td')[7]
-  let squareEight = $('td')[8]
-  //can use a map to go through
+  let squareZero = $('td')[0].textContent
+  let squareOne = $('td')[1].textContent
+  let squareTwo = $('td')[2].textContent
+  let squareThree = $('td')[3].textContent
+  let squareFour = $('td')[4].textContent
+  let squareFive = $('td')[5].textContent
+  let squareSix = $('td')[6].textContent
+  let squareSeven = $('td')[7].textContent
+  let squareEight = $('td')[8].textContent
 
-  let board = [squareZero, squareOne, squareTwo, squareThree, squareFour, squareSix, squareSeven, squareEight]
+  let board = [squareZero, squareOne, squareTwo, squareThree, squareFour, squareFive, squareSix, squareSeven, squareEight]
+  gameBoard.state = board;
 
-  if (!!currentGame()) {
-    //how to access current game/know if a game has been created?
-    // Don't need game object?
-    // Need to save location spots-just in array?
-    //Don't need to save instance of game class! just talk to rails api and get game data!
-    game = currentGame(); //create currentGame() function?
-    game.state = board;
+  if (currentGame) {
+    $.ajax({
+      type: 'PATCH',
+      url: `/games/${currentGame}`,
+      data: gameBoard
+    });
   } else {
-    game = Game.new;
-    game.state = board;
-    game.save; // AJAX request to save game
+    $.post('/games', gameBoard, function(game) { //sends to /games-this is where saved games will be stored
+      currentGame = game.data.id;
+      $('#games').append(`<button id="gameid-${game.data.id}">${game.data.id}</button><br>`);
+      $("#gameid-" + game.data.id).on('click', () => loadGame(game.data.id));
+    });
   }
-  // save game state
-  // game is a class with a state attribute
-  // what is state? the board?
-  // then in win method, just check state
 
 }
 
-function previousGames() {
+function showPreviousGames() {
+  $('#games').text('');
+  $.get('/games', (games) => {
+    if (games.data) {
+    for (const game of games.data) {
+      $('#games').append(`<button id="gameid-${game.id}">${game.id}</button><br>`);
+      $("#gameid-" + game.id).on('click', () => loadGame(game.id));
+     }
+      }
+    });
+  }
 
+
+function loadGame(gameID) {
+  document.getElementById('message').innerHTML = '';
+  $.get(`/games/${gameID}`).done( function(data) {
+    const state = data.data.attributes.state;
+    const id = parseInt(data.id);
+    for (const element of state) {
+        document.getElementById(`${state.indexOf(element)}`).textContent = element;
+      }
+      turn = state.join('').length;
+      console.log(turn)
+      currentGame = id;
+  })
 }
