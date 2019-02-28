099cab6aed7d216ca73732c5f9a9ceb32182bbe7
github username <github email address>
2019-02-27 19:18:46 -0500

    event listeners and game vars

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,41 @@
 // Code your JavaScript / jQuery solution here
+// $(function() {
+//   $('button#save').on("click", function(e) {
+//     e.preventDefault();
+//     console.log("hello");
+//   })
+// })
+
+const WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+
+var turn = 0
+
+var player = function() {
+  return turn % 2 ? 'O' : 'X';
+};
+
+
+$(document).ready(function() {
+  eventListeners();
+});
+
+function eventListeners() {
+  $('button#save').on('click', function() {
+    console.log("save game");
+  });
+  $('button#previous').on('click', function() {
+    console.log("previous games");
+  });
+  $('button#clear').on('click', function() {
+    console.log("clear");
+  });
+};
