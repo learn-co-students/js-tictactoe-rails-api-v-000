2b38fa3db194451a9a6257c2a16dab13d4760a08
stazman <cadistasio@gmail.com>
2019-03-05 10:54:42 -0600

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,3 +1,11 @@
-function player(){
+// import { uptime } from "os";
 
+function player(turnCount){
+
+    if (turnCount === 2 % 0){
+        return "X"
+    } else {
+        return "O"
+    }    
 }
+
