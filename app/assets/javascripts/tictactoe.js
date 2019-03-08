e32a1ec41975a51de50c439de8937dec953734b2
github username <github email address>
2019-03-07 21:47:51 -0800

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,23 @@
 // Code your JavaScript / jQuery solution here
+const winningCombos = [
+   [0, 1, 2],
+   [3, 4, 5],
+   [6, 7, 8],
+   [0, 3, 6],
+   [1, 4, 7],
+   [2, 5, 8],
+   [0, 4, 8],
+   [2, 4, 6]
+ ]
 
 var turn = 0
+
+
+function getBoard() {
+  squares = document.querySelectorAll('td')
+  return squares
+}
+
 
 // sets the player token to X or O
 var player = () => turn % 2 ? 'O' : 'X';
@@ -11,7 +28,21 @@ var setMessage = (note) => {
 }
 
 function updateState(square) {
-  debugger
-    var token = player()
+    var token = player();
     $('square').text(token)
+}
+
+function checkWinner(board) {
+  
+  var position_1 = winningCombos[0]
+  var position_2 = winningCombos[1]
+  var position_3 = winningCombos[2]
+
+  // for (const combo of winningCombos) {
+  //
+  // }
+}
+
+function doTurn() {
+
 }
