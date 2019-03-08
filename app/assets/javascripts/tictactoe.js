22f5ef9dbc7f41755f10a7f79359b710fb4ce8d8
github username <github email address>
2019-03-07 20:18:14 -0800

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,17 @@
 // Code your JavaScript / jQuery solution here
+
+var turn = 0
+
+// sets the player token to X or O
+var player = () => turn % 2 ? 'O' : 'X';
+
+// setMessage() Accepts a string and adds it to the div#message element in the DOM.
+var setMessage = (note) => {
+  $('#message').html('<p>' + note + '</p>')
+}
+
+function updateState(square) {
+  debugger
+    var token = player()
+    $('square').text(token)
+}
