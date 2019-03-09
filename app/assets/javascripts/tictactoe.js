f6ab39437593478f7a07c9f0ffbde20b94e0177c
github username <github email address>
2019-03-08 19:13:05 -0800

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,26 +1,16 @@
 // Code your JavaScript / jQuery solution here
-const winningCombos = [
-   [0, 1, 2],
-   [3, 4, 5],
-   [6, 7, 8],
-   [0, 3, 6],
-   [1, 4, 7],
-   [2, 5, 8],
-   [0, 4, 8],
-   [2, 4, 6]
- ]
+
 
 var turn = 0
 
+
+// sets the player token to X or O
+var player = () => turn % 2 ? 'O' : 'X';
 
 function getBoard() {
   squares = document.querySelectorAll('td')
   return squares
 }
-
-
-// sets the player token to X or O
-var player = () => turn % 2 ? 'O' : 'X';
 
 // setMessage() Accepts a string and adds it to the div#message element in the DOM.
 var setMessage = (note) => {
@@ -29,18 +19,39 @@ var setMessage = (note) => {
 
 function updateState(square) {
     var token = player();
-    $('square').text(token)
+    $(square).text(token)
 }
 
-function checkWinner(board) {
-  
-  var position_1 = winningCombos[0]
-  var position_2 = winningCombos[1]
-  var position_3 = winningCombos[2]
+function checkWinner() {
+  var winner = false
+  const winningCombos = [
+     [0, 1, 2],
+     [3, 4, 5],
+     [6, 7, 8],
+     [0, 3, 6],
+     [1, 4, 7],
+     [2, 5, 8],
+     [0, 4, 8],
+     [2, 4, 6]
+   ]
+  var board = []
+  var currentState = $('td')
 
-  // for (const combo of winningCombos) {
-  //
-  // }
+  currentState.text((i, square) =>  {
+    board.push(square)
+  })
+
+  winningCombos.some(function(element) {
+    var position_1 = board[element[0]]
+    var position_2 = board[element[1]]
+    var position_3 = board[element[2]]
+
+    if (position_1 !== "" && position_1 === position_2 && position_2 === position_3) {
+           setMessage(`Player ${board[element[0]]} Won!`);
+           winner = true;
+      }
+  });
+  return winner
 }
 
 function doTurn() {
