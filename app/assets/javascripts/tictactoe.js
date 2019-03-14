d5b5704cbf7582754336816a5358d9fc620f7811
AAM77 <Adeel.A.Mhd@gmail.com>
2019-03-14 14:04:52 -0400

    Added event listeners and passed the clicked event to it. Got the turn variable to increment by changing it to var.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,10 +1,24 @@
 // Code your JavaScript / jQuery solution here
 
+// ALL OF THE WINNING COMBINATIONS
+winningCombos =  [
+  [0,1,2],            // Top-row
+  [0,3,6],            // Left column
+  [0,4,8],            // T-left-B-right
+  [1,4,7],            // Middle column
+  [2,5,8],            // Right column
+  [2,4,6],            // T-right-B-left
+  [3,4,5],            // Middle row
+  [6,7,8],            // Bottom row
+]
 
+var turn = 1
+let board = [ '', '', '', '', '', '', '', '', '' ]
 
-let turn = 1
-
-let board = ['','','','','','','','','']
+function reset_game() {
+  turn = 1;
+  board = [ '', '', '', '', '', '', '', '', '' ];
+}
 
 function fill_board() {
   board[0] = $('table td[data-x=' + 0 + '][data-y=' + 0 + ']')[0].innerHTML
@@ -18,23 +32,13 @@ function fill_board() {
   board[8] = $('table td[data-x=' + 2 + '][data-y=' + 2 + ']')[0].innerHTML
 }
 
-// ALL OF THE WINNING COMBINATIONS
-winningCombos =  [
-  [0,1,2],            // Top-row
-  [0,3,6],            // Left column
-  [0,4,8],            // T-left-B-right
-  [1,4,7],            // Middle column
-  [2,5,8],            // Right column
-  [2,4,6],            // T-right-B-left
-  [3,4,5],            // Middle row
-  [6,7,8],            // Bottom row
-]
 
 function player() {
   return turn % 2 === 0 ? 'X' : 'O'
 };
 
 function updateState(position) {
+  fill_board()
   position.innerHTML = player()
 };
 
@@ -42,36 +46,49 @@ function setMessage(message) {
   $('div#message')[0].innerHTML = message
 };
 
-
 function checkWinner() {
-  fill_board()
   win_bool = false;
+  fill_board()
 
   for(i=0; i< winningCombos.length; i++) {
+
     if (board.every(el => el === "")) {
       win_bool = false
     } else if (board[winningCombos[i][0]] === board[winningCombos[i][1]] &&
                board[winningCombos[i][1]] === board[winningCombos[i][2]]) {
-        win_bool =  true
-        //debugger;
-        setMessage(`Player ${player()} Won!`);
+
+      setMessage(`Player ${player()} Won!`);
+      win_bool =  true
     }
   }
 
   return win_bool
-  // win_bool = winningCombos.every( function(combo) {
-  //   board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]
-  //   debugger;
-  // })
-  // debugger;
 }
 
-function doTurn () {};
+function doTurn (position) {
 
-function attachListeners() {};
+  if (checkWinner() === false  && turn === 9) {
+    setMessage('Tie game.');
+    reset_game();
+  } else if (checkWinner() === true) {
+    reset_game();
+  } else if (checkWinner() === false  && turn < 9) {
+    updateState(position);
+    turn += 1
+  }
+}
+
+
+
+function attachListeners() {
+  $('table td').on('click', function() {
+    let position = this
+    doTurn(position)
+  })
+};
 
 
 
 $(function() {
-
+  attachListeners()
 })
