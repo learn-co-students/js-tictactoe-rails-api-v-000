0b9f07bbbd8b96a8789d6d3848d75c06aee87c02
AAM77 <Adeel.A.Mhd@gmail.com>
2019-03-14 03:02:56 -0400

    Added the ability to add token for a winner at a given position, to check for a winner, and announce the winner.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,34 +1,25 @@
 // Code your JavaScript / jQuery solution here
-board = [
-  [
-    [''],[''],['']
-  ],
-  [
-    [''],[''],['']
-  ],
-  [
-    [''],[''],['']
-  ]
-]
+
 
 
 let turn = 1
 
-const board = [
-  $('table td[data-x=' + 0 + '][data-y=' + 0 + ']')[0].innerHTML,
-  $('table td[data-x=' + 1 + '][data-y=' + 0 + ']')[0].innerHTML,
-  $('table td[data-x=' + 2 + '][data-y=' + 0 + ']')[0].innerHTML,
-  $('table td[data-x=' + 0 + '][data-y=' + 1 + ']')[0].innerHTML,
-  $('table td[data-x=' + 1 + '][data-y=' + 1 + ']')[0].innerHTML,
-  $('table td[data-x=' + 2 + '][data-y=' + 1 + ']')[0].innerHTML,
-  $('table td[data-x=' + 0 + '][data-y=' + 2 + ']')[0].innerHTML,
-  $('table td[data-x=' + 1 + '][data-y=' + 2 + ']')[0].innerHTML,
-  $('table td[data-x=' + 2 + '][data-y=' + 2 + ']')[0].innerHTML
-]
+let board = ['','','','','','','','','']
+
+function fill_board() {
+  board[0] = $('table td[data-x=' + 0 + '][data-y=' + 0 + ']')[0].innerHTML
+  board[1] = $('table td[data-x=' + 1 + '][data-y=' + 0 + ']')[0].innerHTML
+  board[2] = $('table td[data-x=' + 2 + '][data-y=' + 0 + ']')[0].innerHTML
+  board[3] = $('table td[data-x=' + 0 + '][data-y=' + 1 + ']')[0].innerHTML
+  board[4] = $('table td[data-x=' + 1 + '][data-y=' + 1 + ']')[0].innerHTML
+  board[5] = $('table td[data-x=' + 2 + '][data-y=' + 1 + ']')[0].innerHTML
+  board[6] = $('table td[data-x=' + 0 + '][data-y=' + 2 + ']')[0].innerHTML
+  board[7] = $('table td[data-x=' + 1 + '][data-y=' + 2 + ']')[0].innerHTML
+  board[8] = $('table td[data-x=' + 2 + '][data-y=' + 2 + ']')[0].innerHTML
+}
 
 // ALL OF THE WINNING COMBINATIONS
-function winning_combos() {
-  return [
+winningCombos =  [
   [0,1,2],            // Top-row
   [0,3,6],            // Left column
   [0,4,8],            // T-left-B-right
@@ -37,24 +28,49 @@ function winning_combos() {
   [2,4,6],            // T-right-B-left
   [3,4,5],            // Middle row
   [6,7,8],            // Bottom row
-  ]
-}
+]
 
 function player() {
   return turn % 2 === 0 ? 'X' : 'O'
 };
 
 function updateState(position) {
-
+  position.innerHTML = player()
 };
 
-function setMessage() {};
+function setMessage(message) {
+  $('div#message')[0].innerHTML = message
+};
 
-function checkWinner() {};
+
+function checkWinner() {
+  fill_board()
+  win_bool = false;
+
+  for(i=0; i< winningCombos.length; i++) {
+    if (board.every(el => el === "")) {
+      win_bool = false
+    } else if (board[winningCombos[i][0]] === board[winningCombos[i][1]] &&
+               board[winningCombos[i][1]] === board[winningCombos[i][2]]) {
+        win_bool =  true
+        //debugger;
+        setMessage(`Player ${player()} Won!`);
+    }
+  }
+
+  return win_bool
+  // win_bool = winningCombos.every( function(combo) {
+  //   board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]
+  //   debugger;
+  // })
+  // debugger;
+}
 
 function doTurn () {};
 
 function attachListeners() {};
+
+
 
 $(function() {
 
