133b453e5b744ea39691adf615741fb8c8ee5507
AAM77 <Adeel.A.Mhd@gmail.com>
2019-03-16 13:58:35 -0400

    Added comments and changed a couple of function names.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,7 +1,13 @@
 // Code your JavaScript / jQuery solution here
 
+//////////////////////////////////////////////////////
+// Tracks what game is loaded into the board        //
+//////////////////////////////////////////////////////
 var currentGameNum = 0
-// ALL OF THE WINNING COMBINATIONS
+
+//////////////////////////////////////////////////////
+// A LIST OF ALL OF THE WINNING COMBINATIONS        //
+//////////////////////////////////////////////////////
 winningCombos =  [
   [0,1,2],            // Top-row
   [0,3,6],            // Left column
@@ -13,20 +19,37 @@ winningCombos =  [
   [6,7,8],            // Bottom row
 ]
 
+//////////////////////////////////////////////////////
+// the initial values for the game                  //
+//////////////////////////////////////////////////////
 var turn = 0;
 var board = [ '', '', '', '', '', '', '', '', '' ];
 
+
+//////////////////////////////////////////////////////
+// resets the game completely                       //
+//////////////////////////////////////////////////////
 function reset_game() {
   turn = 0;
   board = [ '', '', '', '', '', '', '', '', '' ];
+  currentGameNum = $('button.prev-game').length + 1
   empty_board();
 }
 
+
+//////////////////////////////////////////////////////
+// empties the board by changing all of the         //
+// positions to ''                                  //
+//////////////////////////////////////////////////////
 function empty_board() {
   jQuery.each($('table td'), function(i, td) { td.innerHTML = '' });
 }
 
-function fill_board() {
+//////////////////////////////////////////////////////
+// fills the backend board with the tokens on the   //
+// browser board                                    //
+//////////////////////////////////////////////////////
+function fillGameBoard() {
   board[0] = $('table td[data-x=' + 0 + '][data-y=' + 0 + ']')[0].innerHTML
   board[1] = $('table td[data-x=' + 1 + '][data-y=' + 0 + ']')[0].innerHTML
   board[2] = $('table td[data-x=' + 2 + '][data-y=' + 0 + ']')[0].innerHTML
@@ -38,7 +61,11 @@ function fill_board() {
   board[8] = $('table td[data-x=' + 2 + '][data-y=' + 2 + ']')[0].innerHTML
 }
 
-function board_fill() {
+//////////////////////////////////////////////////////
+// fills the board on the browser with the contents //
+// of the game board                                //
+//////////////////////////////////////////////////////
+function fillBrowserBoard() {
   $('table td[data-x=' + 0 + '][data-y=' + 0 + ']')[0].innerHTML = board[0]
   $('table td[data-x=' + 1 + '][data-y=' + 0 + ']')[0].innerHTML = board[1]
   $('table td[data-x=' + 2 + '][data-y=' + 0 + ']')[0].innerHTML = board[2]
@@ -50,10 +77,18 @@ function board_fill() {
   $('table td[data-x=' + 2 + '][data-y=' + 2 + ']')[0].innerHTML = board[8]
 }
 
+//////////////////////////////////////////////////////
+// checks if the current player is an 'X' or an 'O' //
+//////////////////////////////////////////////////////
 function player() {
   return turn % 2 === 0 ? 'X' : 'O'
 };
 
+
+//////////////////////////////////////////////////////
+// updates the board if it doesn't                  //
+// have an 'X' or 'O'                               //
+//////////////////////////////////////////////////////
 function updateState(position) {
   if (position.innerHTML === 'X' || position.innerHTML === 'O' ) {
     turn -= 1
@@ -62,10 +97,20 @@ function updateState(position) {
   }
 };
 
+
+//////////////////////////////////////////////////////
+// sets the message to whatever the argument is     //
+// and displays is in the browser                   //
+//////////////////////////////////////////////////////
 function setMessage(message) {
   $('div#message')[0].innerHTML = message
 };
 
+
+//////////////////////////////////////////////////////
+// checks if the winning combination is             //
+// an 'X' or an 'O'                                 //
+//////////////////////////////////////////////////////
 function has_winning_combo() {
   for(i=0; i< winningCombos.length; i++) {
     if ((board[winningCombos[i][0]] === 'X' &&
@@ -85,15 +130,22 @@ function has_winning_combo() {
   }
 }
 
+//////////////////////////////////////////////////////
+// checks if the game is tied                       //
+//////////////////////////////////////////////////////
 function is_a_tied_game() {
   if (board.every(el => el === "X" || el === "O") && !has_winning_combo()) {
     return true
   }
 }
 
+
+//////////////////////////////////////////////////////
+// Determines if there is a winner                  //
+//////////////////////////////////////////////////////
 function checkWinner() {
   winBool = false;
-  fill_board()
+  fillGameBoard()
 
   if (board.every(el => el === "")) {
     winBool = false
@@ -109,16 +161,25 @@ function checkWinner() {
   return winBool
 }
 
+
+//////////////////////////////////////////////////////
+// Performs a turn by updating the board,           //
+// checking for a winner,                           //
+// and incrementing the turn                        //
+//////////////////////////////////////////////////////
 function doTurn (position) {
   updateState(position)
   if (checkWinner() === true) {
-    currentGameNum = $('button.prev-game').length + 1
     reset_game()
   } else {
     turn += 1
   }
 }
 
+//////////////////////////////////////////////////////
+// Invokes the doTurn() method if the player clicks //
+// on any square of the game board                  //
+//////////////////////////////////////////////////////
 function click_for_turn() {
   $('table td').on('click', function(event) {
     event.preventDefault();
@@ -126,6 +187,11 @@ function click_for_turn() {
   })
 }
 
+//////////////////////////////////////////////////////
+// Adds an event listener to each game button       //
+// Fills the browser board with the contents        //
+// of the game instance's Game state (board)        //
+//////////////////////////////////////////////////////
 function selectGame() {
   $('button.prev-game').on('click', function() {
     var id = this.innerHTML
@@ -134,14 +200,21 @@ function selectGame() {
     	method: 'GET'
     })
     .done(function(game) {
-      currentGameNum = game['data']['id']
+      currentGameNum = parseInt(game['data']['id'])
     	board = game['data']['attributes']['state']
       turn = board.filter( x => x === 'X' || x === 'O' ).length
-    	board_fill()
+    	fillBrowserBoard()
     });
   })
 }
 
+
+//////////////////////////////////////////////////////
+// Creates a new button for each previous game      //
+// Activates the selectGame() event listener        //
+// for the buttons only if there are previous       //
+// games                                            //
+//////////////////////////////////////////////////////
 function previousGames() {
   $('button#previous').on('click', function(event) {
     event.preventDefault();
@@ -160,23 +233,50 @@ function previousGames() {
   });
 }
 
+//////////////////////////////////////////////////////
+// Saves the game if the user clicks the save       //
+// button and if there is a Win or Tie              //
+// If Game exists, updates it                       //
+// If Game doesn't exist, creates a new game        //
+//////////////////////////////////////////////////////
 function saveGame() {
   $('button#save').on('click', function(event) {
     event.preventDefault();
-    $.ajax({
-      method: "POST",
-      url: "/games",
-      data: { state: board }
-    })
+
+    if ($(`#game-${currentGameNum}`)[0]) {
+      //debugger;
+      $.ajax({
+        type: "PATCH",
+        url: `/games/${currentGameNum}`,
+        data: { state: board }
+      })
+      //debugger;
+
+    } else {
+
+      $.ajax({
+        method: "POST",
+        url: "/games",
+        data: { state: board }
+      })
+
+    }
+
   });
 }
 
+//////////////////////////////////////////////////////
+// Clears the board & resets the turn count to 0    //
+//////////////////////////////////////////////////////
 function clearGame() {
   $('button#clear').on('click', function() {
     reset_game()
   })
 }
 
+//////////////////////////////////////////////////////
+// All of the event listeners                       //
+//////////////////////////////////////////////////////
 function attachListeners() {
 
   click_for_turn()
@@ -184,13 +284,11 @@ function attachListeners() {
   saveGame()
   clearGame()
 
-
-
-
-
-
 };
 
+//////////////////////////////////////////////////////
+// document.ready() function                        //
+//////////////////////////////////////////////////////
 $(function() {
   attachListeners()
 })
