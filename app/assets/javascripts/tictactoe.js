0656fa1ea4acde47e53714623e8acf8ae973f21a
stazman <cadistasio@gmail.com>
2019-03-16 18:26:26 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,6 @@
 // import { uptime } from "os";
 
-const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
+var WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
                         [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
 var turn = 0
@@ -74,15 +74,17 @@ function checkWinner() {
 //get squares on board 
 //access tokens on squares
 
-
-}
-
-
-function doTurn () {
-
-
-
-}
+// function doTurn () {
+//   var turn = turncount++;
+//   updateState();
+//   checkWinner();
+//   if (??? === tie game){
+//     setMessage();
+//   } elseif(there's a winner){
+      // board = "";
+      // turn = 0;
+//   }
+// };
 
 function attachListeners () {
 
