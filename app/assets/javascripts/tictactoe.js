554f47f3d40c3d22dab3b5f2c588ce25aa5a2b8e
AAM77 <Adeel.A.Mhd@gmail.com>
2019-03-16 02:35:43 -0400

    Got most of the tests working. Working on the save game tests now.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,6 @@
 // Code your JavaScript / jQuery solution here
 
+var currentGameNum = 0
 // ALL OF THE WINNING COMBINATIONS
 winningCombos =  [
   [0,1,2],            // Top-row
@@ -12,26 +13,17 @@ winningCombos =  [
   [6,7,8],            // Bottom row
 ]
 
-var turn = 0
-var board = [ '', '', '', '', '', '', '', '', '' ]
+var turn = 0;
+var board = [ '', '', '', '', '', '', '', '', '' ];
 
 function reset_game() {
   turn = 0;
-  window.turn = 0;
   board = [ '', '', '', '', '', '', '', '', '' ];
   empty_board();
 }
 
 function empty_board() {
-  $('table td[data-x=' + 0 + '][data-y=' + 0 + ']')[0].innerHTML = ''
-  $('table td[data-x=' + 1 + '][data-y=' + 0 + ']')[0].innerHTML = ''
-  $('table td[data-x=' + 2 + '][data-y=' + 0 + ']')[0].innerHTML = ''
-  $('table td[data-x=' + 0 + '][data-y=' + 1 + ']')[0].innerHTML = ''
-  $('table td[data-x=' + 1 + '][data-y=' + 1 + ']')[0].innerHTML = ''
-  $('table td[data-x=' + 2 + '][data-y=' + 1 + ']')[0].innerHTML = ''
-  $('table td[data-x=' + 0 + '][data-y=' + 2 + ']')[0].innerHTML = ''
-  $('table td[data-x=' + 1 + '][data-y=' + 2 + ']')[0].innerHTML = ''
-  $('table td[data-x=' + 2 + '][data-y=' + 2 + ']')[0].innerHTML = ''
+  jQuery.each($('table td'), function(i, td) { td.innerHTML = '' });
 }
 
 function fill_board() {
@@ -44,6 +36,18 @@ function fill_board() {
   board[6] = $('table td[data-x=' + 0 + '][data-y=' + 2 + ']')[0].innerHTML
   board[7] = $('table td[data-x=' + 1 + '][data-y=' + 2 + ']')[0].innerHTML
   board[8] = $('table td[data-x=' + 2 + '][data-y=' + 2 + ']')[0].innerHTML
+}
+
+function board_fill() {
+  $('table td[data-x=' + 0 + '][data-y=' + 0 + ']')[0].innerHTML = board[0]
+  $('table td[data-x=' + 1 + '][data-y=' + 0 + ']')[0].innerHTML = board[1]
+  $('table td[data-x=' + 2 + '][data-y=' + 0 + ']')[0].innerHTML = board[2]
+  $('table td[data-x=' + 0 + '][data-y=' + 1 + ']')[0].innerHTML = board[3]
+  $('table td[data-x=' + 1 + '][data-y=' + 1 + ']')[0].innerHTML = board[4]
+  $('table td[data-x=' + 2 + '][data-y=' + 1 + ']')[0].innerHTML = board[5]
+  $('table td[data-x=' + 0 + '][data-y=' + 2 + ']')[0].innerHTML = board[6]
+  $('table td[data-x=' + 1 + '][data-y=' + 2 + ']')[0].innerHTML = board[7]
+  $('table td[data-x=' + 2 + '][data-y=' + 2 + ']')[0].innerHTML = board[8]
 }
 
 function player() {
@@ -64,16 +68,16 @@ function setMessage(message) {
 
 function has_winning_combo() {
   for(i=0; i< winningCombos.length; i++) {
-    if ((board[winningCombos[i][0]].toLowerCase() === 'x' &&
-         board[winningCombos[i][1]].toLowerCase() === 'x' &&
-         board[winningCombos[i][2]].toLowerCase() === 'x')) {
+    if ((board[winningCombos[i][0]] === 'X' &&
+         board[winningCombos[i][1]] === 'X' &&
+         board[winningCombos[i][2]] === 'X')) {
 
       setMessage(`Player X Won!`);
       return true
 
-    } else if ((board[winningCombos[i][0]].toLowerCase() === 'o' &&
-                board[winningCombos[i][1]].toLowerCase() === 'o' &&
-                board[winningCombos[i][2]].toLowerCase() === 'o')) {
+    } else if ((board[winningCombos[i][0]] === 'O' &&
+                board[winningCombos[i][1]] === 'O' &&
+                board[winningCombos[i][2]] === 'O')) {
 
       setMessage(`Player O Won!`);
       return true
@@ -82,54 +86,106 @@ function has_winning_combo() {
 }
 
 function is_a_tied_game() {
-  if (!has_winning_combo() && board.every(el => el.toLowerCase() === "x" || el.toLowerCase() === "o")) {
+  if (board.every(el => el === "X" || el === "O") && !has_winning_combo()) {
     return true
   }
 }
 
 function checkWinner() {
-  win_bool = false;
+  winBool = false;
   fill_board()
 
   if (board.every(el => el === "")) {
-    win_bool = false
+    winBool = false
 
   } else if (has_winning_combo()) {
-    win_bool = true
-    reset_game()
+    winBool = true
+    $('button#save').trigger('click')
 
   } else if (is_a_tied_game()) {
     setMessage(`Tie game.`)
+    $('button#save').trigger('click')
   }
-  return win_bool
+  return winBool
 }
 
 function doTurn (position) {
   updateState(position)
-  checkWinner()
-  turn += 1
+  if (checkWinner() === true) {
+    currentGameNum = $('button.prev-game').length + 1
+    reset_game()
+  } else {
+    turn += 1
+  }
+}
+
+function click_for_turn() {
+  $('table td').on('click', function(event) {
+    event.preventDefault();
+    doTurn(this)
+  })
+}
+
+function selectGame() {
+  $('button.prev-game').on('click', function() {
+    var id = this.innerHTML
+    $.ajax({
+    	url: `/games/${id}`,
+    	method: 'GET'
+    })
+    .done(function(game) {
+      currentGameNum = game['data']['id']
+    	board = game['data']['attributes']['state']
+      turn = board.filter( x => x === 'X' || x === 'O' ).length
+    	board_fill()
+    });
+  })
+}
+
+function previousGames() {
+  $('button#previous').on('click', function(event) {
+    event.preventDefault();
+
+    $.get("/games", function(prev_games) {
+      if (prev_games['data'].length > 0) {
+        const games_div = $('div#games').empty()
+        jQuery.each(prev_games['data'], function(i, game) {
+          games_div.append(`
+            <button class="prev-game" id="game-${game.id}">${game.id}</button> <br />
+            `)
+        });
+        selectGame()
+      };
+    });
+  });
+}
+
+function saveGame() {
+  $('button#save').on('click', function(event) {
+    event.preventDefault();
+    $.ajax({
+      method: "POST",
+      url: "/games",
+      data: { state: board }
+    })
+  });
+}
+
+function clearGame() {
+  $('button#clear').on('click', function() {
+    reset_game()
+  })
 }
 
 function attachListeners() {
-  $('table td').on('click', function() {
-    doTurn(this)
-  })
 
-  $('button#previous').on('click', function() {
-    $.get("/games")
-      .done(function(data) {
+  click_for_turn()
+  previousGames()
+  saveGame()
+  clearGame()
 
-    })
-  })
 
-  $('button#save').on('click', function() {
-    $.post('/games', function(data) {
-      //debugger;
-    })
-  })
 
-  $('button#clear').on('click', function() {
-  })
 
 
 
