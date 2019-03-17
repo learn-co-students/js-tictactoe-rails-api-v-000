ee5d703f111642fa1880c4571b8f77c078e4ca20
stazman <cadistasio@gmail.com>
2019-03-17 13:30:33 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -7,27 +7,11 @@ var turn = 0
 
 var player = () => turn % 2 === 0 ? "X" : "O";  
 
-    // if (turn % 2 === 0 ){
-    //     return "X"
-    // } else {
-    //     return "O"
-       
-
 function updateState(square){
 
     var token = player();
 
     $(square).text(token);
-    
-    // function that always returns token for current player
-  
-    // var squareChosen = document.querySelector("td");
-
-    // $("#td").attr("data-x", "data-y");
-
-    // squareChosen.addEventListener('click', function(){
-        
-        // turn++;
 }
 
 function setMessage (s) {
@@ -50,82 +34,45 @@ function checkWinner () {
     }
   });
     return winner;
+};
+
+function doTurn (square) {
+  
+  updateState(square);
+  
+  //passing in the same variable to a function called by another function is no prob (and what I was going to code at first :) :) :) )
+  turn++
+
+  if (checkWinner() === true){
+  //don't need to === true explicitly
+  //this automatically calls checkWinner, no need to invoke it separately
+    saveGame();
+    clearBoard();
+
+  } else if (turn === 9){
+    setMessage("Tie game.");
+      //don't have to declare a variable to pass tie game message through because setMessage does that
+    saveGame();
+    clearBoard();
+  }
 }
-
-
-
-function checkWinner() {
-  var board = {};
-  var winner = false;
-
-  $('td').text((index, square) => board[index] = square);
-
-  WINNING_COMBOS.some(function(combo) {
-    if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
-      setMessage(`Player ${board[combo[0]]} Won!`);
-      return winner = true;
-    }
-  });
-
-  return winner;
-}
-
-//if any winning combo matches any three squares on board, checkWinner is true
-//get squares on board 
-//access tokens on squares
-
-// function doTurn () {
-//   var turn = turncount++;
-//   updateState();
-//   checkWinner();
-//   if (??? === tie game){
-//     setMessage();
-//   } elseif(there's a winner){
-      // board = "";
-      // turn = 0;
-//   }
-// };
+  
 
 function attachListeners () {
 
-
-
 }
+
 
 function saveGame () {
-
-
-
+  console.log("this saves the game")
 
 }
 
 
-function previousGames () {
-
-
-
-
+function previousGame () {
+  console.log("this shows the previous games")
 }
 
-
-function updateState(square) {
-    var token = player();
-    $(square).text(token);
-  }
-  
-  // updateState()
-// Invokes player() and adds the returned string ('X' or 'O') to the clicked square on the game board.
-// setMessage()
-// Accepts a string and adds it to the div#message element in the DOM.
-// checkWinner()
-// Returns true if the current board contains any winning combinations (three X or O tokens in a row, vertically, horizontally, or diagonally). Otherwise, returns false.
-// If there is a winning combination on the board, checkWinner() should invoke setMessage(), passing in the appropriate string based on who won: 'Player X Won!' or 'Player O Won!'
-// doTurn()
-// Increments the turn variable by 1.
-// Invokes the updateState() function, passing it the element that was clicked.
-// Invokes checkWinner() to determine whether the move results in a winning play.
-// attachListeners()
-// Attaches the appropriate event listeners to the squares of the game board as well as for the button#save, button#previous, and button#clear elements.
-// When a user clicks on a square on the game board, the event listener should invoke doTurn() and pass it the element that was clicked.
-// NOTE: attachListeners() must be invoked inside either a $(document).ready() (jQuery) or a window.onload = () => {} (vanilla JavaScript). Otherwise, a number of the tests will fail (not to mention that your game probably won't function in the browser).
-// When you name your save and previous functions, make sure to call them something like saveGame() and previousGames(). If you call them save() and previous() you may run into problems with the test suite.
+function clearBoard () {
+  console.log("this clears the game board")
+}
