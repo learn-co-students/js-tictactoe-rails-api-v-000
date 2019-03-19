e0aec49ae520f30f95e1b99e535b6d3bd7fc574d
stazman <cadistasio@gmail.com>
2019-03-19 16:57:18 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -7,6 +7,10 @@ var turn = 0
 
 var currentGame = 0
 
+$(document).ready(function(){
+  attachListeners();
+})
+
 var player = () => turn % 2 === 0 ? "X" : "O";  
 
 function updateState(square){
@@ -14,7 +18,6 @@ function updateState(square){
     var token = player();
 
     $(square).text(token);
-
 
 }
 
@@ -46,7 +49,7 @@ function doTurn (square) {
   //passing in the same variable to a function called by another function is no prob (and what I was going to code at first :) :) :) )
   turn++
 
-  if (checkWinner() === true){
+  if (checkWinner()){
   //don't need to === true explicitly
   //this automatically calls checkWinner, no need to invoke it separately
     saveGame();
@@ -60,11 +63,6 @@ function doTurn (square) {
   }
 }
   
-$(document).ready(function(){
-  attachListeners();
-})
-
-
 function attachListeners () {
   $('td').on('click', function(){
     if (!$.text(this) && !checkWinner()){
@@ -105,8 +103,8 @@ function saveGame () {
       
     currentGame = game.data.id
 
-    $('#games').append(`<button id="gameid-${game.data.id}">${game.data.id}</button>`);
-    $("gameid-" + game.data.id).on('click', reloadGame(game.data.id));
+    $('#games').append(`<button id="gameid-${game.data.id}">${game.data.id}</button><br>`);
+    $("gameid-" + game.data.id).on('click', () => reloadGame(game.data.id));
     
     });
   };
@@ -123,7 +121,7 @@ function showPreviousGames () {
 
 function addButtonsForPreviousGames(game){
   $("#games").append(`<button id="dataid-${game.id}">${game.id}</button><br>`);
-  $(`#dataid-${game.id}`).on('click', reloadGame(game.id)); 
+  $(`#dataid-${game.id}`).on('click', () => reloadGame(game.id)); 
 }
 
 function clearBoard () {
@@ -153,8 +151,9 @@ function reloadGame(gameID) {
     let index = 0;
 
     for(let y=0; y < 3; y++){
+      //outer loop first in multi-dimensional loop
       for(let x=0; x < 3; x++){
-        document.querySelector(`[data-y="${x}"]`, `[data-y="${y}"]`).innerHTML = state[index];
+        document.querySelector(`[data-x="${x}"]`, `[data-y="${y}"]`).innerHTML = state[index];
         index++;
       }
     }
