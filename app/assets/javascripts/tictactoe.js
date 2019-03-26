98c4ab026094a5d33f0c49a57b734a6ac08a5a93
Steve Albonico <steve@albonico.net>
2019-03-26 10:38:34 -0700

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,16 @@
 // Code your JavaScript / jQuery solution here
+
+$(function () {
+  $("td[data-x=0][data-y=0]").on('click', function() {
+    $(this).text("X")
+  })
+
+
+
+
+})
+var turn = 2
+function player(){
+ if (1===1){return true}
+ 	else {return false}
+}
