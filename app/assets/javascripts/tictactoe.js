3ee043483b8f93e5641f90f48e9749110dbabfe1
Steve Albonico <steve@albonico.net>
2019-03-27 11:45:00 -0700

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,16 +1,84 @@
 // Code your JavaScript / jQuery solution here
 
-$(function () {
-  $("td[data-x=0][data-y=0]").on('click', function() {
-    $(this).text("X")
-  })
+var turn = 0
 
 
+// $(function () {
+//   $(".box").on('click', function(event) {
+//   	updateState(event.target)
+//   	turn += 1
+    
+//   })
+// })
 
-
-})
-var turn = 2
-function player(){
- if (1===1){return true}
- 	else {return false}
+function clearBoard(){ 
+const boxes = window.document.querySelectorAll('td');
+  for (let i = 0; i < 9; i++) {
+    boxes[i].innerHTML = "";
+  }
 }
+
+function player(){
+ if (turn % 2 === 0){return "X"}
+ 	else {return "O"}
+}
+
+function updateState(boxid){
+	boxid.innerHTML = player()
+}
+
+function setMessage(string){
+	$('#message').html(string)
+}
+
+function checkWinner(){
+	var board = ["","","","","","","","",""]
+	const WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+	let winner = false
+	const boxes = window.document.querySelectorAll('td');
+  for (let i = 0; i < 9; i++) {
+    board[i] = boxes[i].innerHTML;
+  }
+  WIN_COMBINATIONS.forEach(function(combo){
+  	let tempx = combo.every(function(number){
+  		if (board[number] === "X"){return true}
+  	})
+  	let tempo = combo.every(function(number){
+  		if (board[number] === "O"){return true}
+  	})
+  		if (tempx === true) {
+  			setMessage("Player X Won!")
+  			winner = true
+  		} 
+  		if (tempo === true){
+  			setMessage("Player O Won!")
+  			winner = true
+  		}
+  })
+  if (winner === true) {return true} else {return false}
+}
+
+function doTurn(event){
+
+	updateState(event)
+	if (checkWinner()) {
+		turn = 0
+		clearBoard()
+  	} 
+
+  	else {
+  			turn += 1
+  			setMessage("Tie game.")
+  		}
+	}
+
+function attachListeners(){
+  $("td").on('click', function(event) {
+  	doTurn(event.target)
+
+  })
+}
+
+
+
+
