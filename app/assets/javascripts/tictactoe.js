261544f2ccedacf35a70740b76a23a690008cd93
github username <github email address>
2019-04-12 10:54:49 -0400

    player(), updateState() and setMessage() done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,26 @@
 // Code your JavaScript / jQuery solution here
+var turn = 0;
+
+function player() {
+    const token = (turn % 2) ? "O" : "X";
+    nextTurn();
+    return token;
+}
+
+function nextTurn() { return ++turn }
+
+function updateState(htmlTd) { return htmlTd.innerHTML = player() }
+
+function setMessage(message) { return $("div#message").html(message) }
+
+function checkWinner() {
+
+}
+
+function doTurn() {
+
+}
+
+function attachListeners() {
+
+}
