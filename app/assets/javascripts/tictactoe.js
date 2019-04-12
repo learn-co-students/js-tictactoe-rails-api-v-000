49fda69f74e6c2c82284a960420fbf8c5f3dcae1
github username <github email address>
2019-04-12 14:19:30 -0400

    checkWinner: true if there is a winner combination

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,15 @@
 // Code your JavaScript / jQuery solution here
 var turn = 0;
+var WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 
 function player() {
     const token = (turn % 2) ? "O" : "X";
@@ -15,6 +25,9 @@ function setMessage(message) { return $("div#message").html(message) }
 
 function checkWinner() {
 
+    return !!WINNING_COMBINATIONS.find((winning_row) => {
+        return winning_row.every((cell) => ($("table tr td")[cell].innerText === 'X') || winning_row.every((cell) => $("table tr td")[cell].innerText === 'O'))
+    });
 }
 
 function doTurn() {
