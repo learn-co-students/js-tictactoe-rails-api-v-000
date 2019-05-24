27a296762cea6069f5fab8ae19a1ff5ded403c9f
Rachel Wyatt <rw126@evansville.edu>
2019-05-24 07:53:55 -0400

    add checkWinner function to tictactoe.js

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,4 +1,6 @@
 // Code your JavaScript / jQuery solution here
+const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
 var turn = 0;
 
 function player() {
@@ -16,4 +18,19 @@ function updateState(square) {
 
 function setMessage(message) {
     $('#message').text(message);
+}
+
+function checkWinner() {
+    var winner = false;
+    var board = {};
+    $('td').text((index, square) => board[index] = square);
+
+    WINNING_COMBOS.forEach(function(position) {
+        if (board[position[0]] === board[position[1]] && board[position[1]] === board[position[2]] && board[position[0]] !== ""){
+            setMessage(`Player ${board[position[0]]} Won!`)
+            return winner = true;
+        }
+    })
+
+    return winner;
 }
