37fd0a5cf9d0660ffc795a46e89663e712dd557d
Grace Choi <oiograceoio@gmail.com>
2019-06-14 14:03:42 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,61 @@
 // Code your JavaScript / jQuery solution here
+var turn = 0;
+const WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+  ];
+
+
+function player() {
+	if(turn % 2 === 0) {
+		return 'X';
+	} else {
+		return 'O';
+	}
+}
+
+function updateState(position) {
+	position.innerHTML = player();
+}
+
+function setMessage(msg) {
+	document.getElementById('message').innerHTML = msg;
+}
+
+function checkWinner() {
+	var board = {};
+	var winner = false;
+	$('td').text((index, position) => board[index] = position);
+
+	WIN_COMBINATIONS.some(function(combo) {
+		if(board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
+			setMessage(`Player ${board[combo[0]]} Won!`);
+			return winner = true;
+		}
+	})
+	return winner;
+}
+
+function doTurn(position) {
+	turn += 1;
+	updateState(position);
+	if(checkWinner()) {
+		$('td').empty();
+		turn = 0;
+	} else if (turn === 9) {
+		setMessage('Tie game.');
+		$('td').empty();
+		turn = 0;
+	}
+}
+
+function attachListeners() {
+
+}
+
