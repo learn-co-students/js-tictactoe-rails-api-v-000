071e5115bf99121bed855c70480fda5724861095
github username <github email address>
2019-06-21 17:27:44 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,9 +1,34 @@
-let turn = 0
+const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
+                        [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
 function player() {
-  if (Number.isInteger(turn/2)) {
-    return "X"
-  } else {
+  if ((turn % 2) === 1) {
     return "O"
+  } else {
+    return "X"
   }
+}
+
+function updateState(square) {
+  square.innerHTML = player()
+}
+
+function setMessage(string) {
+  $("#message").html(string)
+}
+
+function checkWinner() {
+  WINNING_COMBOS.forEach(function(combo){
+    if (document.querySelectorAll("td")[combo[0]].innerHTML != "" && document.querySelectorAll("td")[combo[0]].innerHTML === document.querySelectorAll("td")[combo[1]].innerHTML && document.querySelectorAll("td")[combo[1]].innerHTML === document.querySelectorAll("td")[combo[2]].innerHTML) {
+      if document.querySelectorAll("td")[combo[0]].innerHTML === "X" {
+        setMessage("Player X Won!")
+        return true
+      } else {
+        setMessage("Player O Won!")
+        return true
+      }
+    } else {
+      return false
+    }
+  })
 }
