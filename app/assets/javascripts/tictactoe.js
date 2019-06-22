1530a0efc318a2f5cb69cf12af4fc206c9d13a26
Rachel Wyatt <rw126@evansville.edu>
2019-06-22 09:07:08 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -3,7 +3,7 @@ const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0
 
 var turn = 0;
 var currentGame = 0;
-var currentState = ['','','','','','','','',''];
+
 
 $(document).ready(function() {
     attachListeners();
@@ -18,8 +18,7 @@ function player() {
 }
 
 function updateState(square) {
-    var currentPlayer = player();
-    $(square).text(currentPlayer);
+  $(square).text(player());
 }
 
 function setMessage(message) {
@@ -45,13 +44,19 @@ function doTurn(square) {
     updateState(square);
     turn++;
     if (checkWinner()) {
-        saveGame();
-        $(`td`).empty();
-        turn = 0;
+      saveGame();
+      clearBoard();
     } else if (turn === 9) {
-        setMessage("Tie game.");
-        saveGame();
+      saveGame();
+      setMessage("Tie game.");
+      clearBoard();
     }
+}
+
+function clearBoard() {
+  $("td").empty();
+  turn = 0;
+  currentGame = 0;
 }
 
 function attachListeners() {
@@ -74,54 +79,53 @@ function attachListeners() {
        $('#message').empty();
      });
 
-     $('div#games').click(function(e) {
-        currentGame = e.target.innerHTML
-        loadGame();
-        e.preventDefault();
-      });
+
 }
 
 function saveGame() {
-    if (currentGame === 0) {
-      $.post( '/games', { 'state[]': currentState }, function(json) {
-        currentGame = parseInt(json.data['id']);
-      });
-    } else {
-      $.ajax( {
-        type: 'PATCH',
-        url: `/games/${currentGame}`,
-        data: { _method:'PUT', 'state[]': currentState },
-        dataType: 'json'
-      });
-    }
+  var state = Array.from($("td"), td => td.innerText)
+  var params = { state: state }
+  if (currentGame) {
+    $.ajax({
+      type: 'PATCH',
+      url: `/games/${currentGame}`,
+      data: params
+    });
+  } else {
+    $.post("/games", params, function(resp) {
+      currentGame = resp.data.id;
+      setMessage("Game saved.");
+
+    })
   }
+}
 
   function previousGame() {
-    $.get( '/games', function( json ) {
-      let $games = $('#games')
-      $games.html('');
-      json.data.forEach(function(game) {
-        $games.append(`<button id='gameid-${game.id}'>${game.id}</button><br>`)
-      });
-    });
+    $("div#games").html('');
+    $.get('/games', function(games) {
+      if (games.data.length > 0) {
+        games.data.forEach(function(game){
+          var id = game["id"];
+          console.log(id)
+          var button = '<button id="game-' + id + '">' + id + '</button>'
+          $("div#games").append(button);
+          $(`#game-${id}`).on('click', () => loadGame(id));
+        });
+      }
+    })
   }
 
-  function clearBoard() {
-    $('td').each(function () {
-        this.innerHTML = ''
+
+  function loadGame(id){
+    $.get(`/games/${id}`, function(game) {
+      state = game.data.attributes.state
+      squares = document.querySelectorAll("td")
+      currentGame = id;
+      turn = state.join("").length;
+      var i = 0;
+      squares.forEach(function(square) {
+        square.innerHTML = state[i];
+        i++;
       })
-      turn = 0;
-      currentGame = 0;
-  }
-
-  function loadGame() {
-    $.get( `/games/${currentGame}`, function( json ) {
-        let index = 0;
-        $('td').each(function () {
-          this.innerHTML = json.data.attributes['state'][index];
-          currentState[index] = [index];
-          index++;
-        })
-        turn = json.data.attributes['state'].join('').length;
-      });
+    })
   }
