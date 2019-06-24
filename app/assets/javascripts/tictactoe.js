4eee1d60e95e3a6f2e5a91236c7e9ced5ac25e8e
MargaritaKhodzka <mkhodzka@gmail.com>
2019-06-24 17:00:29 -0400

    player, updateState, setMessage functions

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,19 @@
 // Code your JavaScript / jQuery solution here
+var turn = 0;
+
+function player() {
+  if (turn % 2 === 0) {
+    return 'X';
+  } else {
+    return 'O';
+  }
+}
+
+function updateState(grid) {
+  const mark = player();
+  $(grid).text(mark);
+}
+
+function setMessage(message) {
+  $('#message').text(message);
+}
