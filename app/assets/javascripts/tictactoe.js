47095db867282452e0f0e16fc8958a21cc08423e
Samantha brake <sammy.brake@gmail.com>
2019-06-26 22:56:49 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -10,15 +10,14 @@ const WINNING_COMBOS = [
   [2,4,6]
 ];
 
+let turn = 0;
 
-
-let turnCount = 0;
 function player() {
-  if(turnCount % 2 == 0) {
-    turnCount++
+  if(turn % 2 == 0) {
+    turn++
     return "X"
   } else {
-    turnCount++
+    turn++
     return "O"
   };
 };
@@ -35,14 +34,15 @@ function setMessage(string) {
 function checkWinner() {
   let winner = false;
   let board = {}
-  let token = token = player();
-  $('td').text(x, y) => board[index] = y;
-
-WINNING_COMBOS.forEach(position => {
-  if(board[position[0]] == board[position[1]] && board[position[1]] == board[position[2]]) {
-    console.log("hi")
-  }
-})
+  let token = player();
+  $('td').text((x, y) => board[x] = y);
+  WINNING_COMBOS.some(function(position){
+     if (board[position[0]] !== "" && board[position[0]]  === board[position[1]] && board[position[1]] === board[position[2]]){
+       setMessage(`Player ${board[position[0]]} Won!`);
+       return winner = true;
+     }
+   });
+   return winner;
 };
 
 function doTurn() {
