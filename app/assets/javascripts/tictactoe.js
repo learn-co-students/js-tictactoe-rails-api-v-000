675c1421f22434176f5e89647577ca214c4fa6c1
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-06-26 22:04:21 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,21 @@
 // Code your JavaScript / jQuery solution here
+let turn_count = 0;
+
+function player() {
+    let result;
+    turn_count % 2 === 0 ? result = 'X' : result = 'O';
+    return result;
+}
+
+function updateState(td) {
+    //The $ is a shortcut for jQuery, and provides an interface to the library.
+    $(td).text(player());
+}
+
+function setMessage(str) {
+    $('#message').text(str);
+}
+
+function checkWinner() {
+    
+}
