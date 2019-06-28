ddef98ea0b2b2bf55b8cd0590be2d918e3af5862
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-06-28 00:14:30 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,6 @@
 // Code your JavaScript / jQuery solution here
 let turn_count = 0;
+const WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
 function player() {
     let result;
@@ -17,5 +18,28 @@ function setMessage(str) {
 }
 
 function checkWinner() {
+    var board = {};
+    let result = false;
+    $('td').text((index, box) => board[index] = box);
+    WIN_COMBINATIONS.forEach(function(place) {
+        if ((board[place[0]]).length !== 0 && board[place[0]] === board[place[1]] && board[place[1]] === board[place[2]] ) {
+            setMessage(`Player ${board[place[0]]} Won!`)
+            return result = true;
+        } 
+    });
+    return result;
+}
+
+function doTurn(move) { 
+    updateState(move);
+    turn++;
+    if (checkWinner() === true) {
+        turn = 0;
+    } 
+    setMessage("Tie game.")
     
+}
+
+function attachListeners() {
+
 }
