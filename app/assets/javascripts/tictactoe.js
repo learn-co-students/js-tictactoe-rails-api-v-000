b25c63d72a7a19eebf0d4d9dd56b5a6c6a0ebe95
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-06-28 23:10:46 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,11 @@
 // Code your JavaScript / jQuery solution here
 let turn_count = 0;
+var currentGame = 0
 const WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+
+$(document).ready(function() {
+    attachListeners();
+});  
 
 function player() {
     let result;
@@ -33,13 +38,79 @@ function checkWinner() {
 function doTurn(move) { 
     updateState(move);
     turn++;
-    if (checkWinner() === true) {
+    if (checkWinner() === true || tiedGame()) {
         turn = 0;
+        $('td').empty();
     } 
-    setMessage("Tie game.")
-    
+}
+
+function tiedGame () {
+    let response;
+    turn === 9 ? response = true && setMessage("Tie game.") : response = false;
+    return response;
 }
 
 function attachListeners() {
+    $('td').on('click', function() {
+        if (!$.text(this) && !checkWinner()) {
+            doTurn(this);
+        }
+    });
 
+    $('#save').on('click', () => saveGame());
+    $('#previous').on('click', () => previousGames());
+    $('#clear').on('click', () => clearBoard());
+ }
+
+ function clearBoard() {
+    $('td').empty();
+ }
+
+function saveGame() {
+    const state = [];
+    $('td').text((index, place) => {
+        state.push(place);
+    });
+    const gameData = { state: state };
+    if (currentGame) {
+        $.ajax({
+            type: 'PATCH',
+            url: `/games/${currentGame}`,
+            data: gameData
+        });
+    } else {
+        $.post('/games', gameData, function(game) {
+            currentGame = game.data.id;
+            // $('#games').append(`<button id="gameid-${game.data.id}">${game.data.id}</button><br>`);
+            // $("#gameid-" + game.data.id).on('click', () => clearBoard(game.data.id));
+        });
+    };
+}
+  
+function previousGames() {
+    $("div#games").html('');
+    $.get('/games', function(games) {
+        if (games.data.length > 0) {
+            games.data.forEach(function(game){
+                var id = game["id"];
+                var button = '<button id="game-' + id + '">' + id + '</button>'
+                $("div#games").append(button);
+                $(`#game-${id}`).on('click', () => loadGame(id));
+            });
+        }
+    })
+}
+
+function loadGame(id){
+    $.get(`/games/${id}`, function(game) {
+        state = game.data.attributes.state
+        boxes = document.querySelectorAll("td")
+        currentGame = id;
+        turn = state.join("").length;
+        var i = 0;
+        boxes.forEach(function(box) {
+            box.innerHTML = state[i];
+            i++;
+        })
+    })
 }
