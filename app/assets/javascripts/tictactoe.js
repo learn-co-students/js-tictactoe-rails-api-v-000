26b7568b3c10a0eaf63599e30790d7dee7cc6453
sarawall1187 <sarawall1187@gmail.com>
2019-07-01 15:52:21 -0700

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -20,5 +20,18 @@ var turn = 0;
         el.innerHTML = message
     }
 
+    function checkWinner(){
+
+    }
+
+    function doTurn(){
+
+    }
+
+    function attachListeners(){
+
+    }
+
     
 
+// WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
