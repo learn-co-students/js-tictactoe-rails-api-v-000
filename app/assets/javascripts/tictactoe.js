aa7a230e116df35a346e8b090da55f27055b96ac
sarawall1187 <sarawall1187@gmail.com>
2019-07-01 15:31:26 -0700

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,24 @@
 // Code your JavaScript / jQuery solution here
+
+var turn = 0;
+
+  function player(){
+      if (turn % 2 ){
+        return 'O'  
+      }else {
+        return 'X'
+      }   
+    }
+    
+    function updateState(square){
+        var token = player()
+        square.innerHTML = token
+    }
+
+    function setMessage(message){
+        var el = document.querySelector("#message")
+        el.innerHTML = message
+    }
+
+    
+
