f3361e08357dae913476eec1f53ee735f6bea462
sarawall1187 <sarawall1187@gmail.com>
2019-07-02 10:15:26 -0700

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,7 @@
 // Code your JavaScript / jQuery solution here
 
 var turn = 0;
+var WinCombos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
 
   function player(){
       if (turn % 2 ){
@@ -21,7 +22,23 @@ var turn = 0;
     }
 
     function checkWinner(){
-
+      var tds = document.querySelectorAll("td")
+      var board = []
+      var winner = false
+        tds.forEach(function(td){
+          board.push(td.innerHTML)
+        })
+          WinCombos.forEach(function(winCombo){
+            if (board[winCombo[0]] === ' ' || board[winCombo[0]] === ''){
+             return winner = false
+            }
+            else if (board[winCombo[0]] === board[winCombo[1]] && board[winCombo[1]] === board[winCombo[2]] && board[winCombo[0]] === board[winCombo[2]]){
+              debugger   
+              setMessage(`Player ${board[winCombo[0]]} Won!`)
+              return winner = true
+            }  
+          })
+          return winner          
     }
 
     function doTurn(){
@@ -34,4 +51,3 @@ var turn = 0;
 
     
 
-// WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
