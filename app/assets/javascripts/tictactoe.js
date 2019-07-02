446bbe719e6517d1c968cb29d18340e59aad83e3
Ashley <ashleywong678@gmail.com>
2019-07-02 14:42:07 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,6 @@
 // Code your JavaScript / jQuery solution here
-var game_id;
 var turn = 0;
+var WinCombos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
 
 function player(){
   if (turn % 2){
@@ -20,13 +20,24 @@ function setMessage(string){
 }
 
 function checkWinner(){
-
+  var tds = document.querySelectorAll("td")
+  var board = []
+  var winner = false
+    tds.forEach(function(td){
+      board.push(td.innerHTML)
+    })
+  WinCombos.forEach(function(winCombo){
+    if (board[winCombo[0]] !== "" && board[winCombo[0]] === board[winCombo[1]] && board[winCombo[1]] === board[winCombo[2]]){
+      setMessage(`Player ${board[winCombo[0]]} Won!`)
+      return winner = true
+    }})          
+  return winner
 }
-
-
 
 function doTurn(){
   turn += 1
+  win_check = checkWinner()
+  
 }
 
 function attachListeners(){
