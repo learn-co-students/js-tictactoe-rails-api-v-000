85df282d061020c6a57cae5c41349d8a203cd307
github username <github email address>
2019-07-08 15:16:19 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,7 +1,49 @@
 var turn = 0;
 
+const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
+  [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
 var player = () => turn % 2 ? 'O' : 'X';
 
-function updateState() {
-  
+function updateState(square) {
+  var token = player();
+  $(square).text(token);
+}
+
+function setMessage(msg) {
+  $("#message").text(msg) 
+}
+
+function checkWinner() {
+  var board = {};
+  var winner = false;
+
+  $('td').text((index, square) => board[index] = square);
+
+  WINNING_COMBOS.some(function(combo) {
+    if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
+      setMessage(`Player ${board[combo[0]]} Won!`);
+      return winner = true;
+    }
+  });
+  return winner;
+}
+
+function reset() {
+  board = []
+}
+
+// function saveGame (){
+//   board.save 
+// }
+
+function doTurn(td) {
+  updateState(td);
+  turn++;
+  if (checkWinner()) {
+    reset();
+  } else if (turn === 9) {
+    setMessage("Tie game.");
+    reset();
+  }
 }
