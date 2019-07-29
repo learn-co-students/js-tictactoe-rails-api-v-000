6d133f9209e45eef2c1b5f583e2f209c875aa5b2
Naomi Dennis <njdennis94@gmail.com>
2019-07-29 14:40:36 +0000

    Pass player() test

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,4 +1,9 @@
 // Code your JavaScript / jQuery solution here
 /*
-  REMINDER: DO NOT USE ES5+ Sprokects cannot read it and it will result in odd, confusing errors!! 
+  REMINDER: DO NOT USE ES5+ Sprokects cannot read it and it will result in odd, confusing errors!!
 */
+var turn = 0;
+var player = function(){
+   ticTacToePiece = turn % 2 == 0 ? "X" : "O"
+   return ticTacToePiece
+}
