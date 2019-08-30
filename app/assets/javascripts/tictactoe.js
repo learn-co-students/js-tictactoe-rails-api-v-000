feedd1ed20eb0e8d4f0194a00ebae68b16665f0a
github username <github email address>
2019-08-29 17:57:19 -0700

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,58 +1,13 @@
-// this lab doesn't like let and const
+const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
+                        [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+var turn = 0;
+var currentGame = 0;
+
 $(document).ready(function() {
   attachListeners();
 });
 
-var turn = 0;
-var win_combo =[
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-  ];
-
-// Player
-function player() {
-  if (turn % 2 === 0) {
-    return 'X';
-  } else {
-    return 'O';
-  }
-};
-
-// UpdateState
-function updateState(squares) {
-  squares.innerHTML = player();
-}
-
-// SetMessage
-function setMessage(string){
-  var message = document.getElementById('message')
-  message.innerHTML = string
-}
-
-function checkWinner() {
-
-  var board = {};
-  var winner = false;
-
-
-// we grab the td element (which is an array type object), .text() iterates through,
-// and with the arrow function, assigns the value of the square to the board
-  $('td').text((index, square) => board[index] = square);
-
-  win_combo.forEach(function(position) {
-    if (board[position[0]] !== "" && board[position[0]] === board[position[1]] && board[position[1]] === board[position[2]]) {
-      setMessage(`Player ${board[position[0]]} Won!`);
-      return winner = true;
-    }
-  });
-  return winner;
-}
+var player = () => turn % 2 ? 'O' : 'X';
 
 function doTurn(square) {
   updateState(square);
@@ -61,58 +16,120 @@ function doTurn(square) {
     saveGame();
     resetBoard();
   } else if (turn === 9) {
-    setMessage('Tie game.');
+    setMessage("Tie game.");
     saveGame();
     resetBoard();
   }
 }
 
-function saveGame() {
-  var state = []
-  $('td').text((index, square) => state[index] = square);
-  $.post(`/games`,function(data, status){
-    var gameId = data["data"]["id"];
-    debugger
-      if (Number.isInteger(gameId)) {
-        data["data"]["attributes"]["state"] = state;
-      } else {
-        // need to make a $.patch - don't think it works with patch
-      }
-  })
-}
-
-
 function resetBoard() {
- $('td').empty();
- turn = 0;
- currentGame = 0;
+  $('td').empty();
+  turn = 0;
+  currentGame = 0;
 }
 
-function attachListeners(){
-  $( "td" ).on( "click", function() {
-    if(!$.text(this) && !checkWinner()) {
+function attachListeners() {
+  $('td').on('click', function() {
+    if (!$.text(this) && !checkWinner()) {
       doTurn(this);
-    };
+    }
   });
-  // button#previous
-  var previous = window.document.getElementById('previous')
-                                    // Callback function
-   previous.addEventListener('click', previousGame);
-  //button#save
-  var saveButton = window.document.getElementById('save');
-   saveButton.addEventListener('click', saveGame);
+
+  $('#save').on('click', () => saveGame());
+  $('#previous').on('click', () => showPreviousGames());
+  $('#clear').on('click', () => resetBoard());
 }
 
-function previousGame(){
-  $.get("/games", function(data) {
-    var divGames = document.getElementById('games');
-    var anchorTag = document.createElement('a');
-    anchorTag.setAttribute('href', '#');
-    anchorTag.innerText = "Game:" + data["data"][0];
-    console.log(data)
-    if (data == true) {
-      divGames.appendChild(anchorTag)
-    }
-  })
+function checkWinner() {
+  var board = {};
+  var winner = false;
 
+  $('td').text((index, square) => board[index] = square);
+
+  WINNING_COMBOS.some(function(combo) {
+    if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
+      setMessage(`Player ${board[combo[0]]} Won!`);
+      return winner = true;
+    }
+  });
+
+  return winner;
+}
+
+function updateState(square) {
+  var token = player();
+  $(square).text(token);
+}
+
+function setMessage(string) {
+  $('#message').text(string);
+}
+
+function saveGame() {
+  var state = [];
+  var gameData;
+
+  $('td').text((index, square) => {
+    state.push(square);
+  });
+
+  gameData = { state: state };
+
+  if (currentGame) {
+    $.ajax({
+      type: 'PATCH',
+      url: `/games/${currentGame}`,
+      data: gameData
+    });
+  } else {
+    $.post('/games', gameData, function(game) {
+      currentGame = game.data.id;
+      $('#games').append(`<button id="gameid-${game.data.id}">${game.data.id}</button><br>`);
+      $("#gameid-" + game.data.id).on('click', () => reloadGame(game.data.id));
+    });
+  }
+}
+
+function showPreviousGames() {
+  $('#games').empty();
+  $.get('/games', (savedGames) => {
+    if (savedGames.data.length) {
+      savedGames.data.forEach(buttonizePreviousGame);
+    }
+  });
+}
+
+function buttonizePreviousGame(game) {
+  $('#games').append(`<button id="gameid-${game.id}">${game.id}</button><br>`);
+  $(`#gameid-${game.id}`).on('click', () => reloadGame(game.id));
+}
+
+function reloadGame(gameID) {
+  document.getElementById('message').innerHTML = '';
+
+  const xhr = new XMLHttpRequest;
+  xhr.overrideMimeType('application/json');
+  xhr.open('GET', `/games/${gameID}`, true);
+  xhr.onload = () => {
+    const data = JSON.parse(xhr.responseText).data;
+    const id = data.id;
+    const state = data.attributes.state;
+
+    let index = 0;
+    for (let y = 0; y < 3; y++) {
+      for (let x = 0; x < 3; x++) {
+        document.querySelector(`[data-x="${x}"][data-y="${y}"]`).innerHTML = state[index];
+        index++;
+      }
+    }
+
+    turn = state.join('').length;
+    currentGame = id;
+
+    if (!checkWinner() && turn === 9) {
+      setMessage('Tie game.');
+    }
+  };
+
+  xhr.send(null);
 }
