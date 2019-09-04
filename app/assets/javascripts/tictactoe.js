02584a6862d1b883b90e89306d0dbd4411834a5b
sjwakean <swakeman@ca.rr.com>
2019-09-03 17:38:03 -0700

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -5,25 +5,22 @@ $(document).ready(function() {
     attachListeners()
 }) 
 
-var board = {}
-
 var winning_combinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
 var turn = 0
+var board = {}
 
 function player() {  
     //Returns the token of the player whose turn it is, 'X' when the turn variable is even and 'O' when it is odd.
+    //return turn % 2 === 0 ? 'O' : 'X';
     return turn % 2 === 0 ? 'X' : 'O';
-    //return turn % 2 ? 'X' : 'O';
 }
 
 function updateState(td) {
-    //Invokes player() and adds the returned string ('X' or 'O') to the clicked square on the game board.
-
-    //CHECK SQUARE 
-    if ($(td).text[board.to_i-1] != "")
-
-    $(td).text(player())
+    //Gameplay Users cannot place a token in a square that is already taken:
+        //Invokes player() and adds the returned string ('X' or 'O') to the clicked square on the game board.
+    var token = player()
+    $(td).text(token)
 }
 
 function setMessage(winner) {
@@ -31,7 +28,8 @@ function setMessage(winner) {
 }
 
 function checkWinner() {
-    //var board = {}
+    // var board = {}
+    var winner = false
 
     //Returns true if the current board contains any winning combinations
 
@@ -41,17 +39,14 @@ function checkWinner() {
         // sets token to 
         board[board_position] = token
     })   
-    
-    var winner = false
 
     //iterates over winning_combinations for each board[element[index position]]
-    winning_combinations.forEach ((element)=>{
+    winning_combinations.some((element)=>{
         
         // checks board index position [0, 1, 2] for each square in the winning combination that is not empty ("") and has the same token ("X" or "O")
         if (board[element[0]] !== "" && board[element[0]] === board[element[1]] && board[element[0]] === board[element[2]]) {
+            setMessage(`Player ${board[element[0]]} Won!`);
             winner = true
-                setMessage(`Player ${board[element[0]]} Won!`);
-                resetBoard()
         }
     })
 
@@ -62,56 +57,133 @@ function checkWinner() {
 
 function resetBoard() {
     $('td').empty();
-    turn = -1
+    turn = 0
+    currentGame = 0
 }
-
-//Gameplay Users cannot place a token in a square that is already taken:
-function squareTaken() {
-    var taken = $(document.querySelectorAll('td')).text()
-    console.log(taken)
-    // document.querySelectorAll('td')
-    // board_position = board[0][1][2][3][4][5][6][7][8]
-    // $("td").text(function(board_position, token) {
-
-        // sets token to 
-        //if board[0]
-        //if ($(td).text[board.to_i-1] != "")
-        //if (board[board_position] = token)
-        return "Users cannot place a token in a square that is already taken"
-    //})   
-}
-// function squareTaken(i) {
-//     debugger
-//     //if ($(td).text(player()))
-//     if (board[board_position[i]].innerHTML === "X" || "O") {
-//         return "Users cannot place a token in a square that is already taken"
-//         }
-// }
 
 function doTurn(td) {
     //Invokes the updateState() function, passing it the element that was clicked
     updateState(td)
 
-    //Invokes checkWinner() to determine whether the move results in a winning play.
-    var winner = checkWinner()
-
-    //doTurn() invokes the setMessage() function with the argument "Tie game." when the game is tied:
-    if (turn == 8 && !winner) {
-        setMessage('Tie game.');
-    }
-
     //Increments the turn variable by 1.
     turn++;
+
+    //Invokes checkWinner() to determine whether the move results in a winning play.
+    if (checkWinner() === true) {
+        saveGame()
+        resetBoard()
+    }
+
+    //doTurn() invokes the setMessage() function with the argument "Tie game." when the game is tied:
+    else if (turn === 9) {
+        setMessage('Tie game.');
+        saveGame()
+        resetBoard()
+    }
 }
 
 function attachListeners() {
     //Attaches the appropriate event listeners to the squares of the game board as well as for the button#save, button#previous, and button#clear elements.
     $("td").on("click", function(event) {
-        console.log(this) 
-
-    //When a user clicks on a square on the game board, the event listener should invoke doTurn() and pass it the element that was clicked.
-        doTurn(this)
+        if (!$.text(this) && !checkWinner()) {
+            //When a user clicks on a square on the game board, the event listener should invoke doTurn() and pass it the element that was clicked.
+            doTurn(this)
+        }
     })
 
+    //Attaches the appropriate event listeners to the button#save, button#previous, and button#clear elements.
     //When you name your save and previous functions, make sure to call them something like saveGame() and previousGames(). If you call them save() and previous() you may run into problems with the test suite.
+    $("#save").on("click", function() {
+        $
+        saveGame()
+    })
+    $("#previous").on("click", function() {
+        previousGames()
+    })
+    $("#clear").on("click", function() {
+        clearGame()
+    })
+}
+
+var currentGame = 0
+
+function saveGame() {
+    var stateData = []
+    var gameData;
+    //checks each square in the game board for its value
+    $("td").text(function(board_position, token) {
+
+        // sets token to 
+        board[board_position] = token
+        stateData.push(token)
+    }) 
+    gameData = {state: stateData}
+console.log(gameData)
+console.log(stateData)
+    if (currentGame) {
+        $.ajax({
+            headers : {
+                'Accept' : 'application/json',
+                'Content-Type' : 'application/json'
+            },
+          
+            url : `/games/${currentGame}`,
+            type : 'PATCH',
+            data : gameData //{state: stateData}
+         })
+
+    } else {
+        // post request
+        $.post('/games', gameData, function(game) {
+            currentGame = game.data.id
+            $('#games').append(`<button id="gameid-${game.data.id}">Game ${game.data.id}</button><br>`); 
+            $("#gameid-" + game.data.id).on('click', () => reloadGame(game.data.id));
+        })        
+    } 
+}
+
+function previousGames() {
+    //clears all game buttons
+    $("#games").empty()
+
+    //"adds those previous games as buttons in the DOM's div#games element"
+    $.get( "/games", function(games) {
+        //if (games.data) {
+            games.data.forEach (game=> {
+            // //creating button HTML as a string
+             var button = `<button id="currentGame-${game.id}">Game ${game.id}</button><br>`; 
+             $("#games").append(button)
+            $(`#currentGame-${game.id}`).on("click", function () {
+                reloadGame(game.id)
+            })
+        });
+    })
+} 
+
+function clearGame() {
+    $('td').empty();
+    turn = 0
+    currentGame = 0
+}
+
+function reloadGame(gameId) {
+    $.get(`/games/${gameId}`, function(game) {
+        var id = game.data.id
+        var state = game.data.attributes.state
+        var counter = 0
+
+        // Two for loops for Matrix
+        // Outter loop y0, y1, y2 Vertical Rows Increments y by  1 < 3
+        for (var y=0; y < 3; y++) {
+             // Inner loop x0, x1. x2 Horizontal Rows Increments x by 1 < 3
+            for (var x= 0; x < 3; x++) {
+                document.querySelector(`[data-x="${x}"][data-y="${y}"]`).innerHTML = state[counter] 
+                counter++ 
+            }
+        }
+        //Set currentGame to the id of the game button selected
+        currentGame = id
+        //Set turn to the number of tokens on the board 
+        turn = state.join("").length
+    });
 }
